b0f32f86fb85c3879865b4e1b29202b5696a5072
fash <fash@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-03-31 12:20:04 +0000

    PIMCORE-749 - Variants for objects
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@115 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ update/1051/postupdate.php
@@ -0,0 +1,18 @@
+<?php
+
+$sql = "ALTER TABLE pimcore.objects CHANGE o_type o_type ENUM('object', 'folder','variant'); ";
+$sql .= "ALTER TABLE pimcore.classes ADD allowVariants TINYINT(1) DEFAULT '0' AFTER propertyVisibility;";
+
+// get db connection
+$db = Pimcore_Resource_Mysql::get();
+
+$db->exec($sql);
+
+$classList = new Object_Class_List();
+$classes = $classList->load();
+if(is_array($classes)){
+    foreach($classes as $class){
+        $class->save();
+    }
+}
+
