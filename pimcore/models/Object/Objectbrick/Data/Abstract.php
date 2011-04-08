204f8dd9cb65a7a2acb62ec58e1057a82290624c
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-04-08 10:53:23 +0000

    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@156 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/models/Object/Objectbrick/Data/Abstract.php
@@ -0,0 +1,39 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Object_Fieldcollection
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Object_Objectbrick_Data_Abstract extends Pimcore_Model_Abstract {
+    
+    public $fieldname;
+    
+    
+    public function getFieldname () {
+        return $this->fieldname;
+    }
+    
+    public function setFieldname ($fieldname) {
+        $this->fieldname = $fieldname;
+    }
+    
+    public function getType () {
+        return $this->type;
+    }
+    
+    public function getDefinition () {
+        $definition = Object_Objectbrick_Definition::getByKey($this->getType());
+        return $definition;
+    }
+}
---
+++ pimcore/models/Object/Objectbrick/Resource/Mysql.php
@@ -0,0 +1,110 @@
+<?php 
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Object_Class
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Object_Objectbrick_Resource_Mysql extends Object_Fieldcollection_Resource_Mysql {
+    
+//    public function save (Object_Concrete $object) {
+//        $this->delete($object);
+//    }
+    
+    public function load (Object_Concrete $object) {
+        
+        $fieldDef = $object->getClass()->getFieldDefinition($this->model->getFieldname());
+        $values = array();
+
+        
+        foreach ($fieldDef->getAllowedTypes() as $type) {
+            try {
+                $definition = Object_Fieldcollection_Definition::getByKey($type);
+            } catch (Exception $e) {
+                continue;
+            }
+            
+            $tableName = $definition->getTableName($object->getClass());
+            
+            try {
+                $results = $this->db->fetchAll("SELECT * FROM ".$tableName." WHERE o_id = '".$object->getId()."' AND fieldname = '".$this->model->getFieldname()."' ORDER BY `index` ASC");
+            } catch (Exception $e) {
+                $results = array();
+            }            
+            
+            $fieldDefinitions = $definition->getFieldDefinitions();
+            $collectionClass = "Object_ObjectBrick_Data_" . ucfirst($type);
+            
+            
+            foreach ($results as $result) {
+                
+                $collection = new $collectionClass();
+                $collection->setIndex($result["index"]);
+                $collection->setFieldname($result["fieldname"]);
+                
+                foreach ($fieldDefinitions as $key => $fd) {
+                    if (is_array($fd->getColumnType())) {
+                        $multidata = array();
+                        foreach ($fd->getColumnType() as $fkey => $fvalue) {
+                            $multidata[$key . "__" . $fkey] = $result[$key . "__" . $fkey];
+                        }
+                        $collection->setValue(
+                            $key,
+                            $fd->getDataFromResource($multidata));
+    
+                    } else {
+                        $collection->setValue(
+                            $key,
+                            $fd->getDataFromResource($result[$key]));
+                    }
+                }
+                
+                $values[] = $collection;
+            }
+        }
+        
+        $orderedValues = array();
+        foreach ($values as $value) {
+            $orderedValues[$value->getIndex()] = $value;
+        }
+        
+        ksort($orderedValues);
+        
+        $this->model->setItems($orderedValues);
+                
+        return $orderedValues;
+    }
+    
+    public function delete (Object_Concrete $object) {
+        // empty or create all relevant tables 
+        $fieldDef = $object->getClass()->getFieldDefinition($this->model->getFieldname());
+        
+        foreach ($fieldDef->getAllowedTypes() as $type) {
+            
+            try {
+                $definition = Object_ObjectBrick_Definition::getByKey($type);
+            } catch (Exception $e) {
+                continue;
+            }
+              
+            $tableName = $definition->getTableName($object->getClass());
+            
+            try {
+                $this->db->delete($tableName, "o_id = '".$object->getId()."' AND fieldname = '".$this->model->getFieldname()."'");
+            } catch (Exception $e) {
+                // create definition if it does not exist
+                $definition->createUpdateTable($object->getClass());
+            }
+        }
+    }
+}
