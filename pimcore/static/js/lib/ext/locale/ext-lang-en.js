2fdd90b392bb020958d50b32a042979d2ac3096a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-04-22 06:46:24 +0000

    PIMCORE-833
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@199 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/lib/ext/locale/ext-lang-en.js
+++ pimcore/static/js/lib/ext/locale/ext-lang-en.js
@@ -170,7 +170,7 @@ if(Ext.form.DateField){
     minText           : "The date in this field must be after {0}",
     maxText           : "The date in this field must be before {0}",
     invalidText       : "{0} is not a valid date - it must be in the format {1}",
-    format            : "m/d/y",
+    format            : "m/d/Y",
     altFormats        : "m/d/Y|m-d-y|m-d-Y|m/d|m-d|md|mdy|mdY|d|Y-m-d",
     startDay          : 0
   });
