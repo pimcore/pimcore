00f0679bd7b18f7daf5531aa73a6a4bb7dc7f85c
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-06-14 20:49:50 +0000

    installer cleanup
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@401 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/extensionmanager/controllers/ShareController.php
+++ pimcore/modules/extensionmanager/controllers/ShareController.php
@@ -1,10 +1,16 @@
 <?php
 /**
- * Created by JetBrains PhpStorm.
- * User: brusch
- * Date: 07.04.11
- * Time: 07:51
- * To change this template use File | Settings | File Templates.
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
  */
  
 class Extensionmanager_ShareController extends Pimcore_Controller_Action_Admin {
