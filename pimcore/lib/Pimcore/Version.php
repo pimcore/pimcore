bb957626a653d42ef688d5f6b77926b105536c35
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-07-13 06:28:24 +0000

    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@506 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.1";
 
-    public static $revision = 1235;
+    public static $revision = 1243;
 
     public static function getVersion() {
         return self::$version;
