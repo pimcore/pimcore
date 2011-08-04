bc02c284aa9ed5609fae70086467cb9453912d26
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-08-04 05:55:36 +0000

    PIMCORE-986 added the info-object also to the action.php Class
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@623 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Document/Tag/Area/Abstract.php
+++ pimcore/models/Document/Tag/Area/Abstract.php
@@ -14,27 +14,61 @@
  */
 
 abstract class Document_Tag_Area_Abstract {
-    
+
+    /**
+     * @var Zend_View
+     */
     protected $view;
+
+    /**
+     * @var Zend_Config
+     */
     protected $config;
+
+    /**
+     * @var Document_Tag_Area_Info
+     */
+    protected $brick;
+
+    /**
+     * @var array
+     */
     protected $params = array();
-    
+
+    /**
+     * @param $view
+     * @return void
+     */
     public function setView ($view) {
         $this->view = $view;
     }
-    
+
+    /**
+     * @return Zend_View
+     */
     public function getView() {
         return $this->view;
     }
-    
+
+    /**
+     * @param $config
+     * @return void
+     */
     public function setConfig ($config) {
         $this->config = $config;
     }
-    
+
+    /**
+     * @return Zend_Config
+     */
     public function getConfig() {
         return $this->config;
     }
 
+    /**
+     * @param $key
+     * @return mixed
+     */
     public function getParam($key) {
         if(array_key_exists($key, $this->params)) {
             return $this->params[$key];
@@ -42,23 +76,58 @@ abstract class Document_Tag_Area_Abstract {
         return;
     }
 
+    /**
+     * @return array
+     */
     public function getAllParams () {
         return $this->params;
     }
 
+    /**
+     * @param $key
+     * @return mixed
+     */
     public function _getParam($key) {
         return $this->getParam($key);
     }
 
+    /**
+     * @return array
+     */
     public function _getAllParams () {
         return $this->getAllParams();
     }
 
+    /**
+     * @param $key
+     * @param $value
+     * @return void
+     */
     public function addParam ($key, $value) {
         $this->params[$key] = $value;
     }
 
+    /**
+     * @param $params
+     * @return void
+     */
     public function setParams ($params) {
         $this->params = $params;
+    }
+
+    /**
+     * @param \Document_Tag_Area_Info $brick
+     */
+    public function setBrick($brick)
+    {
+        $this->brick = $brick;
+    }
+
+    /**
+     * @return \Document_Tag_Area_Info
+     */
+    public function getBrick()
+    {
+        return $this->brick;
     }
 }
