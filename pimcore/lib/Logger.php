b1222107a9ec8a20d869ca42d482533a1ef3b2fa
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-09-07 05:21:13 +0000

    logger
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@750 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Logger.php
+++ pimcore/lib/Logger.php
@@ -88,7 +88,7 @@ class Logger {
      **/
     
      public static function emergency ($m, $l = null) {
-        Logger::log($m,Zend_Log::EMERG);
+        self::log($m,Zend_Log::EMERG);
     }
     
     public static function emerg ($m, $l = null) {
@@ -96,7 +96,7 @@ class Logger {
     }
     
     public static function critical ($m, $l = null) {
-        Logger::log($m,Zend_Log::CRIT);
+        self::log($m,Zend_Log::CRIT);
     }
     
     public static function crit ($m, $l = null) {
@@ -104,7 +104,7 @@ class Logger {
     }
     
     public static function error ($m, $l = null) {
-        Logger::log($m,Zend_Log::ERR);
+        self::log($m,Zend_Log::ERR);
     }
     
     public static function err ($m, $l = null) {
@@ -112,11 +112,11 @@ class Logger {
     }
     
     public static function alert ($m, $l = null) {
-        Logger::log($m,Zend_Log::ALERT);
+        self::log($m,Zend_Log::ALERT);
     }
     
     public static function warning ($m, $l = null) {
-        Logger::log($m,Zend_Log::WARN);
+        self::log($m,Zend_Log::WARN);
     }
     
     public static function warn ($m, $l = null) {
@@ -124,14 +124,14 @@ class Logger {
     }
     
     public static function notice ($m, $l = null) {
-        Logger::log($m,Zend_Log::NOTICE);
+        self::log($m,Zend_Log::NOTICE);
     }
     
     public static function info ($m, $l = null) {
-        Logger::log($m,Zend_Log::INFO);
+        self::log($m,Zend_Log::INFO);
     }
     
     public static function debug ($m, $l = null) {
-        Logger::log($m,Zend_Log::DEBUG);
+        self::log($m,Zend_Log::DEBUG);
     }
 }
