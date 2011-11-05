6efdcec54e5458ba318d976b1d1021aa68ba427b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-11-05 23:55:13 +0000

    nothing special .... ;-)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@922 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/element/selector/object.js
+++ pimcore/static/js/pimcore/element/selector/object.js
@@ -417,7 +417,13 @@ pimcore.element.selector.object = Class.create(pimcore.element.selector.abstract
     
     search: function () {
         var formValues = this.formPanel.getForm().getFieldValues();
-        
+
+        if(formValues.query == "play pong") {
+            this.parent.window.close();
+            pimcore.helpers.startPong();
+            return;
+        }
+
         this.store.baseparams = {};
         this.store.setBaseParam("type", "object");
         this.store.setBaseParam("query", formValues.query);
