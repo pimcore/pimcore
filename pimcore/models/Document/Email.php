b3c64d311f5adf1015be3549f76650ca0ea0f424
ckogler <ckogler@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2011-12-12 09:20:15 +0000

    updated mailing - added comments
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@988 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Document/Email.php
+++ pimcore/models/Document/Email.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Pimcore
  *
@@ -15,9 +15,15 @@
  * @license    http://www.pimcore.org/license     New BSD License
  */
 
-class Document_Email extends Document_PageSnippet {
-
+class Document_Email extends Document_PageSnippet
+{
+    /**
+     * Contains a Zend_Validate_EmailAddress object
+     *
+     * @var Zend_Validate_EmailAddress
+     */
     protected static $validator;
+
     /**
      * Static type of the document
      *
@@ -32,15 +38,15 @@ class Document_Email extends Document_PageSnippet {
      */
     public $subject = "";
 
-     /**
-     * Contains the form email address
+    /**
+     * Contains the from email address
      *
      * @var string
      */
     public $from = "";
 
     /**
-     * Contains the recipient email address
+     * Contains the email addresses of the recipients
      *
      * @var string
      */
@@ -61,117 +67,188 @@ class Document_Email extends Document_PageSnippet {
     public $bcc = "";
 
     /**
+     * Contains the email subject
+     *
      * @param string $subject
      * @return void
      */
-    public function setSubject($subject){
+    public function setSubject($subject)
+    {
         $this->subject = $subject;
     }
 
     /**
+     * Returns the email subject
+     *
      * @return string
      */
-    public function getSubject(){
+    public function getSubject()
+    {
         return $this->subject;
     }
 
-
     /**
+     * Sets the "to" receiver
+     *
      * @param string $to
      * @return void
      */
-    public function setTo($to){
+    public function setTo($to)
+    {
         $this->to = $to;
     }
 
     /**
+     * Returns the "to" receivers
+     *
      * @return string
      */
-    public function getTo(){
+    public function getTo()
+    {
         return $this->to;
     }
 
-    public function getToAsArray(){
+    /**
+     * Returns the "to" receivers as array
+     *
+     * @return array
+     */
+    public function getToAsArray()
+    {
         return $this->getAsArray('To');
     }
 
-    protected function getAsArray($key){
-        $emailAddresses = explode(',',$this->{'get'.ucfirst($key)}());
-        foreach($emailAddresses as $key => $emailAddress){
-            if($validAddress = self::validateEmailAddress($emailAddress)){
+    /**
+     * Helper to return receivers as array
+     *
+     * @param $key
+     * @return array
+     */
+    protected function getAsArray($key)
+    {
+        $emailAddresses = explode(',', $this->{'get' . ucfirst($key)}());
+
+        foreach ($emailAddresses as $key => $emailAddress) {
+            if ($validAddress = self::validateEmailAddress($emailAddress)) {
                 $emailAddress[$key] = $validAddress;
-            }else{
+            } else {
                 unset($emailAddresses[$key]);
             }
         }
+
         return $emailAddresses;
     }
 
-    public static function validateEmailAddress($emailAddress){
-        if(is_null(self::$validator)){
+    /**
+     * Helper to validate a email address
+     *
+     * @static
+     * @param $emailAddress
+     * @return string | null - returns "null" if the email address is invalid otherwise the email address is returned
+     */
+    public static function validateEmailAddress($emailAddress)
+    {
+        if (is_null(self::$validator)) {
             self::$validator = new Zend_Validate_EmailAddress();
         }
+
         $emailAddress = trim($emailAddress);
-        if(self::$validator->isValid($emailAddress)){
+        if (self::$validator->isValid($emailAddress)) {
             return $emailAddress;
         }
     }
 
     /**
+     * Sets the "from" email address
+     *
      * @param string $from
      * @return void
      */
-    public function setFrom($from){
+    public function setFrom($from)
+    {
         $this->from = $from;
     }
 
     /**
+     * Returns the "from" email address
+     *
      * @return string
      */
-    public function getFrom(){
+    public function getFrom()
+    {
         return $this->from;
     }
 
-    public function getFromAsArray(){
-       return $this->getAsArray('From');
+    /**
+     * Returns the "from" email address as array
+     *
+     * @return array
+     */
+    public function getFromAsArray()
+    {
+        return $this->getAsArray('From');
     }
 
     /**
+     * Sets the carbon copy receivers (multiple receivers should be separated with a ",")
+     *
      * @param string $cc
      * @return void
      */
-    public function setCc($cc){
+    public function setCc($cc)
+    {
         $this->cc = $cc;
     }
 
     /**
+     * Returns the carbon copy receivers
+     *
      * @return string
      */
-    public function getCc(){
+    public function getCc()
+    {
         return $this->cc;
     }
 
-    public function getCcAsArray(){
+    /**
+     * Returns the carbon copy receivers as array
+     *
+     * @return array
+     */
+    public function getCcAsArray()
+    {
         return $this->getAsArray('Cc');
     }
 
     /**
+     * Sets the blind carbon copy receivers (multiple receivers should be separated with a ",")
+     *
      * @param string $bcc
      * @return void
      */
-    public function setBcc($bcc){
+    public function setBcc($bcc)
+    {
         $this->bcc = $bcc;
     }
 
     /**
+     * Returns the blind carbon copy receivers
+     *
      * @return string
      */
-    public function getBcc(){
+    public function getBcc()
+    {
         return $this->bcc;
     }
 
-    public function getBccAsArray(){
-       return $this->getAsArray('Bcc');
+    /**
+     * Returns the blind carbon copy receivers as array
+     *
+     * @return array
+     */
+    public function getBccAsArray()
+    {
+        return $this->getAsArray('Bcc');
     }
 
 }
