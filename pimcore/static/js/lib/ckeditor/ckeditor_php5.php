3921b6ab481814f5f292c50e440b2c23fe60088b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-01-06 12:39:18 +0000

    PIMCORE-1301 - Document_Email -> rendering problem within a plugin
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1054 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/lib/ckeditor/ckeditor_php5.php
+++ pimcore/static/js/lib/ckeditor/ckeditor_php5.php
@@ -20,11 +20,11 @@ class CKEditor
 	/**
 	 * The version of %CKEditor.
 	 */
-	const version = '3.6';
+	const version = '3.6.2';
 	/**
 	 * A constant string unique for each release of %CKEditor.
 	 */
-	const timestamp = 'B49E5BQ';
+	const timestamp = 'B8DJ5M3';
 
 	/**
 	 * URL to the %CKEditor installation directory (absolute or relative to document root).
@@ -78,7 +78,7 @@ class CKEditor
 	 * A string indicating the creation date of %CKEditor.
 	 * Do not change it unless you want to force browsers to not use previously cached version of %CKEditor.
 	 */
-	public $timestamp = "B49E5BQ";
+	public $timestamp = "B8DJ5M3";
 	/**
 	 * An array that holds event listeners.
 	 */
