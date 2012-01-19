ecda807526e6aa84baa4f7798abf6c39f04fd7cd
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-01-19 08:34:55 +0000

    PIMCORE-1329 - changed revision path for 1630 (was 1929)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1093 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/1630/postupdate.php
+++ update/1630/postupdate.php
@@ -7,7 +7,7 @@ function sendQuery ($sql) {
     } catch (Exception $e) {
         echo $e->getMessage();
         echo "Please execute the following query manually: <br />";
-        echo $sql;
+        echo "<pre>" . $sql . "</pre><hr />";
     }
 }
 
