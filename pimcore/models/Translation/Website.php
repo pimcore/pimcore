ceac8ae3f169566bb4c914e5cbab10bccfab0d2b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-01-20 13:05:58 +0000

    strange thing which changed on the remote machine class Translation_Website reverted to version from 1.4.2
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1105 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Translation/Website.php
+++ pimcore/models/Translation/Website.php
@@ -15,67 +15,16 @@
  * @license    http://www.pimcore.org/license     New BSD License
  */
 
-class Translation_Website extends Translation_Abstract
-{
-    /**
-     * @static
-     * @param $id - translation key
-     * @param bool $create - creates an empty translation entry if the key doesn't exists
-     * @param bool $returnIdIfEmpty - returns $id if no translation is available
-     * @return Translation_Website
+class Translation_Website extends Translation_Abstract {
+
+        /**
+     * @param string $key
+     * @return Translation
      */
-    public static function getByKey($id, $create = false, $returnIdIfEmpty = false)
-    {
+    public static function getByKey($id) {
         $translation = new self();
-
-        try {
-            $translation->getResource()->getByKey($id);
-        } catch (Exception $e) {
-            if (!$create) {
-                throw new Exception($e->getMessage());
-            } else {
-                $translation->setKey($id);
-                $translation->setDate(time());
-
-                $translations = array();
-                foreach (Pimcore_Tool::getValidLanguages() as $lang) {
-                    $translations[$lang] = "";
-                }
-                $translation->setTranslations($translations);
-                $translation->save();
-            }
-
-        }
-
-        if ($returnIdIfEmpty) {
-            $translations = $translation->getTranslations();
-            foreach ($translations as $key => $value) {
-                $translations[$key] = $value ? : $id;
-            }
-            $translation->setTranslations($translations);
-        }
+        $translation->getResource()->getByKey($id);
 
         return $translation;
-    }
-
-    /**
-     * Static Helper to get the translation of the current locale
-     *
-     * @static
-     * @param $id - translation key
-     * @param bool $create - creates an empty translation entry if the key doesn't exists
-     * @param bool $returnIdIfEmpty - returns $id if no translation is available
-     * @return string
-     * @throws Exception
-     */
-    public static function getByKeyLocalized($id, $create = false, $returnIdIfEmpty = false)
-    {
-        try {
-            $language = Zend_Registry::get('Zend_Locale')->getLanguage();
-        } catch (Exception $e) {
-            throw new Exception("Couldn't determine current language.");
-        }
-
-        return self::getByKey($id, $create, $returnIdIfEmpty)->getTranslation($language);
     }
 }
