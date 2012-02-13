a2f44541abdee10dbd53117826dac933fc085283
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-02-13 08:48:44 +0000

    fix für FF menu item seperator in extras menu
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1231 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.3";
 
-    public static $revision = 1750;
+    public static $revision = 1751;
 
     public static function getVersion() {
         return self::$version;
