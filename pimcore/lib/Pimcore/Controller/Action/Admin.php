e144addb99d518c7b95b18d692703de45edb753a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-02-19 14:00:33 +0000

    added url to access denied logger message
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1258 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Action/Admin.php
+++ pimcore/lib/Pimcore/Controller/Action/Admin.php
@@ -92,7 +92,7 @@ abstract class Pimcore_Controller_Action_Admin extends Pimcore_Controller_Action
             if (!$this->getUser() instanceof User && !($this->_getParam("module") == "admin" && $this->_getParam("controller") == "login")) {
 
                 // put a detailed message into the debug.log
-                Logger::crit("Prevented access to " . print_r($this->getRequest()->getParams()) . " because there is no user in the session!");
+                Logger::crit("Prevented access to " . $_SERVER["REQUEST_URI"] . " because there is no user in the session!");
                 Logger::crit(array(
                     "server" => $_SERVER,
                     "get" => $_GET,
