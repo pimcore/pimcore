62851bcdab0cd655a6f2dc4e5977dcf23159b92a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-02-21 08:08:00 +0000

    sync with dev-server, nothing special changed
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1268 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.4";
 
-    public static $revision = 1781;
+    public static $revision = 1783;
 
     public static function getVersion() {
         return self::$version;
