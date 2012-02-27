3d66fe99559e2eafba34204b7f7ff5dd7d879390
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-02-27 12:23:49 +0000

    added help message and verbose mode for optimize-table CLI script
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1284 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/cli/optimize-tables.php
+++ pimcore/cli/optimize-tables.php
@@ -15,12 +15,55 @@
 
 include_once("startup.php");
 
+try {
+    $opts = new Zend_Console_Getopt(array(
+        'verbose|v' => 'show detailed information (for debug, ...)',
+        'help|h' => 'display this help'
+    ));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $opts->parse();
+} catch (Zend_Console_Getopt_Exception $e) {
+    echo $e->getMessage();
+}
+
+
+// display help message
+if($opts->getOption("help")) {
+    echo $opts->getUsageMessage();
+    exit;
+}
+
+
+if($opts->getOption("verbose")) {
+    $writer = new Zend_Log_Writer_Stream('php://output');
+    $logger = new Zend_Log($writer);
+    Logger::addLogger($logger);
+
+    // set all priorities
+    Logger::setPriorities(array(
+        Zend_Log::DEBUG,
+        Zend_Log::INFO,
+        Zend_Log::NOTICE,
+        Zend_Log::WARN,
+        Zend_Log::ERR,
+        Zend_Log::CRIT,
+        Zend_Log::ALERT,
+        Zend_Log::EMERG
+    ));
+}
+
+
 $db = Pimcore_Resource::get();
 $tables = $db->fetchAll("SHOW TABLES");
 
 foreach ($tables as $table) {
     $t = current($table);
     try {
+        Logger::debug("Running: OPTIMIZE TABLE " . $t);
         $db->query("OPTIMIZE TABLE " . $t);
     } catch (Exception $e) {
         Logger::error($e);
