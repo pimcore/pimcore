fc8378ba68bd43c2fba2046c6ac65bb5b9827b13
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-03-01 12:04:27 +0000

    changed log level to critical when initializing the cache adapter fails
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1309 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.4";
 
-    public static $revision = 1821;
+    public static $revision = 1822;
 
     public static function getVersion() {
         return self::$version;
