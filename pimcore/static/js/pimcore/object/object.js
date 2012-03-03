deb9815388ebb35485bcdff960909310d7d83a86
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-03-03 18:58:23 +0000

    PIMCORE-1412 - Variants are opened again when pimcore is reloaded - even though they are closed manually before & changed session auth to not always set the cookie in the headers
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1313 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/object/object.js
+++ pimcore/static/js/pimcore/object/object.js
@@ -161,6 +161,7 @@ pimcore.object.object = Class.create(pimcore.object.abstract, {
         this.tab.on("destroy", function () {
             pimcore.globalmanager.remove("object_" + this.id);
             pimcore.helpers.forgetOpenTab("object_" + this.id + "_object");
+            pimcore.helpers.forgetOpenTab("object_" + this.id + "_variant");
         }.bind(this));
 
         this.tab.on("afterrender", function (tabId) {
