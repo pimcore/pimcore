5da7f04433821337f3238e66cc944777976985fa
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-03-04 19:26:11 +0000

    PIMCORE-1403 - Page unload confirmation (added user configuration and check for opened tabs)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1320 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/startup.js
+++ pimcore/static/js/pimcore/startup.js
@@ -31,7 +31,10 @@ Ext.onReady(function() {
     // confirmation to close pimcore
     if(!pimcore.settings.devmode) {
         window.onbeforeunload = function() {
-            if(pimcore.settings.showCloseConfirmation) {
+            // check for opened tabs and if the user has configured the warnings
+            var tabPanel = Ext.getCmp("pimcore_panel_tabs");
+            var user = pimcore.globalmanager.get("user");
+            if(pimcore.settings.showCloseConfirmation && tabPanel.items.getCount() > 0 && user["closeWarning"]) {
                 return t("do_you_really_want_to_close_pimcore");
             }
         };
