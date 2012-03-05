cb74e7396f73e27d5faa269d95d8cd5ffbbfe9a3
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2012-03-05 11:54:44 +0000

    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@34 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- models/OnlineShop/Framework/CategoryFilterDefinitionType.php
+++ models/OnlineShop/Framework/CategoryFilterDefinitionType.php
@@ -1,9 +1,19 @@
 <?php
 
+
 abstract class OnlineShop_Framework_CategoryFilterDefinitionType extends OnlineShop_Framework_AbstractFilterDefinitionType {
 
     public function getField() {
-        return "categoryIds";
+        if($this->getIncludeParentCategories()) {
+            return "parentCategoryIds";
+        } else {
+            return "categoryIds";
+        }
+    }
+
+
+    public function getIncludeParentCategories() {
+        return false;
     }
 
 }
