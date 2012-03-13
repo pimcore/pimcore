e019e9cf134dbd97525faebdbdc0a7b88629bbf7
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-03-13 07:56:43 +0000

    IE compatiblity mode order
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1359 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/views/scripts/index/index.php
+++ pimcore/modules/admin/views/scripts/index/index.php
@@ -2,7 +2,7 @@
 <html xmlns="http://www.w3.org/1999/xhtml" xmlns:v="urn:schemas-microsoft-com:vml">
 <head>
 
-    <meta http-equiv="X-UA-Compatible" content="IE=9" />
+    <meta http-equiv="X-UA-Compatible" content="IE=8; IE=9" />
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
     <meta name="robots" content="noindex, nofollow" />
@@ -439,6 +439,7 @@
     <?php // 3rd party libraries ?>
     <script type="text/javascript" src="https://maps.googleapis.com/maps/api/js?sensor=false&key=<?php echo $this->config->services->googlemaps->apikey ?>"></script>
     
+    <script type="text/javascript" src="/admin/misc/json-transliteration?_dc=<?php echo Pimcore_Version::$revision ?>"></script>
     <script type="text/javascript" src="/admin/misc/json-translations-system/language/<?php echo $this->language ?>/?_dc=<?php echo Pimcore_Version::$revision ?>"></script>
     <script type="text/javascript" src="/admin/misc/json-translations-admin/language/<?php echo $this->language ?>/?_dc=<?php echo Pimcore_Version::$revision ?>"></script>
     <script type="text/javascript" src="/admin/user/get-current-user/?_dc=<?php echo Pimcore_Version::$revision ?>"></script>
