f9db6004e8045a06c525a8cabfd2505aac301a0e
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-03-26 19:34:29 +0000

    PIMCORE-1299 - Website-Error Log => update script + install.sql
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1397 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/install/mysql/install.sql
+++ pimcore/modules/install/mysql/install.sql
@@ -205,6 +205,22 @@ CREATE TABLE `glossary` (
   KEY `language` (`language`)
 ) DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `http_error_log`;
+CREATE TABLE `http_error_log` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `path` varchar(1000) DEFAULT NULL,
+  `code` int(3) DEFAULT NULL,
+  `parametersGet` longtext,
+  `parametersPost` longtext,
+  `cookies` longtext,
+  `serverVars` longtext,
+  `date` bigint(20) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `path` (`path`(255)),
+  KEY `code` (`code`),
+  KEY `date` (`date`)
+) DEFAULT CHARSET=utf8;
+
 DROP TABLE IF EXISTS `objects`;
 CREATE TABLE `objects` (
   `o_id` int(11) unsigned NOT NULL AUTO_INCREMENT,
