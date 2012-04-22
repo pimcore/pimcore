24fa262456260b8a509bcdb8eccbdd1f2f41408f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-04-22 16:41:38 +0000

    event-tracking api (NOT FINISHED)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1483 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/models/Tracking/Event.php
@@ -0,0 +1,211 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Tracking
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Tracking_Event extends Pimcore_Model_Abstract {
+
+
+    /**
+     * @var string
+     */
+    public $id;
+
+    /**
+     * @var string
+     */
+    public $date;
+
+    /**
+     * @var string
+     */
+    public $time;
+
+    /**
+     * @var string
+     */
+    public $category;
+
+    /**
+     * @var string
+     */
+    public $action;
+
+    /**
+     * @var string
+     */
+    public $label;
+
+    /**
+     * @var string
+     */
+    public $value;
+
+    /**
+     * @var string
+     */
+    public $valueType;
+
+
+    /**
+     * @param string $action
+     */
+    public function setAction($action)
+    {
+        $this->action = $action;
+    }
+
+    /**
+     * @return string
+     */
+    public function getAction()
+    {
+        return $this->action;
+    }
+
+    /**
+     * @param string $category
+     */
+    public function setCategory($category)
+    {
+        $this->category = $category;
+    }
+
+    /**
+     * @return string
+     */
+    public function getCategory()
+    {
+        return $this->category;
+    }
+
+    /**
+     * @param int $date
+     */
+    public function setDate($date)
+    {
+        $this->date = $date;
+    }
+
+    /**
+     * @return int
+     */
+    public function getDate()
+    {
+        return $this->date;
+    }
+
+    /**
+     * @param string $id
+     */
+    public function setId($id)
+    {
+        $this->id = $id;
+    }
+
+    /**
+     * @return string
+     */
+    public function getId()
+    {
+        return $this->id;
+    }
+
+    /**
+     * @param string $label
+     */
+    public function setLabel($label)
+    {
+        $this->label = $label;
+    }
+
+    /**
+     * @return string
+     */
+    public function getLabel()
+    {
+        return $this->label;
+    }
+
+    /**
+     * @param string $value
+     */
+    public function setValue($value)
+    {
+        $this->value = $value;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValue()
+    {
+        return $this->value;
+    }
+
+    /**
+     * @param string $valueType
+     */
+    public function setValueType($valueType)
+    {
+        $this->valueType = $valueType;
+    }
+
+    /**
+     * @return string
+     */
+    public function getValueType()
+    {
+        return $this->valueType;
+    }
+
+    /**
+     * @param int $time
+     */
+    public function setTimestamp($time) {
+        $this->setDate(date("Y-m-d", $time));
+        $this->setTime(date("H:i:s", $time));
+    }
+
+    /**
+     *
+     */
+    public function save() {
+        if(empty($this->date)) {
+            $this->setDate(date("Y-m-d"));
+        }
+        if(empty($this->time)) {
+            $this->setTime(date("H:i:s"));
+        }
+
+        $this->getResource()->save();
+    }
+
+    /**
+     * @param string $time
+     */
+    public function setTime($time)
+    {
+        $this->time = $time;
+    }
+
+    /**
+     * @return string
+     */
+    public function getTime()
+    {
+        return $this->time;
+    }
+}
