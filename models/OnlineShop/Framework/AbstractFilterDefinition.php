c57348b3e4596f5019fec0dbd2807e67632db05f
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2012-05-16 12:22:30 +0000

    inheritance of fieldcollections in AbstractFilterDefinition
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@75 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- models/OnlineShop/Framework/AbstractFilterDefinition.php
+++ models/OnlineShop/Framework/AbstractFilterDefinition.php
@@ -30,11 +30,42 @@ abstract class OnlineShop_Framework_AbstractFilterDefinition extends Object_Conc
    /**
    * @return Object_Fieldcollection
    */
-   public abstract function getConditions ();
+   public abstract function getConditions();
 
    /**
    * @return Object_Fieldcollection
    */
-   public abstract function getFilters ();
+   public abstract function getFilters();
+
+
+    public function preGetValue($key) {
+
+        if ($this->getClass()->getAllowInherit()
+            && Object_Abstract::doGetInheritedValues()
+            && $this->getClass()->getFieldDefinition($key) instanceof Object_Class_Data_Fieldcollections
+        ) {
+
+            $checkInheritanceKey = $key . "Inheritance";
+            if ($this->{
+                'get' . $checkInheritanceKey
+                }() == "true"
+            ) {
+                $parentValue = $this->getValueFromParent($key);
+                if (!$this->$key) {
+                    return $parentValue;
+                } else {
+                    $value = new Object_Fieldcollection($this->$key->getItems());
+                    if (!empty($parentValue)) {
+                        foreach ($parentValue as $entry) {
+                            $value->add($entry);
+                        }
+                    }
+                    return $value;
+                }
+            }
+        }
+
+        return parent::preGetValue($key);
+    }
 
 }
