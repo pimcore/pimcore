3e23b5bb70ea8aa0bd91572fc3d40cd4fe225404
fash <fash@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-05-24 17:14:28 +0000

    PIMCORE-1516 - Google Analytics Integration: Add possibility to add custom tracking parameters before track pageview is called
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1528 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/report/analytics/settings.js
+++ pimcore/static/js/pimcore/report/analytics/settings.js
@@ -112,6 +112,14 @@ pimcore.report.analytics.settings = Class.create({
                     id: "report_settings_analytics_additionalcode_" + id,
                     value: this.parent.getValue("analytics.sites." + key + ".additionalcode")
                 },{
+                    xtype: "textarea",
+                    fieldLabel: t("analytics_additional_code_before_pageview"),
+                    name: "additionalcodebeforepageview" + id,
+                    height: 100,
+                    width: 350,
+                    id: "report_settings_analytics_additionalcodebeforepageview_" + id,
+                    value: this.parent.getValue("analytics.sites." + key + ".additionalcodebeforepageview")
+                },{
                     xtype: "displayfield",
                     hideLabel: true,
                     width: 500,
@@ -184,6 +192,7 @@ pimcore.report.analytics.settings = Class.create({
             profile: Ext.getCmp("report_settings_analytics_profile_default").getValue(),
             trackid: Ext.getCmp("report_settings_analytics_trackid_default").getValue(),
             additionalcode: Ext.getCmp("report_settings_analytics_additionalcode_default").getValue(),
+            additionalcodebeforepageview: Ext.getCmp("report_settings_analytics_additionalcodebeforepageview_default").getValue(),
             accountid: Ext.getCmp("report_settings_analytics_accountid_default").getValue(),
             advanced: Ext.getCmp("report_settings_analytics_advanced_default").getValue()
         };
@@ -193,6 +202,7 @@ pimcore.report.analytics.settings = Class.create({
                 profile: Ext.getCmp("report_settings_analytics_profile_" + record.data.id).getValue(),
                 trackid: Ext.getCmp("report_settings_analytics_trackid_" + record.data.id).getValue(),
                 additionalcode: Ext.getCmp("report_settings_analytics_additionalcode_" + record.data.id).getValue(),
+                additionalcodebeforepageview: Ext.getCmp("report_settings_analytics_additionalcodebeforepageview_" + record.data.id).getValue(),
                 accountid: Ext.getCmp("report_settings_analytics_accountid_" + record.data.id).getValue(),
                 advanced: Ext.getCmp("report_settings_analytics_advanced_" + record.data.id).getValue()
             };
