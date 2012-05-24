9f46d80ae364f4579ffa688c246fb6a8c910a005
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2012-05-24 11:32:31 +0000

    added comments
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@81 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- lib/OnlineShop/Framework/FilterService.php
+++ lib/OnlineShop/Framework/FilterService.php
@@ -11,14 +11,21 @@ class OnlineShop_Framework_FilterService {
 
     protected $config;
 
+    /**
+     * @param $config OnlineShop Configuration
+     * @param Zend_View $view View in which the filters are rendered
+     */
     public function __construct($config, Zend_View $view) {
         $this->config = $config;
         $this->view = $view;
     }
 
     /**
+     * Returns instance of FilterType or
+     * just the name if the name is not defined in the OnlineShop configuration
+     *
      * @param $name
-     * @return OnlineShop_Framework_FilterService_IFilterService
+     * @return OnlineShop_Framework_FilterService_AbstractFilterType | string
      */
     public function getFilterDefinitionClass($name) {
         if($this->config->$name) {
@@ -29,6 +36,14 @@ class OnlineShop_Framework_FilterService {
     }
 
 
+    /**
+     * Initializes the FilterService, adds all conditions to the ProductList and returns an array of the currently set filters
+     *
+     * @param OnlineShop_Framework_AbstractFilterDefinition $filterObject filter definition object to use
+     * @param OnlineShop_Framework_ProductList $productList product list to use and add conditions to
+     * @param array $params request params with eventually set filter conditions
+     * @return array returns set filters
+     */
     public function initFilterService(OnlineShop_Framework_AbstractFilterDefinition $filterObject, OnlineShop_Framework_ProductList $productList, $params = array()) {
         $currentFilter = array();
 
@@ -55,11 +70,29 @@ class OnlineShop_Framework_FilterService {
         return $currentFilter;
 
     }
-    
+
+    /**
+     * Returns filter frontend script for given filter type (delegates )
+     *
+     * @param OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition filter definition to get frontend script for
+     * @param OnlineShop_Framework_ProductList $productList current product list (with all set filters) to get available options and counts
+     * @param $currentFilter current filter for this filter definition
+     * @return string view snippet
+     */
     public function getFilterFrontend(OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition, OnlineShop_Framework_ProductList $productList, $currentFilter) {
         return $this->getFilterDefinitionClass($filterDefinition->getType())->getFilterFrontend($filterDefinition, $productList, $currentFilter);
     }
 
+    /**
+     * Adds condition - delegates it to the OnlineShop_Framework_FilterService_AbstractFilterType instance
+     *
+     * @param OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition
+     * @param OnlineShop_Framework_ProductList $productList
+     * @param $currentFilter
+     * @param $params
+     * @param bool $isPrecondition
+     * @return array updated currentFilter array
+     */
     public function addCondition(OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition, OnlineShop_Framework_ProductList $productList, $currentFilter, $params, $isPrecondition = false) {
         return $this->getFilterDefinitionClass($filterDefinition->getType())->addCondition($filterDefinition, $productList, $currentFilter, $params, $isPrecondition);
     }
--- lib/OnlineShop/Framework/FilterService/AbstractFilterType.php
+++ lib/OnlineShop/Framework/FilterService/AbstractFilterType.php
@@ -7,12 +7,33 @@ abstract class OnlineShop_Framework_FilterService_AbstractFilterType {
     protected $view;
     protected $script;
 
+    /**
+     * @param $view view to render the filter frontend into
+     * @param $script script for rendering the filter frontend
+     */
     public function __construct($view, $script) {
         $this->view = $view;
         $this->script = $script;
     }
 
+    /**
+     * @abstract
+     * @param OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition
+     * @param OnlineShop_Framework_ProductList $productList
+     * @param $currentFilter
+     * @return string
+     */
     public abstract function getFilterFrontend(OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition, OnlineShop_Framework_ProductList $productList, $currentFilter);
+
+    /**
+     * @abstract
+     * @param OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition
+     * @param OnlineShop_Framework_ProductList $productList
+     * @param $currentFilter
+     * @param $params
+     * @param bool $isPrecondition
+     * @return array
+     */
     public abstract function addCondition(OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition, OnlineShop_Framework_ProductList $productList, $currentFilter, $params, $isPrecondition = false);
 
 }
