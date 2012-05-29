ef0eb27ae6562ba872ee691e66d828dd7504b1b6
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-05-29 19:58:52 +0000

    PIMCORE-1518 - Workspace shows more objects as it should
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1534 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Document/Resource.php
+++ pimcore/models/Document/Resource.php
@@ -384,7 +384,7 @@ class Document_Resource extends Element_Resource {
             // exception for list permission
             if(empty($permissionsParent) && $type == "list") {
                 // check for childs with permissions
-                $permissionsChilds = $this->db->fetchOne("SELECT list FROM users_workspaces_document WHERE cpath LIKE ? AND userId IN (" . implode(",",$userIds) . ") LIMIT 1", $this->model->getRealFullPath()."%");
+                $permissionsChilds = $this->db->fetchOne("SELECT list FROM users_workspaces_document WHERE cpath LIKE ? AND userId IN (" . implode(",",$userIds) . ") LIMIT 1", $this->model->getRealFullPath()."/%");
                 if($permissionsChilds) {
                     return true;
                 }
