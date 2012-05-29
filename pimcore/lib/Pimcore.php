982af2df403a9e7c82503ce70615536389e25a36
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-05-29 20:37:11 +0000

    PIMCORE-1509 - With mbstring enabled and mbstring.func_overload set to overload strlen() by mb_strlen(), Pimcore generates wrong Content-Length header with (gzip) compression.
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1536 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore.php
+++ pimcore/lib/Pimcore.php
@@ -823,16 +823,17 @@ class Pimcore {
 
             // gzip the contents and send connection close to that the process can run in the background to finish
             // some tasks like writing the cache ...
+            // using mb_strlen() because of PIMCORE-1509
             if($gzipIt) {
                 $output = "\x1f\x8b\x08\x00\x00\x00\x00\x00".
                     substr(gzcompress($data, 2), 0, -4).
                     pack('V', crc32($data)). // packing the CRC and the strlen is still required
-                    pack('V', strlen($data)); // (although all modern browsers don't need it anymore) to work properly with google adwords check & co.
+                    pack('V', mb_strlen($data, "latin1")); // (although all modern browsers don't need it anymore) to work properly with google adwords check & co.
 
                 // send headers & contents
                 header("Connection: close\r\n");
                 header("Content-Encoding: $contentEncoding\r\n");
-                header("Content-Length: " . strlen($output));
+                header("Content-Length: " . mb_strlen($output, "latin1"));
                 header("X-Powered-By: pimcore");
 
                 return $output;
