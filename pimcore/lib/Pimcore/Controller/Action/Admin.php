43c5ea0cca69ca377f39816baec8c32edbe1dddf
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-05-30 13:07:54 +0000

    changed the way how the inital request in Pimcore_Controller_Action_Admin was determined, it was a registry variable, now it is a protected static directly in Pimcore_Controller_Action_Admin
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1542 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Action/Admin.php
+++ pimcore/lib/Pimcore/Controller/Action/Admin.php
@@ -25,6 +25,12 @@ abstract class Pimcore_Controller_Action_Admin extends Pimcore_Controller_Action
      */
     protected $language = "en";
 
+    /**
+     * Indicates if this is the first call or a following
+     * @var bool
+     */
+    protected static $adminInitialized = false;
+
     public function init() {
 
         parent::init();
@@ -47,11 +53,14 @@ abstract class Pimcore_Controller_Action_Admin extends Pimcore_Controller_Action
             }
         }
 
-        try {
-            Zend_Registry::get("pimcore_admin_initialized");
-            $this->setUser(Zend_Registry::get("pimcore_admin_user"));
-        }
-        catch (Exception $e) {
+        if(self::$adminInitialized) {
+            try {
+                $this->setUser(Zend_Registry::get("pimcore_admin_user"));
+            } catch (Exception $e) {
+                Logger::emerg("adminInitialized was set to true although there was no user set in the registry -> to be save the process was killed");
+                exit;
+            }
+        } else {
             // general definitions
             Document::setHideUnpublished(false);
             Object_Abstract::setHideUnpublished(false);
@@ -82,6 +91,7 @@ abstract class Pimcore_Controller_Action_Admin extends Pimcore_Controller_Action
                     $user = Pimcore_Tool_Authentication::authenticateDigest();
                     if($user instanceof User) {
                         $this->setUser($user);
+                        self::$adminInitialized = true;
                         return;
                     }
                 }
@@ -118,7 +128,8 @@ abstract class Pimcore_Controller_Action_Admin extends Pimcore_Controller_Action
             }
 
             Zend_Registry::set("pimcore_admin_user", $this->getUser());
-            Zend_Registry::set("pimcore_admin_initialized", true);
+            self::$adminInitialized = true;
+
         }
     }
 
