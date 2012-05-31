c3691e70a4cf1c8c674e02ef5e1201069516edee
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-05-31 18:31:30 +0000

    Merge pull request #22 from grobmeier/master
    Upgrade to Sabre 1.6.2
    
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1547 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Sabre/DAV/Property/ResourceType.php
+++ pimcore/lib/Sabre/DAV/Property/ResourceType.php
@@ -4,76 +4,121 @@
  * This class represents the {DAV:}resourcetype property
  *
  * Normally for files this is empty, and for collection {DAV:}collection.
- * However, other specs define different values for this. 
- * 
+ * However, other specs define different values for this.
+ *
  * @package Sabre
  * @subpackage DAV
- * @copyright Copyright (C) 2007-2010 Rooftop Solutions. All rights reserved.
+ * @copyright Copyright (C) 2007-2012 Rooftop Solutions. All rights reserved.
  * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
 class Sabre_DAV_Property_ResourceType extends Sabre_DAV_Property {
 
     /**
-     * resourceType 
-     * 
-     * @var string 
+     * resourceType
+     *
+     * @var array
      */
-    public $resourceType = null;
+    public $resourceType = array();
 
     /**
-     * __construct 
-     * 
-     * @param mixed $resourceType 
-     * @return void
+     * __construct
+     *
+     * @param mixed $resourceType
      */
-    public function __construct($resourceType = null) {
+    public function __construct($resourceType = array()) {
 
         if ($resourceType === Sabre_DAV_Server::NODE_FILE)
-            $this->resourceType = null;
+            $this->resourceType = array();
         elseif ($resourceType === Sabre_DAV_Server::NODE_DIRECTORY)
-            $this->resourceType = '{DAV:}collection';
-        else 
+            $this->resourceType = array('{DAV:}collection');
+        elseif (is_array($resourceType))
             $this->resourceType = $resourceType;
+        else
+            $this->resourceType = array($resourceType);
 
     }
 
     /**
-     * serialize 
-     * 
-     * @param DOMElement $prop 
+     * serialize
+     *
+     * @param Sabre_DAV_Server $server
+     * @param DOMElement $prop
      * @return void
      */
-    public function serialize(Sabre_DAV_Server $server,DOMElement $prop) {
+    public function serialize(Sabre_DAV_Server $server, DOMElement $prop) {
 
         $propName = null;
         $rt = $this->resourceType;
-        if (!is_array($rt)) $rt = array($rt);
-        
+
         foreach($rt as $resourceType) {
-            if (preg_match('/^{([^}]*)}(.*)$/',$resourceType,$propName)) { 
-       
+            if (preg_match('/^{([^}]*)}(.*)$/',$resourceType,$propName)) {
+
                 if (isset($server->xmlNamespaces[$propName[1]])) {
                     $prop->appendChild($prop->ownerDocument->createElement($server->xmlNamespaces[$propName[1]] . ':' . $propName[2]));
                 } else {
                     $prop->appendChild($prop->ownerDocument->createElementNS($propName[1],'custom:' . $propName[2]));
                 }
-            
+
             }
         }
 
     }
 
     /**
-     * Returns the value in clark-notation
+     * Returns the values in clark-notation
      *
-     * For example '{DAV:}collection'
-     * 
-     * @return string 
+     * For example array('{DAV:}collection')
+     *
+     * @return array
      */
     public function getValue() {
 
         return $this->resourceType;
+
+    }
+
+    /**
+     * Checks if the principal contains a certain value
+     *
+     * @param string $type
+     * @return bool
+     */
+    public function is($type) {
+
+        return in_array($type, $this->resourceType);
+
+    }
+
+    /**
+     * Adds a resourcetype value to this property
+     *
+     * @param string $type
+     * @return void
+     */
+    public function add($type) {
+
+        $this->resourceType[] = $type;
+        $this->resourceType = array_unique($this->resourceType);
+
+    }
+
+    /**
+     * Unserializes a DOM element into a ResourceType property.
+     *
+     * @param DOMElement $dom
+     * @return Sabre_DAV_Property_ResourceType
+     */
+    static public function unserialize(DOMElement $dom) {
+
+        $value = array();
+        foreach($dom->childNodes as $child) {
+
+            $value[] = Sabre_DAV_XMLUtil::toClarkNotation($child);
+
+        }
+
+        return new self($value);
 
     }
 
--- pimcore/lib/Sabre/DAV/Tree.php
+++ pimcore/lib/Sabre/DAV/Tree.php
@@ -1,49 +1,75 @@
 <?php
 
 /**
- * Abstract tree object 
- * 
+ * Abstract tree object
+ *
  * @package Sabre
  * @subpackage DAV
- * @copyright Copyright (C) 2007-2010 Rooftop Solutions. All rights reserved.
- * @author Evert Pot (http://www.rooftopsolutions.nl/) 
+ * @copyright Copyright (C) 2007-2012 Rooftop Solutions. All rights reserved.
+ * @author Evert Pot (http://www.rooftopsolutions.nl/)
  * @license http://code.google.com/p/sabredav/wiki/License Modified BSD License
  */
 abstract class Sabre_DAV_Tree {
-    
+
     /**
      * This function must return an INode object for a path
-     * If a Path doesn't exist, thrown an Exception_FileNotFound
-     * 
-     * @param string $path 
-     * @throws Exception_FileNotFound
-     * @return Sabre_DAV_INode 
+     * If a Path doesn't exist, thrown a Exception_NotFound
+     *
+     * @param string $path
+     * @throws Sabre_DAV_Exception_NotFound
+     * @return Sabre_DAV_INode
      */
     abstract function getNodeForPath($path);
+
+    /**
+     * This function allows you to check if a node exists.
+     *
+     * Implementors of this class should override this method to make
+     * it cheaper.
+     *
+     * @param string $path
+     * @return bool
+     */
+    public function nodeExists($path) {
+
+        try {
+
+            $this->getNodeForPath($path);
+            return true;
+
+        } catch (Sabre_DAV_Exception_NotFound $e) {
+
+            return false;
+
+        }
+
+    }
 
     /**
      * Copies a file from path to another
      *
      * @param string $sourcePath The source location
      * @param string $destinationPath The full destination path
-     * @return void 
+     * @return void
      */
     public function copy($sourcePath, $destinationPath) {
 
         $sourceNode = $this->getNodeForPath($sourcePath);
-       
+
         // grab the dirname and basename components
         list($destinationDir, $destinationName) = Sabre_DAV_URLUtil::splitPath($destinationPath);
 
         $destinationParent = $this->getNodeForPath($destinationDir);
         $this->copyNode($sourceNode,$destinationParent,$destinationName);
 
+        $this->markDirty($destinationDir);
+
     }
 
     /**
-     * Moves a file from one location to another 
-     * 
-     * @param string $sourcePath The path to the file which should be moved 
+     * Moves a file from one location to another
+     *
+     * @param string $sourcePath The path to the file which should be moved
      * @param string $destinationPath The full destination path, so not just the destination parent node
      * @return int
      */
@@ -59,14 +85,69 @@ abstract class Sabre_DAV_Tree {
             $this->copy($sourcePath,$destinationPath);
             $this->getNodeForPath($sourcePath)->delete();
         }
+        $this->markDirty($sourceDir);
+        $this->markDirty($destinationDir);
 
     }
 
     /**
-     * copyNode 
-     * 
-     * @param Sabre_DAV_INode $source 
-     * @param Sabre_DAV_ICollection $destination 
+     * Deletes a node from the tree
+     *
+     * @param string $path
+     * @return void
+     */
+    public function delete($path) {
+
+        $node = $this->getNodeForPath($path);
+        $node->delete();
+
+        list($parent) = Sabre_DAV_URLUtil::splitPath($path);
+        $this->markDirty($parent);
+
+    }
+
+    /**
+     * Returns a list of childnodes for a given path.
+     *
+     * @param string $path
+     * @return array
+     */
+    public function getChildren($path) {
+
+        $node = $this->getNodeForPath($path);
+        return $node->getChildren();
+
+    }
+
+    /**
+     * This method is called with every tree update
+     *
+     * Examples of tree updates are:
+     *   * node deletions
+     *   * node creations
+     *   * copy
+     *   * move
+     *   * renaming nodes
+     *
+     * If Tree classes implement a form of caching, this will allow
+     * them to make sure caches will be expired.
+     *
+     * If a path is passed, it is assumed that the entire subtree is dirty
+     *
+     * @param string $path
+     * @return void
+     */
+    public function markDirty($path) {
+
+
+    }
+
+    /**
+     * copyNode
+     *
+     * @param Sabre_DAV_INode $source
+     * @param Sabre_DAV_ICollection $destinationParent
+     * @param string $destinationName
      * @return void
      */
     protected function copyNode(Sabre_DAV_INode $source,Sabre_DAV_ICollection $destinationParent,$destinationName = null) {
@@ -83,14 +164,14 @@ abstract class Sabre_DAV_Tree {
                 fwrite($stream,$data);
                 rewind($stream);
                 $data = $stream;
-            } 
+            }
             $destinationParent->createFile($destinationName,$data);
             $destination = $destinationParent->getChild($destinationName);
 
         } elseif ($source instanceof Sabre_DAV_ICollection) {
 
             $destinationParent->createDirectory($destinationName);
-            
+
             $destination = $destinationParent->getChild($destinationName);
             foreach($source->getChildren() as $child) {
 
