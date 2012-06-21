bef9e67f5d9738943c00db9de04a2f625bf4971a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-06-21 20:33:41 +0000

    PIMCORE-1538 - Creating Thumbnails not "on the fly" - CLI script to generate thumbnails
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1598 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/cli/image-thumbnails.php
@@ -0,0 +1,110 @@
+<?php 
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+include_once("startup.php");
+
+try {
+    $opts = new Zend_Console_Getopt(array(
+        'verbose|v' => 'show detailed information (for debug, ...)',
+        'help|h' => 'display this help',
+        "parent|p=i" => "only create thumbnails of images in this folder (ID)",
+        "thumbnails|t=s" => "only create specified thumbnails (comma separated eg.: thumb1,thumb2)"
+    ));
+} catch (Exception $e) {
+    echo $e->getMessage();
+}
+
+try {
+    $opts->parse();
+} catch (Zend_Console_Getopt_Exception $e) {
+    echo $e->getMessage();
+}
+
+
+// display help message
+if($opts->getOption("help")) {
+    echo $opts->getUsageMessage();
+    exit;
+}
+
+if($opts->getOption("verbose")) {
+    $writer = new Zend_Log_Writer_Stream('php://output');
+    $logger = new Zend_Log($writer);
+    Logger::addLogger($logger);
+
+    // set all priorities
+    Logger::setPriorities(array(
+        Zend_Log::DEBUG,
+        Zend_Log::INFO,
+        Zend_Log::NOTICE,
+        Zend_Log::WARN,
+        Zend_Log::ERR,
+        Zend_Log::CRIT,
+        Zend_Log::ALERT,
+        Zend_Log::EMERG
+    ));
+}
+
+// get all thumbnails
+$dir = Asset_Image_Thumbnail_Config::getWorkingDir();
+$thumbnails = array();
+$files = scandir($dir);
+foreach ($files as $file) {
+    if(strpos($file, ".xml")) {
+        $thumbnails[] = str_replace(".xml", "", $file);
+    }
+}
+
+$allowedThumbs = array();
+if($opts->getOption("thumbnails")) {
+    $allowedThumbs = explode(",", $opts->getOption("thumbnails"));
+}
+
+
+// get only images
+$conditions = array("type = 'image'");
+
+if($opts->getOption("parent")) {
+    $parent = Asset::getById($opts->getOption("parent"));
+    if($parent instanceof Asset_Folder) {
+        $conditions[] = "path LIKE '" . $parent->getFullPath() . "/%'";
+    } else {
+        echo $opts->getOption("parent") . " is not a valid asset folder ID!\n";
+        exit;
+    }
+}
+
+$list = new Asset_List();
+$list->setCondition(implode(" AND ", $conditions));
+$total = $list->getTotalCount();
+$perLoop = 10;
+
+for($i=0; $i<(ceil($total/$perLoop)); $i++) {
+    $list->setLimit($perLoop);
+    $list->setOffset($i*$perLoop);
+
+    $images = $list->load();
+    foreach ($images as $image) {
+        foreach ($thumbnails as $thumbnail) {
+            if(empty($allowedThumbs) || in_array($thumbnail, $allowedThumbs)) {
+                echo "generating thumbnail for image: " . $image->getFullpath() . " | " . $image->getId() . " | Thumbnail: " . $thumbnail . " : " . formatBytes(memory_get_usage()) . " \n";
+                $image->getThumbnail($thumbnail);
+            }
+        }
+    }
+    Pimcore::collectGarbage();
+}
+
+
