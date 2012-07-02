c24dc725a5a886b3a5124bcf5e5edbe7e2ffda9a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-07-02 08:22:27 +0000

    improved example page (default/default)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1625 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- website_example/views/scripts/default/default.php
+++ website_example/views/scripts/default/default.php
@@ -1,20 +1,40 @@
 <!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml">
 <head>
-<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-<title>Example</title>
+	<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+	<title>Example</title>
 </head>
 
 <body>
 
-<h1>Hello World!</h1>
-<p>
-    This is just a simple example.
-    <br />
-    
-</p>
+	<style type="text/css">
+		body {
+			padding:10px;
+			font-family: "Lucida Sans Unicode", Arial;
+			font-size: 14px;
+		}
+		
+		h1, h2 {
+			font-size: 18px;
+			padding: 0 0 10px 0;
+		}
+		
+		p {
+			padding: 0 0 5px 0;
+		}
+		
+		a {
+			color: #000;
+		}
+	</style>
 
+
+	<h1>Hello World!</h1>
+	<p>
+		This is just a simple example page.
+		<br />
+		To learn how to create templates with pimcore, please visit our <a href="http://www.pimcore.org/wiki/" target="_blank">documentation</a> or install the example data package. 
+	</p>
 
 </body>
 </html>
-
