a56e30a1baa7eddc2ca7f9db0afe3358ea252826
rtippler <rtippler@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2012-08-09 13:18:04 +0000

    initial commit of selectProxy
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@119 6df6f5fa-e861-40d1-bdbc-1b55999331ee

---
+++ lib/OnlineShop/Framework/FilterService/SelectProxy.php
@@ -0,0 +1,49 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: rtippler
+ * Date: 09.08.12
+ * Time: 15:12
+ * To change this template use File | Settings | File Templates.
+ */
+class OnlineShop_Framework_FilterService_SelectProxy extends OnlineShop_Framework_FilterService_AbstractFilterType
+{
+    /** @var $proxy OnlineShop_Framework_FilterService_AbstractFilterType*/
+    private $proxy;
+    private $field;
+
+
+    function __construct($view, $script,$config)
+    {
+        parent::__construct($view,$script,$config);
+        if (!$config->proxyclass){
+            throw new Exception("wrong configuration for " .  __CLASS__ . ": config setting proxyclass is missing!");
+        }
+        if (!$config->field){
+            throw new Exception("wrong configuration for " .  __CLASS__ . ": config setting field is missing!");
+        }
+        $this->proxy = new $config->proxyclass;
+        $this->field= $config->field;
+    }
+
+    public function getFilterFrontend(
+        OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition,
+        OnlineShop_Framework_ProductList $productList, $currentFilter
+    )
+    {
+        $filterDefinition->field=$this->field;
+        $this->proxy->getFilterFrontend($filterDefinition,$productList,$currentFilter);
+    }
+
+    public function addCondition(
+        OnlineShop_Framework_AbstractFilterDefinitionType $filterDefinition,
+        OnlineShop_Framework_ProductList $productList, $currentFilter, $params,
+        $isPrecondition = false
+    ) {
+        $filterDefinition->field=$this->field;
+        $this->proxy->addCondition($filterDefinition,$productList,$currentFilter,$params,$isPrecondition);
+    }
+
+
+
+}
