eb4038d290b985f2c9fa446510a2165a649d4a74
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-09-04 20:22:39 +0000

    targeting: integrated outputfilter, some improvements
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1808 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Plugin/Targeting.php
+++ pimcore/lib/Pimcore/Controller/Plugin/Targeting.php
@@ -13,13 +13,27 @@
  * @license    http://www.pimcore.org/license     New BSD License
  */
 
-class Pimcore_Controller_Plugin_Analytics extends Zend_Controller_Plugin_Abstract {
+class Pimcore_Controller_Plugin_Targeting extends Zend_Controller_Plugin_Abstract {
 
+    /**
+     * @var bool
+     */
     protected $enabled = true;
+
+    /**
+     * @var Document
+     */
+    protected $document;
 
     public function routeShutdown(Zend_Controller_Request_Abstract $request) {
         if(!Pimcore_Tool::useFrontendOutputFilters($request)) {
             return $this->disable();
+        }
+        
+        if($request->getParam("document") instanceof Document_Page) {
+            $this->document = $request->getParam("document");
+        } else {
+            $this->disable();
         }
     }
 
@@ -34,8 +48,30 @@ class Pimcore_Controller_Plugin_Analytics extends Zend_Controller_Plugin_Abstrac
             return;
         }
         
-        if ($this->enabled && $code = Pimcore_Google_Analytics::getCode()) {
-            
+        if ($this->enabled && $this->document) {
+
+            $targets = array();
+            $list = new Document_Page_Targeting_List();
+            $list->setCondition("documentId = ?", $this->document->getId());
+
+            foreach($list->load() as $target) {
+
+                $redirectUrl = $target->getActions()->getRedirectUrl();
+                if(is_numeric($redirectUrl)) {
+                    $doc = Document::getById($redirectUrl);
+                    if($doc instanceof Document) {
+                        $target->getActions()->redirectUrl = $doc->getFullPath();
+                    }
+                }
+
+                $targets[] = $target;
+            }
+
+            $controlCode = file_get_contents(__DIR__ . "/Targeting/targeting.js");
+            $controlCode = JSMinPlus::minify($controlCode);
+
+            $code = '<script type="text/javascript">var _ptd = ' . Zend_Json::encode($targets) . '</script>';
+            $code .= '<script type="text/javascript">' . $controlCode . '</script>' . "\n";
             // analytics
             $body = $this->getResponse()->getBody();
 
