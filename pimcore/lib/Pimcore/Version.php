451111c5ec82f0f97f626b7807f9ad2296f60fc1
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2012-09-13 18:56:36 +0000

    PIMCORE-1681 - TYPO in localization files
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@1835 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.7";
 
-    public static $revision = 2226;
+    public static $revision = 2227;
 
     public static function getVersion() {
         return self::$version;
