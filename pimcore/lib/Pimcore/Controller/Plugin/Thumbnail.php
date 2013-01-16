f4d2ba44c5777fb1170db11cb65735604e5b64ec
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-01-16 07:26:00 +0000

    direct route to thumbnails for dynamic generation /website/var/tmp/...thumbnail ....
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2198 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Plugin/Thumbnail.php
+++ pimcore/lib/Pimcore/Controller/Plugin/Thumbnail.php
@@ -23,9 +23,32 @@ class Pimcore_Controller_Plugin_Thumbnail extends Zend_Controller_Plugin_Abstrac
         // this is a filter which checks for common used files (by browser, crawlers, ...) and prevent the default
         // error page, because this is more resource-intensive than exiting right here
         if(preg_match("@^/website/var/tmp/thumb_([0-9]+)__([a-zA-Z0-9_\-]+)@",$request->getPathInfo(),$matches)) {
+            $assetId = $matches[1];
+            $thumbnailName = $matches[2];
 
+            if($asset = Asset::getById($assetId)) {
+                try {
+                    // just check if the thumbnail exists -> throws exception otherwise
+                    $thumbnailConfig = Asset_Image_Thumbnail_Config::getByName($thumbnailName);
+                    $thumbnailFile = PIMCORE_DOCUMENT_ROOT . $asset->getThumbnail($thumbnailName);
+                    $imageContent = file_get_contents($thumbnailFile);
 
-            exit;
+                    $fileExtension = Pimcore_File::getFileExtension($thumbnailFile);
+                    if(in_array($fileExtension, array("gif","jpeg","jpeg","png","pjpeg"))) {
+                        header("Content-Type: image/".$fileExtension, true);
+                    } else {
+                        header("Content-Type: " . $asset->getMimetype(), true);
+                    }
+
+                    header("Content-Length: " . filesize($thumbnailFile), true);
+                    echo $imageContent;
+                    exit;
+
+                } catch (Exception $e) {
+                    // nothing to do
+                    Logger::error("Thumbnail with name '" . $thumbnailName . "' doesn't exist");
+                }
+            }
         }
     }
 }
