970ecda2d5fb6659ffc5dc040fdbe8cff77f12c2
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-01-30 08:31:54 +0000

    create cache dir on demand
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2264 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Model/Cache.php
+++ pimcore/lib/Pimcore/Model/Cache.php
@@ -195,6 +195,9 @@ class Pimcore_Model_Cache {
         );
         $config["customBackendNaming"] = false;
 
+        // create cache dir
+        mkdir(PIMCORE_CACHE_DIRECTORY, 0777, true);
+
         return $config;
     }
     
