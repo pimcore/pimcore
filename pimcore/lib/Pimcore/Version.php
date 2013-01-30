568e2520964848a10f233cb4ee774975ad59b7ca
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-01-30 12:59:37 +0000

    style editor controller plugin, document type check for Document_PageSnippet instead of Document_Page
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2271 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,7 +17,7 @@ class Pimcore_Version {
 
     public static $version = "1.4.8";
 
-    public static $revision = 2469;
+    public static $revision = 2473;
 
     public static function getVersion() {
         return self::$version;
