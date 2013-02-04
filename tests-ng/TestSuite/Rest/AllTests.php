f969f1f397ef4df6c0e1063baefcd2886cb7bbe8
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-02-04 11:58:06 +0000

    added more tests
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2323 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- tests-ng/TestSuite/Rest/AllTests.php
+++ tests-ng/TestSuite/Rest/AllTests.php
@@ -8,7 +8,8 @@ class TestSuite_Rest_AllTests extends Test_SuiteBase
     public static function suite() {
         $suite = new TestSuite_Rest_AllTests('RestTests');
 
-        $tests = array('TestSuite_Rest_AssetTest', 'TestSuite_Rest_ObjectTest','TestSuite_Rest_DocumentTest');
+//        $tests = array('TestSuite_Rest_AssetTest', 'TestSuite_Rest_ObjectTest','TestSuite_Rest_DocumentTest');
+        $tests = array('TestSuite_Rest_DocumentTest');
 
         $success = shuffle($tests);
         print("Created the following execution order:\n");
