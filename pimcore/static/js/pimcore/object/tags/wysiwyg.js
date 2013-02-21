9de5c9b90d6df0e477ce8f9387e7e8947be7c2d0
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-02-21 09:21:04 +0000

    checkstyle
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2496 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/object/tags/wysiwyg.js
+++ pimcore/static/js/pimcore/object/tags/wysiwyg.js
@@ -310,7 +310,8 @@ pimcore.object.tags.wysiwyg = Class.create(pimcore.object.tags.abstract, {
             }
         }
 
-        if (data.node.attributes.elementType == "document" && (data.node.attributes.type=="page" || data.node.attributes.type=="hardlink" || data.node.attributes.type=="link")){
+        if (data.node.attributes.elementType == "document" && (data.node.attributes.type=="page"
+                                || data.node.attributes.type=="hardlink" || data.node.attributes.type=="link")){
             this.ckeditor.insertHtml('<a href="' + uri + '" pimcore_type="document" pimcore_id="'
                                 + id + '">' + wrappedText + '</a>');
             return true;
