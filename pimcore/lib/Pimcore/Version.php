4e9c1f92e10ebfda5abeab02fc3f6bab59358ce5
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-02-21 11:26:32 +0000

    fix for PIMCORE-1876: only when parent id is 1
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2498 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,18 +17,18 @@ class Pimcore_Version {
 
     public static $version = "1.4.8";
 
-    public static $revision = 2520;
+    public static $revision = 2535;
 
     public static $svnInfo = array (
   'Path' => 'trunk',
   'URL' => 'http',
   'Repository Root' => 'http',
   'Repository UUID' => '1f8fe7d8-47f0-464c-8d0a-336f4953ab05',
-  'Revision' => '2459',
+  'Revision' => '2494',
   'Node Kind' => 'directory',
   'Last Changed Author' => 'brusch',
-  'Last Changed Rev' => '2459',
-  'Last Changed Date' => '2013-02-17 17',
+  'Last Changed Rev' => '2494',
+  'Last Changed Date' => '2013-02-20 23',
 );
 
     public static function getVersion() {
