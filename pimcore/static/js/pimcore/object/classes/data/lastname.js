27611fb63d2ec81cc3b1383e81097b02a815a89f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-02-22 13:07:11 +0000

    added CRM data-types: firstname, lastname, gender and email
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2512 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/static/js/pimcore/object/classes/data/lastname.js
@@ -0,0 +1,71 @@
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @copyright  Copyright (c) 2009-2010 elements.at New Media Solutions GmbH (http://www.elements.at)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+pimcore.registerNS("pimcore.object.classes.data.lastname");
+pimcore.object.classes.data.lastname = Class.create(pimcore.object.classes.data.data, {
+
+    type: "input",
+    /**
+     * define where this datatype is allowed
+     */
+    allowIn: {
+        object: true,
+        objectbrick: true,
+        fieldcollection: true,
+        localizedfield: true
+    },
+
+    initialize: function (treeNode, initData) {
+        this.type = "lastname";
+
+        this.initData(initData);
+
+        this.treeNode = treeNode;
+    },
+
+    getTypeName: function () {
+        return t("lastname");
+    },
+
+    getGroup: function () {
+            return "crm";
+    },
+
+    getIconClass: function () {
+        return "pimcore_icon_lastname";
+    },
+
+    getLayout: function ($super) {
+
+        $super();
+
+        this.specificPanel.removeAll();
+        this.specificPanel.add([
+            {
+                xtype: "spinnerfield",
+                fieldLabel: t("width"),
+                name: "width",
+                value: this.datax.width
+            },{
+                xtype: "spinnerfield",
+                fieldLabel: t("columnlength"),
+                name: "columnLength",
+                value: this.datax.columnLength
+            }
+        ]);
+
+        return this.layout;
+    }
+
+});
