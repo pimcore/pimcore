0cff093af27aafe4280a6efc5d15cdd8b6f8d633
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-03-20 15:46:14 +0000

    fixed Webservice import for Object_Class_Data_Link
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2637 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Object/Class/Data/Link.php
+++ pimcore/models/Object/Class/Data/Link.php
@@ -292,7 +292,11 @@ class Object_Class_Data_Link extends Object_Class_Data {
      * @param mixed $value
      * @return mixed
      */
-    public function getFromWebserviceImport($value, $relatedObject = null) {
+    public function getFromWebserviceImport($value, $relatedObject = null, $idMapper = null) {
+        if ($value instanceof stdclass) {
+            $value = (array) $value;
+        }
+
         if (empty($value)) {
             return null;
         } else if (is_array($value) and !empty($value['text']) and !empty($value['direct'])) {
@@ -308,10 +312,23 @@ class Object_Class_Data_Link extends Object_Class_Data {
             return $link;
 
         } else if (is_array($value) and !empty($value['text']) and !empty($value['internalType']) and !empty($value['internal'])) {
-            $element = Element_Service::getElementById($value['internalType'],$value['internal']);
-            if(!$element){
-                throw new Exception("cannot get values from web service import - referencing unknown internal element with type [ ".$value['internalType']." ] and id [ ".$value['internal']." ]");
+            $id = $value['internal'];
+
+            if ($idMapper) {
+                $id = $idMapper->getMappedId($value['internalType'], $id);
             }
+
+
+            $element = Element_Service::getElementById($value['internalType'],$id);
+            if(!$element){
+                if ($idMapper && $idMapper->ignoreMappingFailures()) {
+                    $idMapper->recordMappingFailure($relatedObject->getId(),$value['internalType'], $value['internal']);
+                    return null;
+                } else {
+                    throw new Exception("cannot get values from web service import - referencing unknown internal element with type [ ".$value['internalType']." ] and id [ ".$value['internal']." ]");
+                }
+            }
+
             $link = new Object_Data_Link();
             foreach ($value as $key => $value) {
                 $method = "set" . ucfirst($key);
