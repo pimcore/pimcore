7104769c733f372c17735ceaf0a0bead73769041
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-03-27 10:30:22 +0000

    removed targeting support in documents (will be a condition in the future) + some adoptions
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2670 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/2605/postupdate.php
+++ update/2605/postupdate.php
@@ -3,4 +3,5 @@
 // get db connection
 $db = Pimcore_Resource::get();
 $db->query("ALTER TABLE `targeting` DROP INDEX `name_documentId`;");
+$db->query("ALTER TABLE `targeting` DROP INDEX `documentId`;");
 $db->query("ALTER TABLE `targeting` DROP COLUMN `documentId`;");
