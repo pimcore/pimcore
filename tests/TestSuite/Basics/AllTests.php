0aa26a5a6a2c1fc48a67712b492cbf78cf5ec06c
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-04-05 08:18:31 +0000

    parent ID checks
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2717 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ tests/TestSuite/Basics/AllTests.php
@@ -0,0 +1,24 @@
+<?php
+//require_once 'PHPUnit/Framework.php';
+
+
+
+class TestSuite_Basics_AllTests extends Test_SuiteBase
+{
+    public static function suite() {
+        $suite = new TestSuite_Rest_AllTests('Basics');
+
+        $tests = array('TestSuite_Basics_ObjectTest');
+
+        $success = shuffle($tests);
+        print("Created the following execution order:\n");
+
+        foreach ($tests as $test) {
+            print("    - " . $test . "\n");
+            $suite->addTestSuite($test);
+        }
+
+        return $suite;
+    }
+}
+?>
