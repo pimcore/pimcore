6ee8c0fa6989c824474e568c82fff3dd391e80fa
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2013-04-23 10:22:32 +0000

    server-sync
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@236 6df6f5fa-e861-40d1-bdbc-1b55999331ee

---
+++ lib/OnlineShop/Framework/Impl/CartPriceModificatorShipping.php
@@ -0,0 +1,15 @@
+<?php
+
+class OnlineShop_Framework_Impl_CartPriceModificator_Shipping implements OnlineShop_Framework_ICartPriceModificator {
+
+    public function getName() {
+        return "shipping";
+    }
+
+    /**
+     * @return OnlineShop_Framework_IPrice
+     */
+    public function modify(OnlineShop_Framework_IPrice $currentSubTotal, OnlineShop_Framework_ICart $cart) {
+        return new OnlineShop_Framework_Impl_Price("55.99", new Zend_Currency(new Zend_Locale("de_AT")));
+    }
+}
