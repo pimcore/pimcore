cbbcc4668b049c247e2ef4c65f55342cfa62feb0
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-06 14:35:31 +0000

    bugfix in update-script 2665
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2805 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/2665/postupdate.php
+++ update/2665/postupdate.php
@@ -5,4 +5,3 @@ $db = Pimcore_Resource::get();
 $db->query("ALTER TABLE `documents` ADD UNIQUE INDEX `unique_fullpath` (`path`, `key`);");
 $db->query("ALTER TABLE `assets` ADD UNIQUE INDEX `unique_fullpath` (`path`, `filename`);");
 $db->query("ALTER TABLE `objects` ADD UNIQUE INDEX `unique_fullpath` (`o_path`,`o_key`);");
-tzu
