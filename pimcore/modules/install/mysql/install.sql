424d2a0ddbae8e469c18afb74d1abd60a919d9eb
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-17 11:45:42 +0000

    removed unique key from documents, objects and assets table
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2850 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/install/mysql/install.sql
+++ pimcore/modules/install/mysql/install.sql
@@ -13,7 +13,6 @@ CREATE TABLE `assets` (
   `userModification` int(11) unsigned DEFAULT NULL,
   `customSettings` text,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_fullpath` (`path`,`filename`),
   KEY `parentId` (`parentId`),
   KEY `filename` (`filename`),
   KEY `path` (`path`)
@@ -126,7 +125,6 @@ CREATE TABLE `documents` (
   `userOwner` int(11) unsigned DEFAULT NULL,
   `userModification` int(11) unsigned DEFAULT NULL,
   PRIMARY KEY (`id`),
-  UNIQUE KEY `unique_fullpath` (`path`,`key`),
   KEY `parentId` (`parentId`),
   KEY `key` (`key`),
   KEY `path` (`path`),
@@ -350,7 +348,6 @@ CREATE TABLE `objects` (
   `o_classId` int(11) unsigned DEFAULT NULL,
   `o_className` varchar(255) DEFAULT NULL,
   PRIMARY KEY (`o_id`),
-  UNIQUE KEY `unique_fullpath` (`o_path`,`o_key`),
   KEY `key` (`o_key`),
   KEY `path` (`o_path`),
   KEY `published` (`o_published`),
