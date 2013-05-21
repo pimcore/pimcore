454bf6af670ba8bbce0aafb06fd2a280c8126a4f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-21 15:42:33 +0000

    removed redundant code in libfixes (composite)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2871 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/libfixes.js
+++ pimcore/static/js/pimcore/libfixes.js
@@ -1,17 +1,6 @@
 
 // fixes for composite field => getFieldValues() doesn't work
 // read more here http://www.sencha.com/forum/showthread.php?99021&mode=linear
-Ext.override(Ext.form.CompositeField, {
-    bubble : Ext.Container.prototype.bubble,
-    cascade : Ext.Container.prototype.cascade,
-    findById : Ext.Container.prototype.findById,
-    findByType : Ext.Container.prototype.findByType,
-    find : Ext.Container.prototype.find,
-    findBy : Ext.Container.prototype.findBy,
-    get : Ext.Container.prototype.get
-});
-
-
 var _initComponent = Ext.form.CompositeField.prototype.initComponent;
 Ext.override(Ext.form.CompositeField, {
     initComponent: function(){
