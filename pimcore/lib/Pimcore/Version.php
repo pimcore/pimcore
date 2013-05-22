0f28aca7c9f6e5217c0393dad2daab3cc069a688
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-22 08:21:37 +0000

    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2876 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,18 +17,18 @@ class Pimcore_Version {
 
     public static $version = "1.4.10";
 
-    public static $revision = 2696;
+    public static $revision = 2700;
 
     public static $svnInfo = array (
   'Path' => 'trunk',
   'URL' => 'http',
   'Repository Root' => 'http',
   'Repository UUID' => '1f8fe7d8-47f0-464c-8d0a-336f4953ab05',
-  'Revision' => '2864',
+  'Revision' => '2873',
   'Node Kind' => 'directory',
   'Last Changed Author' => 'brusch',
-  'Last Changed Rev' => '2864',
-  'Last Changed Date' => '2013-05-19 16',
+  'Last Changed Rev' => '2873',
+  'Last Changed Date' => '2013-05-22 08',
 );
 
     public static function getVersion() {
