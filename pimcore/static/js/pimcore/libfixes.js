b09784223bcf80c25f7d2397b314e25a8b58f15d
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-24 07:25:17 +0000

    Ext DnD fix
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2898 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/libfixes.js
+++ pimcore/static/js/pimcore/libfixes.js
@@ -18,28 +18,20 @@ Ext.override(Ext.form.CompositeField, {
 });
 
 
-// fixes problem with Ext.Slider in IE9
-// http://www.sencha.com/forum/showthread.php?141254-Ext.Slider-not-working-properly-in-IE9
-Ext.override(Ext.dd.DragTracker, {
-    onMouseMove: function (e, target) {
-        if (this.active && Ext.isIE && !Ext.isIE9 && !e.browserEvent.button) {
-            e.preventDefault();
-            this.onMouseUp(e);
-            return;
-        }
-        e.preventDefault();
-        var xy = e.getXY(), s = this.startXY;
-        this.lastXY = xy;
-        if (!this.active) {
-            if (Math.abs(s[0] - xy[0]) > this.tolerance || Math.abs(s[1] - xy[1]) > this.tolerance) {
-                this.triggerStart(e);
-            } else {
-                return;
-            }
-        }
-        this.fireEvent('mousemove', this, e);
-        this.onDrag(e);
-        this.fireEvent('drag', this, e);
-    }
-});
+// fixes Drag & Drop#
+Ext.dd.DragDropMgr.getZIndex = function(element) {
+    var body = document.body,
+        z,
+        zIndex = -1;
 
+    element = Ext.getDom(element);
+    while (element !== body) {
+        if(!element) break; // this fixes the problem
+
+        if (!isNaN(z = Number(Ext.fly(element).getStyle('zIndex')))) {
+            zIndex = z;
+        }
+        element = element.parentNode;
+    }
+    return zIndex;
+};
