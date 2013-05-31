c778d1d6fffe4c8b3805ac555aecde88f61cb4ff
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-05-31 11:07:14 +0000

    added test for localized fields inheritance
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2951 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- tests/TestSuite/Basics/AllTests.php
+++ tests/TestSuite/Basics/AllTests.php
@@ -6,7 +6,7 @@
 class TestSuite_Basics_AllTests extends Test_SuiteBase
 {
     public static function suite() {
-        $suite = new TestSuite_Rest_AllTests('Basics');
+        $suite = new TestSuite_Basics_AllTests('Basics');
 
         $tests = array('TestSuite_Basics_ObjectTest');
 
