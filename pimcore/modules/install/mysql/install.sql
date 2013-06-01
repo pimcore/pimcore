dd535e4b0b71826423943733fc823c8c3f103b2c
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-06-01 16:50:27 +0000

    added new deployment tables to install.sql
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@2972 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/install/mysql/install.sql
+++ pimcore/modules/install/mysql/install.sql
@@ -110,6 +110,26 @@ CREATE TABLE `dependencies` (
   KEY `targettype` (`targettype`)
 ) DEFAULT CHARSET=utf8;
 
+DROP TABLE IF EXISTS `deployment_packages`;
+CREATE TABLE `deployment_packages` (
+  `id` BIGINT(20) NOT NULL AUTO_INCREMENT,
+  `type` VARCHAR(50) NOT NULL,
+  `subType` VARCHAR(50) NOT NULL,
+  `creationDate` BIGINT(20) NOT NULL,
+  `version` BIGINT(20) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
+
+
+DROP TABLE IF EXISTS `deployment_target`;
+CREATE TABLE `deployment_target` (
+  `id` BIGINT(20) UNSIGNED NOT NULL AUTO_INCREMENT,
+  `parentId` BIGINT(20) UNSIGNED NOT NULL DEFAULT '0',
+  `name` VARCHAR(255) NOT NULL,
+  `creationDate` BIGINT(20) UNSIGNED NOT NULL,
+  `status` VARCHAR(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) DEFAULT CHARSET=utf8;
 
 DROP TABLE IF EXISTS `documents` ;
 CREATE TABLE `documents` (
