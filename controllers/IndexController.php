3a152d467f959ef60adca97e373ee9ca1b2cfbc3
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2013-06-05 13:24:08 +0000

    bugfixing
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@248 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- controllers/IndexController.php
+++ controllers/IndexController.php
@@ -15,21 +15,25 @@ class OnlineShop_IndexController extends Pimcore_Controller_Action_Admin {
         $fields = array();
 
         if($this->_getParam("add_empty") == "true") {
-            $fields[] = array("key" => "", "name" => "(" . $this->view->translate("empty") . ")");
+            $fields[" "] = array("key" => "", "name" => "(" . $this->view->translate("empty") . ")");
         }
 
         $_REQUEST['systemLocale'] = $this->getUser()->getLanguage();
         $adminTranslator = new Pimcore_View_Helper_TranslateAdmin();
 
         foreach($indexColumns as $c) {
-                $fields[] = array("key" => $c, "name" => $adminTranslator->translateAdmin($c));
+            $fields[$c] = array("key" => $c, "name" => $adminTranslator->translateAdmin($c));
         }
 
         if($this->_getParam("specific_price_field") == "true") {
-            $fields[] = array("key" => OnlineShop_Framework_ProductList::ORDERKEY_PRICE, "name" => $adminTranslator->translateAdmin(OnlineShop_Framework_ProductList::ORDERKEY_PRICE));
+            $fields[OnlineShop_Framework_ProductList::ORDERKEY_PRICE] = array("key" => OnlineShop_Framework_ProductList::ORDERKEY_PRICE, "name" => $adminTranslator->translateAdmin(OnlineShop_Framework_ProductList::ORDERKEY_PRICE));
         }
-        $this->_helper->json(array("data" => $fields));
+
+        ksort($fields);
+
+        $this->_helper->json(array("data" => array_values($fields)));
     }
+
 
     public function getAllTenantsAction() {
         $adminTranslator = new Pimcore_View_Helper_TranslateAdmin();
