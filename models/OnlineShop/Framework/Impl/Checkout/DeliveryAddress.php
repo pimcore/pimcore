66bceef1b435bb83484153e8d548c6353ad45304
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2013-06-11 15:11:25 +0000

    bugfixing
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@255 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- models/OnlineShop/Framework/Impl/Checkout/DeliveryAddress.php
+++ models/OnlineShop/Framework/Impl/Checkout/DeliveryAddress.php
@@ -2,15 +2,18 @@
 
 class OnlineShop_Framework_Impl_Checkout_DeliveryAddress extends OnlineShop_Framework_Impl_Checkout_AbstractStep implements OnlineShop_Framework_ICheckoutStep {
 
-    const LINE1 = "delivery_address_line1";
-    const LINE2 = "delivery_address_line2";
-    const LINE3 = "delivery_address_line3";
-    const LINE4 = "delivery_address_line4";
-    const LINE5 = "delivery_address_line5";
-    const LINE6 = "delivery_address_line6";
-    const LINE7 = "delivery_address_line7";
-    const LINE8 = "delivery_address_line8";
-    const IS_ALTERNATIVE = "is_alternative";
+    const DELIVERY_ADDRESS = "delivery_address";
+
+    const FIRSTNAME = "delivery_firstname";
+    const LASTNAME = "delivery_lastname";
+    const COMPANY = "delivery_company";
+    const ADDRESS = "delivery_address";
+    const ZIP = "delivery_zip";
+    const CITY = "delivery_city";
+    const COUNTRY = "delivery_country";
+
+    const IS_ALTERNATIVE = "delivery_is_alternative";
+
 
     /**
      * @return string
@@ -25,14 +28,15 @@ class OnlineShop_Framework_Impl_Checkout_DeliveryAddress extends OnlineShop_Fram
      * @return bool
      */
     public function commit($data) {
-        $this->cart->setCheckoutData(self::LINE1, $data->line1);
-        $this->cart->setCheckoutData(self::LINE2, $data->line2);
-        $this->cart->setCheckoutData(self::LINE3, $data->line3);
-        $this->cart->setCheckoutData(self::LINE4, $data->line4);
-        $this->cart->setCheckoutData(self::LINE5, $data->line5);
-        $this->cart->setCheckoutData(self::LINE6, $data->line6);
-        $this->cart->setCheckoutData(self::LINE7, $data->line7);
-        $this->cart->setCheckoutData(self::LINE8, $data->line8);
+        $this->cart->setCheckoutData(self::DELIVERY_ADDRESS, json_encode($data));
+//        $this->cart->setCheckoutData(self::LINE1, $data->line1);
+//        $this->cart->setCheckoutData(self::LINE2, $data->line2);
+//        $this->cart->setCheckoutData(self::LINE3, $data->line3);
+//        $this->cart->setCheckoutData(self::LINE4, $data->line4);
+//        $this->cart->setCheckoutData(self::LINE5, $data->line5);
+//        $this->cart->setCheckoutData(self::LINE6, $data->line6);
+//        $this->cart->setCheckoutData(self::LINE7, $data->line7);
+//        $this->cart->setCheckoutData(self::LINE8, $data->line8);
         return true;
     }
 
@@ -40,44 +44,47 @@ class OnlineShop_Framework_Impl_Checkout_DeliveryAddress extends OnlineShop_Fram
      * @return mixed
      */
     public function getData() {
-        $data = new stdClass();
-
-        if($this->cart->getCheckoutData(self::LINE1)) {
-            $data->line1 = $this->cart->getCheckoutData(self::LINE1);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE2)) {
-            $data->line2 = $this->cart->getCheckoutData(self::LINE2);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE3)) {
-            $data->line3 = $this->cart->getCheckoutData(self::LINE3);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE4)) {
-            $data->line4 = $this->cart->getCheckoutData(self::LINE4);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE5)) {
-            $data->line5 = $this->cart->getCheckoutData(self::LINE5);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE6)) {
-            $data->line6 = $this->cart->getCheckoutData(self::LINE6);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE7)) {
-            $data->line7 = $this->cart->getCheckoutData(self::LINE7);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::LINE8)) {
-            $data->line9 = $this->cart->getCheckoutData(self::LINE8);
-            $data->isSet = true;
-        }
-        if($this->cart->getCheckoutData(self::IS_ALTERNATIVE)) {
-            $data->isAlternative = $this->cart->getCheckoutData(self::IS_ALTERNATIVE);
-            $data->isSet = $data->isAlternative;
-        }
+        $data = json_decode($this->cart->getCheckoutData(self::DELIVERY_ADDRESS));
         return $data;
+
+//        $data = new stdClass();
+//
+//        if($this->cart->getCheckoutData(self::LINE1)) {
+//            $data->line1 = $this->cart->getCheckoutData(self::LINE1);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE2)) {
+//            $data->line2 = $this->cart->getCheckoutData(self::LINE2);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE3)) {
+//            $data->line3 = $this->cart->getCheckoutData(self::LINE3);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE4)) {
+//            $data->line4 = $this->cart->getCheckoutData(self::LINE4);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE5)) {
+//            $data->line5 = $this->cart->getCheckoutData(self::LINE5);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE6)) {
+//            $data->line6 = $this->cart->getCheckoutData(self::LINE6);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE7)) {
+//            $data->line7 = $this->cart->getCheckoutData(self::LINE7);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::LINE8)) {
+//            $data->line9 = $this->cart->getCheckoutData(self::LINE8);
+//            $data->isSet = true;
+//        }
+//        if($this->cart->getCheckoutData(self::IS_ALTERNATIVE)) {
+//            $data->isAlternative = $this->cart->getCheckoutData(self::IS_ALTERNATIVE);
+//            $data->isSet = $data->isAlternative;
+//        }
+//        return $data;
     }
 }
