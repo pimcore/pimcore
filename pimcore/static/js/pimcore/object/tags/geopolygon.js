146d374f5c0e864a22735929f78d86a25962ac8c
Rafal Galka <rafal.galka@modernweb.pl>
2013-06-14 17:40:52 +0200

    pimcore / geo data abstract classes

--- pimcore/static/js/pimcore/object/tags/geopolygon.js
+++ pimcore/static/js/pimcore/object/tags/geopolygon.js
@@ -13,48 +13,11 @@
  */
 
 /*global google */
-pimcore.registerNS("pimcore.object.tags.geopolygon");
-pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
+pimcore.registerNS('pimcore.object.tags.geopolygon');
+pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.geo.abstract, {
 
-    type: "geopolygon",
+    type: 'geopolygon',
     dirty: false,
-
-    initialize: function (data, fieldConfig) {
-        this.data = data;
-        this.fieldConfig = fieldConfig;
-
-        // extend google maps to support the getBounds() method
-        if (!google.maps.Polygon.prototype.getBounds) {
-
-            google.maps.Polygon.prototype.getBounds = function(latLng) {
-
-                var bounds = new google.maps.LatLngBounds();
-                var paths = this.getPaths();
-                var path;
-
-                for (var p = 0; p < paths.getLength(); p++) {
-                    path = paths.getAt(p);
-                    for (var i = 0; i < path.getLength(); i++) {
-                        bounds.extend(path.getAt(i));
-                    }
-                }
-
-                return bounds;
-            };
-        }
-    },
-
-    getGridColumnConfig: function(field) {
-        return {
-            header: ts(field.label),
-            width: 150,
-            sortable: false,
-            dataIndex: field.key,
-            renderer: function (key, value, metaData, record) {
-                return t('not_supported');
-            }.bind(this, field.key)
-        };
-    },
 
     getLayoutEdit: function () {
 
@@ -64,54 +27,31 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
             title: this.fieldConfig.title,
             height: 370,
             width: 490,
-            cls: "object_field",
+            cls: 'object_field',
             html: '<div id="google_maps_container_' + this.mapImageID + '" align="center">'
-                            + '<img align="center" width="300" height="300" src="' + this.getMapUrl() + '" /></div>',
+                        + '<img align="center" width="300" height="300" src="' + this.getMapUrl() + '" /></div>',
             bbar: [{
-                xtype: "button",
-                text: t("empty"),
-                icon: "/pimcore/static/img/icon/bin.png",
+                xtype: 'button',
+                text: t('empty'),
+                icon: '/pimcore/static/img/icon/bin.png',
                 handler: function () {
                     this.data = null;
                     this.updatePreviewImage();
                     this.dirty = true;
                 }.bind(this)
-            },"->",{
-                xtype: "button",
-                text: t("open_select_editor"),
-                icon: "/pimcore/static/img/icon/magnifier.png",
+            }, '->', {
+                xtype: 'button',
+                text: t('open_select_editor'),
+                icon: '/pimcore/static/img/icon/magnifier.png',
                 handler: this.openPicker.bind(this)
             }]
         });
 
-        this.component.on("afterrender", function () {
+        this.component.on('afterrender', function () {
             this.updatePreviewImage();
         }.bind(this));
 
         return this.component;
-    },
-
-    getLayoutShow: function () {
-
-        this.component = this.getLayoutEdit();
-        this.component.disable();
-
-        return this.component;
-    },
-
-    updatePreviewImage: function () {
-
-        var width = Ext.get("google_maps_container_" + this.mapImageID).getWidth();
-
-        if (width > 640) {
-            width = 640;
-        }
-        if (width < 10) {
-            window.setTimeout(this.updatePreviewImage.bind(this), 1000);
-        }
-
-        Ext.get("google_maps_container_" + this.mapImageID).dom.innerHTML = '<img align="center" width="'
-                                            + width + '" height="300" src="' + this.getMapUrl(width) + '" />';
     },
 
     getMapUrl: function (width) {
@@ -134,7 +74,7 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
 
                 var bounds = new google.maps.LatLngBounds();
 
-                for (var i=0; i<this.data.length; i++) {
+                for (var i = 0; i < this.data.length; i++) {
                     bounds.extend(new google.maps.LatLng(this.data[i].latitude, this.data[i].longitude));
                     pointConfig.push(this.data[i].latitude + "," + this.data[i].longitude);
                 }
@@ -147,14 +87,14 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
 
                 var path = 'weight:0|fillcolor:0x00000073|' + pointConfig.join('|');
                 mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?center=' + center.lat() + ','
-                                + center.lng() + '&zoom=' + mapZoom + '&size=' + px + 'x' + py
-                                + '&path=' + path + '&sensor=false';
+                    + center.lng() + '&zoom=' + mapZoom + '&size=' + px + 'x' + py
+                    + '&path=' + path + '&sensor=false&key=' + pimcore.settings.google_maps_api_key;
             }
             else {
                 mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?center='
                     + this.fieldConfig.lat + ',' + this.fieldConfig.lng
                     + '&zoom=' + mapZoom + '&size='
-                    + px + 'x' + py + '&sensor=false';
+                    + px + 'x' + py + '&sensor=false&key=' + pimcore.settings.google_maps_api_key;
             }
         }
         catch (e) {
@@ -169,9 +109,9 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
 
         this.searchfield = new Ext.form.TextField({
             width: 300,
-            name: "mapSearch",
-            style: "float: left;",
-            fieldLabel: t("search")
+            name: 'mapSearch',
+            style: 'float: left;',
+            fieldLabel: t('search')
         });
 
         this.mapPanel = new Ext.Panel({
@@ -184,17 +124,17 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
             height: 500,
             resizable: false,
             tbar: [{
-                xtype: "button",
-                text: t("empty"),
-                icon: "/pimcore/static/img/icon/bin.png",
+                xtype: 'button',
+                text: t('empty'),
+                icon: '/pimcore/static/img/icon/bin.png',
                 handler: this.removePolygon.bind(this)
             }],
-            bbar: [this.searchfield,{
+            bbar: [this.searchfield, {
                 xtype: 'button',
                 text: t('search'),
                 icon: '/pimcore/static/img/icon/magnifier.png',
                 handler: this.geocode.bind(this)
-            },"->",{
+            }, '->', {
                 xtype: 'button',
                 text: t('cancel'),
                 icon: '/pimcore/static/img/icon/cancel.png',
@@ -349,34 +289,6 @@ pimcore.object.tags.geopolygon = Class.create(pimcore.object.tags.abstract, {
                 }
             }.bind(this));
         }
-    },
-
-    getBoundsZoomLevel: function (bounds, mapDim) {
-        var WORLD_DIM = { height: 256, width: 256 };
-        var ZOOM_MAX = 21;
-
-        function latRad(lat) {
-            var sin = Math.sin(lat * Math.PI / 180);
-            var radX2 = Math.log((1 + sin) / (1 - sin)) / 2;
-            return Math.max(Math.min(radX2, Math.PI), -Math.PI) / 2;
-        }
-
-        function zoom(mapPx, worldPx, fraction) {
-            return Math.floor(Math.log(mapPx / worldPx / fraction) / Math.LN2);
-        }
-
-        var ne = bounds.getNorthEast();
-        var sw = bounds.getSouthWest();
-
-        var latFraction = (latRad(ne.lat()) - latRad(sw.lat())) / Math.PI;
-
-        var lngDiff = ne.lng() - sw.lng();
-        var lngFraction = ((lngDiff < 0) ? (lngDiff + 360) : lngDiff) / 360;
-
-        var latZoom = zoom(mapDim.height, WORLD_DIM.height, latFraction);
-        var lngZoom = zoom(mapDim.width, WORLD_DIM.width, lngFraction);
-
-        return Math.min(latZoom, lngZoom, ZOOM_MAX);
     },
 
     getValue: function () {
