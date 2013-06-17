9a5213f9d1d38e3634ae9852cc1416251bf3491a
pimcore-team <pimcore-team@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-06-17 18:38:35 +0000

    pimcore / geo bounds improvements
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3112 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/object/classes/data/geobounds.js
+++ pimcore/static/js/pimcore/object/classes/data/geobounds.js
@@ -12,10 +12,11 @@
  * @license    http://www.pimcore.org/license     New BSD License
  */
 
-pimcore.registerNS("pimcore.object.classes.data.geobounds");
-pimcore.object.classes.data.geobounds = Class.create(pimcore.object.classes.data.data, {
+pimcore.registerNS('pimcore.object.classes.data.geobounds');
+pimcore.object.classes.data.geobounds = Class.create(pimcore.object.classes.data.geoabstract, {
 
     type: "geobounds",
+
     /**
      * define where this datatype is allowed
      */
@@ -27,7 +28,7 @@ pimcore.object.classes.data.geobounds = Class.create(pimcore.object.classes.data
     },
 
     initialize: function (treeNode, initData) {
-        this.type = "geobounds";
+        this.type = 'geobounds';
 
         this.initData(initData);
 
@@ -38,23 +39,15 @@ pimcore.object.classes.data.geobounds = Class.create(pimcore.object.classes.data
     },
 
     getTypeName: function () {
-        return t("geobounds");
+        return t('geobounds');
     },
 
     getGroup: function () {
-            return "geo";
+            return 'geo';
     },
 
     getIconClass: function () {
-        return "pimcore_icon_geobounds";
-    },
-
-    getLayout: function ($super) {
-
-        $super();
-
-
-        return this.layout;
+        return 'pimcore_icon_geobounds';
     }
 
 });
