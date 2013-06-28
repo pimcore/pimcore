0d31eb98a60a6aa6cffc16d4c1e71fa22a36500e
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-06-28 10:30:38 +0000

    object geo datatypes: use maptype also for staticmaps (preview)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3184 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/object/tags/geopoint.js
+++ pimcore/static/js/pimcore/object/tags/geopoint.js
@@ -144,12 +144,12 @@ pimcore.object.tags.geopoint = Class.create(pimcore.object.tags.geo.abstract, {
                 + lat + "," + lng + "&zoom=" + mapZoom +
                 '&size=' + px + 'x' + py
                 + '&markers=color:red|' + lat + ',' + lng
-                + '&sensor=false';
+                + '&sensor=false&maptype=' + this.fieldConfig.mapType;
         } else {
             mapUrl = 'https://maps.googleapis.com/maps/api/staticmap?center='
                 + lat + "," + lng + "&zoom=" + mapZoom +
                 '&size=' + px + 'x' + py
-                + '&sensor=false';
+                + '&sensor=false&maptype=' + this.fieldConfig.mapType;
         }
 
         if (pimcore.settings.google_maps_api_key) {
