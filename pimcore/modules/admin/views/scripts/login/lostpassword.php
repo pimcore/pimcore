0d4e222234866adb6c0263e80c09fff5fa13808a
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-04 19:11:37 +0000

    new login style
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3225 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/views/scripts/login/lostpassword.php
+++ pimcore/modules/admin/views/scripts/login/lostpassword.php
@@ -2,97 +2,86 @@
 <html>
 <head>
 
-    <title>pimcore - Lost Password Service</title>
+    <title>Welcome to pimcore!</title>
 
     <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
-    <meta name="robots" content="noindex, nofollow" />
+    <meta name="robots" content="noindex, follow" />
 
-    <link rel="stylesheet" href="/pimcore/static/css/login-reloaded.css" type="text/css" />
+    <link rel="stylesheet" href="/pimcore/static/css/login.css" type="text/css" />
 
     <?php
-        // load plugin scripts
-        try {
-            $pluginBroker = Zend_Registry::get("Pimcore_API_Plugin_Broker");
-            if ($pluginBroker instanceof Pimcore_API_Plugin_Broker) {
-                foreach ($pluginBroker->getPlugins() as $plugin) {
-                    if ($plugin->isInstalled()) {
-                        $cssPaths = $plugin->getCssPaths();
-                        if (!empty($cssPaths)) {
-                            foreach ($cssPaths as $cssPath) {
-                                $cssPath = trim($cssPath);
-                                if (!empty($cssPath)) {
-                                    ?>
-                                    <link rel="stylesheet" type="text/css" href="<?php echo $cssPath ?>?_dc=<?php echo time() ?>"/>
-                                    <?php
+    // load plugin scripts
+    try {
+        $pluginBroker = Zend_Registry::get("Pimcore_API_Plugin_Broker");
+        if ($pluginBroker instanceof Pimcore_API_Plugin_Broker) {
+            foreach ($pluginBroker->getPlugins() as $plugin) {
+                if ($plugin->isInstalled()) {
+                    $cssPaths = $plugin->getCssPaths();
+                    if (!empty($cssPaths)) {
+                        foreach ($cssPaths as $cssPath) {
+                            $cssPath = trim($cssPath);
+                            if (!empty($cssPath)) {
+                                ?>
+                                <link rel="stylesheet" type="text/css" href="<?php echo $cssPath ?>?_dc=<?php echo time() ?>"/>
+                            <?php
 
-                                }
                             }
                         }
                     }
                 }
             }
         }
-        catch (Exception $e) {}
+    }
+    catch (Exception $e) {}
     ?>
 
 </head>
 <body>
 
-<img src="/pimcore/static/img/login-reloaded/background.jpg" class="background" id="backgroundimage" />
-
 <div id="vcenter">
     <div id="content">
+        <div id="right">
 
-        <?php if ($this->error) { ?>
-            <div class="error">
-                <?php echo $this->translate($this->error) ?>
-            </div>
-        <?php } else { ?>
-            <div class="logo"></div>
-        <?php } ?>
-
-        <?php if($this->success) { ?>
-            <form action="/admin/login/" id="loginform" method="post">
-                <p style="text-align: center;">
+            <?php if($this->success) { ?>
+                <div class="info">
                     <?php echo $this->translate("A temporary login link has been sent to your email address."); ?>
                     <br />
                     <?php echo $this->translate("Please check your mailbox."); ?>
-                </p>
-                <p class="submit">
-                    <input class="submit" type="submit" value="Back to Login" />
-                </p>
-            </form>
-        <?php } else { ?>
-            <form action="/admin/login/lostpassword" id="loginform" method="post" enctype="application/x-www-form-urlencoded">
-                <p>
-                    <?php echo $this->translate("Enter your username and pimcore will send a login link to your email address"); ?>:
-                </p>
-                <p>
+
+                    <br />
+                    <br />
+
+                    <a href="/admin/login/"><?php echo $this->translate("Back to login"); ?></a>
+                </div>
+            <?php } else { ?>
+                <div class="info" style="padding-bottom: 40px;">
+                    <?php echo $this->translate("Enter your username and pimcore will send a login link to your email address"); ?>
+                </div>
+                <form method="post" action="/admin/login/lostpassword">
                     <label><?php echo $this->translate("Username"); ?></label>
-                    <input class="credential" name="username" id="username" type="text" />
-                    <span class="clear"></span>
-                </p>
-                <p class="submit">
-                    <input class="submit" type="submit" value="<?php echo $this->translate("Submit"); ?>" />
-                    <a href="/admin/login/" class="lostpassword"><?php echo $this->translate("Back to Login"); ?></a>
-                </p>
-            </form>
-        <?php } ?>
+                    <input type="text" name="username" />
+
+                    <input type="submit" name="submit" value="<?php echo $this->translate("Submit"); ?>" />
+
+                    <?php if ($this->error) { ?>
+                        <div class="error">
+                            <?php echo $this->translate($this->error) ?>
+                        </div>
+                    <?php } ?>
+
+                    <div class="info" style="padding-top: 30px;">
+                        <a href="/admin/login/"><?php echo $this->translate("Back to login"); ?></a>
+                    </div>
+                </form>
+            <?php } ?>
+        </div>
     </div>
 </div>
-
 <div id="footer">
-    <div class="left" id="imageinfo"></div>
-    <div class="right">pimcore. Open Source Framework for Content and Product Information Management<br />&copy; <?php echo date("Y") ?>-2011 elements.at New Media Solutions GmbH</div>
-    <div class="background"></div>
+    <a href="http://www.pimcore.org/">pimcore. Open Source Multichannel Experience and Engagement Platform</a>
+    <br />
+    &copy; 2009-<?php echo date("Y") ?> <a href="http://www.pimcore.org/">pimcore GmbH</a>, a proud member of the <a href="http://www.elements.at/">elements group</a>
 </div>
-
-
-<script type="text/javascript">
-    if(document.getElementById("username")) {
-        document.getElementById("username").focus();
-    }
-</script>
 
 </body>
 </html>
