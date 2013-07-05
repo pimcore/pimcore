bb1d42758335f65e17888d0826335860502275f0
pimcore-team <pimcore-team@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-05 06:45:58 +0000

    build auto version (2791) ...
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3228 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -17,18 +17,18 @@ class Pimcore_Version {
 
     public static $version = "1.4.10";
 
-    public static $revision = 2790;
+    public static $revision = 2791;
 
     public static $svnInfo = array (
   'Path' => 'trunk',
   'URL' => 'http',
   'Repository Root' => 'http',
   'Repository UUID' => '1f8fe7d8-47f0-464c-8d0a-336f4953ab05',
-  'Revision' => '3225',
+  'Revision' => '3227',
   'Node Kind' => 'directory',
   'Last Changed Author' => 'brusch',
-  'Last Changed Rev' => '3225',
-  'Last Changed Date' => '2013-07-04 21',
+  'Last Changed Rev' => '3227',
+  'Last Changed Date' => '2013-07-05 08',
 );
 
     public static function getVersion() {
