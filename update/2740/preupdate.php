f0c800deba14f821183d6749e8a5214173123c8b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-16 09:53:39 +0000

    PIMCORE-2012 - MYSQL syntax bug in Install.sql
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3300 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/2740/preupdate.php
+++ update/2740/preupdate.php
@@ -5,7 +5,7 @@ $db = Pimcore_Resource::get();
 $db->query("DROP TABLE IF EXISTS `cache`;");
 $db->query("CREATE TABLE `cache` (
   `id` varchar(165) NOT NULL DEFAULT '',
-  `data` longtext NOT NULL DEFAULT '',
+  `data` longtext,
   `mtime` bigint(20) DEFAULT NULL,
   `expire` bigint(20) DEFAULT NULL,
   PRIMARY KEY (`id`)
