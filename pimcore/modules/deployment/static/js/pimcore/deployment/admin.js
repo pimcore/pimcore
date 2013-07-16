e0594165e7014ec7ad958c1fbeff357fc22f18ee
ckogler <ckogler@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-16 20:37:53 +0000

    deployment
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3308 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/deployment/static/js/pimcore/deployment/admin.js
+++ pimcore/modules/deployment/static/js/pimcore/deployment/admin.js
@@ -12,10 +12,34 @@
  * @license    http://www.pimcore.org/license     New BSD License
  */
 
-pimcore.registerNS("pimcore.modules.deployment.admin");
-pimcore.modules.deployment.admin = Class.create({
 
-    initialize: function () {
+pimcore.registerNS("pimcore.plugin.deployment");
+pimcore.plugin.deployment = Class.create(pimcore.plugin.admin, {
+    getClassName: function() {
+        return "pimcore.plugin.deployment";
+    },
+    initialize: function() {
+        pimcore.plugin.broker.registerPlugin(this);
+        //var navContainer = Ext.get(Ext.query("#pimcore_navigation > ul > li:last"));
+        //this.navEl = Ext.get(navContainer.insertSibling('<li id="pimcore_menu_deployment" class="pimcore_menu_item icon-flash">' + t('deployment') + '</li>'));
+        //var navContainer = Ext.get(Ext.query("#pimcore_navigation > ul")[0]);
+        //this.navEl = Ext.get(navContainer.insertHtml("beforeEnd", '<li id="pimcore_menu_custom" class="pimcore_menu_item icon-truck">' + t('deployment') + '</li>'));
+    },
+    pimcoreReady: function (params,broker){
+       /* var menu = new Ext.menu.Menu({
+            items: [{
+                text: "Item 1",
+                iconCls: "pimcore_icon_apply",
+                handler: function () {alert("pressed 1")}
+            }, {
+                text: "Item 2",
+                iconCls: "pimcore_icon_delete",
+                handler: function () {alert("pressed 2")}
+            }],
+            cls: "pimcore_navigation_flyout"
+        });
+        var toolbar = pimcore.globalmanager.get("layout_toolbar");
+        this.navEl.on("mousedown", toolbar.showSubMenu.bind(menu));*/
     }
 });
-pimcore.modules.deployment.admin = new pimcore.modules.deployment.admin();
+var deployment = new pimcore.plugin.deployment();
