3c4d275152eb2ccc0dadeeaad8452a631965a031
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-19 14:44:45 +0000

    removed subType from uuids
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3328 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/install/mysql/install.sql
+++ pimcore/modules/install/mysql/install.sql
@@ -708,7 +708,6 @@ CREATE TABLE IF NOT EXISTS `uuids` (
   `uuid` CHAR(36) NOT NULL,
   `itemId` BIGINT(20) UNSIGNED NOT NULL,
   `type` VARCHAR(25) NOT NULL,
-  `subType` VARCHAR(20) NULL DEFAULT NULL,
   `instanceIdentifier` VARCHAR(50) NOT NULL,
   UNIQUE INDEX `itemId_type_uuid` (`itemId`, `type`, `uuid`)
 ) DEFAULT CHARSET=utf8;
