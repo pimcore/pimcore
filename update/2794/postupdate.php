52260c227be990646bba66f8106f1a61e22cbead
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-07-19 10:19:28 +0000

    cleanups and fixes
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3319 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/2794/postupdate.php
+++ update/2794/postupdate.php
@@ -3,12 +3,11 @@
 // get db connection
 $db = Pimcore_Resource::get();
 $db->query("CREATE TABLE IF NOT EXISTS `uuids` (
-	`uuid` CHAR(36) NOT NULL,
-	`itemId` BIGINT(20) UNSIGNED NOT NULL,
-	`type` VARCHAR(25) NOT NULL,
-	`subType` VARCHAR(20) NULL DEFAULT NULL,
-	`instanceIdentifier` VARCHAR(50) NOT NULL,
-	UNIQUE INDEX `itemId_type_uuid` (`itemId`, `type`, `uuid`)
-)
-COLLATE='utf8_general_ci'
-ENGINE=InnoDB;");
+  `uuid` CHAR(36) NOT NULL,
+  `itemId` BIGINT(20) UNSIGNED NOT NULL,
+  `type` VARCHAR(25) NOT NULL,
+  `subType` VARCHAR(20) NULL DEFAULT NULL,
+  `instanceIdentifier` VARCHAR(50) NOT NULL,
+  UNIQUE INDEX `itemId_type_uuid` (`itemId`, `type`, `uuid`)
+) DEFAULT CHARSET=utf8;");
+
