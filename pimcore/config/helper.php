33d8385fb88c66294cfb631a2d8a7c1b2eaab4fc
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-08-07 08:45:38 +0000

    asset folder in recyclebin: do not include children if the total size is too big for the memory_limit
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3422 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/config/helper.php
+++ pimcore/config/helper.php
@@ -14,6 +14,27 @@
  */
 
 
+function foldersize($path) {
+    $total_size = 0;
+    $files = scandir($path);
+    $cleanPath = rtrim($path, '/'). '/';
+
+    foreach($files as $t) {
+        if ($t<>"." && $t<>"..") {
+            $currentFile = $cleanPath . $t;
+            if (is_dir($currentFile)) {
+                $size = foldersize($currentFile);
+                $total_size += $size;
+            }
+            else {
+                $size = filesize($currentFile);
+                $total_size += $size;
+            }
+        }
+    }
+
+    return $total_size;
+}
 
 function replace_pcre_backreferences($string, $values) {
 
