b7d18afb068057d7708b6c75a9f8836406d5ea7a
markus <markus@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-09-19 14:47:32 +0000

    added google analytics adapter for custom reports
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3649 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/views/scripts/index/index.php
+++ pimcore/modules/admin/views/scripts/index/index.php
@@ -422,6 +422,7 @@ $scripts = array(
     "pimcore/report/custom/settings.js",
     "pimcore/report/custom/report.js",
     "pimcore/report/custom/definitions/sql.js",
+    "pimcore/report/custom/definitions/analytics.js",
 
     "pimcore/settings/tagmanagement/panel.js",
     "pimcore/settings/tagmanagement/item.js",
