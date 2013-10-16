99ba2fa6f71d49d37de10716742aa0c360e0b0f6
fash <fash@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-10-16 14:58:02 +0000

    PIMCORE-2087 Remove Order-Arrows in object data type NonOwnerObjects
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3813 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/object/tags/nonownerobjects.js
+++ pimcore/static/js/pimcore/object/tags/nonownerobjects.js
@@ -115,40 +115,6 @@ pimcore.object.tags.nonownerobjects = Class.create(pimcore.object.tags.objects, 
                     {id: "path", header: t("path"), dataIndex: 'path', width: 200},
                     {header: t("type"), dataIndex: 'type', width: 100},
                     {
-                        xtype:'actioncolumn',
-                        width:30,
-                        items:[
-                            {
-                                tooltip:t('up'),
-                                icon:"/pimcore/static/img/icon/arrow_up.png",
-                                handler:function (grid, rowIndex) {
-                                    if (rowIndex > 0) {
-                                        var rec = grid.getStore().getAt(rowIndex);
-                                        grid.getStore().removeAt(rowIndex);
-                                        grid.getStore().insert(rowIndex - 1, [rec]);
-                                    }
-                                }.bind(this)
-                            }
-                        ]
-                    },
-                    {
-                        xtype:'actioncolumn',
-                        width:30,
-                        items:[
-                            {
-                                tooltip:t('down'),
-                                icon:"/pimcore/static/img/icon/arrow_down.png",
-                                handler:function (grid, rowIndex) {
-                                    if (rowIndex < (grid.getStore().getCount() - 1)) {
-                                        var rec = grid.getStore().getAt(rowIndex);
-                                        grid.getStore().removeAt(rowIndex);
-                                        grid.getStore().insert(rowIndex + 1, [rec]);
-                                    }
-                                }.bind(this)
-                            }
-                        ]
-                    },
-                    {
                         xtype: 'actioncolumn',
                         width: 30,
                         items: [
