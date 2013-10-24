72f6ef1fafd4e575e4774959edc89e276b4663be
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-10-24 07:18:10 +0000

    added modificationdate to version preview
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3858 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/views/scripts/object/preview-version.php
+++ pimcore/modules/admin/views/scripts/object/preview-version.php
@@ -10,7 +10,7 @@
 <body>
 
 
-<?php $fields = $this->object->geto_class()->getFieldDefinitions(); ?>
+<?php $fields = $this->object->getClass()->getFieldDefinitions(); ?>
 
 <table class="preview" border="0" cellpadding="0" cellspacing="0">
     <tr>
@@ -18,7 +18,11 @@
         <th>Key</th>
         <th>Value</th>
     </tr>
-
+    <tr class="system">
+        <td>Date</td>
+        <td>o_modificationDate</td>
+        <td><?php echo date('Y-m-d H:m:s', $this->object->getModificationDate()); ?></td>
+    </tr>
     <tr class="system">
         <td>Path</td>
         <td>o_path</td>
@@ -49,33 +53,33 @@
             } ?>
     <?php } ?>
     <?php } else if($definition instanceof Object_Class_Data_Objectbricks){ ?>
-        <?php foreach($definition->getAllowedTypes() as $asAllowedType) { ?>
-            <?php
-            $collectionDef = Object_Objectbrick_Definition::getByKey($asAllowedType);
+            <?php foreach($definition->getAllowedTypes() as $asAllowedType) { ?>
+                <?php
+                $collectionDef = Object_Objectbrick_Definition::getByKey($asAllowedType);
 
-            foreach ($collectionDef->getFieldDefinitions() as $lfd) { ?>
-                <?php
-                $value = null;
-                $bricks = $this->object->{"get" . ucfirst($fieldName)}();
-                if ($bricks) {
-                    $brickValue = $bricks->{"get" . $asAllowedType}();
-                    if ($brickValue) {
-                        $value = $lfd->getVersionPreview($brickValue->getValueForFieldName($lfd->getName()));
+                foreach ($collectionDef->getFieldDefinitions() as $lfd) { ?>
+                    <?php
+                    $value = null;
+                    $bricks = $this->object->{"get" . ucfirst($fieldName)}();
+                    if ($bricks) {
+                        $brickValue = $bricks->{"get" . $asAllowedType}();
+                        if ($brickValue) {
+                            $value = $lfd->getVersionPreview($brickValue->getValueForFieldName($lfd->getName()));
+                        }
                     }
-                }
-                if (!$bricks) {
-                    continue;
-                }
-                ?>
-                <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
-                    <td><?php echo ucfirst($asAllowedType) . " - " . $lfd->getTitle() ?> (<?php echo $language; ?>)</td>
-                    <td><?php echo $lfd->getName() ?></td>
-                    <td><?php echo $value ?></td>
-                </tr>
-                <?php
-                $c++;
-            } ?>
-        <?php } ?>
+                    if (!$bricks) {
+                        continue;
+                    }
+                    ?>
+                     <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
+                        <td><?php echo ucfirst($asAllowedType) . " - " . $lfd->getTitle() ?> (<?php echo $language; ?>)</td>
+                        <td><?php echo $lfd->getName() ?></td>
+                        <td><?php echo $value ?></td>
+                    </tr>
+                    <?php
+                    $c++;
+                } ?>
+            <?php } ?>
     <?php } else { ?>
         <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
             <td><?php echo $definition->getTitle() ?></td>
