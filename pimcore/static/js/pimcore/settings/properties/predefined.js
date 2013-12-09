884b507227b505827c46e09f9dbf33fd9d68ecc6
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-12-09 13:41:02 +0000

    PIMCORE-2114 Add timestamps to configuration tables
     add dates to predifned properties grid and hide them by default
    
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@3979 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/settings/properties/predefined.js
+++ pimcore/static/js/pimcore/settings/properties/predefined.js
@@ -71,7 +71,9 @@ pimcore.settings.properties.predefined = Class.create({
             {name: 'data', allowBlank: true},
             {name: 'config', allowBlank: true},
             {name: 'ctype', allowBlank: false},
-            {name: 'inheritable', allowBlank: true}
+            {name: 'inheritable', allowBlank: true},
+            {name: 'creationDate', allowBlank: true},
+            {name: 'modificationDate', allowBlank: true}
         ]);
 
         var writer = new Ext.data.JsonWriter();
@@ -209,7 +211,30 @@ pimcore.settings.properties.predefined = Class.create({
                         }
                     }.bind(this)
                 }]
-            }
+            },
+            {header: t("creationDate"), sortable: true, dataIndex: 'creationDate', editable: false,
+                hidden: true,
+                renderer: function(d) {
+                    if (d !== undefined) {
+                        var date = new Date(d * 1000);
+                        return date.format("Y-m-d H:i:s");
+                    } else {
+                        return "";
+                    }
+                }
+            },
+            {header: t("modificationDate"), sortable: true, dataIndex: 'modificationDate', editable: false,
+                hidden: true,
+                renderer: function(d) {
+                    if (d !== undefined) {
+                        var date = new Date(d * 1000);
+                        return date.format("Y-m-d H:i:s");
+                    } else {
+                        return "";
+                    }
+                }
+            },
+
         ];
 
         this.grid = new Ext.grid.EditorGridPanel({
