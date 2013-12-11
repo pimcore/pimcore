a5fae4a2a1f713ae626c585c5f8aeee1547bea5f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-12-11 09:40:44 +0000

    asset document converter: fixed problem with set_time_limit() in CLI mode
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4003 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Document/Adapter/LibreOffice.php
+++ pimcore/lib/Pimcore/Document/Adapter/LibreOffice.php
@@ -88,7 +88,10 @@ class Pimcore_Document_Adapter_LibreOffice extends Pimcore_Document_Adapter_Ghos
     public function load($path) {
 
         // avoid timeouts
-        set_time_limit(250);
+        $maxExecTime = (int) ini_get("max_execution_time");
+        if($maxExecTime > 1 && $maxExecTime < 250) {
+            set_time_limit(250);
+        }
 
         if(!$this->isFileTypeSupported($path)) {
             $message = "Couldn't load document " . $path . " only Microsoft/Libre/Open-Office/PDF documents are currently supported";
