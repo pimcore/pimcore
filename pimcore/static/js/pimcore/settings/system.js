1597d2a7f7a9454f4892a34591c49ba9a2926616
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-12-20 11:49:10 +0000

    increased width of language combo
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4041 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/settings/system.js
+++ pimcore/static/js/pimcore/settings/system.js
@@ -257,7 +257,7 @@ pimcore.settings.system = Class.create({
                         }, {
                             xtype: "compositefield",
                             fieldLabel: t("add_language"),
-                            width: 300,
+                            width: 250,
                             items: [{
                                 xtype: "combo",
                                 id: "system.settings.general.languageSelection",
