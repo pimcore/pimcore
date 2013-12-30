8d4ba8205c87c0133d19dc967776ab5b2cb8f0ce
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2013-12-30 14:24:41 +0000

    demo: added newsletter
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4105 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- website_demo/controllers/NewsletterController.php
+++ website_demo/controllers/NewsletterController.php
@@ -1,77 +1,79 @@
 <?php
 
-class AdvancedController extends Website_Controller_Action
-{
-    public function indexAction() {
+class NewsletterController extends Website_Controller_Action {
+
+    public function subscribeAction () {
+
         $this->enableLayout();
 
-        $list = new Document_List();
-        $list->setCondition("parentId = ?", array($this->document->getId()));
-        $list->load();
+        $newsletter = new Pimcore_Tool_Newsletter("person"); // replace "crm" with the class name you have used for your class above (mailing list)
+        $params = $this->getAllParams();
 
-        $this->view->documents = $list;
+        $this->view->success = false;
+
+        if($newsletter->checkParams($params)) {
+            try {
+                $params["parentId"] = 1; // default folder (home) where we want to save our subscribers
+                $newsletterFolder = Object_Abstract::getByPath("/crm/newsletter");
+                if($newsletterFolder) {
+                    $params["parentId"] = $newsletterFolder->getId();
+                }
+
+                $user = $newsletter->subscribe($params);
+
+                // user and email document
+                // parameters available in the email: gender, firstname, lastname, email, token, object
+                // ==> see mailing framework
+                $newsletter->sendConfirmationMail($user, Document::getByPath("/en/advanced-examples/newsletter/confirmation-email")); // replace "3076" with the ID of the email document you want to use as confirmation email
+
+                // do some other stuff with the new user
+                $user->setDateRegister(Zend_Date::now());
+                $user->save();
+
+                $this->view->success = true;
+            } catch (\Exception $e) {
+                echo $e->getMessage();
+            }
+        }
     }
 
+    public function confirmAction() {
 
-    public function objectFormAction() {
         $this->enableLayout();
 
+        $this->view->success = false;
+
+        $newsletter = new Pimcore_Tool_Newsletter("person"); // replace "crm" with the class name you have used for your class above (mailing list)
+
+        if($newsletter->confirm($this->getParam("token"))) {
+            $this->view->success = true;
+        }
+    }
+
+    public function unsubscribeAction() {
+
+        $this->enableLayout();
+
+        $newsletter = new Pimcore_Tool_Newsletter("person"); // replace "crm" with the class name you have used for your class above (mailing list)
+
+        $unsubscribeMethod = null;
         $success = false;
 
-        // getting parameters is very easy ... just call $this->getParam("yorParamKey"); regardless if's POST or GET
-        if($this->getParam("firstname") && $this->getParam("lastname") && $this->getParam("email") && $this->getParam("terms")) {
-            $success = true;
-
-            // for this example the class "person" and "inquiry" is used
-            // first we create a person, then we create an inquiry object and link them together
-
-            // check for an existing person with this name
-            $person = Object_Person::getByEmail($this->getParam("email"),1);
-
-            if(!$person) {
-                // if there isn't an existing, ... create one
-                $filename = Pimcore_File::getValidFilename($this->getParam("email"));
-
-                // first we need to create a new object, and fill some system-related information
-                $person = new Object_Person();
-                $person->setParent(Object_Abstract::getByPath("/crm")); // we store all objects in /crm
-                $person->setKey($filename); // the filename of the object
-                $person->setPublished(true); // yep, it should be published :)
-
-                // of course this needs some validation here in production...
-                $person->setGender($this->getParam("gender"));
-                $person->setFirstname($this->getParam("firstname"));
-                $person->setLastname($this->getParam("lastname"));
-                $person->setEmail($this->getParam("email"));
-                $person->setDateRegister(Zend_Date::now());
-                $person->save();
-            }
-
-            // now we create the inquiry object and link the person in it
-            $inquiryFilename = Pimcore_File::getValidFilename(Zend_Date::now()->get(Zend_Date::DATETIME_MEDIUM) . "~" . $person->getEmail());
-            $inquiry = new Object_Inquiry();
-            $inquiry->setParent(Object_Abstract::getByPath("/inquiries")); // we store all objects in /inquiries
-            $inquiry->setKey($inquiryFilename); // the filename of the object
-            $inquiry->setPublished(true); // yep, it should be published :)
-
-            // now we fill in the data
-            $inquiry->setMessage($this->getParam("message"));
-            $inquiry->setPerson($person);
-            $inquiry->setDate(Zend_Date::now());
-            $inquiry->setTerms((bool) $this->getParam("terms"));
-            $inquiry->save();
-        } else if ($this->getRequest()->isPost()) {
-            $this->view->error = true;
+        if($this->getParam("email")) {
+            $unsubscribeMethod = "email";
+            $success = $newsletter->unsubscribeByEmail($this->getParam("email"));
         }
 
-        // do some validation & assign the parameters to the view
-        foreach (array("firstname", "lastname", "email", "message", "terms") as $key) {
-            if($this->getParam($key)) {
-                $this->view->$key = htmlentities(strip_tags($this->getParam($key)));
-            }
+        if($this->getParam("token")) {
+            $unsubscribeMethod = "token";
+            $success = $newsletter->unsubscribeByToken($this->getParam("token"));
         }
 
-        // assign the status to the view
         $this->view->success = $success;
+        $this->view->unsubscribeMethod = $unsubscribeMethod;
+    }
+
+    public function standardMailAction() {
+
     }
 }
