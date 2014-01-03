184532e8009bf70c668bffa98bb710eab4ef1cf7
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-01-03 09:31:04 +0000

    refactored hardlinks to make use of traits
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4133 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/models/Document/Hardlink/Wrapper.php
@@ -0,0 +1,128 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Document
+ * @copyright  Copyright (c) 2009-2013 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+trait Document_Hardlink_Wrapper {
+
+    /**
+     * @var Document_Hardlink
+     */
+    protected $hardLinkSource;
+
+    // OVERWRITTEN METHODS
+    public function save() {
+        $this->raiseHardlinkError();
+    }
+
+    protected function update() {
+        $this->raiseHardlinkError();
+    }
+
+    public function delete() {
+        $this->raiseHardlinkError();
+    }
+
+    public function getProperties() {
+
+        if($this->properties == null) {
+
+            if($this->getHardLinkSource()->getPropertiesFromSource()) {
+                $sourceProperties = $this->getResource()->getProperties();
+            } else {
+                $sourceProperties = array();
+            }
+
+            $hardLinkProperties = array();
+            $hardLinkSourceProperties = $this->getHardLinkSource()->getProperties();
+            foreach ($hardLinkSourceProperties as $key => $prop) {
+                $prop = clone $prop;
+                $prop->setInherited(true);
+
+                // if the property doesn't exist in the source-properties just add it
+                if(!array_key_exists($key, $sourceProperties)) {
+                    $hardLinkProperties[$key] = $prop;
+                } else {
+                    // if the property does exist in the source properties but it is inherited, then overwrite it with the hardlink property
+                    if($sourceProperties[$key]->isInherited()) {
+                        $hardLinkProperties[$key] = $prop;
+                    }
+                }
+            }
+
+
+            $properties = array_merge($sourceProperties, $hardLinkProperties);
+            $this->setProperties($properties);
+        }
+
+        return $this->properties;
+    }
+
+    public function getChilds() {
+
+        if ($this->childs === null) {
+            $hardLink = $this->getHardLinkSource();
+            $childs = array();
+
+            if($hardLink->getChildsFromSource() && $hardLink->getSourceDocument() && !Pimcore::inAdmin()) {
+                $childs = parent::getChilds();
+                foreach($childs as &$c) {
+                    $c = Document_Hardlink_Service::wrap($c);
+                    $c->setHardLinkSource($hardLink);
+                    $c->setPath(preg_replace("@^" . preg_quote($hardLink->getSourceDocument()->getRealFullpath()) . "@", $hardLink->getRealFullpath(), $c->getRealPath()));
+                }
+            }
+
+            $this->setChilds($childs);
+        }
+
+        return $this->childs;
+    }
+
+    public function hasChilds() {
+        $hardLink = $this->getHardLinkSource();
+
+        if($hardLink->getChildsFromSource() && $hardLink->getSourceDocument() && !Pimcore::inAdmin()) {
+            return parent::hasChilds();
+        }
+
+        return false;
+    }
+
+    /**
+     * @throws Exception
+     * @return void
+     */
+    protected function raiseHardlinkError () {
+        throw new Exception("Method no supported by hardlinked documents");
+    }
+
+    /**
+     * @param \Document_Hardlink $hardLinkSource
+     */
+    public function setHardLinkSource($hardLinkSource)
+    {
+        $this->hardLinkSource = $hardLinkSource;
+        return $this;
+    }
+
+    /**
+     * @return \Document_Hardlink
+     */
+    public function getHardLinkSource()
+    {
+        return $this->hardLinkSource;
+    }
+}
