0ef41834828cb16b724a51d85c1eca14c981b2fa
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-02-06 14:19:07 +0000

    document hardlink wrapper class
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4305 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/models/Document/Hardlink/Wrapper/Email.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Document
+ * @copyright  Copyright (c) 2009-2013 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Document_Hardlink_Wrapper_Email extends Document_Hardlink implements Document_Hardlink_Wrapper_Interface {
+
+    use Document_Hardlink_Wrapper;
+}
