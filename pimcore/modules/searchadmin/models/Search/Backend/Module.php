681e9ccc860e3939dec632130970d2424ab5c3ad
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-03-09 17:43:46 +0000

    changed backend search module to new event based API
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4462 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/searchadmin/models/Search/Backend/Module.php
+++ pimcore/modules/searchadmin/models/Search/Backend/Module.php
@@ -15,22 +15,34 @@
 
 class Search_Backend_Module extends Pimcore_API_Module_Abstract{
 
-    protected function postAddElement(Element_Interface $element){
-        $searchEntry = new Search_Backend_Data($element);
+    public function init() {
+
+        // attach event-listener
+        foreach (["asset","object","document"] as $type) {
+            Pimcore::getEventManager()->attach($type . ".postAdd", array($this, "postAddElement"));
+            Pimcore::getEventManager()->attach($type . ".postUpdate", array($this, "postUpdateElement"));
+            Pimcore::getEventManager()->attach($type . ".preDelete", array($this, "preDeleteElement"));
+        }
+
+    }
+
+    public function postAddElement($e){
+        $searchEntry = new Search_Backend_Data($e->getTarget());
         $searchEntry->save();
 
     }
 
-    protected function preDeleteElement(Element_Interface $element){
+    public function preDeleteElement($e){
         
-        $searchEntry = Search_Backend_Data::getForElement($element);
+        $searchEntry = Search_Backend_Data::getForElement($e->getTarget());
         if($searchEntry instanceof Search_Backend_Data and $searchEntry->getId() instanceof Search_Backend_Data_Id){
             $searchEntry->delete();
         }
 
     }
 
-    protected function postUpdateElement(Element_Interface $element){
+    public function postUpdateElement($e){
+        $element = $e->getTarget();
         $searchEntry = Search_Backend_Data::getForElement($element);
         if($searchEntry instanceof Search_Backend_Data and $searchEntry->getId() instanceof Search_Backend_Data_Id ){
             $searchEntry->setDataFromElement($element);
@@ -39,90 +51,4 @@ class Search_Backend_Module extends Pimcore_API_Module_Abstract{
             $this->postAddElement($element);
         }
     }
-
-    /**
-     *
-     * Hook called after an asset was added
-     *
-     * @param Asset $asset
-     */
-    public function postAddAsset(Asset $asset) {
-        $this->postAddElement($asset);
-    }
-
-
-    /**
-     * Hook called before an asset is deleted
-     *
-     * @param Asset $asset
-     */
-    public function preDeleteAsset(Asset $asset) {
-        $this->preDeleteElement($asset);
-    }
-
-    /**
-     * Hook called after an asset is updated
-     *
-     * @param Asset $asset
-     */
-    public function postUpdateAsset(Asset $asset) {
-        $this->postUpdateElement($asset);
-    }
-
-    /**
-     *
-     * Hook called after a document was added
-     *
-     * @param Document $document
-     */
-    public function postAddDocument(Document $document) {
-        $this->postAddElement($document);
-    }
-
-    /**
-     * Hook called before a document is deleted
-     *
-     * @param Document $document
-     */
-    public function preDeleteDocument(Document $document) {
-        $this->preDeleteElement($document);
-    }
-
-    /**
-     * Hook called after  a document is updated
-     *
-     * @param Document $document
-     */
-    public function postUpdateDocument(Document $document) {
-       $this->postUpdateElement($document);
-    }
-
-    /**
-     * Hook after an object was is added
-     *
-     * @param Object_Abstract $object
-     */
-    public function postAddObject(Object_Abstract $object) {
-        $this->postAddElement($object);
-    }
-
-    /**
-     * Hook called before an object is deleted
-     *
-     * @param Object_Abstract $object
-     */
-    public function preDeleteObject(Object_Abstract $object) {
-        $this->preDeleteElement($object);
-    }
-
-    /**
-     * Hook called after an object was updated
-     *
-     * @param Object_Abstract $object
-     */
-    public function postUpdateObject(Object_Abstract $object) {
-        $this->postUpdateElement($object);
-    }
-
-
 }
