38add1559e6c09cc39384b494077785ffa9200b6
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-03-09 17:49:04 +0000

    changed uuid module to new event based API
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4463 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Tool/UUID/Module.php
+++ pimcore/models/Tool/UUID/Module.php
@@ -15,38 +15,22 @@
 
 class Tool_UUID_Module extends Pimcore_API_Module_Abstract{
 
-    protected function createUuid($item){
-        Tool_UUID::create($item);
-    }
-
-    protected function deleteUuid($item){
-        $uuidObject = Tool_UUID::getByItem($item);
-        if($uuidObject instanceof Tool_UUID){
-            $uuidObject->delete();
+    public function init() {
+        // attach event-listener
+        foreach (["asset","object","document","object.class"] as $type) {
+            Pimcore::getEventManager()->attach($type . ".postAdd", array($this, "createUuid"));
+            Pimcore::getEventManager()->attach($type . ".postDelete", array($this, "deleteUuid"));
         }
     }
 
-    public function postAddObject($object){
-        $this->createUuid($object);
+    public function createUuid($e){
+        Tool_UUID::create($e->getTarget());
     }
 
-    public function postDeleteObject($object){
-        $this->deleteUuid($object);
-    }
-
-    public function postAddAsset($asset) {
-        $this->createUuid($asset);
-    }
-
-    public function postDeleteAsset($asset) {
-        $this->deleteUuid($asset);
-    }
-
-    public function postAddDocument($document) {
-        $this->createUuid($document);
-    }
-
-    public function postDeleteDocument($document){
-        $this->deleteUuid($document);
+    public function deleteUuid($e){
+        $uuidObject = Tool_UUID::getByItem($e->getTarget());
+        if($uuidObject instanceof Tool_UUID){
+            $uuidObject->delete();
+        }
     }
 }
