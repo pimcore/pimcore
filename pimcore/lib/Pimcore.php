456d43a38c8471886001eb4fbd7b9a687ed70bda
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-03-12 12:49:14 +0000

    whoops ajax request handler
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4479 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore.php
+++ pimcore/lib/Pimcore.php
@@ -75,6 +75,9 @@ class Pimcore {
         if(self::inDebugMode() && $frontend) {
             $whoops = new \Whoops\Run;
             $whoops->pushHandler(new \Whoops\Handler\PrettyPageHandler);
+            $jsonErrorHandler = new \Whoops\Handler\JsonResponseHandler;
+            $jsonErrorHandler->onlyForAjaxRequests(true);
+            $whoops->pushHandler($jsonErrorHandler);
             $whoops->register();
 
             // add event handler before Pimcore::shutdown() to ensure fatal errors are handled by Whoops
