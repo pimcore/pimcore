de5d6a2f6f869b450a77915dad95b5df970c01d9
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-03-12 10:13:39 +0000

    Document_Tag_Snippet: fixed webservice import
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4470 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Document/Tag/Snippet.php
+++ pimcore/models/Document/Tag/Snippet.php
@@ -203,14 +203,18 @@ class Document_Tag_Snippet extends Document_Tag {
             $this->id = $data->id;
             if (is_numeric($this->id)) {
                 $this->snippet = Document_Snippet::getById($this->id);
+
                 if (!$this->snippet instanceof Document_Snippet) {
-                    throw new Exception("cannot get values from web service import - referenced snippet with id [ " . $this->id . " ] is unknown");
+                    if ($idMapper && $idMapper->ignoreMappingFailures()) {
+                        $idMapper->recordMappingFailure("document", $this->getDocumentId(), "document", $this->id);
+                    } else {
+                        throw new Exception("cannot get values from web service import - referenced snippet with id [ " . $this->id . " ] is unknown");
+                    }
                 }
+
             } else {
                 throw new Exception("cannot get values from web service import - id is not valid");
             }
-
-
         }
     }
 
