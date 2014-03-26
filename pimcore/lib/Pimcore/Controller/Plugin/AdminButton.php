3aae18ad3c082599fd204cfa8d4d8abb54e3726b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-03-26 11:36:42 +0000

    Pimcore_Controller_Plugin_AdminButton exception handling (wrong session id)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4614 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Plugin/AdminButton.php
+++ pimcore/lib/Pimcore/Controller/Plugin/AdminButton.php
@@ -30,52 +30,55 @@ class Pimcore_Controller_Plugin_AdminButton extends Zend_Controller_Plugin_Abstr
         }
 
         if(isset($_COOKIE["pimcore_admin_sid"])) {
+            try {
+                $user = Pimcore_Tool_Authentication::authenticateSession();
+                if($user instanceof User) {
+                    $body = $this->getResponse()->getBody();
 
-            $user = Pimcore_Tool_Authentication::authenticateSession();
-            if($user instanceof User) {
-                $body = $this->getResponse()->getBody();
+                    $document = $this->getRequest()->getParam("document");
+                    if($document instanceof Document && !Staticroute::getCurrentRoute()) {
+                        $documentId = $document->getId();
+                    }
 
-                $document = $this->getRequest()->getParam("document");
-                if($document instanceof Document && !Staticroute::getCurrentRoute()) {
-                    $documentId = $document->getId();
+                    $config = Pimcore_Config::getSystemConfig();
+                    $contactEnabled = false;
+                    if($config->general->contactemail) {
+                        $contactEnabled = true;
+                    }
+
+                    if(!$documentId) {
+                        $documentId = "null";
+                    }
+
+                    $personas = array();
+                    $list = new Tool_Targeting_Persona_List();
+                    foreach($list->load() as $persona) {
+                        $personas[$persona->getId()] = $persona->getName();
+                    }
+
+                    $code = "\n\n\n<!-- pimcore admin console -->\n";
+                    $code .= '<script type="text/javascript">
+                        try {
+                            var pimcore = pimcore || {};
+                            pimcore["admin"] = {documentId: ' . $documentId . ', contactEnabled: ' . Zend_Json::encode($contactEnabled) .'};
+                            pimcore["personas"] = ' . Zend_Json::encode($personas) .';
+                        } catch (e) {}
+                    </script>';
+
+                    $code .= '<script type="text/javascript" src="/pimcore/static/js/frontend/admin/admin.js"></script>';
+                    $code .= '<link rel="stylesheet" type="text/css" href="/pimcore/static/js/frontend/admin/admin.css" />' . "\n\n\n";
+
+                    // search for the end <head> tag, and insert the google analytics code before
+                    // this method is much faster than using simple_html_dom and uses less memory
+                    $bodyEndPosition = stripos($body, "</body>");
+                    if($bodyEndPosition !== false) {
+                        $body = substr_replace($body, $code . "\n\n</body>\n", $bodyEndPosition, 7);
+                    }
+
+                    $this->getResponse()->setBody($body);
                 }
-
-                $config = Pimcore_Config::getSystemConfig();
-                $contactEnabled = false;
-                if($config->general->contactemail) {
-                    $contactEnabled = true;
-                }
-
-                if(!$documentId) {
-                    $documentId = "null";
-                }
-
-                $personas = array();
-                $list = new Tool_Targeting_Persona_List();
-                foreach($list->load() as $persona) {
-                    $personas[$persona->getId()] = $persona->getName();
-                }
-
-                $code = "\n\n\n<!-- pimcore admin console -->\n";
-                $code .= '<script type="text/javascript">
-                    try {
-                        var pimcore = pimcore || {};
-                        pimcore["admin"] = {documentId: ' . $documentId . ', contactEnabled: ' . Zend_Json::encode($contactEnabled) .'};
-                        pimcore["personas"] = ' . Zend_Json::encode($personas) .';
-                    } catch (e) {}
-                </script>';
-
-                $code .= '<script type="text/javascript" src="/pimcore/static/js/frontend/admin/admin.js"></script>';
-                $code .= '<link rel="stylesheet" type="text/css" href="/pimcore/static/js/frontend/admin/admin.css" />' . "\n\n\n";
-
-                // search for the end <head> tag, and insert the google analytics code before
-                // this method is much faster than using simple_html_dom and uses less memory
-                $bodyEndPosition = stripos($body, "</body>");
-                if($bodyEndPosition !== false) {
-                    $body = substr_replace($body, $code . "\n\n</body>\n", $bodyEndPosition, 7);
-                }
-
-                $this->getResponse()->setBody($body);
+            } catch (\Exception $e) {
+                Logger::error($e);
             }
         }
     }
