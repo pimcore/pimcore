18f354cbfa54ace324be246060e8a25b8986512f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-04-15 04:16:39 +0000

    extension manager, plugin skeleton generator: added event-based examples
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4780 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/extensionmanager/example-plugin/Example/lib/Example/Plugin.php
+++ pimcore/modules/extensionmanager/example-plugin/Example/lib/Example/Plugin.php
@@ -2,7 +2,29 @@
 
 
 class Example_Plugin  extends Pimcore_API_Plugin_Abstract implements Pimcore_API_Plugin_Interface {
-    
+
+    public function init() {
+        // register your events here
+
+        // using anonymous function
+        Pimcore::getEventManager()->attach("document.postAdd", function ($document) {
+            // do something
+        });
+
+        // using methods
+        Pimcore::getEventManager()->attach("document.postUpdate", array($this, "handleDocument"));
+
+        // for more information regarding events, please visit:
+        // http://www.pimcore.org/wiki/display/PIMCORE/Event+API+%28EventManager%29+since+2.1.1
+        // http://framework.zend.com/manual/1.12/de/zend.event-manager.event-manager.html
+        // http://www.pimcore.org/wiki/pages/viewpage.action?pageId=12124202
+
+    }
+
+    public function handleDocument ($document) {
+        // do something
+    }
+
 	public static function install (){
         // implement your own logic here
         return true;
