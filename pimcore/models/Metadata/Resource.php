fb96b7329f32e87163015a41975c0d56da7419fc
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-06-13 09:00:23 +0000

    PIMCORE-2320 - Predefined Asset Metadata Sets
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@4945 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

---
+++ pimcore/models/Metadata/Resource.php
@@ -0,0 +1,69 @@
+<?php
+/**
+ * Pimcore
+ *
+ * LICENSE
+ *
+ * This source file is subject to the new BSD license that is bundled
+ * with this package in the file LICENSE.txt.
+ * It is also available through the world-wide-web at this URL:
+ * http://www.pimcore.org/license
+ *
+ * @category   Pimcore
+ * @package    Metadata
+ * @copyright  Copyright (c) 2009-2014 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     New BSD License
+ */
+
+class Metadata_Resource extends Pimcore_Model_Resource_Abstract {
+
+
+    public function getRawData(){
+        $cid = $this->model->getCid();
+        $type = $this->model->getType();
+        $name = $this->model->getName();
+        $raw = null;
+        if($cid){
+            $data = $this->db->fetchRow("SELECT * FROM assets_metadata_predefined WHERE type=? AND cid = ? AND name=?",array($type,$cid,$name) );
+            $raw = $data['data'];
+        }
+        return $raw;
+    }
+
+    /**
+     * Save object to database
+     *
+     * @return void
+     */
+    public function save() {
+
+        $data = $this->model->getData();
+
+        if ($this->model->getType() == "object" || $this->model->getType() == "asset" || $this->model->getType() == "document") {
+
+            if ($data instanceof Element_Interface) {
+                $data = $data->getId();
+            }
+            else {
+                $data = null;
+            }
+        }
+
+
+        if (is_array($data) || is_object($data)) {
+            $data = Pimcore_Tool_Serialize::serialize($data);
+        }
+
+        $saveData = array(
+            "cid" => $this->model->getCid(),
+            "ctype" => $this->model->getCtype(),
+            "cpath" => $this->model->getCpath(),
+            "name" => $this->model->getName(),
+            "type" => $this->model->getType(),
+            "inheritable" => (int)$this->model->getInheritable(),
+            "data" => $data
+        );
+
+        $this->db->insertOrUpdate("properties", $saveData);
+    }
+}
