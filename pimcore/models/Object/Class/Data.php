a6839b428e181748ace00679878e65ae86f4a8de
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-07-18 08:26:06 +0000

    format generated code
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5089 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Object/Class/Data.php
+++ pimcore/models/Object/Class/Data.php
@@ -613,7 +613,7 @@ abstract class Object_Class_Data
         $code = "";
 
         $code .= '/**' . "\n";
-        $code .= '* ' . str_replace(array("/**", "*/", "//"), "", $this->getName()) . " - " . str_replace(array("/**", "*/", "//"), "", $this->getTitle()) . "\n";
+        $code .= '* Get ' . str_replace(array("/**", "*/", "//"), "", $this->getName()) . " - " . str_replace(array("/**", "*/", "//"), "", $this->getTitle()) . "\n";
         $code .= '* @return ' . $this->getPhpdocType() . "\n";
         $code .= '*/' . "\n";
         $code .= "public function get" . ucfirst($key) . " () {\n";
@@ -654,7 +654,7 @@ abstract class Object_Class_Data
         $code = "";
 
         $code .= '/**' . "\n";
-        $code .= '* ' . str_replace(array("/**", "*/", "//"), "", $this->getName()) . " - " . str_replace(array("/**", "*/", "//"), "", $this->getTitle()) . "\n";
+        $code .= '* Set ' . str_replace(array("/**", "*/", "//"), "", $this->getName()) . " - " . str_replace(array("/**", "*/", "//"), "", $this->getTitle()) . "\n";
         $code .= '* @param ' . $this->getPhpdocType() . ' $' . $key . "\n";
         $code .= "* @return Object_" . ucfirst($class->getName()) . "\n";
         $code .= '*/' . "\n";
