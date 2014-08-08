5e2abeabde9e8b936fcb13500ccb1501f0ccd843
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-08-08 13:35:55 +0000

    PIMCORE-2407 - Issue with too many assets in folders
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5176 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/controllers/AssetController.php
+++ pimcore/modules/admin/controllers/AssetController.php
@@ -524,16 +524,17 @@ class Admin_AssetController extends Pimcore_Controller_Action_Admin
             $tmpAsset["permissions"]["create"] = $asset->isAllowed("create");
 
             $folderThumbs = array();
-            foreach ($asset->getChilds() as $child) {
+            $children = new Asset_List();
+            $children->setCondition("path LIKE ?", [$asset->getFullPath() . "/%"]);
+            $children->setLimit(35);
+
+            foreach ($children as $child) {
                 if ($thumbnailUrl = $this->getThumbnailUrl($child)) {
                     $folderThumbs[] = $thumbnailUrl;
                 }
             }
 
             if (!empty($folderThumbs)) {
-                if (count($folderThumbs) > 35) {
-                    $folderThumbs = array_splice($folderThumbs, 0, 35);
-                }
                 $tmpAsset["thumbnails"] = $folderThumbs;
             }
         } else {
