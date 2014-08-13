8a88dd4f1bdf6dec51311d441680071809dff2cb
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-08-13 04:11:49 +0000

    output-cache fixed warning
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5182 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Controller/Plugin/Cache.php
+++ pimcore/lib/Pimcore/Controller/Plugin/Cache.php
@@ -170,7 +170,8 @@ class Pimcore_Controller_Plugin_Cache extends Zend_Controller_Plugin_Abstract {
 
         $this->cacheKey = "output_" . md5(Pimcore_Tool::getHostname() . $requestUri) . $appendKey;
 
-        if ($cacheItem = Pimcore_Model_Cache::load($this->cacheKey, true)) {
+        $cacheItem = Pimcore_Model_Cache::load($this->cacheKey, true);
+        if (is_array($cacheItem) && !empty($cacheItem)) {
             header("X-Pimcore-Output-Cache-Tag: " . $this->cacheKey, true, 200);
             header("X-Pimcore-Output-Cache-Date: " . $cacheItem["date"]);
             
