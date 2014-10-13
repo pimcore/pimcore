ab406707207797848ed2e9224dd4ea79167cc756
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-10-13 06:59:48 +0000

    asset thumbnail improvement: use filemtime instead of Asset::getModificationDate()
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5407 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Asset/Image/Thumbnail/Processor.php
+++ pimcore/models/Asset/Image/Thumbnail/Processor.php
@@ -84,7 +84,9 @@ class Asset_Image_Thumbnail_Processor {
 
         if($asset instanceof Asset) {
             $id = $asset->getId();
-            $modificationDate = $asset->getModificationDate();
+            // do not use the asset modification date because not every modification of an asset has an impact on the
+            // binary data on the hdd (e.g. meta-data, properties, ...), so it's better to use the filemtime instead
+            $modificationDate = filemtime($asset->getFileSystemPath());
         } else {
             $id = "dyn~" . crc32($fileSystemPath);
             if(file_exists($fileSystemPath)) {
