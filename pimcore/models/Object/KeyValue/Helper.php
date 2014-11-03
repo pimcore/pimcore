689b9740833e32831982ea63201ac0ea98f4636a
pimcore-team <pimcore-team@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-11-03 05:23:20 +0000

    Merge pull request #118 from WebForPeople/fix-key-value-config-import
    Added missing Key Value Config import
    
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5471 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Object/KeyValue/Helper.php
+++ pimcore/models/Object/KeyValue/Helper.php
@@ -1,4 +1,5 @@
 <?php
+
 /**
  * Pimcore
  *
@@ -25,30 +26,30 @@ class Helper {
     /** Returns the group/key config as XML.
      * @return mixed
      */
-    public function export() {
+    public static function export() {
         $xml = new \SimpleXMLElement('<xml/>');
 
-        $list = new Object\KeyValue\GroupConfig\Listing();
-        $list->load();
-        $items = $list->getList();
+        $groupConfigList = new Object\KeyValue\GroupConfig\Listing();
+        $groupConfigList->load();
+        $groupConfigItems = $groupConfigList->getList();
 
         $groups = $xml->addChild('groups');
 
-        foreach ($items as $item) {
+        foreach ($groupConfigItems as $item) {
             $group = $groups->addChild('group');
             $group->addChild("id", $item->getId());
             $group->addChild("name", $item->getName());
             $group->addChild("description", $item->getDescription());
         }
 
-        $list = new Object\KeyValue\KeyConfig\Listing();
-        $list->load();
-        $items = $list->getList();
+        $keyConfigList = new Object\KeyValue\KeyConfig\Listing();
+        $keyConfigList->load();
+        $keyConfigItems = $keyConfigList->getList();
 
         $keys = $xml->addChild('keys');
 
-        foreach ($items as $item) {
-            $key= $keys->addChild('key');
+        foreach ($keyConfigItems as $item) {
+            $key = $keys->addChild('key');
             $id = $key->addChild('id', $item->getId());
             $name = $key->addChild('name', $item->getName());
             $description = $key->addChild('description', $item->getDescription());
@@ -64,47 +65,58 @@ class Helper {
     /** Imports the group/key config from XML.
      * @param $config
      */
-    public function import($config) {
-        $groups = $config["groups"]["group"];
-        $groupIdMapping = array();
-
-        foreach ($groups as $groupConfig) {
-            $name = $groupConfig["name"];
-            $group = Object\KeyValue\GroupConfig::getByName($name);
-            if (!$group) {
-                $group = new Object\KeyValue\GroupConfig();
-                $group->setName($name);
+    public static function import($config) {
+        if (is_array($config["groups"])) {
+            $groups = $config["groups"]["group"];
+            if (!isset($groups[0])) {
+                $groups = array($groups);
             }
-            $group->setDescription($groupConfig["description"]);
-            $group->save();
-            // mapping of remote id to local id
-            $groupIdMapping[$groupConfig["id"]] = $group->getId();
+            $groupIdMapping = array();
+
+            foreach ($groups as $groupConfig) {
+                $name = $groupConfig["name"];
+                $group = Object\KeyValue\GroupConfig::getByName($name);
+                if (!$group) {
+                    $group = new Object\KeyValue\GroupConfig();
+                    $group->setName($name);
+                }
+                $group->setDescription($groupConfig["description"]);
+                $group->save();
+                // mapping of remote id to local id
+                $groupIdMapping[$groupConfig["id"]] = $group->getId();
+            }
         }
 
-        $keys = $config["keys"]["key"];
-        foreach ($keys as $keyConfig) {
-            $name = $keyConfig["name"];
-            $key = Object\KeyValue\KeyConfig::getByName($name);
-            if (!$key) {
-                $key = new Object\KeyValue\KeyConfig();
-                $key->setName($name);
+        if (is_array($config["keys"])) {
+            $keys = $config["keys"]["key"];
+            if (!isset($keys[0])) {
+                $keys = array($keys);
             }
+            foreach ($keys as $keyConfig) {
+                $name = $keyConfig["name"];
+                $key = Object\KeyValue\KeyConfig::getByName($name);
+                if (!$key) {
+                    $key = new Object\KeyValue\KeyConfig();
+                    $key->setName($name);
+                }
 
-            $key->setDescription($keyConfig["description"]);
-            $key->setType($keyConfig["type"]);
-            if (!empty($keyConfig["unit"])) {
-                $key->setUnit($keyConfig["unit"]);
-            }
-            if (!empty($keyConfig["possiblevalues"])) {
-                $key->setPossibleValues($keyConfig["possiblevalues"]);
-            }
+                $key->setDescription($keyConfig["description"]);
+                $key->setType($keyConfig["type"]);
+                if (!empty($keyConfig["unit"])) {
+                    $key->setUnit($keyConfig["unit"]);
+                }
+                if (!empty($keyConfig["possiblevalues"])) {
+                    $key->setPossibleValues($keyConfig["possiblevalues"]);
+                }
 
-            $originalGroupId = $keyConfig["group"];
-            if (!empty($originalGroupId)) {
-                $mappedGroupId = $groupIdMapping[$originalGroupId];
-                $key->setGroup($mappedGroupId);
+                $originalGroupId = $keyConfig["group"];
+                if (!empty($originalGroupId)) {
+                    $mappedGroupId = $groupIdMapping[$originalGroupId];
+                    $key->setGroup($mappedGroupId);
+                }
+                $key->save();
             }
-            $key->save();
         }
     }
+
 }
