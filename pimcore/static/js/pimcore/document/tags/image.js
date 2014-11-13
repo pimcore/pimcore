4de029baafa96e2cc57df02f8a8b75b7282f526f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-11-13 11:52:39 +0000

    document tag image: problem with auto image height
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5496 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static/js/pimcore/document/tags/image.js
+++ pimcore/static/js/pimcore/document/tags/image.js
@@ -22,12 +22,13 @@ pimcore.document.tags.image = Class.create(pimcore.document.tag, {
         this.options = this.parseOptions(options);
 
         this.options = options;
+        this.options.style = '';
 
         if (!this.options["height"]) {
             if (this.options["defautHeight"]){
-                this.options.height = this.options["defautHeight"];
+                this.options.style += (" min-height:" + this.options["defautHeight"] + "px");
             }else{
-                this.options.height = 100;
+                this.options.style += (" min-height:100px");
             }
         }
 
@@ -297,15 +298,17 @@ pimcore.document.tags.image = Class.create(pimcore.document.tag, {
         }
 
 
-        if (!this.options["thumbnail"] && !this.originalDimensions["width"] && !this.originalDimensions["height"]) {
-            path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/width/" + this.element.getEl().getWidth()
-                + "/aspectratio/true?" + Ext.urlEncode(this.datax);
-        } else if (this.originalDimensions["width"]) {
-            path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/width/" + this.originalDimensions["width"]
-                + "/aspectratio/true?" + Ext.urlEncode(this.datax);
-        } else if (this.originalDimensions["height"]) {
-            path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/height/"
+        if (!this.options["thumbnail"]) {
+            if(!this.originalDimensions["width"] && !this.originalDimensions["height"]) {
+                path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/width/" + this.element.getEl().getWidth()
+                    + "/aspectratio/true?" + Ext.urlEncode(this.datax);
+            } else if (this.originalDimensions["width"]) {
+                path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/width/" + this.originalDimensions["width"]
+                    + "/aspectratio/true?" + Ext.urlEncode(this.datax);
+            } else if (this.originalDimensions["height"]) {
+                path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/height/"
                 + this.originalDimensions["height"] + "/aspectratio/true?" + Ext.urlEncode(this.datax);
+            }
         } else {
             if (typeof this.options.thumbnail == "string") {
                 path = "/admin/asset/get-image-thumbnail/id/" + this.datax.id + "/thumbnail/" + this.options.thumbnail
