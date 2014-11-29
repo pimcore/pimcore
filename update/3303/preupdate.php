fc9026ba3d880ac69490d55a8b2310346f35d4ec
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-11-29 23:05:41 +0000

    improved update script for build 3303 - flush cache
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5564 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/3303/preupdate.php
+++ update/3303/preupdate.php
@@ -1,5 +1,8 @@
 <?php
 
+// flush cache
+\Pimcore\Model\Cache::clearAll();
+
 // delete the autoload class map
 $file = PIMCORE_CONFIGURATION_DIRECTORY . "/autoload-classmap.php";
 if(file_exists($file)) {
