49224d6f4da8f68facbe2dda228b7c013dfa4788
fash <fash@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-12-01 12:56:32 +0000

    bugfixing in classdefinition\data\time
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5569 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Object/ClassDefinition/Data/Time.php
+++ pimcore/models/Object/ClassDefinition/Data/Time.php
@@ -46,7 +46,7 @@ class Time extends Model\Object\ClassDefinition\Data\Input {
 
         parent::checkValidity($data, $omitMandatoryCheck);
 
-        if((is_string($data) && strlen($data) != 5 && !empty($data)) || (!empty($data) && !is_string(§data))) {
+        if((is_string($data) && strlen($data) != 5 && !empty($data)) || (!empty($data) && !is_string($data))) {
             throw new \Exception("Wrong time format given must be a 5 digit string (eg: 06:49) [ ".$this->getName()." ]");
         }
     }
