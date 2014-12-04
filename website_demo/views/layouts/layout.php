17ff72e49b5f3336d5af77a1b1113d152a4fd099
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-12-04 10:59:18 +0000

    enable google analtics integration in demo
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5587 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- website_demo/views/layouts/layout.php
+++ website_demo/views/layouts/layout.php
@@ -55,15 +55,6 @@
     <script src="/website/static/js/respond.min.js"></script>
     <![endif]-->
 
-    <script>
-        (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
-            (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
-            m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
-        })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
-
-        ga('create', 'UA-12436865-5', 'pimcore.org');
-        ga('send', 'pageview');
-    </script>
 </head>
 
 <body class="<?= $isPortal ? "portal-page" : "" ?>">
