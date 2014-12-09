28e01338ce9dd19fea0621f1e3fcc9df0ecd97cc
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2014-12-09 07:35:37 +0000

    live update: take care of zend optimizer also in update-script 3303
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5624 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- update/3303/preupdate.php
+++ update/3303/preupdate.php
@@ -8,3 +8,20 @@ $file = PIMCORE_CONFIGURATION_DIRECTORY . "/autoload-classmap.php";
 if(file_exists($file)) {
     unlink($file);
 }
+
+
+// clear the opcache (as of PHP 5.5)
+if(function_exists("opcache_reset")) {
+    opcache_reset();
+}
+
+// clear the APC opcode cache (<= PHP 5.4)
+if(function_exists("apc_clear_cache")) {
+    apc_clear_cache();
+}
+
+// clear the Zend Optimizer cache (Zend Server <= PHP 5.4)
+if (function_exists('accelerator_reset')) {
+    return accelerator_reset();
+}
+
