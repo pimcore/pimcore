901f2f6dde7451ebec4082c321dc68a3ac94a00f
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-01-27 13:38:34 +0000

    travis-ci: added hhvm (fix)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5817 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- build/travis/setup-hhvm.sh
+++ build/travis/setup-hhvm.sh
@@ -2,4 +2,4 @@
 
 hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
 
-sudo cp -f ~/build/travis/apache-hhvm.conf /etc/apache2/sites-available/default
+sudo cp -f build/travis/apache-hhvm.conf /etc/apache2/sites-available/default
