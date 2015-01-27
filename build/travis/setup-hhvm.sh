1fd70c8561c3b8c8470b95e046b4213bd80b79b7
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-01-27 16:11:10 +0000

    travis-ci / hhvm
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5820 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- build/travis/setup-hhvm.sh
+++ build/travis/setup-hhvm.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
-hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
+hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true -vLog.File=/tmp/hhvm.log
 
 sudo cp -f build/travis/apache-hhvm.conf /etc/apache2/sites-available/default
