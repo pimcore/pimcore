a4c35dc53707f30c1a506de343d1c3a2a69399c5
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-01-27 13:32:47 +0000

    travis-ci: added hhvm (fix)
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5816 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- build/travis/setup-fpm.sh
+++ build/travis/setup-fpm.sh
@@ -5,3 +5,5 @@ phpenv config-add ~/build/travis/php.ini
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+sudo cp -f ~/build/travis/apache-fpm.conf /etc/apache2/sites-available/default
--- build/travis/setup-hhvm.sh
+++ build/travis/setup-hhvm.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 
 hhvm -m daemon -vServer.Type=fastcgi -vServer.Port=9000 -vServer.FixPathInfo=true
+
+sudo cp -f ~/build/travis/apache-hhvm.conf /etc/apache2/sites-available/default
