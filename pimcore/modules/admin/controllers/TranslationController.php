1fc828e56c2f1e81dfb4c0182adf58793d5003e7
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-02-04 11:29:26 +0000

    better error message in translation/xliff-import
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5872 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/modules/admin/controllers/TranslationController.php
+++ pimcore/modules/admin/controllers/TranslationController.php
@@ -690,7 +690,11 @@ class Admin_TranslationController extends \Pimcore\Controller\Action\Admin {
                 }
             }
 
-            $element->save();
+            try {
+                $element->save();
+            } catch (\Exception $e) {
+                throw new \Exception("Unable to save " . Element\Service::getElementType($element) . " with id " . $element->getId() . " because of the following reason: " . $e->getMessage());
+            }
         }
 
         $this->_helper->json(array(
