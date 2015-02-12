6a2a6e3f8a282a9565027f6ddfea6378c184a71f
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-02-12 14:44:31 +0000

    fixed dependencies layout
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@5934 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static5/js/pimcore/element/dependencies.js
+++ pimcore/static5/js/pimcore/element/dependencies.js
@@ -171,7 +171,7 @@ pimcore.element.dependencies = Class.create({
             store: this.requiredByStore,
             columns: [
                 {header: "ID", sortable: true, dataIndex: 'id'},
-                {header: t("path"), sortable: true, dataIndex: 'path', },
+                {header: t("path"), sortable: true, dataIndex: 'path', flex: 1},
                 {header: t("type"), sortable: true, dataIndex: 'type'},
                 {header: t("subtype"), sortable: true, dataIndex: 'subtype'}
             ],
