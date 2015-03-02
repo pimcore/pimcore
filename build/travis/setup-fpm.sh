fe124ddae0dabfa03d8d1f44f2973be5303def1b
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-02 07:53:06 +0000

    travis nightly php
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6060 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- build/travis/setup-fpm.sh
+++ build/travis/setup-fpm.sh
@@ -2,11 +2,10 @@
 
 phpenv config-add build/travis/php.ini
 
-if [[ "$TRAVIS_PHP_VERSION" == "7" ]]; then sudo ln -s /home/travis/.phpenv/versions/7 /home/travis/.phpenv/versions/nightly; fi
-
-cat /home/travis/.phpenv/versions/nightly/etc/php-fpm.d/*.conf
-
 sudo cp ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf.default ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+
+cat ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+
 echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
 ~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
 
