9dad11d167f8ca70eb5675e54a828100e288d692
ckogler <ckogler@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-04 15:50:39 +0000

    Updated XMLWriter to automatically add CDATA Tag if necessary
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6094 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Tool/XmlWriter.php
+++ pimcore/lib/Pimcore/Tool/XmlWriter.php
@@ -14,7 +14,15 @@
  */
 
 namespace Pimcore\Tool;
+use Symfony\Component\Config\Definition\Exception\Exception;
 
+class SimpleXMLExtended extends \SimpleXMLElement {
+  public function addCData($cdata_text) {
+    $node = dom_import_simplexml($this);
+    $no   = $node->ownerDocument;
+    $node->appendChild($no->createCDATASection($cdata_text));
+  }
+}
 class XmlWriter extends \Zend_Config_Writer_Xml {
 
     /**
@@ -142,7 +150,15 @@ class XmlWriter extends \Zend_Config_Writer_Xml {
         return $this;
     }
 
+    protected function addChildConsiderCdata($xml,$key,$data){
+        $sData = (string) $data;
+        $child = @$xml->addChild($key, $sData);
 
+        if((string)$child != $sData){
+            $child = $xml->$key->addCData((string) $data);
+        }
+        return $child;
+    }
 
     /**
      * returns the XML string
@@ -153,7 +169,7 @@ class XmlWriter extends \Zend_Config_Writer_Xml {
     public function render()
     {
 
-        $xml         = new \SimpleXMLElement('<'.$this->getRootElementName().' />');
+        $xml         = new SimpleXMLExtended('<'.$this->getRootElementName().' />');
         if($this->_config){
             $extends     = $this->_config->getExtends();
             $sectionName = $this->_config->getSectionName();
@@ -164,14 +180,12 @@ class XmlWriter extends \Zend_Config_Writer_Xml {
             } else {
                 foreach ($this->_config as $sectionName => $data) {
                     if (!($data instanceof \Zend_Config)) {
-                        $xml->addChild($sectionName, (string) $data);
+                        $this->addChildConsiderCdata($xml,$sectionName,$data);
                     } else {
                         $child = $xml->addChild($sectionName);
-
                         if (isset($extends[$sectionName])) {
                             $child->addAttribute('zf:extends', $extends[$sectionName], \Zend_Config_Xml::XML_NAMESPACE);
                         }
-
                         $this->_addBranch($data, $child, $xml);
                     }
                 }
@@ -182,7 +196,7 @@ class XmlWriter extends \Zend_Config_Writer_Xml {
         if($encoding = $this->getEncoding()){
             $dom->encoding = $encoding;
         }
-
+        
         $dom->formatOutput = $this->getFormatOutput();
 
         $xmlString = $dom->saveXML();
@@ -191,10 +205,60 @@ class XmlWriter extends \Zend_Config_Writer_Xml {
     }
 
     /**
+     * Add a branch to an XML object recursively
+     *
+     * @param  \Zend_Config      $config
+     * @param  \SimpleXMLElement $xml
+     * @param  \SimpleXMLElement $parent
+     * @return void
+     */
+    protected function _addBranch(\Zend_Config $config, \SimpleXMLElement $xml, \SimpleXMLElement $parent)
+    {
+        $branchType = null;
+
+        foreach ($config as $key => $value) {
+            if ($branchType === null) {
+                if (is_numeric($key)) {
+                    $branchType = 'numeric';
+                    $branchName = $xml->getName();
+                    $xml        = $parent;
+
+                    unset($parent->{$branchName});
+                } else {
+                    $branchType = 'string';
+                }
+            } else if ($branchType !== (is_numeric($key) ? 'numeric' : 'string')) {
+                // require_once 'Zend/Config/Exception.php';
+                throw new \Zend_Config_Exception('Mixing of string and numeric keys is not allowed');
+            }
+
+            if ($branchType === 'numeric') {
+                if ($value instanceof \Zend_Config) {
+                    $child = $parent->addChild($branchName);
+
+                    $this->_addBranch($value, $child, $parent);
+                } else {
+                    $parent->addChild($branchName, (string) $value);
+                }
+            } else {
+                if ($value instanceof \Zend_Config) {
+                    $child = $xml->addChild($key);
+
+                    $this->_addBranch($value, $child, $xml);
+                } else {
+                    $this->addChildConsiderCdata($xml,$key,$value);
+                }
+            }
+        }
+    }
+
+
+
+    /**
      *  displays the rendered XML
      */
     public function displayXML(){
-        header("Content-Type: application/xml");
+       # header("Content-Type: application/xml");
         die($this->render());
     }
 }
