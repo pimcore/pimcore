97bbf97bf35cabebf8d5a6af02e6fcc81943b83c
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-06 11:04:18 +0000

    dnd: migrated deprecated stuff
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6118 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static5/js/pimcore/document/edit/dnd.js
+++ pimcore/static5/js/pimcore/document/edit/dnd.js
@@ -25,9 +25,11 @@ pimcore.document.edit.dnd = Class.create({
         this.dndManager = parentExt.dd.DragDropMgr;
         var iFrameElement = parent.Ext.get('document_iframe_' + window.editWindow.document.id);
 
-        parentExt.EventManager.on(body, 'mousemove', this.ddMouseMove.bind(this));
-        parentExt.EventManager.on(body, 'mouseup', this.ddMouseUp.bind(this));
+        //parentExt.EventManager.on(body, 'mousemove', this.ddMouseMove.bind(this));
+        //parentExt.EventManager.on(body, 'mouseup', this.ddMouseUp.bind(this));
 
+        body.addListener('mousemove', this.ddMouseMove.bind(this));
+        body.addListener('mouseup', this.ddMouseUp.bind(this));
 
         this.globalDropZone = new parent.Ext.dd.DropZone(iframeElement, {
             ddGroup: "element",
