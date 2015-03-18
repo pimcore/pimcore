4c472192d05372e51929a0db5ec2a08d929f3e52
Gabriel Somoza <work@gabrielsomoza.com>
2015-03-18 14:23:41 +0100

    Added compatibility with Windows

--- pimcore/cli/reset-password.php
+++ pimcore/cli/reset-password.php
@@ -22,17 +22,29 @@ use Pimcore\Model\User;
 echo "\n";
 
 function prompt_silent($prompt = "Enter new password:") {
+  if (preg_match('/^win/i', PHP_OS)) {
+    $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
+    file_put_contents(
+      $vbscript, 'wscript.echo(InputBox("'
+      . addslashes($prompt)
+      . '", "", "password here"))');
+    $command = "cscript //nologo " . escapeshellarg($vbscript);
+    $password = rtrim(shell_exec($command));
+    unlink($vbscript);
+    return $password;
+  } else {
     $command = "/usr/bin/env bash -c 'echo OK'";
     if (rtrim(shell_exec($command)) !== 'OK') {
-        trigger_error("Can't invoke bash");
-        return false;
+      trigger_error("Can't invoke bash");
+      return;
     }
     $command = "/usr/bin/env bash -c 'read -s -p \""
-        . addslashes($prompt)
-        . "\" mypassword && echo \$mypassword'";
+      . addslashes($prompt)
+      . "\" mypassword && echo \$mypassword'";
     $password = rtrim(shell_exec($command));
     echo "\n";
     return $password;
+  }
 }
 
 if ($argc <= 1) { // if no arguments
