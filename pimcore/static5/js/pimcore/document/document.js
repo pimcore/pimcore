2be67d5d9e03a55e323e734367dcd3c10773c9f6
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-20 07:03:07 +0000

    document editor: fixed tree node  style update upon publish/unpublish
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6235 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static5/js/pimcore/document/document.js
+++ pimcore/static5/js/pimcore/document/document.js
@@ -173,9 +173,16 @@ pimcore.document.document = Class.create(pimcore.element.abstract, {
 
         // remove class in tree panel
         try {
-            pimcore.globalmanager.get("layout_document_tree").tree.getNodeById(this.data.id).getUI()
-                                                                                    .removeClass("pimcore_unpublished");
+            var tree = pimcore.globalmanager.get("layout_document_tree").tree;
+            var store = tree.getStore();
+            var record = store.getById(this.data.id);
+            var view = tree.getView();
+            var nodeEl = Ext.fly(view.getNodeByRecord(record));
+            if (nodeEl) {
+                nodeEl.removeCls("pimcore_unpublished");
+            }
         } catch (e) {
+            console.log(e);
         }
 
 
@@ -194,9 +201,17 @@ pimcore.document.document = Class.create(pimcore.element.abstract, {
 
         // set class in tree panel
         try {
-            pimcore.globalmanager.get("layout_document_tree").tree.getNodeById(this.data.id).getUI()
-                                                                                    .addCls("pimcore_unpublished");
+            var tree = pimcore.globalmanager.get("layout_document_tree").tree;
+            var store = tree.getStore();
+            var record = store.getById(this.data.id);
+            var view = tree.getView();
+            var nodeEl = Ext.fly(view.getNodeByRecord(record));
+            if (nodeEl) {
+                nodeEl.addCls("pimcore_unpublished");
+            }
+
         } catch (e) {
+            console.log(e);
         }
 
         this.save("unpublish");
