7f1cb528dc59f379f1d9cd5e9c40a505abcc7d9d
pimcore-team <pimcore-team@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-27 10:36:46 +0000

    build auto version (3461) ...
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6326 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -25,7 +25,7 @@ class Version {
     /**
      * @var int
      */
-    public static $revision = 3460;
+    public static $revision = 3461;
 
     /**
      * @var array
@@ -35,11 +35,11 @@ class Version {
   'URL' => 'http',
   'Repository Root' => 'http',
   'Repository UUID' => '1f8fe7d8-47f0-464c-8d0a-336f4953ab05',
-  'Revision' => '6248',
+  'Revision' => '6325',
   'Node Kind' => 'directory',
   'Last Changed Author' => 'brusch',
-  'Last Changed Rev' => '6248',
-  'Last Changed Date' => '2015-03-20 17',
+  'Last Changed Rev' => '6325',
+  'Last Changed Date' => '2015-03-27 11',
 );
 
     /**
