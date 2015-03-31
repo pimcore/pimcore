9d4510e2a02f1db49d98ebf0faa3f5ae63a5bde2
memleak <memleak@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-03-31 07:23:19 +0000

    redirects: fixed date renderer, dnd and td css
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6347 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/static5/js/pimcore/settings/redirects.js
+++ pimcore/static5/js/pimcore/settings/redirects.js
@@ -201,7 +201,8 @@ pimcore.settings.redirects = Class.create({
             passThroughParametersCheck,
             {header: t("target"), flex: 200, sortable: false, dataIndex: 'target',
                 editor: new Ext.form.TextField({}),
-                css: "background: url(/pimcore/static/img/icon/drop-16.png) right 2px no-repeat;"},
+                tdCls: "input_drop_target"
+            },
             {header: t("target_site"), flex: 200, sortable:true, dataIndex: "targetSite",
                 editor: new Ext.form.ComboBox({
                 store: pimcore.globalmanager.get("sites"),
@@ -249,18 +250,19 @@ pimcore.settings.redirects = Class.create({
                 forceSelection: true,
                 triggerAction: "all"
             })},
-            {header: t("expiry"), width: 60, sortable:true, dataIndex: "expiry", editor: new Ext.form.DateField(),
+            {header: t("expiry"), width: 80, sortable:true, dataIndex: "expiry", editor: new Ext.form.DateField(),
                                                                             renderer: function(d) {
                 if(d instanceof Date) {
-                    return d.format("Y-m-d");
+                    return Ext.Date.format(d, "Y-m-d");
                 }
             }},
             {header: t("creationDate"), sortable: true, dataIndex: 'creationDate', editable: false,
                 hidden: true,
+                width: 150,
                 renderer: function(d) {
                     if (d !== undefined) {
                         var date = new Date(d * 1000);
-                        return date.format("Y-m-d H:i:s");
+                        return Ext.Date.format(date, "Y-m-d H:i:s");
                     } else {
                         return "";
                     }
@@ -268,10 +270,11 @@ pimcore.settings.redirects = Class.create({
             },
             {header: t("modificationDate"), sortable: true, dataIndex: 'modificationDate', editable: false,
                 hidden: true,
+                width: 150,
                 renderer: function(d) {
                     if (d !== undefined) {
                         var date = new Date(d * 1000);
-                        return date.format("Y-m-d H:i:s");
+                        return Ext.Date.format(date, "Y-m-d H:i:s");
                     } else {
                         return "";
                     }
@@ -378,9 +381,9 @@ pimcore.settings.redirects = Class.create({
                     try {
                         var record = data.records[0];
                         var data = record.data;
-                        if (in_array(data.node.attributes.type, ["page", "link", "hardlink"])) {
+                        if (in_array(data.type, ["page", "link", "hardlink"])) {
                             var rec = this.grid.getStore().getAt(myRowIndex);
-                            rec.set("target", data.node.attributes.path);
+                            rec.set("target", data.path);
                             this.updateRows();
                             return true;
                         }
