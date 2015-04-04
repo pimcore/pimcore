294a5a8236d69af3ba4d6e1a6c95a2a0c8166f0c
cfasching <cfasching@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2015-04-04 09:41:45 +0000

    changed crc columns to bigint
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@416 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- models/OnlineShop/Framework/ProductList/DefaultMysql.php
+++ models/OnlineShop/Framework/ProductList/DefaultMysql.php
@@ -85,7 +85,7 @@ class OnlineShop_Framework_ProductList_DefaultMysql implements OnlineShop_Framew
     protected $relationConditions = array();
 
     /**
-     * @var string[]
+     * @var string[][]
      */
     protected $queryConditions = array();
 
@@ -449,32 +449,6 @@ class OnlineShop_Framework_ProductList_DefaultMysql implements OnlineShop_Framew
         // not supported with mysql tables
     }
 
-    /**
-     * prepares all group by values for given field names and cache them in local variable
-     * considers both - normal values and relation values
-     *
-     * @param string $fieldname
-     *
-     * @return void
-     */
-    public function prepareGroupByRelationValues($fieldname, $countValues = false, $fieldnameShouldBeExcluded = true)
-    {
-        // TODO: Implement prepareGroupByRelationValues() method.
-    }
-
-    /**
-     * prepares all group by values for given field names and cache them in local variable
-     * considers both - normal values and relation values
-     *
-     * @param string $fieldname
-     *
-     * @return void
-     */
-    public function prepareGroupBySystemValues($fieldname, $countValues = false, $fieldnameShouldBeExcluded = true)
-    {
-        // TODO: Implement prepareGroupBySystemValues() method.
-    }
-
 
     /**
      * @param $fieldname
@@ -515,22 +489,6 @@ class OnlineShop_Framework_ProductList_DefaultMysql implements OnlineShop_Framew
         } else {
             throw new Exception("Not supported yet");
         }
-    }
-
-    /**
-     * loads group by values based on relation fieldname either from local variable if prepared or directly from product index
-     *
-     * @param      $fieldname
-     * @param bool $countValues
-     * @param bool $fieldnameShouldBeExcluded => set to false for and-conditions
-     *
-     * @return array
-     * @throws Exception
-     */
-    public function getGroupBySystemValues($fieldname, $countValues = false, $fieldnameShouldBeExcluded = true)
-    {
-        // TODO: Implement getGroupBySystemValues() method.
-        return [];
     }
 
 
