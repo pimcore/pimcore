29d4b8c3badf0535d93ee8517aff65d54f92d8ed
brusch <brusch@1f8fe7d8-47f0-464c-8d0a-336f4953ab05>
2015-05-08 08:49:00 +0000

    thumbnails: picture element: use smallest image as the fallback image used in <img> => mobile first
    git-svn-id: http://www.pimcore.org/svn/pimcore/private/core/trunk@6479 1f8fe7d8-47f0-464c-8d0a-336f4953ab05

--- pimcore/models/Asset/Image/Thumbnail.php
+++ pimcore/models/Asset/Image/Thumbnail.php
@@ -291,11 +291,6 @@ class Thumbnail {
         // build html tag
         $htmlImgTag = '<img '.implode(' ', $attr).' />';
 
-        $attrCleanedForPicture = $attr;
-        unset($attrCleanedForPicture["width"]);
-        unset($attrCleanedForPicture["height"]);
-        $htmlImgTagForpicture = '<img '.implode(' ', $attrCleanedForPicture).' />';
-
         // $this->getConfig() can be empty, the original image is returned
         if(!$this->getConfig() || !$this->getConfig()->hasMedias()) {
             return $htmlImgTag;
@@ -305,6 +300,9 @@ class Thumbnail {
             // set this variable so that Pimcore_Controller_Plugin_Thumbnail::dispatchLoopShutdown() knows that
             // the picture polyfill script needs to be included
             self::$pictureElementInUse = true;
+
+            // mobile first => fallback image is the smallest possible image
+            $fallBackImageThumb = null;
 
             $html = '<picture ' . implode(" ", $pictureAttribs) . ' data-default-src="' . $path . '">' . "\n";
                 $mediaConfigs = $thumbConfig->getMedias();
@@ -321,6 +319,10 @@ class Thumbnail {
                         $thumbConfigRes->setHighResolution($highRes);
                         $thumb = $image->getThumbnail($thumbConfigRes, true);
                         $srcSetValues[] = $thumb . " " . $highRes . "x";
+
+                        if(!$fallBackImageThumb) {
+                            $fallBackImageThumb = $thumb;
+                        }
                     }
 
                     $html .= "\t" . '<source srcset="' . implode(", ", $srcSetValues) .'"';
@@ -334,7 +336,15 @@ class Thumbnail {
                 }
 
                 //$html .= "\t" . '<noscript>' . "\n\t\t" . $htmlImgTag . "\n\t" . '</noscript>' . "\n";
+
+                $attrCleanedForPicture = $attr;
+                unset($attrCleanedForPicture["width"]);
+                unset($attrCleanedForPicture["height"]);
+                $attrCleanedForPicture["src"] = 'src="' . (string) $fallBackImageThumb . '"';
+                $htmlImgTagForpicture = '<img '.implode(' ', $attrCleanedForPicture).' />';
+
                 $html .= $htmlImgTagForpicture . "\n";
+
             $html .= '</picture>' . "\n";
 
             return $html;
