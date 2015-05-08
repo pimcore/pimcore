b25a7e19474bb76b475b34309ab30543af8d1b97
mmoser <mmoser@6df6f5fa-e861-40d1-bdbc-1b55999331ee>
2015-05-08 14:36:14 +0000

    FilterService MultiSelect Bugfix
    git-svn-id: http://svn/php/pimcore_plugins/onlineshop/trunk/www@452 6df6f5fa-e861-40d1-bdbc-1b55999331ee

--- lib/OnlineShop/Framework/FilterService/MultiSelect.php
+++ lib/OnlineShop/Framework/FilterService/MultiSelect.php
@@ -28,7 +28,9 @@ class OnlineShop_Framework_FilterService_MultiSelect extends OnlineShop_Framewor
         $value = $params[$field];
 
         if(empty($value) && !$params['is_reload']) {
-            $value = explode(",", $preSelect);
+            if(!empty($preSelect) || $preSelect == '0') {
+                $value = explode(",", $preSelect);
+            }
         } else if(!empty($value) && in_array(OnlineShop_Framework_FilterService_AbstractFilterType::EMPTY_STRING, $value)) {
             $value = null;
         }
