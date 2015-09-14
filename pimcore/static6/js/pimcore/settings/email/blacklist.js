a46fff6c16b2acf28b78f256ca7f9c7ac33a241e
Bernhard Rusch <bernhard.rusch@elements.at>
2015-09-14 18:09:37 +0200

    extjs6 - misc

--- pimcore/static6/js/pimcore/settings/email/blacklist.js
+++ pimcore/static6/js/pimcore/settings/email/blacklist.js
@@ -120,7 +120,7 @@ pimcore.settings.email.blacklist = Class.create({
                 items:[
                     {
                         tooltip:t('delete'),
-                        icon:"/pimcore/static/img/icon/cross.png",
+                        icon:"/pimcore/static6/img/icon/cross.png",
                         handler:function (grid, rowIndex) {
                             grid.getStore().removeAt(rowIndex);
                         }.bind(this)
