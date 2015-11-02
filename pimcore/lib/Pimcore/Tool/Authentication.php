a0220525a899f48a27ed0ed25ba8270b42ba13bd
Bernhard Rusch <bernhard.rusch@elements.at>
2015-11-02 10:43:24 +0100

    removed password_compatibility.php script - as new requirement is PHP 5.5 which has native support for the password api

--- pimcore/lib/Pimcore/Tool/Authentication.php
+++ pimcore/lib/Pimcore/Tool/Authentication.php
@@ -14,9 +14,6 @@ namespace Pimcore\Tool;
 
 use Pimcore\Model\User;
 
-// PHP 5.5 Crypt-API password compatibility layer for PHP version < PHP 5.5
-include_once("password_compatibility.php");
-
 class Authentication {
 
     /**
