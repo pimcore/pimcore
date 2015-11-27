24cfd4e4c34708fd0178bce9b7594edcec742a58
weisswurstkanone <josef.aichhorn@elements.at>
2015-11-27 08:13:18 +0100

    asset tree - change node data right after entering the new filename but revert the change in case of error

--- pimcore/static6/js/pimcore/asset/tree.js
+++ pimcore/static6/js/pimcore/asset/tree.js
@@ -1120,20 +1120,29 @@ pimcore.asset.tree = Class.create({
                 }
 
                 value = pimcore.helpers.getValidFilename(value);
+                record.getOwnerTree().loadMask.enable();
+
+                var originalText = record.get("text");
+                var originalPath = record.get("path");
 
                 record.set("text", value);
                 record.set("path", record.data.basePath + value);
 
-                record.getOwnerTree().loadMask.enable();
-
                 this.updateAsset(record.data.id, {filename: value}, function (response) {
+
+                    var rdata = Ext.decode(response.responseText);
+                    if (!rdata || !rdata.success) {
+                        record.set("text", originalText);
+                        record.set("path", originalPath);
+                        pimcore.helpers.showNotification(t("error"), t("there_was_a_problem_renaming_a_folder"),
+                            "error");
+                        return;
+                    }
 
                     //record.getOwnerTree().loadMask.disable();
                     this.refresh(record);
-
                     if (pimcore.globalmanager.exists("asset_" + record.data.id)) {
                         try {
-                            var rdata = Ext.decode(response.responseText);
 
                             if (rdata && rdata.success) {
                                 pimcore.helpers.closeAsset(record.data.id);
