d3d37797418fc1e4c071bd925e071eab9be968e7
weisswurstkanone <josef.aichhorn@elements.at>
2016-01-11 09:09:39 +0100

    #369 - ExtJs 6 - Publish of objects via context menu bug

--- pimcore/static6/js/pimcore/object/tree.js
+++ pimcore/static6/js/pimcore/object/tree.js
@@ -1059,18 +1059,26 @@ pimcore.object.tree = Class.create({
                     var view = ownerTree.getView();
                     var nodeEl = Ext.fly(view.getNodeByRecord(record));
 
+                    if (nodeEl) {
+                        var nodeElInner = nodeEl.down(".x-grid-td");
+                    }
+
                     var rdata = Ext.decode(response.responseText);
                     if (rdata && rdata.success) {
 
                         if (task == 'unpublish') {
-                            nodeEl.addCls('pimcore_unpublished');
+                            if (nodeElInner) {
+                                nodeElInner.addCls('pimcore_unpublished');
+                            }
                             record.data.published = false;
                             if (pimcore.globalmanager.exists("object_" + record.data.id)) {
                                 pimcore.globalmanager.get("object_" + record.data.id).toolbarButtons.unpublish.hide();
                             }
 
                         } else {
-                            nodeEl.removeCls('pimcore_unpublished');
+                            if (nodeElInner) {
+                                nodeElInner.removeCls('pimcore_unpublished');
+                            }
 
                             record.data.published = true;
                             if (pimcore.globalmanager.exists("object_" + record.data.id)) {
