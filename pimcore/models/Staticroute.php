53500bbf96c1310cc4e031ce1bb03aec54e52428
Bernhard Rusch <bernhard.rusch@elements.at>
2016-01-19 16:26:36 +0100

    staticroutes: order by key length, longer key have priority  (%abcd prior %ab, so that %ab doesn't replace %ab in [%ab]cd)

--- pimcore/models/Staticroute.php
+++ pimcore/models/Staticroute.php
@@ -449,6 +449,12 @@ class Staticroute extends AbstractModel {
         $urlEncodeEscapeCharacters = "~|urlen" . md5(microtime()) . "code|~";
 
         // replace named variables
+        uksort($parametersInReversePattern, function ($a, $b) {
+            // order by key length, longer key have priority
+            // (%abcd prior %ab, so that %ab doesn't replace %ab in [%ab]cd)
+            return strlen($b) - strlen($a);
+        });
+
         foreach ($parametersInReversePattern as $key => $value) {
             $value = str_replace($forbiddenCharacters, "", $value);
             if(strlen($value) > 0) {
