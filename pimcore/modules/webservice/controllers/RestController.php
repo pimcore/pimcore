326b87f431cc2e44d659f6ea04ad2b4fa5890852
Bernhard Rusch <bernhard.rusch@elements.at>
2016-01-21 13:28:46 +0100

    video thumbnails rest service fix (json) - see #359

--- pimcore/modules/webservice/controllers/RestController.php
+++ pimcore/modules/webservice/controllers/RestController.php
@@ -319,21 +319,19 @@ class Webservice_RestController extends \Pimcore\Controller\Action\Webservice {
     public function imageThumbnailsAction () {
         $this->checkUserPermission("thumbnails");
 
-        $dir = Asset\Video\Thumbnail\Config::getWorkingDir();
+        $thumbnails = [];
 
-        $pipelines = array();
-        $files = scandir($dir);
-        foreach ($files as $file) {
-            if(strpos($file, ".xml")) {
-                $name = str_replace(".xml", "", $file);
-                $pipelines[] = array(
-                    "id" => $name,
-                    "text" => $name
-                );
-            }
+        $list = new Asset\Video\Thumbnail\Config\Listing();
+        $items = $list->load();
+
+        foreach($items as $item) {
+            $thumbnails[] = array(
+                "id" => $item->getName(),
+                "text" => $item->getName()
+            );
         }
 
-        $this->encoder->encode(array("success" => true, "data" => $pipelines));
+        $this->encoder->encode(array("success" => true, "data" => $thumbnails));
     }
 
 
