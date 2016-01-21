af92054e67da029300f6bf7abb6c083aab71f459
Bernhard Rusch <bernhard.rusch@elements.at>
2016-01-21 13:27:48 +0100

    migrated custom reports to json - see #359

--- update/3610/preupdate.php
+++ update/3610/preupdate.php
@@ -30,3 +30,27 @@ if(!is_dir($legacyFolder)) {
 // move data
 rename($dir, $legacyFolder . "/qrcodes");
 
+
+// SQL REPORTS
+$dir = PIMCORE_CONFIGURATION_DIRECTORY . "/sqlreport";
+
+$file = Pimcore\Config::locateConfigFile("custom-reports.json");
+$json = \Pimcore\Db\JsonFileTable::get($file);
+$json->truncate();
+
+$files = scandir($dir);
+foreach ($files as $file) {
+    if(strpos($file, ".xml")) {
+        $name = str_replace(".xml", "", $file);
+        $thumbnail = \Pimcore\Model\Tool\CustomReport\Config::getByName($name);
+        $thumbnail = object2array($thumbnail);
+
+        $thumbnail["id"] = $thumbnail["name"];
+        unset($thumbnail["name"]);
+
+        $json->insertOrUpdate($thumbnail, $thumbnail["id"]);
+    }
+}
+
+// move data
+rename($dir, $legacyFolder . "/sqlreport");
