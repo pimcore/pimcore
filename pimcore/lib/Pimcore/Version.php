8bf5076956705c1970d909f0be1d1b98a076dda2
Bernhard Rusch <bernhard.rusch@elements.at>
2016-02-02 12:24:10 +0100

    beta 4

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Pimcore
  *
@@ -18,7 +18,7 @@ class Version
     /**
      * @var string
      */
-    public static $version = "4.0.0-b3";
+    public static $version = "4.0.0-b4";
 
     /**
      * @var int
