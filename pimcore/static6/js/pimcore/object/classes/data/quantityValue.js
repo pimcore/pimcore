c83f12f4deed17b2f84e0e57a853bc692fd28cca
weisswurstkanone <josef.aichhorn@elements.at>
2016-02-08 11:36:58 +0100

    classeditor - fixed quantityValue combobox issue

--- pimcore/static6/js/pimcore/object/classes/data/quantityValue.js
+++ pimcore/static6/js/pimcore/object/classes/data/quantityValue.js
@@ -61,7 +61,7 @@ pimcore.object.classes.data.quantityValue = Class.create(pimcore.object.classes.
                 xtype: 'combobox',
                 name: 'defaultUnit',
                 triggerAction: "all",
-                editable: false,
+                editable: true,
                 typeAhead: true,
                 selectOnFocus: true,
                 fieldLabel: t('default_unit'),
