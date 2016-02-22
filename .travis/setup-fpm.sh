8c24a171892d24bc1be4f13f3b537fd94c0da5f0
Bernhard Rusch <bernhard.rusch@elements.at>
2016-02-22 06:14:27 +0100

    travis-ci: moved travis related stuff from build/ to .travis/

---
+++ .travis/setup-fpm.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "Setting up FPM ..."
+
+phpenv config-add .travis/php.ini
+
+sudo cp -f .travis/php-fpm.conf ~/.phpenv/versions/$(phpenv version-name)/etc/php-fpm.conf
+
+echo "cgi.fix_pathinfo = 1" >> ~/.phpenv/versions/$(phpenv version-name)/etc/php.ini
+~/.phpenv/versions/$(phpenv version-name)/sbin/php-fpm
+
+sudo cp -f .travis/apache-fpm.conf /etc/apache2/sites-available/default
