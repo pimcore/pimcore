89d6183908b49a92c9b37b13b6870c7a4b96e6d5
Bernhard Rusch <bernhard.rusch@elements.at>
2016-04-06 14:47:59 +0200

    cleaned up header comments

--- pimcore/modules/admin/controllers/AssetController.php
+++ pimcore/modules/admin/controllers/AssetController.php
@@ -4,7 +4,7 @@
  *
  * This source file is subject to the GNU General Public License version 3 (GPLv3)
  * For the full copyright and license information, please view the LICENSE.md and gpl-3.0.txt
- * files that are distributed with this source code. dsf sdaf asdf asdf
+ * files that are distributed with this source code.
  *
  * @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
  * @license    http://www.pimcore.org/license     GNU General Public License version 3 (GPLv3)
