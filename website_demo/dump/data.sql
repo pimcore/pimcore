8af9179e6a4f25c6487dbab8f323ba4dddc22c80
Bernhard Rusch <bernhard.rusch@elements.at>
2016-04-07 09:15:27 +0200

    fixed demo.sql -> users insert were wrong

--- website_demo/dump/data.sql
+++ website_demo/dump/data.sql
@@ -2670,8 +2670,8 @@ INSERT INTO `tree_locks` VALUES ('46','document','self');
 
 
 
-INSERT INTO `users` VALUES ('0','0','user','system','',NULL,NULL,NULL,'','','1','1','','','0','0','0','','',NULL);
-INSERT INTO `users` VALUES ('38','0','user','admin','$2y$10$nzibZ5Ck0.ZZy.mYhjpjrO.HAMGgWTDsNgGY.KT8KIuY/y1OO.cJ2',NULL,NULL,NULL,'en',NULL,'1','1','','','0','1','1','','',NULL);
+INSERT INTO `users` VALUES ('0','0','user','system','',NULL,NULL,NULL,'','','1','1','','','0','0','0','','',NULL,NULL,NULL);
+INSERT INTO `users` VALUES ('2','0','user','admin','$2y$10$nzibZ5Ck0.ZZy.mYhjpjrO.HAMGgWTDsNgGY.KT8KIuY/y1OO.cJ2',NULL,NULL,NULL,'en',NULL,'1','1','','','0','1','1','','',NULL,NULL,NULL);
 
 
 
