87bfef793580b0ac41586db9f95056a342c452ee
Bernhard Rusch <bernhard.rusch@elements.at>
2016-04-14 12:16:42 +0200

    header comments: removed trailing space

--- pimcore/static6/js/pimcore/startup.js
+++ pimcore/static6/js/pimcore/startup.js
@@ -4,7 +4,7 @@
  * This source file is available under two different licenses:
  * - GNU General Public License version 3 (GPLv3)
  * - Pimcore Enterprise License (PEL)
- * Full copyright and license information is available in 
+ * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
  * @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
