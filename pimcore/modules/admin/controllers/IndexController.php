cc48db70bb8c0b6e6c46e65d108dcaf1d90fa1b3
pimcore-team <team@pimcore.com>
2016-04-15 10:01:10 +0200

    build 3742

--- pimcore/modules/admin/controllers/IndexController.php
+++ pimcore/modules/admin/controllers/IndexController.php
@@ -95,7 +95,7 @@ class Admin_IndexController extends \Pimcore\Controller\Action\Admin
 
         // session lifetime (gc)
         $session_gc_maxlifetime = ini_get("session.gc_maxlifetime");
-        if(empty($session_gc_maxlifetime)) {
+        if (empty($session_gc_maxlifetime)) {
             $session_gc_maxlifetime = 120;
         }
         $this->view->session_gc_maxlifetime = $session_gc_maxlifetime;
