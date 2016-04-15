dd88c7b17ce1e9d7d64918429f6c55d731c79f9c
Christian Fasching <christian.fasching@elements.at>
2016-04-15 11:56:32 +0200

    insert dump during installation -> replace document root placeholder with current document root

--- pimcore/models/Tool/Setup/Dao.php
+++ pimcore/models/Tool/Setup/Dao.php
@@ -57,7 +57,10 @@ class Dao extends Model\Dao\AbstractDao
     public function insertDump($file)
     {
         $sql = file_get_contents($file);
-        
+
+        //replace document root placeholder with current document root
+        $sql = str_replace("~~DOCUMENTROOT~~", PIMCORE_DOCUMENT_ROOT, $sql);
+
         // we have to use the raw connection here otherwise \Zend_Db uses prepared statements, which causes problems with inserts (: placeholders)
         // and mysqli causes troubles because it doesn't support multiple queries
         if ($this->db->getResource() instanceof \Zend_Db_Adapter_Mysqli) {
