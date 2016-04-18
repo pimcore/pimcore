25a3b1ce944c24f30fa7c84b088a639a687c7d43
Bernhard Rusch <bernhard.rusch@elements.at>
2016-04-18 07:41:52 +0200

    deprecated InkscapeImagick -> use ImageMagick delegates instead

--- pimcore/lib/Pimcore/Image/Adapter/InkscapeImagick.php
+++ pimcore/lib/Pimcore/Image/Adapter/InkscapeImagick.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Pimcore
  *
@@ -17,110 +17,11 @@ namespace Pimcore\Image\Adapter;
 use Pimcore\Image\Adapter\Imagick;
 use Pimcore\Tool\Console;
 
+/**
+ * @deprecated
+ */
 class InkscapeImagick extends Imagick
 {
-
-    protected $isOriginal = true;
-
-    /**
-     * @return string
-     */
-    protected static function getBinary()
-    {
-        return "/usr/bin/inkscape";
-    }
-
-    /**
-     * @return bool
-     */
-    protected function isSvg()
-    {
-        return (bool) preg_match("/\.svgz?$/", $this->imagePath);
-    }
-
-    /**
-     * @param $width
-     * @return $this|\Pimcore\Image\Adapter
-     */
-    public function scaleByWidth($width)
-    {
-        if (!$this->isOriginal || !$this->isSvg()) {
-            return parent::scaleByWidth($width);
-        }
-
-        $width  = (int)$width;
-
-        $tmpFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . "_pimcore_image_svg_width_tmp_file.png";
-        $this->tmpFiles[] = $tmpFile;
-
-        Console::exec(self::getBinary() . " -w " . $width . " -D -f " . $this->imagePath . " -e " . $tmpFile);
-        $this->initImagick($tmpFile);
-
-        return $this;
-    }
-
-    /**
-     * @param $height
-     * @return $this|\Pimcore\Image\Adapter
-     */
-    public function scaleByHeight($height)
-    {
-        if (!$this->isOriginal || !$this->isSvg()) {
-            return parent::scaleByHeight($height);
-        }
-
-        $height = (int)$height;
-
-        $tmpFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . "_pimcore_image_svg_height_tmp_file.png";
-        $this->tmpFiles[] = $tmpFile;
-
-        Console::exec(self::getBinary() . " -h " . $height . " -D -f " . $this->imagePath . " -e " . $tmpFile);
-        $this->initImagick($tmpFile);
-
-
-        return $this;
-    }
-
-    /**
-     * @param $width
-     * @param $height
-     * @return $this|Imagick
-     */
-    public function resize($width, $height)
-    {
-        if (!$this->isOriginal || !$this->isSvg()) {
-            return parent::resize($width, $height);
-        }
-
-        $width  = (int)$width;
-        $height = (int)$height;
-
-        $tmpFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . "/" . uniqid() . "_pimcore_image_svg_resize_tmp_file.png";
-        $this->tmpFiles[] = $tmpFile;
-
-        Console::exec(self::getBinary() . " -w " . $width . " -h " . $height . " -D -f " . $this->imagePath . " -e " . $tmpFile);
-        $this->initImagick($tmpFile);
-
-        return $this;
-    }
-
-    /**
-     * @param $tmpFile
-     */
-    protected function initImagick($tmpFile)
-    {
-        $this->isOriginal = false;
-
-        $this->destroy();
-        $this->load($tmpFile);
-    }
-
-    /**
-     *
-     */
-    protected function reinitializeImage()
-    {
-        $this->isOriginal = false;
-        parent::reinitializeImage();
-    }
+    // this is deprecated
+    // use ImageMagick delegates instead
 }
