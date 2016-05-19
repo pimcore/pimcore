d3ede56f8452517e7f4f9ee887592e5dc18d5967
Bernhard Rusch <bernhard.rusch@elements.at>
2016-05-19 08:21:03 +0200

    Possibility for marking redirects as inactive (yes/no) - resolves #462 - fixes update-script

--- update/3786/postupdate.php
+++ update/3786/postupdate.php
@@ -2,3 +2,4 @@
 
 $db = \Pimcore\Db::get();
 $db->query("ALTER TABLE `redirects` ADD COLUMN `active` tinyint(1) NULL DEFAULT NULL AFTER `priority`;");
+$db->update("redirects", ["active" => 1]); // set all existing redirects to active
