e55ee9b5d5b9837b6e23a90044525bf80f98eb91
weisswurstkanone <josef.aichhorn@elements.at>
2016-05-24 09:43:54 +0200

    perspectives example

--- website_demo/config/perspectives.example.php
+++ website_demo/config/perspectives.example.php
@@ -1,19 +1,15 @@
 <?php
 
 return [
-    "default" => [
+    "default" => [                                              // this is the config for the default view
         "iconCls" => "pimcore_icon_perspective",
         "elementTree" => [
             [
-                "name" => "view1",
-                "hidden" => true
-            ],
-            [
-                "type" => "documents",
+                "type" => "documents",                          // document tree
                 "position" => "left",
-                "expanded" => false,
+                "expanded" => false,                            //  there can be only one expanded view on each side
                 "hidden" => false,
-                "sort" => -3
+                "sort" => -3                                    // trees with lower values are shown first
             ],
             [
                 "type" => "assets",
@@ -28,21 +24,22 @@ return [
                 "expanded" => false,
                 "hidden" => false,
                 "sort" => -1
-            ],
+            ]
+
         ],
-        "dashboards" => [
-            // allows you disable portlets by name
-//            "disabledPortlets" => array(
-//                "pimcore.layout.portlets.modificationStatistic" => 1,
-//                "pimcore.layout.portlets.feed" => 1
-//            ),
-            "predefined" => array(
-                "welcome" => array(
+        "dashboards" => array(                                  // this is the standard setting for the welcome screen
+            "predefined" => [
+                "welcome" => array(                             // internal key of the dashboard
                     "positions" => array(
-                        array(
+                        array(                                  // left column
                             array(
                                 "id" => 1,
                                 "type" => "pimcore.layout.portlets.modificationStatistic",
+                                "config" => null                // additional config
+                            ),
+                            array(
+                                "id" => 2,
+                                "type" => "pimcore.layout.portlets.modifiedAssets",
                                 "config" => null
                             )
                         ),
@@ -60,25 +57,15 @@ return [
                         )
                     )
                 )
-            )
-        ]
+            ]
+        )
+
     ],
-    "first alternative" => [
+    "Alternative view" => [
         "icon" => "/pimcore/static6/img/flat-color-icons/biohazard.svg",
         "toolbar" => [
-            "file" => [
-                "hidden" => false,
-                "items" => [
-                    "dashboards" => 0
-// Is the same as
-//                    "dashboards" => [
-//                        "hidden" => true
-//                    ]
-                    ,
-                    "openAsset" => false
-
-                ]
-            ],
+            "file" => 1
+            ,
             "extras" => [
                 "hidden" => false,
                 "items" => [
@@ -92,14 +79,14 @@ return [
                 ]
 
             ],
-            "marketing" => [
+            "marketing" => [                                                // hide the marketing menu
                 "hidden" => 1
             ],
             "settings" => [
                 "items" => [
                     "cache" => [
                         "items" => [
-                            "clearAll" => 0
+                            "clearAll" => 0                                 // hide "Clear All" but show the other Cache menu entries
                         ]
                     ]
                 ]
@@ -115,60 +102,72 @@ return [
                 "type" => "documents",
                 "position" => "left",
                 "expanded" => false,
-                "hidden" => false,
-                "sort" => 3
+                "hidden" => true,                                           // hide the document tree
+                "sort" => 3                                                 // show it on the bottom
             ],
             [
                 "type" => "assets",
-                "position" => "right",
-                "expanded" => true,
+                "position" => "right",                                      // show the asset tree on the right side
+                "expanded" => false,                                         // expand it
                 "hidden" => false,
                 "sort" => -2
             ],
             [
                 "type" => "objects",
                 "position" => "left",
-                "expanded" => false,
+                "expanded" => true,
                 "hidden" => false,
                 "sort" => -1
             ],
-        ]
+            [
+                "type" => "customview",                         // include custom view
+                "position" => "right",
+                "sort" => -10,                                  // show it on the top
+                "expanded" => true,
+                "id" => 2,                                      // show alternative document tree on the right side
+                "treeContextMenu" => [                          // hide the "Add document" tree context menu
+                    "document" => [
+                        "items" => [
+                            "add" => 0,
+                            "cut" => 0,
+                            "rename" => 0
+                        ]
+                    ]
+                ]
+            ],
+        ],
+        "dashboards" => array(                                  // this is the standard setting for the welcome screen
+            "disabledPortlets" => array(                        // disallows access to the given portlets
+                "pimcore.layout.portlets.modificationStatistic" => 1,
+                "pimcore.layout.portlets.feed" => 1
+            ),
+            "predefined" => [
+                "welcome" => array(                             // internal key of the dashboard
+                    "positions" => array(
+                        array(                                  // left column
+                        ),
+                        array(                                  // only show modified objects in the right column
+                            array(
+                                "id" => 3,
+                                "type" => "pimcore.layout.portlets.modifiedObjects",
+                                "config" => null
+                            )
+                        )
+                    )
+                )
+            ]
+        )
     ],
-    "SECOND alternative" => [
-        "icon" => "/pimcore/static6/img/flat-color-icons/low_battery.svg",
-        "extjsDev" => true,
-        "nicename" => "second nice name",
+    "Assets only" => [
+        "icon" => "/pimcore/static6/img/flat-color-icons/webcam.svg",
         "elementTree" => [
             [
-                "type" => "documents",
-                "position" => "right",
-                "expanded" => false,
-                "hidden" => false,
-                "sort" => 15
-            ],
-            [
                 "type" => "assets",
-                "position" => "right",
-                "expanded" => true,
+                "position" => "left",                                      // show the asset tree on the right side
+                "expanded" => false,                                       // expand it
                 "hidden" => false,
-                "sort" => -1
-            ],
-            [
-                "type" => "objects",
-                "position" => "left",
-                "expanded" => false,
-                "hidden" => true,
                 "sort" => -2
-            ],
-            [
-                "type" => "customview",
-                "name" => "view1",
-                "hidden" => 1
-            ],
-            "toolbar" => [
-                "search" => 0
             ]
         ]
-
     ]
 ];
