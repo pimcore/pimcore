3417a12d2c346e2fdfa2b3df72e2e488a3b95ac5
Bernhard Rusch <bernhard.rusch@elements.at>
2016-05-27 15:35:25 +0200

    asset image version preview: cleanup temp. files

--- pimcore/modules/admin/views/scripts/asset/show-version-image.php
+++ pimcore/modules/admin/views/scripts/asset/show-version-image.php
@@ -28,18 +28,20 @@
 <body>
 
 <?php
-    $thumbnail = $this->asset->getImageThumbnailSavePath() . "/asset-version-preview-" . $this->asset->getId() . "-" . time() . ".png";
+    $thumbnail = PIMCORE_SYSTEM_TEMP_DIRECTORY . "/image-version-preview-" . uniqid() . ".png";
     $convert = \Pimcore\Image::getInstance();
     $convert->load($this->asset->getTemporaryFile());
     $convert->contain(500,500);
     $convert->save($thumbnail, "png");
-    $thumbnail = str_replace(PIMCORE_DOCUMENT_ROOT, "", $thumbnail);
+
+    $dataUri = "data:image/png;base64," . base64_encode(file_get_contents($thumbnail));
+    unlink($thumbnail);
 ?>
 
 <table id="wrapper" cellpadding="0" cellspacing="0" border="0">
     <tr>
         <td align="center">
-            <img src="<?= $thumbnail ?>"/>
+            <img src="<?= $dataUri ?>"/>
         </td>
     </tr>
 </table>
