c8ef853a814df849fcd02f5e087ad03bb8b0d368
Bernhard Rusch <bernhard.rusch@elements.at>
2016-05-31 08:06:58 +0200

    added constants.php example

---
+++ constants.example.php
@@ -0,0 +1,6 @@
+<?php
+
+// you can use this file to overwrite the constants defined in /pimcore/config/startup.php
+
+define("PIMCORE_ASSET_DIRECTORY", "/custom/path/to/assets");
+define("PIMCORE_TEMPORARY_DIRECTORY", "/my/tmp/path");
