ce1b376691082eb1ff0b2d40f78adb68263395dd
pimcore-team <team@pimcore.com>
2016-06-01 13:23:51 +0200

    build 3809

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -20,12 +20,12 @@ class Version
     /**
      * @var string
      */
-    public static $version = "4.0.0";
+    public static $version = "4.0.1";
 
     /**
      * @var int
      */
-    public static $revision = 3808;
+    public static $revision = 3809;
 
 
     /**
