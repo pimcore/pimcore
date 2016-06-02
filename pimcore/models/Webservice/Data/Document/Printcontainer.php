cf2d913196933a0a180be9db609007e1352a0a71
cfasching <christian.fasching@elements.at>
2016-06-02 10:23:58 +0200

    fixes #634 - Add webservice support for web2print documents

---
+++ pimcore/models/Webservice/Data/Document/Printcontainer.php
@@ -0,0 +1,33 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @category   Pimcore
+ * @package    Webservice
+ * @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+
+namespace Pimcore\Model\Webservice\Data\Document;
+
+use \Pimcore\Model\Webservice\Data\Document;
+
+class Printcontainer extends Document\PageSnippet {
+
+
+    public $lastGenerated;
+
+    public $inProgress;
+
+    public $css;
+
+    public $lastGenerateMessage;
+
+}
---
+++ pimcore/models/Webservice/Data/Document/Printpage/Out.php
@@ -0,0 +1,24 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @category   Pimcore
+ * @package    Webservice
+ * @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+
+namespace Pimcore\Model\Webservice\Data\Document\Printpage;
+
+use \Pimcore\Model\Webservice\Data\Document;
+
+class Out extends Document\Printpage {
+
+}
