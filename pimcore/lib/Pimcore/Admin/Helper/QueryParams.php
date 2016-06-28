7295760ad782c1befb71a5d4ff7afac49d82d56b
ckogler <t23rKIgoelxSI22>
2016-06-28 16:25:01 +0200

    Added a helper to get a condition string/array from extJs filter types (currently supported: string,numeric,date (the date has to be passed as timestamp))

--- pimcore/lib/Pimcore/Admin/Helper/QueryParams.php
+++ pimcore/lib/Pimcore/Admin/Helper/QueryParams.php
@@ -72,4 +72,74 @@ class QueryParams
             return $param['id'];
         }
     }
+
+    /**
+     * Creates a condition string from the passed ExtJs filter definitions
+     *
+     * @param $filterString
+     * @param array $matchExact
+     * @param bool $returnString
+     * @return array|string
+     * @throws \Exception
+     */
+    public static function getFilterCondition($filterString, $matchExact = ['id', 'o_id'], $returnString = true)
+    {
+        if (!$filterString) {
+            return '';
+        }
+        $conditions = [];
+
+        $filters = json_decode($filterString);
+        $db = \Pimcore\Db::get();
+        foreach ($filters as $f) {
+            if ($f->type == 'string') {
+                if (in_array($f->property, $matchExact)) {
+                    $conditions[$f->property][] = ' ' . $db->getQuoteIdentifierSymbol() . $f->property . $db->getQuoteIdentifierSymbol() . " = " . $db->quote($f->value) . ' ';
+                } else {
+                    $conditions[$f->property][] = ' ' . $db->getQuoteIdentifierSymbol() . $f->property . $db->getQuoteIdentifierSymbol() . " LIKE " . $db->quote("%" . $f->value . "%") . ' ';
+                }
+            } elseif ($f->type == 'numeric') {
+                if ($f->operator == 'eq') {
+                    $symbol = ' = ';
+                } elseif ($f->operator == 'lt') {
+                    $symbol = ' < ';
+                } elseif ($f->operator == 'gt') {
+                    $symbol = ' > ';
+                }
+                $conditions[$f->property][] = ' ' . $db->getQuoteIdentifierSymbol() . $f->property . $db->getQuoteIdentifierSymbol() . $symbol . $db->quote($f->value) . ' ';
+            } elseif ($f->type == 'date') {
+                /**
+                 * make sure you pass the date as timestamp
+                 *
+                 * filter: {type : 'date',dateFormat: 'timestamp'}
+                 */
+                $date = Carbon::createFromTimestamp($f->value)->setTime(0,0,0);
+
+                if ($f->operator == 'eq') {
+                    $conditions[$f->property][] = ' ' . $f->property . ' >= ' . $db->quote($date->getTimestamp());
+                    $conditions[$f->property][] = ' ' . $f->property . ' <= ' . $db->quote($date->addDay(1)->subSecond(1)->getTimestamp());
+                } elseif ($f->operator == 'lt') {
+                    $conditions[$f->property][] = ' ' . $f->property . ' < ' . $db->quote($date->getTimestamp());
+                } elseif ($f->operator == 'gt') {
+                    $conditions[$f->property][] = ' ' . $f->property . ' > ' . $db->quote($date->addDay(1)->subSecond(1)->getTimestamp());
+                }
+            } else {
+                throw new \Exception("Filer of type " . $f->type . " not jet supported.");
+            }
+        }
+
+        $conditionsGrouped = [];
+        foreach ($conditions as $fieldName => $fieldConditions) {
+            if (count($fieldConditions) > 1) {
+                $conditionsGrouped[$fieldName] = ' (' . implode(' AND ', $fieldConditions) . ') ';
+            } else {
+                $conditionsGrouped[$fieldName] = $fieldConditions[0];
+            }
+        }
+        if ($returnString) {
+            return implode(' OR ', $conditionsGrouped);
+        } else {
+            return $conditionsGrouped;
+        }
+    }
 }
