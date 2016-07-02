93b5fe52cb733c1718b3710438f7f97299498683
cfasching <christian.fasching@elements.at>
2016-07-02 22:06:51 +0200

    fixes #712 - Adding Workflows to objects with objectsWithMetadata breaks them

--- pimcore/lib/Pimcore/WorkflowManagement/EventHandler.php
+++ pimcore/lib/Pimcore/WorkflowManagement/EventHandler.php
@@ -115,6 +115,7 @@ class EventHandler
                         if ($validLayouts && $validLayouts[$workflowLayoutId]) {
                             $customLayout = ClassDefinition\CustomLayout::getById($workflowLayoutId);
                             $customLayoutDefinition = $customLayout->getLayoutDefinitions();
+                            Object\Service::enrichLayoutDefinition($customLayoutDefinition  , $e->getParam('object'));
                             $data["layout"] = $customLayoutDefinition;
                         }
                     }
