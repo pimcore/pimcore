8e0a208069dffd10219ef181909a0b13a161ee62
Bernhard Rusch <bernhard.rusch@elements.at>
2016-07-05 18:35:10 +0200

    Objects - datatype email - no validation - fixes  #714

--- pimcore/models/Object/ClassDefinition/Data/Email.php
+++ pimcore/models/Object/ClassDefinition/Data/Email.php
@@ -1,4 +1,4 @@
-<?php 
+<?php
 /**
  * Pimcore
  *
@@ -22,9 +22,29 @@ class Email extends Model\Object\ClassDefinition\Data\Input
 {
 
     /**
-     * Static type of this element
-     *
      * @var string
      */
     public $fieldtype = "email";
+
+
+    public function checkValidity($data, $omitMandatoryCheck = false)
+    {
+
+        if (!$omitMandatoryCheck && strlen($data) > 0) {
+            $hostnameValidator = new \Zend_Validate_Hostname([
+                "idn" => false,
+                "tld" => false
+            ]);
+            $validator = new \Zend_Validate_EmailAddress([
+                "mx" => false,
+                "deep" => false,
+                "hostname" => $hostnameValidator
+            ]);
+            if(!$validator->isValid($data)) {
+                throw new Model\Element\ValidationException("Value in field [ " . $this->getName() . " ] isn't a valid email address");
+            }
+        }
+
+        parent::checkValidity($data, $omitMandatoryCheck);
+    }
 }
