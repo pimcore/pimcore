08efa972a5eefdfc8e5307df945aada6a46fbbe5
mrudnicki <maciej.rudnicki@pimcore.com>
2016-08-24 11:33:46 +0200

    Resolves #796 Add  filter to asset filename in REST controller

--- pimcore/modules/webservice/controllers/RestController.php
+++ pimcore/modules/webservice/controllers/RestController.php
@@ -1056,6 +1056,9 @@ class Webservice_RestController extends \Pimcore\Controller\Action\Webservice
         if($wsData instanceof Pimcore\Model\Webservice\Data\Object || $wsData instanceof Pimcore\Model\Webservice\Data\Document) {
             /** @var Pimcore\Model\Webservice\Data\Object|Pimcore\Model\Webservice\Data\Document key */
             $wsData->key = \Pimcore\File::getValidFilename($wsData->key); //adds key filter to new objects and documents
+        } elseif($wsData instanceof Pimcore\Model\Webservice\Data\Asset) {
+            /** @var Pimcore\Model\Webservice\Data\Asset $wsData */
+            $wsData->filename = \Pimcore\File::getValidFilename($wsData->filename);
         }
 
         return $wsData;
