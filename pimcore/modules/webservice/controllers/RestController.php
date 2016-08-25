a3931e04ccb62dd3896d5d290dcae277e5c4dd1f
pimcore-team <team@pimcore.com>
2016-08-25 07:48:09 +0200

    build 3939

--- pimcore/modules/webservice/controllers/RestController.php
+++ pimcore/modules/webservice/controllers/RestController.php
@@ -1053,10 +1053,10 @@ class Webservice_RestController extends \Pimcore\Controller\Action\Webservice
             $wsData->$key = $value;
         }
 
-        if($wsData instanceof Pimcore\Model\Webservice\Data\Object || $wsData instanceof Pimcore\Model\Webservice\Data\Document) {
+        if ($wsData instanceof Pimcore\Model\Webservice\Data\Object || $wsData instanceof Pimcore\Model\Webservice\Data\Document) {
             /** @var Pimcore\Model\Webservice\Data\Object|Pimcore\Model\Webservice\Data\Document key */
             $wsData->key = \Pimcore\File::getValidFilename($wsData->key); //adds key filter to new objects and documents
-        } elseif($wsData instanceof Pimcore\Model\Webservice\Data\Asset) {
+        } elseif ($wsData instanceof Pimcore\Model\Webservice\Data\Asset) {
             /** @var Pimcore\Model\Webservice\Data\Asset $wsData */
             $wsData->filename = \Pimcore\File::getValidFilename($wsData->filename);
         }
