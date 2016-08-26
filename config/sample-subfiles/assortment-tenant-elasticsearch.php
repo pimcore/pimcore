4e4762ddb60c88ef5842a39e81d85bb071c0e4ea
cfasching <christian.fasching@elements.at>
2016-08-26 16:29:15 +0200

    fixes onlineshop config sample

--- config/sample-subfiles/assortment-tenant-elasticsearch.php
+++ config/sample-subfiles/assortment-tenant-elasticsearch.php
@@ -8,64 +8,60 @@ return [
         "indexSettingsJson" => '{"number_of_shards": 5, "number_of_replicas": 0}',
         "elasticSearchClientParamsJson" => '{"hosts": ["elasticsearch"]}',
         "generalSearchColumns" => [
-            "column" => [
-                "name" => "searchText"
-            ]
+            "name" => "searchText"
         ],
         "columns" => [
-            "column" => [
-                [
-                    "name" => "matnr",
-                    "json" => '{"type": "string","store": true}'
-                ],
-                [
-                    "name" => "ean",
-                    "json" => '{"type": "string"}'
-                ],
-                [
-                    "name" => "sellingFrequency",
-                    "json" => '{"type": "integer","store": true}'
-                ],
-                [
-                    "name" => "shortDescription",
-                    "json" => '{"type": "string", "store": true}'
-                ],
-                [
-                    "name" => "searchText",
-                    "json" => '{"type": "string"}'
-                ],
-                [
-                    "name" => "OSName",
-                    "json" => '{"type": "string"}'
-                ],
-                [
-                    "name" => "Rating",
-                    "fieldname" => "ESRating",
-                    "json" => '{"type": "object"}'
-                ],
-                [
-                    "name" => "herst",
-                    "interpreter" => "\\OnlineShop\\Framework\\IndexService\\Interpreter\\DefaultObjects"
-                ],
-                [
-                    "name" => "marke",
-                    "interpreter" => "\\OnlineShop\\Framework\\IndexService\\Interpreter\\DefaultObjects"
-                ],
-                [
-                    "name" => "properties",
-                    "fieldname" => "ESProperties",
-                    "json" => '{"type": "object", "dynamic": true}'
-                ],
-                [
-                    "name" => "prices",
-                    "fieldname" => "ESPrices",
-                    "json" => '{"type": "object", "dynamic": true}'
-                ],
-                [
-                    "name" => "types",
-                    "fieldname" => "ESTypes",
-                    "json" => '{"type": "object", "dynamic": true}'
-                ]
+            [
+                "name" => "matnr",
+                "json" => '{"type": "string","store": true}'
+            ],
+            [
+                "name" => "ean",
+                "json" => '{"type": "string"}'
+            ],
+            [
+                "name" => "sellingFrequency",
+                "json" => '{"type": "integer","store": true}'
+            ],
+            [
+                "name" => "shortDescription",
+                "json" => '{"type": "string", "store": true}'
+            ],
+            [
+                "name" => "searchText",
+                "json" => '{"type": "string"}'
+            ],
+            [
+                "name" => "OSName",
+                "json" => '{"type": "string"}'
+            ],
+            [
+                "name" => "Rating",
+                "fieldname" => "ESRating",
+                "json" => '{"type": "object"}'
+            ],
+            [
+                "name" => "herst",
+                "interpreter" => "\\OnlineShop\\Framework\\IndexService\\Interpreter\\DefaultObjects"
+            ],
+            [
+                "name" => "marke",
+                "interpreter" => "\\OnlineShop\\Framework\\IndexService\\Interpreter\\DefaultObjects"
+            ],
+            [
+                "name" => "properties",
+                "fieldname" => "ESProperties",
+                "json" => '{"type": "object", "dynamic": true}'
+            ],
+            [
+                "name" => "prices",
+                "fieldname" => "ESPrices",
+                "json" => '{"type": "object", "dynamic": true}'
+            ],
+            [
+                "name" => "types",
+                "fieldname" => "ESTypes",
+                "json" => '{"type": "object", "dynamic": true}'
             ]
         ],
         "filtertypes" => [
