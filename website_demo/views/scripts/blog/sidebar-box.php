79cc87b26f390cd92aca6230d8cfd472084c5310
Bernhard Rusch <bernhard.rusch@elements.at>
2016-09-01 08:00:35 +0200

    demo: bug fix

--- website_demo/views/scripts/blog/sidebar-box.php
+++ website_demo/views/scripts/blog/sidebar-box.php
@@ -6,17 +6,19 @@
         <div class="list-group">
             <?php foreach ($this->articles as $article) { ?>
                 <?php
-                    $detailLink = $this->url([
-                        "id" => $article->getId(),
-                        "text" => $article->getTitle(),
-                        "prefix" => $this->document->getProperty("blog")->getFullPath()
-                    ], "blog", true);
+                $detailLink = $this->url([
+                    "id" => $article->getId(),
+                    "text" => $article->getTitle(),
+                    "prefix" => $this->document->getProperty("blog")->getFullPath()
+                ], "blog", true);
                 ?>
                 <a href="<?= $detailLink ?>" class="list-group-item">
                     <h5 class="list-group-item-heading"><?= $article->getTitle(); ?></h5>
-                    <p class="list-group-item-text">
-                        <i class="glyphicon glyphicon-calendar"></i> <?= $article->getDate()->format("d/m/Y"); ?>
-                    </p>
+                    <?php if($article->getDate()) { ?>
+                        <p class="list-group-item-text">
+                            <i class="glyphicon glyphicon-calendar"></i> <?= $article->getDate()->format("d/m/Y"); ?>
+                        </p>
+                    <?php } ?>
                 </a>
             <?php } ?>
         </div>
