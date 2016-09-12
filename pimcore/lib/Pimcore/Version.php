4550d1e1ec54ac21ec25174002ac4868c2f387dc
pimcore-team <team@pimcore.com>
2016-09-12 13:56:24 +0200

    build 3961

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -20,12 +20,12 @@ class Version
     /**
      * @var string
      */
-    public static $version = "4.3.0";
+    public static $version = "4.3.1";
 
     /**
      * @var int
      */
-    public static $revision = 3959;
+    public static $revision = 3961;
 
 
     /**
