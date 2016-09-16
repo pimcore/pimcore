af954b0ddbbc2fb2a90066e991c4678d3eb6d1ed
pimcore-team <team@pimcore.com>
2016-09-16 15:13:22 +0200

    build 3966

--- pimcore/modules/admin/controllers/DocumentController.php
+++ pimcore/modules/admin/controllers/DocumentController.php
@@ -952,7 +952,7 @@ class Admin_DocumentController extends \Pimcore\Controller\Action\Admin\Element
         if ($childDocument instanceof Document\Page) {
             $tmpDocument["url"] = $childDocument->getFullPath();
             $site = Tool\Frontend::getSiteForDocument($childDocument);
-            if($site) {
+            if ($site) {
                 $tmpDocument["url"] = "http://" . $site->getMainDomain() . preg_replace("@^" . $site->getRootPath() . "/?@", "/", $childDocument->getRealFullPath());
             }
         }
