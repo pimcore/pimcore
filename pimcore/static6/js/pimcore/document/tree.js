45d2402c0063b88c238dfebc79b9bfc148abd070
Bernhard Rusch <bernhard.rusch@elements.at>
2016-09-26 08:33:49 +0200

    document tree reordered context menu

--- pimcore/static6/js/pimcore/document/tree.js
+++ pimcore/static6/js/pimcore/document/tree.js
@@ -422,6 +422,12 @@ pimcore.document.tree = Class.create({
                 //don't add emails, newsletters and links below print containers - makes no sense
                 if(addDocuments && record.data.type != "printcontainer") {
                     menu.add(new Ext.menu.Item({
+                        text: t('add_link'),
+                        iconCls: "pimcore_icon_link pimcore_icon_overlay_add",
+                        handler: this.addDocument.bind(this, tree, record, "link")
+                    }));
+
+                    menu.add(new Ext.menu.Item({
                         text: t('add_email'),
                         iconCls: "pimcore_icon_email pimcore_icon_overlay_add",
                         menu: documentMenu.email,
@@ -434,13 +440,6 @@ pimcore.document.tree = Class.create({
                         menu: documentMenu.newsletter,
                         hideOnClick: false
                     }));
-
-                    menu.add(new Ext.menu.Item({
-                        text: t('add_link'),
-                        iconCls: "pimcore_icon_link pimcore_icon_overlay_add",
-                        handler: this.addDocument.bind(this, tree, record, "link")
-                    }));
-
                 }
 
                 menu.add(new Ext.menu.Item({
