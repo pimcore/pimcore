610b69e45e3550feaf95f0c53c062072116c2463
pimcore-team <team@pimcore.com>
2016-10-05 14:55:05 +0200

    build 3980

--- pimcore/models/Asset/Image/Thumbnail/Processor.php
+++ pimcore/models/Asset/Image/Thumbnail/Processor.php
@@ -130,7 +130,7 @@ class Processor
             } elseif ($format == "svg") {
                 return self::returnPath($fileSystemPath, $returnAbsolutePath);
             }
-        } else if ($format == "tiff") {
+        } elseif ($format == "tiff") {
             if (\Pimcore\Tool::isFrontentRequestByAdmin()) {
                 // return a webformat in admin -> tiff cannot be displayed in browser
                 $format = "png";
