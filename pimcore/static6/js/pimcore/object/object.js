9bd06ccffdb1defde355562b83c125fd46bd647e
mrudnicki <maciej.rudnicki@pimcore.com>
2016-10-07 14:00:44 +0200

    fixes #903 user with only "save" action allowed: strange effet

--- pimcore/static6/js/pimcore/object/object.js
+++ pimcore/static6/js/pimcore/object/object.js
@@ -315,7 +315,7 @@ pimcore.object.object = Class.create(pimcore.object.abstract, {
                 text: t('save'),
                 iconCls: "pimcore_icon_save",
                 scale: "medium",
-                handler: this.unpublish.bind(this),
+                handler: this.save.bind(this),
                 menu:[{
                     text: t('save_close'),
                     iconCls: "pimcore_icon_save",
