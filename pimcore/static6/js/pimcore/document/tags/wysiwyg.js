7a2b04a3394842c0cd5945fb9df7fa0bd6ffeb8a
Bernhard Rusch <bernhard.rusch@elements.at>
2016-10-10 07:53:22 +0200

    CKEditor: removed IE hack, doesn't seem to be necessary anymore

--- pimcore/static6/js/pimcore/document/tags/wysiwyg.js
+++ pimcore/static6/js/pimcore/document/tags/wysiwyg.js
@@ -290,13 +290,3 @@ pimcore.document.tags.wysiwyg = Class.create(pimcore.document.tag, {
 });
 
 CKEDITOR.disableAutoInline = true;
-
-// IE Hack see: http://dev.ckeditor.com/ticket/9958
-// problem is that every button in a CKEDITOR window fires the onbeforeunload event
-CKEDITOR.on('instanceReady', function (event) {
-    event.editor.on('dialogShow', function (dialogShowEvent) {
-        if (CKEDITOR.env.ie) {
-            $(dialogShowEvent.data._.element.$).find('a[href*="void(0)"]').removeAttr('href');
-        }
-    });
-});
--- pimcore/static6/js/pimcore/object/tags/wysiwyg.js
+++ pimcore/static6/js/pimcore/object/tags/wysiwyg.js
@@ -319,13 +319,3 @@ pimcore.object.tags.wysiwyg = Class.create(pimcore.object.tags.abstract, {
 });
 
 CKEDITOR.disableAutoInline = true;
-
-// IE Hack see: http://dev.ckeditor.com/ticket/9958
-// problem is that every button in a CKEDITOR window fires the onbeforeunload event
-CKEDITOR.on('instanceReady', function (event) {
-    event.editor.on('dialogShow', function (dialogShowEvent) {
-        if (CKEDITOR.env.ie) {
-            $(dialogShowEvent.data._.element.$).find('a[href*="void(0)"]').removeAttr('href');
-        }
-    });
-});
