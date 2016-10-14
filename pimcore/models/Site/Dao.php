a53df14c96fa8da9ffbe2bb23f70dc8e862aee2c
Bernhard Rusch <bernhard.rusch@elements.at>
2016-10-14 13:55:48 +0200

    Sites: Additional domains with hyphens causes warnings - fixes #943

--- pimcore/models/Site/Dao.php
+++ pimcore/models/Site/Dao.php
@@ -69,6 +69,7 @@ class Dao extends Model\Dao\AbstractDao
                     if (is_array($siteDomains) && count($siteDomains) > 0) {
                         foreach ($siteDomains as $siteDomain) {
                             if (strpos($siteDomain, "*") !==  false) {
+                                $siteDomain = str_replace(".*", "*", $siteDomain); // backward compatibility
                                 $wildcardDomains[$siteDomain] = $site["id"];
                             }
                         }
@@ -77,6 +78,8 @@ class Dao extends Model\Dao\AbstractDao
             }
 
             foreach ($wildcardDomains as $wildcardDomain => $siteId) {
+                $wildcardDomain = preg_quote($wildcardDomain, "#");
+                $wildcardDomain = str_replace("\\*", ".*", $wildcardDomain);
                 if (preg_match("#^" . $wildcardDomain . "$#", $domain)) {
                     $data = $this->db->fetchRow("SELECT * FROM sites WHERE id = ?", [$siteId]);
                 }
@@ -144,7 +147,7 @@ class Dao extends Model\Dao\AbstractDao
         }
 
         $this->db->update("sites", $data, $this->db->quoteInto("id = ?", $this->model->getId()));
-        
+
         $this->model->clearDependentCache();
     }
 
@@ -156,7 +159,7 @@ class Dao extends Model\Dao\AbstractDao
     public function delete()
     {
         $this->db->delete("sites", $this->db->quoteInto("id = ?", $this->model->getId()));
-        
+
         $this->model->clearDependentCache();
     }
 }
--- pimcore/static6/js/pimcore/document/tree.js
+++ pimcore/static6/js/pimcore/document/tree.js
@@ -1011,7 +1011,7 @@ pimcore.document.tree = Class.create({
                     name: "domains",
                     height: 150,
                     style: "word-wrap: normal;",
-                    fieldLabel: t("additional_domains") + "<br /><br />RegExp are supported. eg. .*example.com",
+                    fieldLabel: t("additional_domains") + "<br /><br />Wildcards are supported. eg. *example.com",
                     value: data.domains.join("\n")
                 }, {
                     xtype: "textfield",
