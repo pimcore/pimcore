ed173dbea38e5f0737c3df5abcbe6659a00799ba
pimcore-team <team@pimcore.com>
2016-10-14 16:26:05 +0200

    build 3990

--- pimcore/lib/Pimcore/Image/Adapter/ImageMagick.php
+++ pimcore/lib/Pimcore/Image/Adapter/ImageMagick.php
@@ -652,10 +652,11 @@ class ImageMagick extends Adapter
      */
     public function getConvertScriptPath()
     {
-        if(null === $this->convertScriptPath) {
+        if (null === $this->convertScriptPath) {
             //in some cases get_proces_status in \Symfony\Component\Process\Process::updateStatus returns 1, than the default command path is set to convert
             $this->convertScriptPath = Console::getExecutable('convert') ?: 'convert';
         }
+
         return $this->convertScriptPath;
     }
 
@@ -679,7 +680,7 @@ class ImageMagick extends Adapter
      */
     public function getCompositeScriptPath()
     {
-        if(null === $this->compositeScriptPath) {
+        if (null === $this->compositeScriptPath) {
             //in some cases get_proces_status in \Symfony\Component\Process\Process::updateStatus returns 1, than the default command path is set to composite
             $this->compositeScriptPath = Console::getExecutable('composite') ?: 'composite';
         }
--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -25,7 +25,7 @@ class Version
     /**
      * @var int
      */
-    public static $revision = 3989;
+    public static $revision = 3990;
 
 
     /**
