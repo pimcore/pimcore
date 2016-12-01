f75abbdedcbc9f51d250d2f2200e258e59c44d94
Mathias Geat <mathias.geat@elements.at>
2016-12-01 16:58:56 +0100

    Add an AppBundle for testing, defining a controller with route annotations

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -2,4 +2,12 @@
 
 class AppKernel extends \Pimcore\Kernel
 {
+    public function registerBundles()
+    {
+        $bundles = array_merge(parent::registerBundles(), [
+            new \AppBundle\AppBundle()
+        ]);
+
+        return $bundles;
+    }
 }
---
+++ src/AppBundle/Controller/DefaultController.php
@@ -0,0 +1,39 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Psr\Http\Message\ServerRequestInterface;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Zend\Diactoros\Response\JsonResponse;
+
+class DefaultController extends Controller
+{
+    /**
+     * @Route("/", name="homepage")
+     */
+    public function indexAction(Request $request)
+    {
+        return new Response('foobar');
+    }
+
+    /**
+     * @Route("/foobar", name="test")
+     */
+    public function foobarAction(Request $request)
+    {
+        return new Response('test it');
+    }
+
+    /**
+     * @Route("/p-s-r-7", name="PSR-7")
+     */
+    public function psr7Action(ServerRequestInterface $request)
+    {
+        return new JsonResponse([
+            'psr-7' => true
+        ]);
+    }
+}
