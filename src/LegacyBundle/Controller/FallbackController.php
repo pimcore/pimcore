9a79487d31107a7a439256949d00b700aa17bfbc
Mathias Geat <mathias.geat@elements.at>
2016-12-05 17:47:20 +0100

    Use ZF1/PSR bridge to build a response in legacy controller

--- src/LegacyBundle/Controller/FallbackController.php
+++ src/LegacyBundle/Controller/FallbackController.php
@@ -2,15 +2,25 @@
 
 namespace LegacyBundle\Controller;
 
-use Symfony\Component\HttpFoundation\Response;
+use maff\Zend1MvcPsrMessageBridge\Factory\DiactorosFactory;
+use maff\Zend1MvcPsrMessageBridge\Factory\ZendMessageFactory;
+use Psr\Http\Message\ServerRequestInterface;
+use Zend\Diactoros\Response\JsonResponse;
 
 class FallbackController
 {
-    public function fallbackAction()
+    public function fallbackAction(ServerRequestInterface $psrRequest)
     {
-        $response = \Pimcore::run(true);
+        $diactorosFactory = new DiactorosFactory();
+        $zendFactory      = new ZendMessageFactory();
 
-        // TODO transform Zend_Response to PSR-7
-        return new Response($response->getBody());
+        // TODO not implemented yet
+        // $zendRequest = $zendFactory->createRequest($psrRequest);
+        $zendRequest = new \Zend_Controller_Request_Http();
+
+        $zendResponse = \Pimcore::run(true, $zendRequest);
+        $psrResponse  = $diactorosFactory->createResponse($zendResponse);
+
+        return $psrResponse;
     }
 }
