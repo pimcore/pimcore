deb578308b1c12cc5183d88bd2400e9a28d616b0
cfasching <christian.fasching@elements.at>
2016-12-09 08:45:58 +0100

    empty testcontroller in demo

--- website_demo/controllers/TestController.php
+++ website_demo/controllers/TestController.php
@@ -11,18 +11,4 @@ class TestController extends Action
     public function testAction()
     {
     }
-
-    public function thumbnailAction()
-    {
-        $a = Asset::getById(22);
-        $a->clearThumbnails(true);
-        $t = $a->getThumbnail("content");
-
-        header("Content-Type: image/jpeg"); while (@ob_end_flush()) ;
-        flush();
-
-
-        readfile($t->getFileSystemPath());
-        exit;
-    }
 }
