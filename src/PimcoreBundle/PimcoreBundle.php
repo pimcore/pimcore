d97560367de298c15098dc56ff8dd570a8af969c
Mathias Geat <mathias.geat@elements.at>
2017-01-02 14:10:00 +0100

    Remove support for wildcard helper in PHP templating - just use the zend helper to access ZF view helpers

--- src/PimcoreBundle/PimcoreBundle.php
+++ src/PimcoreBundle/PimcoreBundle.php
@@ -22,22 +22,4 @@ class PimcoreBundle extends Bundle
     {
         return new PimcoreExtension();
     }
-
-    /**
-     * Builds the bundle.
-     *
-     * It is only ever called once when the cache is empty.
-     *
-     * This method can be overridden to register compilation passes,
-     * other extensions, ...
-     *
-     * @param ContainerBuilder $container A ContainerBuilder instance
-     */
-    public function build(ContainerBuilder $container)
-    {
-        parent::build($container);
-
-        $container->addCompilerPass(new TemplatingNameResolversCompilerPass());
-        $container->addCompilerPass(new OverrideTemplatingPhpEngineCompilerPass());
-    }
 }
