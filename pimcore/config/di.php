d0b50aa87520c98c3df133d699d1915f9ec5bf1c
Mathias Geat <mathias.geat@elements.at>
2017-01-12 12:35:55 +0100

    [CACHE] Validate if the redis DSN is set if the redis adapter is used and add comments on the single cache services

--- pimcore/config/di.php
+++ pimcore/config/di.php
@@ -32,12 +32,27 @@ return [
         return $cacheLogger;
     },
 
-    'pimcore.cache.redis.connection.core' => function (ContainerInterface $container) {
-        $dsn = $container->get('pimcore.cache.redis.dsn');
+    // alias for the standard core cache adapter - this key will be injected into the core cache
+    // if you define your own cache service, make sure you set this alias to your service
+    'pimcore.cache.adapter.core' => DI\get('pimcore.cache.adapter.core.redis'),
 
-        $options = [];
+    // redis connection
+    'pimcore.cache.redis.connection.core' => function (ContainerInterface $container) {
+        $dsnKey     = 'pimcore.cache.redis.dsn';
         $optionsKey = 'pimcore.cache.redis.options';
 
+        if (!$container->has('pimcore.cache.redis.dsn')) {
+            throw new \DI\DependencyException(
+                sprintf(
+                    'Need a Redis DSN configured as parameter "%s" to use the core redis adapter. Please update your DI configuration.',
+                    $dsnKey
+                )
+            );
+        }
+
+        $dsn = $container->get($dsnKey);
+
+        $options = [];
         if ($container->has($optionsKey)) {
             $options = $container->get($optionsKey);
         }
@@ -45,6 +60,7 @@ return [
         return RedisAdapter::createConnection($dsn, $options);
     },
 
+    // redis cache adapter
     'pimcore.cache.adapter.core.redis' => DI\object(RedisAdapter::class)
         ->constructor(
             DI\get('pimcore.cache.redis.connection.core'),
@@ -53,11 +69,11 @@ return [
         )
         ->method('setLogger', DI\get('pimcore.logger.cache')),
 
-    // alias for the standard core cache adapter
-    'pimcore.cache.adapter.core' => DI\get('pimcore.cache.adapter.core.redis'),
 
+    // item factory creates cache items
     'pimcore.cache.item_factory' => DI\object(CacheItemFactory::class),
 
+    // write lock handles locking between processes
     'pimcore.cache.write_lock' => DI\object(WriteLock::class)
         ->constructor(
             DI\get('pimcore.cache.adapter.core'),
@@ -65,6 +81,7 @@ return [
         )
         ->method('setLogger', DI\get('pimcore.logger.cache')),
 
+    // the actual core handler which is used from Pimcore\Cache
     'pimcore.cache.handler.core' => DI\object(CoreHandler::class)
         ->constructor(
             DI\get('pimcore.cache.adapter.core'),
