c210e0f2d03a77965a63685779a441313bc9f5a8
Mathias Geat <mathias.geat@elements.at>
2017-01-20 15:52:52 +0100

    [DEMO] Start website demo PoC

--- src/AppBundle/Controller/ContentController.php
+++ src/AppBundle/Controller/ContentController.php
@@ -2,6 +2,8 @@
 
 namespace AppBundle\Controller;
 
+use Pimcore\Model\Document;
+use PimcoreBundle\View\ZendViewHelperBridge;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Symfony\Component\HttpFoundation\Request;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
@@ -28,6 +30,35 @@ class ContentController extends Controller
     public function twigAction(Request $request)
     {
         return $this->resolveContent($request);
+    }
+
+    /**
+     * @Template("AppBundle:Content:full-content.html.twig")
+     *
+     * @param Request $request
+     * @return array
+     */
+    public function fullContentAction(Request $request)
+    {
+        $vars = $this->resolveContent($request);
+
+        /** @var Document $document */
+        $document = $vars['document'];
+
+        $mainNavStartNode = $document->getProperty("mainNavStartNode");
+        if (!$mainNavStartNode) {
+            $mainNavStartNode = Document::getById(1);
+        }
+
+        /** @var ZendViewHelperBridge $bridge */
+        $bridge = $this->container->get('pimcore.view.zend_view_helper_bridge');
+
+        $mainNavigation = $bridge->execute('pimcoreNavigation', $document, $mainNavStartNode);
+
+        $vars['mainNavigation']   = $mainNavigation;
+        $vars['mainNavStartNode'] = $mainNavStartNode;
+
+        return $vars;
     }
 
     /**
