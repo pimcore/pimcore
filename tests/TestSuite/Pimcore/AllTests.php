65472611067eb13a6a825a172dd0a58f3b80436e
Mathias Geat <mathias.geat@elements.at>
2017-01-30 12:19:21 +0100

    [CI] Resolve pimcore unit test suite tests dynamically

--- tests/TestSuite/Pimcore/AllTests.php
+++ tests/TestSuite/Pimcore/AllTests.php
@@ -1,17 +1,38 @@
 <?php
 //require_once 'PHPUnit/Framework.php';
 
+use Symfony\Component\Finder\Finder;
+
 class TestSuite_Pimcore_AllTests extends Test_SuiteBase
 {
     public static function suite()
     {
         $suite = new static();
 
-        $tests = [
-            \TestSuite\Pimcore\MailTest::class,
-            \TestSuite\Pimcore\Cache\Core\ArrayCoreHandlerTest::class,
-            \TestSuite\Pimcore\Cache\Core\PdoMysqlCoreHandlerTest::class
-        ];
+        $tests = [];
+
+        $finder = new Finder();
+        $finder
+            ->files()
+            ->in(__DIR__)
+            ->name('*Test.php');
+
+        foreach ($finder as $testFile) {
+            $relativePath = str_replace(__DIR__, '', $testFile->getRealPath());
+            $relativePath = preg_replace('/^\//', '', $relativePath);
+
+            $className = str_replace('/', '\\', $relativePath);
+            $className = preg_replace('/\.php$/', '', $className);
+            $className = '\\TestSuite\\Pimcore\\' . $className;
+
+            if (class_exists($className)) {
+                $reflector = new ReflectionClass($className);
+
+                if ($reflector->isInstantiable() && $reflector->isSubclassOf(\PHPUnit_Framework_TestCase::class)) {
+                    $tests[] = $className;
+                }
+            }
+        }
 
         foreach ($tests as $test) {
             print("    - " . $test . "\n");
