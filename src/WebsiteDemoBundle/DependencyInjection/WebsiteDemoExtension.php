a6d3bc438956825b2ca2a2d260c024d0f06a3d52
Mathias Geat <mathias.geat@elements.at>
2017-02-01 09:53:34 +0100

    [DEMO] Add a dedicated language switcher service to WebsiteDemoBundle

--- src/WebsiteDemoBundle/DependencyInjection/WebsiteDemoExtension.php
+++ src/WebsiteDemoBundle/DependencyInjection/WebsiteDemoExtension.php
@@ -2,20 +2,23 @@
 
 namespace WebsiteDemoBundle\DependencyInjection;
 
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
 use Symfony\Component\DependencyInjection\Extension\Extension;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class WebsiteDemoExtension extends Extension
 {
     /**
-     * Loads a specific configuration.
-     *
-     * @param array $configs An array of configuration values
-     * @param ContainerBuilder $container A ContainerBuilder instance
-     *
-     * @throws \InvalidArgumentException When provided tag is not defined in this extension
+     * {@inheritdoc}
      */
     public function load(array $configs, ContainerBuilder $container)
     {
+        $loader = new YamlFileLoader(
+            $container,
+            new FileLocator(__DIR__ . '/../Resources/config')
+        );
+
+        $loader->load('services.yml');
     }
 }
