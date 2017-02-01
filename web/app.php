635ee4d0d97909f0c7e5a44c7aac2683604a10df
Mathias Geat <mathias.geat@elements.at>
2017-02-01 11:34:50 +0100

    [STRUCTURE] Use our symfony-style index.php instead of the standard edition app.php

--- web/app.php
+++ web/app.php
@@ -1,18 +1,40 @@
 <?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) 2009-2016 pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
 
 use Symfony\Component\HttpFoundation\Request;
 
-/** @var \Composer\Autoload\ClassLoader $loader */
-$loader = require __DIR__.'/../app/autoload.php';
-include_once __DIR__.'/../var/bootstrap.php.cache';
+/** @var \Pimcore\Kernel $kernel */
+$kernel = require_once __DIR__ . '/pimcore/config/startup.php';
 
-$kernel = new AppKernel('prod', false);
-$kernel->loadClassCache();
-//$kernel = new AppCache($kernel);
+if (defined('PIMCORE_SYMFONY_MODE') && PIMCORE_SYMFONY_MODE) {
+    $request  = Request::createFromGlobals();
+    $response = $kernel->handle($request);
+    $response->send();
 
-// When using the HttpCache, you need to call the method in your front controller instead of relying on the configuration parameter
-//Request::enableHttpMethodParameterOverride();
-$request = Request::createFromGlobals();
-$response = $kernel->handle($request);
-$response->send();
-$kernel->terminate($request, $response);
+    $kernel->terminate($request, $response);
+} else {
+    try {
+        // initialize environment
+        $kernel->boot();
+
+        Pimcore::run();
+    } catch (Exception $e) {
+        // handle exceptions, log to file
+        if (class_exists("Pimcore\\Logger")) {
+            \Pimcore\Logger::emerg($e);
+        }
+
+        throw $e;
+    }
+}
