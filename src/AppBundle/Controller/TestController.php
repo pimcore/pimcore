3cf2ac230d23cd43b3bd434c48861c93d7f24a10
Bernhard Rusch <bernhard.rusch@elements.at>
2017-02-02 13:45:50 +0100

    added legacy class name aliasing service in Legacy Bundle

---
+++ src/AppBundle/Controller/TestController.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace AppBundle\Controller;
+
+use Pimcore\Model\Asset;
+use Pimcore\Model\Document;
+use PimcoreBundle\Controller\DocumentAwareInterface;
+use PimcoreBundle\Controller\Traits\DocumentAwareTrait;
+use PimcoreBundle\View\ZendViewHelperBridge;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Sensio\Bundle\FrameworkExtraBundle\Configuration\Template;
+use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Request;
+
+class TestController extends Controller implements DocumentAwareInterface
+{
+    use DocumentAwareTrait;
+
+    /**
+     * @Route("/test/test")
+     * @param Request $request
+     * @return array
+     */
+    public function testAction(Request $request)
+    {
+
+
+
+        exit;
+    }
+}
--- src/PimcoreLegacyBundle/PimcoreLegacyBundle.php
+++ src/PimcoreLegacyBundle/PimcoreLegacyBundle.php
@@ -3,7 +3,13 @@
 namespace PimcoreLegacyBundle;
 
 use Symfony\Component\HttpKernel\Bundle\Bundle;
+use PimcoreLegacyBundle\ClassLoader\LegacyClassLoader;
 
 class PimcoreLegacyBundle extends Bundle
 {
+    public function boot()
+    {
+        $loader = new LegacyClassLoader();
+        $loader->register();
+    }
 }
