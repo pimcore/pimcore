67bf8c8caa8c91c4fbefcdd5ae710b51bed0d757
Bernhard Rusch <bernhard.rusch@elements.at>
2017-02-08 15:58:07 +0100

    proper alias also for IDEs for Pimcore\Model\Object\AbstractObject <=> Pimcore\Model\Object

--- pimcore/models/Object.php
+++ pimcore/models/Object.php
@@ -14,5 +14,19 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
+namespace {
 // this is just an alias ;-)
-class_alias("Pimcore\\Model\\Object\\AbstractObject", "Pimcore\\Model\\Object");
+    class_alias("Pimcore\\Model\\Object\\AbstractObject", "Pimcore\\Model\\Object");
+}
+
+
+// the following is for IDEs to support auto-complete
+namespace Pimcore\Model {
+    if(!\Pimcore\Tool::classExists("Pimcore\\Model\\Object")) {
+
+        class Object extends \Pimcore\Model\Object\AbstractObject
+        {
+
+        }
+    }
+}
