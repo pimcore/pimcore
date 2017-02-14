c0145ba1e06411311b58a74f77795e8673883a54
Mathias Geat <mathias.geat@elements.at>
2017-02-14 11:37:04 +0100

    Remove TODO on global vars - we don't want to keep document and editmode as globals as they're request-specific

--- pimcore/lib/Pimcore/Bundle/PimcoreBundle/Templating/GlobalVariables.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreBundle/Templating/GlobalVariables.php
@@ -5,7 +5,6 @@ namespace Pimcore\Bundle\PimcoreBundle\Templating\GlobalVariables;
 use Symfony\Bundle\FrameworkBundle\Templating\GlobalVariables as BaseGlobalVariables;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
-// TODO add document and editmode here?
 class GlobalVariables extends BaseGlobalVariables
 {
     /**
