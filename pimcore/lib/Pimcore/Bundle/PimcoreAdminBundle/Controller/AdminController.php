d94d3ae9c1c5f36d478af3036d0eab27976b530c
Mathias Geat <mathias.geat@elements.at>
2017-02-21 16:36:08 +0100

    [ADMIN] Add protectCsrf to AdminController and use the same exception for CSRF protection and permissions

--- pimcore/lib/Pimcore/Bundle/PimcoreAdminBundle/Controller/AdminController.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreAdminBundle/Controller/AdminController.php
@@ -4,8 +4,11 @@ namespace Pimcore\Bundle\PimcoreAdminBundle\Controller;
 
 use Pimcore\Bundle\PimcoreAdminBundle\Security\User\User;
 use Pimcore\Logger;
+use Pimcore\Tool\Session;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
-use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
+use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 
 abstract class AdminController extends Controller implements AdminControllerInterface
 {
@@ -26,14 +29,37 @@ abstract class AdminController extends Controller implements AdminControllerInte
      * Check user permission
      *
      * @param $permission
-     * @throws UnauthorizedHttpException
+     *
+     * @throws AccessDeniedHttpException
      */
     protected function checkPermission($permission)
     {
         if (!$this->getUser() || !$this->getUser()->isAllowed($permission)) {
             $message = "Attempt to access " . $permission . ", but has no permission to do so.";
             Logger::err($message);
-            throw new UnauthorizedHttpException($message);
+
+            throw new AccessDeniedHttpException($message);
+        }
+    }
+
+    /**
+     * Check CSRF token
+     *
+     * @param Request $request
+     *
+     * @throws AccessDeniedHttpException
+     *      if CSRF token does not match
+     */
+    protected function protectCsrf(Request $request)
+    {
+        // TODO use isCsrfTokenValid() and the native CSRF token storage?
+
+        $csrfToken = Session::useSession(function (AttributeBagInterface $adminSession) {
+            return $adminSession->get('csrfToken');
+        });
+
+        if (!$csrfToken || $csrfToken !== $request->headers->get('x_pimcore_csrf_token')) {
+            throw new AccessDeniedHttpException('Detected CSRF Attack! Do not do evil things with pimcore ... ;-)');
         }
     }
 }
