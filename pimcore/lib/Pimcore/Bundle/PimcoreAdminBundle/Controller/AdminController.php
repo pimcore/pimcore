9c0dd2633048733ce8c975a8e12c0c014ab42a3f
Mathias Geat <mathias.geat@elements.at>
2017-02-21 12:11:49 +0100

    [ADMIN] Add checkPermission() to admin base controller

--- pimcore/lib/Pimcore/Bundle/PimcoreAdminBundle/Controller/AdminController.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreAdminBundle/Controller/AdminController.php
@@ -3,7 +3,9 @@
 namespace Pimcore\Bundle\PimcoreAdminBundle\Controller;
 
 use Pimcore\Bundle\PimcoreAdminBundle\Security\User\User;
+use Pimcore\Logger;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpKernel\Exception\UnauthorizedHttpException;
 
 abstract class AdminController extends Controller
 {
@@ -17,6 +19,21 @@ abstract class AdminController extends Controller
         $user = parent::getUser();
         if ($user && $user instanceof User) {
             return $user->getUser();
+        }
+    }
+
+    /**
+     * Check user permission
+     *
+     * @param $permission
+     * @throws UnauthorizedHttpException
+     */
+    protected function checkPermission($permission)
+    {
+        if (!$this->getUser() || !$this->getUser()->isAllowed($permission)) {
+            $message = "Attempt to access " . $permission . ", but has no permission to do so.";
+            Logger::err($message);
+            throw new UnauthorizedHttpException($message);
         }
     }
 }
