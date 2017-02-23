9963d19028e6926dfb73d344728dcef8ac1a8508
Christian Fasching <christian.fasching@elements.at>
2017-02-23 17:08:12 +0100

    [ViewHelper] added Translate & TranslateAdmin

---
+++ pimcore/lib/Pimcore/Bundle/PimcoreBundle/Templating/Helper/Translate.php
@@ -0,0 +1,54 @@
+<?php
+
+namespace Pimcore\Bundle\PimcoreBundle\Templating\Helper;
+
+use Pimcore\Bundle\PimcoreBundle\Component\Translation\Translator;
+use Symfony\Component\Templating\Helper\Helper;
+
+class Translate extends Helper
+{
+
+    /**
+     * @var Translator
+     */
+    protected $translator;
+
+    /**
+     * @var string
+     */
+    protected $domain;
+
+    public function __construct(Translator $translator)
+    {
+        $this->translator = $translator;
+    }
+
+
+    /**
+     * Returns the canonical name of this helper.
+     *
+     * @return string The canonical name
+     */
+    public function getName()
+    {
+        return "translate";
+    }
+
+    /**
+     * @param $key
+     * @param array $parameters
+     * @return string
+     */
+    public function __invoke($key, $parameters = [])
+    {
+        return $this->translator->trans($key, $parameters, $this->domain);
+    }
+
+    /**
+     * @param string $domain
+     */
+    public function setDomain($domain) {
+        $this->domain = $domain;
+    }
+
+}
