ca90da8a48b52d523550cb1402fb2000ce403eb4
Christian Fasching <christian.fasching@elements.at>
2017-02-27 10:11:58 +0100

    [Demo] fixed sitemap with layout

--- src/WebsiteDemoBundle/Resources/views/Advanced/sitemap.html.php
+++ src/WebsiteDemoBundle/Resources/views/Advanced/sitemap.html.php
@@ -1,12 +1,7 @@
-
-
 <?php
 $this->extend('WebsiteDemoBundle::layout.html.php')
 ?>
 
 <ul>
-
     <?= $this->action("sitemapPartial", "advanced", "websitedemo", ["doc" => $this->doc]) ?>
-
-
 </ul>
