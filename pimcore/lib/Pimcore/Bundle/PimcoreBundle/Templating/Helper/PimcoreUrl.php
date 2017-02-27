6d35f27f43a81be123453a81c4eda4fa5845dc2b
Christian Fasching <christian.fasching@elements.at>
2017-02-27 16:18:45 +0100

    [ViewHelper] fixes PimcoreUrl and make it zend-independent

--- pimcore/lib/Pimcore/Bundle/PimcoreBundle/Templating/Helper/PimcoreUrl.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreBundle/Templating/Helper/PimcoreUrl.php
@@ -14,10 +14,31 @@
 
 namespace Pimcore\Bundle\PimcoreBundle\Templating\Helper;
 
+use Pimcore\Http\RequestHelper;
 use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
+use Symfony\Component\Templating\Helper\Helper;
 
-class PimcoreUrl extends AbstractrUrlHelper
+class PimcoreUrl extends Helper
 {
+    /**
+     * @var UrlGeneratorInterface
+     */
+    protected $generator;
+
+    /**
+     * @var RequestHelper
+     */
+    protected $requestHelper;
+
+    /**
+     * @param UrlGeneratorInterface $generator
+     */
+    public function __construct(UrlGeneratorInterface $generator, RequestHelper $requestHelper)
+    {
+        $this->generator     = $generator;
+        $this->requestHelper = $requestHelper;
+    }
+
     /**
      * @param array $urlOptions
      * @param null $name
@@ -28,13 +49,51 @@ class PimcoreUrl extends AbstractrUrlHelper
      */
     public function __invoke(array $urlOptions = [], $name = null, $reset = false, $encode = true, $relative = false)
     {
-        $request = $this->requestHelper->getCurrentRequest();
 
         // merge all parameters from request to parameters
         if (!$reset) {
-            $urlOptions = array_replace($urlOptions, $request->request->all());
+            $urlOptions = array_replace($_GET, $urlOptions);
         }
 
         return $this->generateUrl($name, $urlOptions, $relative ? UrlGeneratorInterface::RELATIVE_PATH : UrlGeneratorInterface::ABSOLUTE_PATH);
+    }
+
+    /**
+     * Generate URL with support to only pass parameters ZF1 style (defaults to current route).
+     *
+     * @param null $name
+     * @param array $parameters
+     * @param int $referenceType
+     * @return string
+     */
+    protected function generateUrl($name = null, $parameters = [], $referenceType = UrlGeneratorInterface::ABSOLUTE_PATH)
+    {
+        // if name is an array, treat it as parameters
+        if (is_array($name)) {
+            if (is_array($parameters)) {
+                $parameters = array_merge($name, $parameters);
+            } else {
+                $parameters = $name;
+            }
+
+            $name = null;
+        }
+
+        // get name from current route
+        if ($name === null) {
+            $name = $this->requestHelper->getCurrentRequest()->attributes->get('_route');
+        }
+
+        return $this->generator->generate($name, $parameters, $referenceType);
+    }
+
+    /**
+     * Returns the canonical name of this helper.
+     *
+     * @return string The canonical name
+     */
+    public function getName()
+    {
+        return "pimcoreUrl";
     }
 }
