a8f6a073a8bea6f74bc09563cf6bd891b6fb476e
Mathias Geat <mathias.geat@elements.at>
2017-02-28 09:25:01 +0100

    [API] Ignore apiDocJs annotations as we use those on API controllers

--- app/autoload.php
+++ app/autoload.php
@@ -1,5 +1,6 @@
 <?php
 
+use Doctrine\Common\Annotations\AnnotationReader;
 use Doctrine\Common\Annotations\AnnotationRegistry;
 use Composer\Autoload\ClassLoader;
 
@@ -7,5 +8,34 @@ use Composer\Autoload\ClassLoader;
 $loader = require __DIR__.'/../vendor/autoload.php';
 
 AnnotationRegistry::registerLoader([$loader, 'loadClass']);
+
+// ignore apiDoc params (see http://apidocjs.com/) as we use apiDoc in webservice
+// controllers. TODO: use @ApiDoc (NelmioApiDocBundle) instead?
+$apiDocAnnotations = [
+    'api',
+    'apiDefine',
+    'apiDeprecated',
+    'apiDescription',
+    'apiError',
+    'apiErrorExample',
+    'apiExample',
+    'apiGroup',
+    'apiHeader',
+    'apiHeaderExample',
+    'apiIgnore',
+    'apiName',
+    'apiParam',
+    'apiParamExample',
+    'apiPermission',
+    'apiSampleRequest',
+    'apiSuccess',
+    'apiSuccessExample',
+    'apiUse',
+    'apiVersion',
+];
+
+foreach ($apiDocAnnotations as $apiDocAnnotation) {
+    AnnotationReader::addGlobalIgnoredName($apiDocAnnotation);
+}
 
 return $loader;
