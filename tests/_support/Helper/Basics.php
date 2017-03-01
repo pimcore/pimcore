bc7270973af470ac25fadd79428eccc1b2b8a76a
Mathias Geat <mathias.geat@elements.at>
2017-03-01 16:24:35 +0100

    [TESTS] Bootstrap codeception

---
+++ tests/_support/Helper/Basics.php
@@ -0,0 +1,9 @@
+<?php
+namespace Pimcore\Tests\Helper;
+
+// here you can define custom actions
+// all public methods declared in helper class will be available in $I
+
+class Basics extends \Codeception\Module
+{
+}
