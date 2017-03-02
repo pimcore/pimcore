pimcore/pimcore

commit 7df3c62e863bf33c9f9eb257d656f6523d5652a6

Implement JsonSerializable in AbstractModel

pimcore/lib/Pimcore/Model/AbstractModel.php
@@ -27,7 +27,7 @@ use Pimcore\Logger;
  * @method array getValidTableColumns(string $table, bool $cache)
  * @method void resetValidTableColumnsCache(string $table)
  */
-abstract class AbstractModel
+abstract class AbstractModel implements \JsonSerializable
 {
 
     /**
@@ -243,6 +243,19 @@ abstract class AbstractModel
         }
 
         return $finalVars;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public function jsonSerialize()
+    {
+        $data = [];
+        foreach ($this->__sleep() as $var) {
+            $data[$var] = $this->$var;
+        }
+
+        return $data;
     }
 
     /**

commit 536bbe8fb4d31058985f18f2dc9e1f3fbb03b80b

reverted: 7df3c62e863bf33c9f9eb257d656f6523d5652a6 caused many problems since controllers inject data on models before sending/serializing

pimcore/lib/Pimcore/Model/AbstractModel.php
@@ -27,7 +27,7 @@ use Pimcore\Logger;
  * @method array getValidTableColumns(string $table, bool $cache)
  * @method void resetValidTableColumnsCache(string $table)
  */
-abstract class AbstractModel implements \JsonSerializable
+abstract class AbstractModel
 {
 
     /**
@@ -243,19 +243,6 @@ abstract class AbstractModel implements \JsonSerializable
         }
 
         return $finalVars;
-    }
-
-    /**
-     * @inheritDoc
-     */
-    public function jsonSerialize()
-    {
-        $data = [];
-        foreach ($this->__sleep() as $var) {
-            $data[$var] = $this->$var;
-        }
-
-        return $data;
     }
 
     /**