4da894a4d60f4e3d01f0941f853fa0efdca848de
Bernhard Rusch <bernhard.rusch@elements.at>
2017-03-03 14:00:45 +0100

    Pimcore\Composer adoptions for v5

--- pimcore/lib/Pimcore/Composer.php
+++ pimcore/lib/Pimcore/Composer.php
@@ -32,20 +32,10 @@ class Composer
         // cleanup
         @unlink($rootPath . '/.travis.yml');
 
-        if (!is_dir($rootPath . '/plugins')) {
-            rename($rootPath . '/plugins_example', $rootPath . '/plugins');
-        }
-
-        if (!is_dir($rootPath . '/website')) {
-            rename($rootPath . '/website_example', $rootPath . '/website');
-        }
-
         $filesystem = new Filesystem();
-        $filesystem->removeDirectory($rootPath . '/update');
-        $filesystem->removeDirectory($rootPath . '/build');
-        $filesystem->removeDirectory($rootPath . '/tests');
-        $filesystem->removeDirectory($rootPath . '/.svn');
-        $filesystem->removeDirectory($rootPath . '/.git');
+        if(is_dir($rootPath . '/update')) {
+            $filesystem->removeDirectory($rootPath . '/update');
+        }
     }
 
     /**
@@ -78,34 +68,35 @@ class Composer
 
         // strips all require_once out of the sources
         // see also: http://framework.zend.com/manual/1.10/en/performance.classloading.html#performance.classloading.striprequires.sed
-
         $zfPath = $rootPath . "/vendor/zendframework/zendframework1/library/Zend/";
 
-        $directory = new \RecursiveDirectoryIterator($zfPath);
-        $iterator = new \RecursiveIteratorIterator($directory);
-        $regex = new \RegexIterator($iterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH);
+        if(is_dir($zfPath)) {
+            $directory = new \RecursiveDirectoryIterator($zfPath);
+            $iterator = new \RecursiveIteratorIterator($directory);
+            $regex = new \RegexIterator($iterator, '/^.+\.php$/i', \RecursiveRegexIterator::GET_MATCH);
 
-        $excludePatterns = [
-            "/Loader/Autoloader.php$",
-            "/Loader/ClassMapAutoloader.php$",
-            "/Application.php$",
-        ];
+            $excludePatterns = [
+                "/Loader/Autoloader.php$",
+                "/Loader/ClassMapAutoloader.php$",
+                "/Application.php$",
+            ];
 
-        foreach ($regex as $file) {
-            $file = $file[0];
+            foreach ($regex as $file) {
+                $file = $file[0];
 
-            $excluded = false;
-            foreach ($excludePatterns as $pattern) {
-                if (preg_match("@".$pattern."@", $file)) {
-                    $excluded = true;
-                    break;
+                $excluded = false;
+                foreach ($excludePatterns as $pattern) {
+                    if (preg_match("@" . $pattern . "@", $file)) {
+                        $excluded = true;
+                        break;
+                    }
                 }
-            }
 
-            if (!$excluded) {
-                $content = file_get_contents($file);
-                $content = preg_replace("@([^/])(require_once)@", "$1//$2", $content);
-                file_put_contents($file, $content);
+                if (!$excluded) {
+                    $content = file_get_contents($file);
+                    $content = preg_replace("@([^/])(require_once)@", "$1//$2", $content);
+                    file_put_contents($file, $content);
+                }
             }
         }
     }
