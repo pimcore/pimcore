fa7eb98d65605461154e1d285f4ba977b685df03
weisswurstkanone <josef.aichhorn@elements.at>
2017-03-03 13:06:20 +0100

    fixed phpdoc

--- pimcore/models/Document.php
+++ pimcore/models/Document.php
@@ -219,7 +219,7 @@ class Document extends Element\AbstractElement
 
     /**
      * get possible types
-     * @return string
+     * @return array
      */
     public static function getTypes()
     {
