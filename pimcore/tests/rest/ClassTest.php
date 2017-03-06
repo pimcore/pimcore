090ce090a28e65e15aa66768a653d5caeffcb18b
Mathias Geat <mathias.geat@elements.at>
2017-03-06 13:34:55 +0100

    [TESTS] Update ClassTest for new test structure

--- pimcore/tests/rest/ClassTest.php
+++ pimcore/tests/rest/ClassTest.php
@@ -1,34 +1,49 @@
 <?php
-/**
- * Created by IntelliJ IDEA.
- * User: Michi
- * Date: 11.11.2010
- * Time: 10:35:07
- */
 
+namespace Pimcore\Tests\Rest;
 
-class TestSuite_Rest_ClassTest extends Test_BaseRest
+use Pimcore\Model\Object\ClassDefinition;
+use Pimcore\Tests\RestTester;
+use Pimcore\Tests\Test\RestTestCase;
+use Pimcore\Tests\Util\TestHelper;
+
+class ClassTest extends RestTestCase
 {
+    /**
+     * @var RestTester
+     */
+    protected $tester;
+
+    /**
+     * @var RestClient
+     */
+    protected $restClient;
+
     public function setUp()
     {
-        // every single rest test assumes a clean database
-        Test_Tool::cleanUp();
         parent::setUp();
+
+        // every single rest test assumes a clean database
+        TestHelper::cleanUp();
+
+        // authenticate as rest user
+        $this->tester->addApiKeyParam('rest');
+
+        // setup test rest client
+        $this->restClient = new RestClient($this->tester);
     }
-
-
 
     public function testGetClass()
     {
-        $this->printTestName();
-        $object = Test_Tool::createEmptyObject();
+        $object = TestHelper::createEmptyObject();
         $classId = $object->getClassId();
 
-        $this->assertEquals("unittest", Object_Class::getById($classId)->getName());
-        $restClass1 = self::getRestClient()->getClassById($classId);
+        $this->assertEquals("unittest", ClassDefinition::getById($classId)->getName());
+
+        $restClass1 = $this->restClient->getClassById($classId);
         $this->assertEquals("unittest", $restClass1->getName());
 
-        $restClass2 = self::getRestClient()->getObjectMetaById($object->getId());
+        $restClass2 = $this->restClient->getObjectMetaById($object->getId());
         $this->assertEquals("unittest", $restClass2->getName());
     }
 }
