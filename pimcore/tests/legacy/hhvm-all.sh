c68115671789d9a796744941dfc3a408e56ea3c2
Mathias Geat <mathias.geat@elements.at>
2017-03-07 16:54:01 +0100

    [TESTS] Remove legacy tests

--- pimcore/tests/legacy/hhvm-all.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p output
-
-hhvm phpunit.php --verbose --bootstrap bootstrap.php --log-json output/log.xml AllTests
-
--- pimcore/tests/legacy/singletest.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-mkdir -p output
-
-php phpunit.php --verbose --bootstrap bootstrap.php --log-json output/log.xml SingleTest
-
