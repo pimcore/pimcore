dd6bd09c4185f7babf80e2e4e788e1a794890154
Mathias Geat <mathias.geat@elements.at>
2017-03-30 10:36:01 +0200

    [CONSOLE] Update reset-password command to accept the user as argument and to use the question helper for hidden password prompt

--- pimcore/lib/Pimcore/Bundle/PimcoreBundle/Command/ResetPasswordCommand.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreBundle/Command/ResetPasswordCommand.php
@@ -15,10 +15,13 @@
 namespace Pimcore\Bundle\PimcoreBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
+use Pimcore\Tool\Authentication;
+use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
 use Pimcore\Model\User;
+use Symfony\Component\Console\Question\Question;
 
 class ResetPasswordCommand extends AbstractCommand
 {
@@ -28,9 +31,9 @@ class ResetPasswordCommand extends AbstractCommand
             ->setName('pimcore:user:reset-password')
             ->setAliases(['reset-password'])
             ->setDescription("Reset a user's password")
-            ->addOption(
-                'user', 'u',
-                InputOption::VALUE_REQUIRED,
+            ->addArgument(
+                'user',
+                InputArgument::REQUIRED,
                 "Username or ID of user"
             )
             ->addOption(
@@ -45,68 +48,48 @@ class ResetPasswordCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $user = $input->getOption("user");
+        $userArgument = $input->getArgument("user");
 
-        if (!$user) {
-            $this->writeError("No username/ID given");
-        }
+        $method = is_numeric($userArgument) ? 'getById' : 'getByName';
 
-        $method = is_numeric($user) ? 'getById' : 'getByName';
         /** @var $user User */
-        $user = User::$method($user);
+        $user = User::$method($userArgument);
 
         if (!$user) {
-            $this->writeError("User with name " . $user . " could not be found. Exiting");
+            $this->writeError("User with name/ID " . $userArgument . " could not be found. Exiting");
             exit;
         }
 
         if ($input->getOption("password")) {
             $plainPassword = $input->getOption("password");
         } else {
-            $plainPassword = false;
-            while (empty($plainPassword)) {
-                $plainPassword = $this->promtSilent();
-            }
+            $plainPassword = $this->askForPassword($input, $output);
         }
 
-        $password = \Pimcore\Tool\Authentication::getPasswordHash($user->getName(), $plainPassword);
+        $password = Authentication::getPasswordHash($user->getName(), $plainPassword);
         $user->setPassword($password);
         $user->save();
 
         $this->output->writeln("Password for user " . $user->getName() . " reset successfully.");
     }
 
-    /**
-     * @param string $prompt
-     * @return string|void
-     */
-    protected function promtSilent($prompt = "Enter new password:")
+    protected function askForPassword(InputInterface $input, OutputInterface $output)
     {
-        if (preg_match('/^win/i', PHP_OS)) {
-            $vbscript = sys_get_temp_dir() . 'prompt_password.vbs';
-            file_put_contents(
-                $vbscript, 'wscript.echo(InputBox("'
-                . addslashes($prompt)
-                . '", "", "password here"))');
-            $command = "cscript //nologo " . escapeshellarg($vbscript);
-            $password = rtrim(shell_exec($command));
-            unlink($vbscript);
+        $helper = $this->getHelper('question');
 
-            return $password;
-        } else {
-            $command = "/usr/bin/env bash -c 'echo OK'";
-            if (rtrim(shell_exec($command)) !== 'OK') {
-                trigger_error("Can't invoke bash");
-
-                return;
+        $question = new Question('Please enter the new password: ');
+        $question->setValidator(function ($value) {
+            if (empty(trim($value))) {
+                throw new \Exception('The password cannot be empty');
             }
-            $command = "/usr/bin/env bash -c 'read -s -p \""
-                . addslashes($prompt)
-                . "\" mypassword && echo \$mypassword'";
-            $password = rtrim(shell_exec($command));
-            echo "\n";
 
-            return $password;
-        }
+            return $value;
+        });
+
+        $question->setHidden(true);
+
+        $password = $helper->ask($input, $output, $question);
+
+        return $password;
     }
 }
