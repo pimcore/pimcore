cf0e4698450d4a0a3bd546ba3889ff9e5e484599
Christian Fasching <christian.fasching@elements.at>
2017-03-30 15:12:29 +0200

    [Docs] & [Ecommerce] - updated console commands

--- pimcore/lib/Pimcore/Bundle/PimcoreEcommerceFrameworkBundle/Console/Command/Voucher/CleanupReservationsCommand.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreEcommerceFrameworkBundle/Console/Command/Voucher/CleanupReservationsCommand.php
@@ -12,19 +12,19 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
-namespace Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Command\Voucher;
+namespace Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Console\Command\Voucher;
 
 use Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Factory;
 use Pimcore\Console\AbstractCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CleanupStatisticsCommand extends AbstractCommand
+class CleanupReservationsCommand extends AbstractCommand
 {
     protected function configure()
     {
-        $this->setName('ecommerce:voucher:cleanup-statistics');
-        $this->setDescription("House keeping for Voucher Usage Statistics - cleans up all old data.");
+        $this->setName('ecommerce:voucher:cleanup-reservations');
+        $this->setDescription("Cleans the token reservations due to sysConfig duration settings");
     }
 
     /**
@@ -34,7 +34,7 @@ class CleanupStatisticsCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->output->writeln('<comment>*</comment> Cleaning up <info>statistics</info>');
-        Factory::getInstance()->getVoucherService()->cleanUpStatistics();
+        $this->output->writeln('<comment>*</comment> Cleaning up <info>reservations</info>');
+        Factory::getInstance()->getVoucherService()->cleanUpReservations();
     }
 }
--- pimcore/lib/Pimcore/Bundle/PimcoreEcommerceFrameworkBundle/Console/Command/Voucher/CleanupStatisticsCommand.php
+++ pimcore/lib/Pimcore/Bundle/PimcoreEcommerceFrameworkBundle/Console/Command/Voucher/CleanupStatisticsCommand.php
@@ -12,19 +12,19 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
-namespace Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Command\Voucher;
+namespace Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Console\Command\Voucher;
 
 use Pimcore\Bundle\PimcoreEcommerceFrameworkBundle\Factory;
 use Pimcore\Console\AbstractCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 
-class CleanupReservationsCommand extends AbstractCommand
+class CleanupStatisticsCommand extends AbstractCommand
 {
     protected function configure()
     {
-        $this->setName('ecommerce:voucher:cleanup-reservations');
-        $this->setDescription("Cleans the token reservations due to sysConfig duration settings");
+        $this->setName('ecommerce:voucher:cleanup-statistics');
+        $this->setDescription("House keeping for Voucher Usage Statistics - cleans up all old data.");
     }
 
     /**
@@ -34,7 +34,7 @@ class CleanupReservationsCommand extends AbstractCommand
      */
     protected function execute(InputInterface $input, OutputInterface $output)
     {
-        $this->output->writeln('<comment>*</comment> Cleaning up <info>reservations</info>');
-        Factory::getInstance()->getVoucherService()->cleanUpReservations();
+        $this->output->writeln('<comment>*</comment> Cleaning up <info>statistics</info>');
+        Factory::getInstance()->getVoucherService()->cleanUpStatistics();
     }
 }
