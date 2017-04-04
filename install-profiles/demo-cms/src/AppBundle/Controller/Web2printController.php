7d3445d7ce3906f7b50cf907930a6b40ecd39abb
Christian Fasching <christian.fasching@elements.at>
2017-04-04 15:15:35 +0200

    fixed web2print controller

--- install-profiles/demo-cms/src/AppBundle/Controller/Web2printController.php
+++ install-profiles/demo-cms/src/AppBundle/Controller/Web2printController.php
@@ -12,8 +12,6 @@ class Web2printController extends AbstractController
 
     public function containerAction(Request $request)
     {
-        //TODO check, if this works all the time
-        $document = $request->get("document");
         $allChildren = $this->document->getAllChildren();
 
         $this->view->allChildren = $allChildren;
