1923d4404e2322fbf37481fd8c306d97cb4b3cae
Mathias Geat <mathias.geat@elements.at>
2017-04-05 16:53:26 +0200

    [ROUTING] Add missing routing functionalities to DynamicRouter (pretty URLs, SEO redirect, type checking)  #1374

--- pimcore/lib/Pimcore/Routing/DynamicRouteProvider.php
+++ pimcore/lib/Pimcore/Routing/DynamicRouteProvider.php
@@ -14,9 +14,12 @@
 
 namespace Pimcore\Routing;
 
+use Pimcore\Config;
+use Pimcore\Http\RequestHelper;
 use Pimcore\Model\Document;
 use Pimcore\Service\Document\NearestPathResolver;
 use Pimcore\Service\MvcConfigNormalizer;
+use Pimcore\Service\Request\SiteResolver;
 use Symfony\Cmf\Component\Routing\RouteProviderInterface;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\Routing\Exception\RouteNotFoundException;
@@ -25,9 +28,24 @@ use Symfony\Component\Routing\RouteCollection;
 class DynamicRouteProvider implements RouteProviderInterface
 {
     /**
+     * @var Document\Service
+     */
+    protected $documentService;
+
+    /**
+     * @var SiteResolver
+     */
+    protected $siteResolver;
+
+    /**
      * @var NearestPathResolver
      */
     protected $nearestPathResolver;
+
+    /**
+     * @var RequestHelper
+     */
+    protected $requestHelper;
 
     /**
      * @var MvcConfigNormalizer
@@ -35,13 +53,48 @@ class DynamicRouteProvider implements RouteProviderInterface
     protected $configNormalizer;
 
     /**
+     * @var array
+     */
+    protected $directRouteDocumentTypes = ['page', 'snippet', 'email', 'newsletter', 'printpage', 'printcontainer'];
+
+    /**
+     * @param Document\Service $documentService
+     * @param SiteResolver $siteResolver
      * @param NearestPathResolver $nearestPathResolver
+     * @param RequestHelper $requestHelper
      * @param MvcConfigNormalizer $configNormalizer
      */
-    public function __construct(NearestPathResolver $nearestPathResolver, MvcConfigNormalizer $configNormalizer)
+    public function __construct(
+        Document\Service $documentService,
+        SiteResolver $siteResolver,
+        NearestPathResolver $nearestPathResolver,
+        RequestHelper $requestHelper,
+        MvcConfigNormalizer $configNormalizer
+    )
     {
+        $this->documentService     = $documentService;
+        $this->siteResolver        = $siteResolver;
         $this->nearestPathResolver = $nearestPathResolver;
+        $this->requestHelper       = $requestHelper;
         $this->configNormalizer    = $configNormalizer;
+    }
+
+    /**
+     * @return array
+     */
+    public function getDirectRouteDocumentTypes()
+    {
+        return $this->directRouteDocumentTypes;
+    }
+
+    /**
+     * @param string $type
+     */
+    public function addDirectRouteDocumentType($type)
+    {
+        if (!in_array($type, $this->directRouteDocumentTypes)) {
+            $this->directRouteDocumentTypes[] = $type;
+        }
     }
 
     /**
@@ -50,34 +103,48 @@ class DynamicRouteProvider implements RouteProviderInterface
     public function getRouteCollectionForRequest(Request $request)
     {
         $collection = new RouteCollection();
-        $path       = urldecode($request->getPathInfo());
+        $path       = $originalPath = urldecode($request->getPathInfo());
 
-        // handled by SiteListener which runs before routing is started
-        if ($request->attributes->has('_site_path')) {
-            $path = $request->attributes->get('_site_path');
+        // site path handled by FrontendRoutingListener which runs before routing is started
+        if (null !== $sitePath = $this->siteResolver->getSitePath($request)) {
+            $path = $sitePath;
         }
 
-        $document = Document::getByPath($path);
-        if ($document) {
-            $document = $this->handleHardlink($document, $path);
-
-            if ($document && $document instanceof Document) {
-                if ($route = $this->buildRouteForDocument($document)) {
-                    $collection->add($route->getRouteKey(), $route);
-                }
-            }
-        }
+        $this->matchDocuments($collection, $request, $path, $originalPath);
 
         return $collection;
     }
 
     /**
-     * @param Document|mixed $document
+     * Add documents to route collection
+     *
+     * @param RouteCollection $collection
+     * @param Request $request
      * @param string $path
-     * @return Document
+     * @param string $originalPath
      */
-    protected function handleHardlink($document, $path)
+    protected function matchDocuments(RouteCollection $collection, Request $request, $path, $originalPath)
     {
+        $document = Document::getByPath($path);
+
+        // check for a pretty url inside a site
+        if (!$document && $this->siteResolver->isSiteRequest($request)) {
+            $site = $this->siteResolver->getSite($request);
+
+            $sitePrettyDocId = $this->documentService->getDao()->getDocumentIdByPrettyUrlInSite($site, $originalPath);
+            if ($sitePrettyDocId) {
+                if ($sitePrettyDoc = Document::getById($sitePrettyDocId)) {
+                    $document = $sitePrettyDoc;
+
+                    // TODO set pretty path via siteResolver?
+                    // undo the modification of the path by the site detection (prefixing with site root path)
+                    // this is not necessary when using pretty-urls and will cause problems when validating the
+                    // prettyUrl later (redirecting to the prettyUrl in the case the page was called by the real path)
+                    $path = $originalPath;
+                }
+            }
+        }
+
         // check for a parent hardlink with childs
         if (!$document instanceof Document) {
             $hardlinkedParentDocument = $this->nearestPathResolver->getNearestDocumentByPath($path, true);
@@ -88,52 +155,172 @@ class DynamicRouteProvider implements RouteProviderInterface
             }
         }
 
-        return $document;
+        if ($document && $document instanceof Document) {
+            if ($route = $this->buildRouteForDocument($document, $request, $path, $originalPath)) {
+                $collection->add($route->getRouteKey(), $route);
+            }
+        }
     }
 
     /**
-     * @param Document|Document\Page $document
+     * Build a route for a document. Request, path and originalPath are only set from match mode, not when generating URLs.
+     *
+     * @param Document $document
+     * @param Request|null $request
+     * @param string|null $path
+     * @param string|null $originalPath
+     *
      * @return DocumentRoute
      */
-    protected function buildRouteForDocument(Document $document)
+    protected function buildRouteForDocument(Document $document, Request $request = null, $path = null, $originalPath = null)
     {
+        // check for direct hardlink
+        if ($document instanceof Document\Hardlink) {
+            $document = Document\Hardlink\Service::wrap($document);
+
+            if (!$document) {
+                return null;
+            }
+        }
+
+        // check if document should be handled (not legacy)
         if (!$this->handleDocument($document)) {
             return null;
         }
 
         $locale = $document->getProperty('language');
 
-        // check for direct hardlink
-        if ($document instanceof Document\Hardlink) {
-            $hardlinkParentDocument = $document;
-            $document = Document\Hardlink\Service::wrap($hardlinkParentDocument);
-        }
-
         $route = new DocumentRoute($document->getRealFullPath());
         $route->setDefault('_locale', $locale);
         $route->setDocument($document);
 
-        if ($document instanceof Document\Link) {
-            // TODO use RedirectRoute?
-            $route->setDefault('_controller', 'FrameworkBundle:Redirect:urlRedirect');
-            $route->setDefault('path', $document->getHref());
-            $route->setDefault('permanent', true);
-        } else {
-            $controller = $this->configNormalizer->formatController(
-                $document->getModule(),
-                $document->getController(),
-                $document->getAction()
-            );
-
-            $route->setDefault('_controller', $controller);
-
-            if ($document->getTemplate()) {
-                $template = $this->configNormalizer->normalizeTemplate($document->getTemplate());
-                $route->setDefault('_template', $template);
-            }
+        if ($this->isDirectRouteDocument($document)) {
+            /** @var Document\PageSnippet $document */
+            $route = $this->buildRouteForDirectRouteDocument($document, $route, $request, $path, $originalPath);
+        } else if ($document->getType() === 'link') {
+            /** @var Document\Link $document */
+            $route = $this->buildRouteForLinkDocument($document, $route);
         }
 
         return $route;
+    }
+
+    /**
+     * @param Document\PageSnippet $document
+     * @param DocumentRoute $route
+     * @param Request|null $request
+     * @param null $path
+     * @param null $originalPath
+     *
+     * @return null|DocumentRoute
+     */
+    protected function buildRouteForDirectRouteDocument(Document\PageSnippet $document, DocumentRoute $route, Request $request = null, $path = null, $originalPath = null)
+    {
+        // if we have a request we're currently in match mode (not generating URLs) -> only match when frontend request by admin
+        $isAdminRequest = null !== $request && $this->requestHelper->isFrontendRequestByAdmin($request);
+
+        // abort if document is not published and the request is no admin request
+        if (!$document->isPublished() && !$isAdminRequest) {
+            return null;
+        }
+
+        if (!$isAdminRequest && null !== $request) {
+            $redirectTargetUrl = null;
+
+            // check for a pretty url, and if the document is called by that, otherwise redirect to pretty url
+            if ($document instanceof Document\Page && !$document instanceof Document\Hardlink\Wrapper\WrapperInterface) {
+                if ($prettyUrl = $document->getPrettyUrl()) {
+                    if (rtrim(strtolower($prettyUrl), ' /') !== rtrim(strtolower($originalPath), '/')) {
+                        $redirectTargetUrl = $prettyUrl;
+                    }
+                }
+            }
+
+            // check for a trailing slash in path, if exists, redirect to this page without the slash
+            // the only reason for this is: SEO, Analytics, ... there is no system specific reason, pimcore would work also with a trailing slash without problems
+            // use $originalPath because of the sites
+            // only do redirecting with GET requests
+            if ($request->getMethod() === 'GET') {
+                $config = Config::getSystemConfig();
+
+                if ($config->documents->allowtrailingslash) {
+                    if ($config->documents->allowtrailingslash === 'no') {
+                        if ($redirectTargetUrl !== '/' && substr($redirectTargetUrl, -1) === '/') {
+                            $redirectTargetUrl = rtrim($redirectTargetUrl, '/');
+                        }
+                    }
+                }
+
+                // only allow the original key of a document to be the URL (lowercase/uppercase)
+                if ($redirectTargetUrl !== rawurldecode($document->getFullPath())) {
+                    $redirectTargetUrl = $document->getFullPath();
+                }
+            }
+
+            if (null !== $redirectTargetUrl && $redirectTargetUrl !== $originalPath) {
+                $url = $redirectTargetUrl;
+                if ($qs = $request->getQueryString()) {
+                    if (false === strpos($url, '?')) {
+                        $url .= '&' . $qs;
+                    } else {
+                        $url .= '?' . $qs;
+                    }
+                }
+
+                $route->setDefault('_controller', 'FrameworkBundle:Redirect:urlRedirect');
+                $route->setDefault('path', $url);
+                $route->setDefault('permanent', true);
+
+                return $route;
+            }
+        }
+
+        return $this->buildRouteForPageSnippetDocument($document, $route);
+    }
+
+    protected function buildRouteForPageSnippetDocument(Document\PageSnippet $document, DocumentRoute $route)
+    {
+        $controller = $this->configNormalizer->formatController(
+            $document->getModule(),
+            $document->getController(),
+            $document->getAction()
+        );
+
+        $route->setDefault('_controller', $controller);
+
+        if ($document->getTemplate()) {
+            $template = $this->configNormalizer->normalizeTemplate($document->getTemplate());
+            $route->setDefault('_template', $template);
+        }
+
+        return $route;
+    }
+
+    protected function buildRouteForLinkDocument(Document\Link $document, DocumentRoute $route)
+    {
+        $route->setDefault('_controller', 'FrameworkBundle:Redirect:urlRedirect');
+        $route->setDefault('path', $document->getHref());
+        $route->setDefault('permanent', true);
+
+        return $route;
+    }
+
+    /**
+     * Check if document is can be used to generate a route
+     *
+     * @param $document
+     *
+     * @return bool
+     */
+    protected function isDirectRouteDocument($document)
+    {
+        if ($document instanceof Document\PageSnippet) {
+            if (in_array($document->getType(), $this->getDirectRouteDocumentTypes())) {
+                return true;
+            }
+        }
+
+        return false;
     }
 
     /**
@@ -144,7 +331,7 @@ class DynamicRouteProvider implements RouteProviderInterface
         if (preg_match('/^document_(\d+)$/', $name, $match)) {
             $document = Document::getById($match[1]);
 
-            if ($document && $this->handleDocument($document)) {
+            if ($this->isDirectRouteDocument($document) && $this->handleDocument($document)) {
                 return $this->buildRouteForDocument($document);
             }
         }
