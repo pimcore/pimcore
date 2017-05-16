af06eac515df34dd3577c72dea9337cddb6b8a66
Bernhard Rusch <bernhard.rusch@elements.at>
2017-05-16 10:41:53 +0200

    removed end ?> tag in PDFreactor client lib - see also #1533

--- pimcore/lib/Pimcore/Web2Print/Processor/api/v9.0/PDFreactor.class.php
+++ pimcore/lib/Pimcore/Web2Print/Processor/api/v9.0/PDFreactor.class.php
@@ -2,23 +2,23 @@
 /**
  * RealObjects PDFreactor PHP Wrapper version 4
  * http://www.pdfreactor.com
- * 
+ *
  * Released under the following license:
- * 
+ *
  * The MIT License (MIT)
- * 
+ *
  * Copyright (c) 2015-2017 RealObjects GmbH
- * 
+ *
  * Permission is hereby granted, free of charge, to any person obtaining a copy
  * of this software and associated documentation files (the "Software"), to deal
  * in the Software without restriction, including without limitation the rights
  * to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
  * copies of the Software, and to permit persons to whom the Software is
  * furnished to do so, subject to the following conditions:
- * 
+ *
  * The above copyright notice and this permission notice shall be included in all
  * copies or substantial portions of the Software.
- * 
+ *
  * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
  * IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
  * FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
@@ -1025,4 +1025,4 @@ abstract class XmpPriority {
     const LOW = "LOW";
     const NONE = "NONE";
 }
-?>
+
