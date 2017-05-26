7f77d5c699bbc683582e53bef490044ef503b93c
Mathias Geat <mathias.geat@elements.at>
2017-05-26 15:48:09 +0200

    [TWIG-DEMO] Add instanceof test and galleryRenderlet template

---
+++ pimcore/lib/Pimcore/Twig/HelpersExtension.php
@@ -0,0 +1,33 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Twig;
+
+/**
+ * Simple helpers that do not need a dedicated extension
+ */
+class HelpersExtension extends \Twig_Extension
+{
+    public function getTests()
+    {
+        return [
+            new \Twig_SimpleTest('instanceof', function($object, $class) {
+                return is_object($object) && $object instanceof $class;
+            })
+        ];
+    }
+}
