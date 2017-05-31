b9b678fa3845a28faa5a5919ab60ac308d91a86c
Bernhard Rusch <bernhard.rusch@elements.at>
2017-05-31 11:37:01 +0200

    fixed annotations on listing classes

--- pimcore/models/Asset/Listing.php
+++ pimcore/models/Asset/Listing.php
@@ -26,8 +26,6 @@ use Zend\Paginator\AdapterAggregateInterface;
  * @method int getTotalCount()
  * @method int getCount()
  * @method int loadIdList()
- */
-/**
  * @method \Pimcore\Model\Asset\Listing\Dao getDao()
  */
 class Listing extends Model\Listing\AbstractListing implements \Zend_Paginator_Adapter_Interface, \Zend_Paginator_AdapterAggregate, \Iterator, AdapterInterface, AdapterAggregateInterface
