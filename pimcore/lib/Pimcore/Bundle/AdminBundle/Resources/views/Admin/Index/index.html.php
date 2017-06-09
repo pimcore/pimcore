46ad96422b6b6c58174aa89896abb64111cbb1b9
Bernhard Rusch <bernhard.rusch@elements.at>
2017-06-09 08:55:52 +0200

    Problems with IE company wide compatibly settings - fixes #1579

--- pimcore/lib/Pimcore/Bundle/AdminBundle/Resources/views/Admin/Index/index.html.php
+++ pimcore/lib/Pimcore/Bundle/AdminBundle/Resources/views/Admin/Index/index.html.php
@@ -16,6 +16,7 @@ $user      = $userProxy->getUser();
 <head>
     <meta charset="utf-8">
     <meta name="robots" content="noindex, nofollow"/>
+    <meta http-equiv="X-UA-Compatible" content="IE=edge" />
 
     <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no"/>
     <meta name="apple-mobile-web-app-capable" content="yes"/>
