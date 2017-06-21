46b1a85bcc620e5d53a4fa17d785b7b76c9ef34a
Bernhard Rusch <bernhard.rusch@elements.at>
2017-06-21 13:38:38 +0200

    Backport of #1581 It seems that the DnD component in ExtJS 6 doesn't rely on the body's height anymore, so this functionality is obsolete.

--- pimcore/static6/js/pimcore/document/edit/startup.js
+++ pimcore/static6/js/pimcore/document/edit/startup.js
@@ -103,12 +103,6 @@ Ext.onReady(function () {
        parent.Ext.menu.MenuMgr.hideAll();
     });
 
-    // this sets the height of the body and html element to the current absolute height of the page
-    // this is because some pages set the body height, and the positioning is then done by "absolute"
-    // the problem is that ExtJS relies on the body height for DnD, so if the body isn't as high as the whole page
-    // dnd works only in the section covered by the specified body height
-    window.setInterval(pimcore.edithelpers.setBodyHeight, 1000);
-
     Ext.QuickTips.init();
     Ext.MessageBox.minPromptWidth = 500;
     
