b75c4a541b4c1edad6058c1a5b41bbd8a47f2b8e
Mathias Geat <mathias.geat@elements.at>
2017-07-04 12:56:39 +0200

    [ECOMMERCE][TESTS] Add option to omit ecommerce installer in ecommerce module

--- pimcore/tests/_support/Helper/Ecommerce.php
+++ pimcore/tests/_support/Helper/Ecommerce.php
@@ -2,17 +2,32 @@
 
 namespace Pimcore\Tests\Helper;
 
+use Codeception\Lib\ModuleContainer;
 use Codeception\Module;
 
 class Ecommerce extends Module
 {
+    /**
+     * @inheritDoc
+     */
+    public function __construct(ModuleContainer $moduleContainer, $config = null)
+    {
+        $this->config = array_merge($this->config, [
+            'run_installer' => true
+        ]);
+
+        parent::__construct($moduleContainer, $config);
+    }
+
     public function _beforeSuite($settings = [])
     {
-        /** @var Pimcore $pimcoreModule */
-        $pimcoreModule = $this->getModule('\\' . Pimcore::class);
+        if ($this->config['run_installer']) {
+            /** @var Pimcore $pimcoreModule */
+            $pimcoreModule = $this->getModule('\\' . Pimcore::class);
 
-        // install ecommerce framework
-        $installer = $pimcoreModule->getContainer()->get('pimcore.ecommerceframework.installer');
-        $installer->install();
+            // install ecommerce framework
+            $installer = $pimcoreModule->getContainer()->get('pimcore.ecommerceframework.installer');
+            $installer->install();
+        }
     }
 }
