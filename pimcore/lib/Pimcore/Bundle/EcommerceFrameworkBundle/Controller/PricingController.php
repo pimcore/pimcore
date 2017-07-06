30215c710bad061672b48a0e49bedce442605f17
andreas-gruenwald <andreas.gruenwald@elements.at>
2017-07-06 22:12:35 +0200

    Simplified code by reusing isAllowed method.

--- pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Controller/PricingController.php
+++ pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/Controller/PricingController.php
@@ -37,17 +37,8 @@ class PricingController extends AdminController implements EventedControllerInte
     public function onKernelController(FilterControllerEvent $event)
     {
         // permission check
-        $key = 'bundle_ecommerce_pricing_rules';
-        $access = $this->getUser()->getPermission($key);
+        $access = $this->getUser()->isAllowed('bundle_ecommerce_pricing_rules');
         if (!$access) {
-            $roles = $this->getUser()->getRoles();
-            $roles = $roles ? : [];
-            foreach ($roles as $roleId) {
-                $role = \Pimcore\Model\User\Role::getById($roleId);
-                if ($role->getPermission($key)) {
-                    return true;
-                }
-            }
             throw new \Exception('this function requires "bundle_ecommerce_pricing_rules" permission!');
         }
     }
