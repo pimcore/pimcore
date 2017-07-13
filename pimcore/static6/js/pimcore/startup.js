dfb0d77e24da30ca8894f657ad3ea12ebf1b96c5
Bernhard Rusch <bernhard.rusch@elements.at>
2017-07-13 15:29:11 +0200

    Add timestamp to generic error window in admin UI - fixes #1728

--- pimcore/static6/js/pimcore/startup.js
+++ pimcore/static6/js/pimcore/startup.js
@@ -199,10 +199,11 @@ Ext.onReady(function () {
             } else {
                 //do not remove notification, otherwise user is never informed about server exception (e.g. element cannot
                 // be saved due to HTTP 500 Response)
-                var errorMessage = "";
+                var date = new Date();
+                var errorMessage = "Timestamp: " + date.toString() + "\n";
 
                 try {
-                    errorMessage = "Status: " + response.status + " | " + response.statusText + "\n";
+                    errorMessage += "Status: " + response.status + " | " + response.statusText + "\n";
                     errorMessage += "URL: " + options.url + "\n";
                     if (options["params"]) {
                         errorMessage += "Params:\n";
