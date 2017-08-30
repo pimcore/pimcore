ab3578be8718d22ef957549a2256ac37fbc92372
dpfaffenbauer <dominik@lineofcode.at>
2017-08-30 10:13:07 +0100

    [ExtJs] add combo-box selection for bundle/controller/action/template to document-types as well

--- web/pimcore/static6/js/pimcore/settings/docTypes.js
+++ web/pimcore/static6/js/pimcore/settings/docTypes.js
@@ -33,7 +33,7 @@ pimcore.settings.document.doctypes = Class.create({
                 iconCls: "pimcore_icon_doctypes",
                 border: false,
                 layout: "fit",
-                closable:true,
+                closable: true,
                 items: [this.getRowEditor()]
             });
 
@@ -64,32 +64,174 @@ pimcore.settings.document.doctypes = Class.create({
         });
 
         var typesColumns = [
-            {header: t("name"), flex: 100, sortable: true, dataIndex: 'name',
-                                                                        editor: new Ext.form.TextField({})},
-            {header: t("bundle_optional"), flex: 50, sortable: true, dataIndex: 'module',
-                                                                        editor: new Ext.form.TextField({})},
-            {header: t("controller"), flex: 50, sortable: true, dataIndex: 'controller',
-                                                                        editor: new Ext.form.TextField({})},
-            {header: t("action"), flex: 50, sortable: true, dataIndex: 'action',
-                                                                        editor: new Ext.form.TextField({})},
-            {header: t("template"), flex: 50, sortable: true, dataIndex: 'template',
-                                                                        editor: new Ext.form.TextField({})},
-            {header: t("type"), flex: 50, sortable: true, dataIndex: 'type',
-                                                                        editor: new Ext.form.ComboBox({
-                triggerAction: 'all',
-                editable: false,
-                store: ["page","snippet","email","newsletter","printpage","printcontainer"]
-            })},
-            {header: t("priority"), flex: 50, sortable: true, dataIndex: 'priority', editor: new Ext.form.ComboBox({
-                store: [1,2,3,4,5,6,7,8,9,10],
-                mode: "local",
-                editable: false,
-                triggerAction: "all"
-            })},
+            {
+                header: t("name"),
+                flex: 100,
+                sortable: true,
+                dataIndex: 'name',
+                editor: new Ext.form.TextField({})
+            },
+            {
+                header: t("bundle_optional"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'module',
+                editor: new Ext.form.ComboBox({
+                    store: new Ext.data.JsonStore({
+                        autoDestroy: true,
+                        proxy: {
+                            type: 'ajax',
+                            url: "/admin/misc/get-available-modules",
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'data'
+                            }
+                        },
+                        fields: ["name"]
+                    }),
+                    triggerAction: "all",
+                    displayField: 'name'
+                })
+            },
+            {
+                header: t("controller"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'controller',
+                editor: new Ext.form.ComboBox({
+                    store: new Ext.data.JsonStore({
+                        autoDestroy: true,
+                        proxy: {
+                            type: 'ajax',
+                            url: "/admin/misc/get-available-controllers",
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'data'
+                            }
+                        },
+                        fields: ["name"]
+                    }),
+                    queryMode: 'local',
+                    triggerAction: "all",
+                    displayField: 'name',
+                    valueField: 'name',
+                    listeners: {
+                        "focus": function (el) {
+                            var currentRecord = this.grid.getSelection();
+                            el.getStore().reload({
+                                params: {
+                                    moduleName: currentRecord[0].data.module
+                                },
+                                callback: function () {
+                                    el.expand();
+                                }
+                            });
+                        }.bind(this)
+                    }
+                })
+            },
+            {
+                header: t("action"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'action',
+                editor: new Ext.form.ComboBox({
+                    store: new Ext.data.Store({
+                        autoDestroy: true,
+                        proxy: {
+                            type: 'ajax',
+                            url: "/admin/misc/get-available-actions",
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'data'
+                            }
+                        },
+                        fields: ["name"]
+                    }),
+                    queryMode: 'local',
+                    triggerAction: "all",
+                    displayField: 'name',
+                    valueField: 'name',
+                    listeners: {
+                        "focus": function (el) {
+                            var currentRecord = this.grid.getSelection();
+                            el.getStore().reload({
+                                params: {
+                                    controllerName: currentRecord[0].data.controller
+                                },
+                                callback: function () {
+                                    el.expand();
+                                }
+                            });
+                        }.bind(this),
+                    }
+                })
+            },
+            {
+                header: t("template"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'template',
+                editor: new Ext.form.ComboBox({
+                    store: new Ext.data.Store({
+                        autoDestroy: true,
+                        proxy: {
+                            type: 'ajax',
+                            url: "/admin/misc/get-available-templates",
+                            reader: {
+                                type: 'json',
+                                rootProperty: 'data'
+                            }
+                        },
+                        fields: ["path"]
+                    }),
+                    queryMode: 'local',
+                    triggerAction: "all",
+                    displayField: 'path',
+                    valueField: 'path',
+                    listeners: {
+                        "focus": function (el) {
+                            el.getStore().reload({
+                                callback: function () {
+                                    el.expand();
+                                }
+                            });
+                        }.bind(this),
+                    }
+                })
+            },
+            {
+                header: t("type"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'type',
+                editor: new Ext.form.ComboBox({
+                    triggerAction: 'all',
+                    editable: false,
+                    store: ["page", "snippet", "email", "newsletter", "printpage", "printcontainer"]
+                })
+            },
+            {
+                header: t("priority"),
+                flex: 50,
+                sortable: true,
+                dataIndex: 'priority',
+                editor: new Ext.form.ComboBox({
+                    store: [1, 2, 3, 4, 5, 6, 7, 8, 9, 10],
+                    mode: "local",
+                    editable: false,
+                    triggerAction: "all"
+                })
+            },
             legacyCheck,
-            {header: t("creationDate"), sortable: true, dataIndex: 'creationDate', editable: false, width: 130,
+            {
+                header: t("creationDate"),
+                sortable: true,
+                dataIndex: 'creationDate',
+                editable: false,
+                width: 130,
                 hidden: true,
-                renderer: function(d) {
+                renderer: function (d) {
                     if (d !== undefined) {
                         var date = new Date(d * 1000);
                         return Ext.date.format(date, "Y-m-d H:i:s");
@@ -98,9 +240,14 @@ pimcore.settings.document.doctypes = Class.create({
                     }
                 }
             },
-            {header: t("modificationDate"), sortable: true, dataIndex: 'modificationDate', editable: false, width: 130,
+            {
+                header: t("modificationDate"),
+                sortable: true,
+                dataIndex: 'modificationDate',
+                editable: false,
+                width: 130,
                 hidden: true,
-                renderer: function(d) {
+                renderer: function (d) {
                     if (d !== undefined) {
                         var date = new Date(d * 1000);
                         return Ext.date.format(date, "Y-m-d H:i:s");
@@ -119,20 +266,20 @@ pimcore.settings.document.doctypes = Class.create({
                         grid.getStore().removeAt(rowIndex);
                     }.bind(this)
                 }]
-            },{
+            }, {
                 xtype: 'actioncolumn',
                 width: 30,
                 items: [{
                     tooltip: t('translate'),
                     icon: "/pimcore/static6/img/flat-color-icons/collaboration.svg",
-                    handler: function(grid, rowIndex){
+                    handler: function (grid, rowIndex) {
                         var rec = grid.getStore().getAt(rowIndex);
                         try {
                             pimcore.globalmanager.get("translationadminmanager").activate(rec.data.name);
                         }
                         catch (e) {
                             pimcore.globalmanager.add("translationadminmanager",
-                                                    new pimcore.settings.translation.admin(rec.data.name));
+                                new pimcore.settings.translation.admin(rec.data.name));
                         }
                     }.bind(this)
                 }]
@@ -149,7 +296,7 @@ pimcore.settings.document.doctypes = Class.create({
             autoScroll: true,
             bodyCls: "pimcore_editable_grid",
             store: this.store,
-            columns : typesColumns,
+            columns: typesColumns,
             columnLines: true,
             trackMouseOver: true,
             stripeRows: true,
