66330d2ad0865b6c19ba370cad0a689d2cd356c0
Mathias Geat <mathias.geat@elements.at>
2017-09-07 15:39:26 +0200

    [SERVICE-DEFINITIONS][CONTROLLERS] Add webservice service as constructor argument in webservice controllers

--- pimcore/lib/Pimcore/Bundle/AdminBundle/Controller/Rest/AbstractRestController.php
+++ pimcore/lib/Pimcore/Bundle/AdminBundle/Controller/Rest/AbstractRestController.php
@@ -37,14 +37,9 @@ abstract class AbstractRestController extends AdminController
      */
     protected $service;
 
-    /**
-     * @inheritDoc
-     */
-    public function setContainer(ContainerInterface $container = null)
+    public function __construct(Service $service)
     {
-        parent::setContainer($container);
-
-        $this->service = $container->get(Service::class);
+        $this->service = $service;
     }
 
     /**
