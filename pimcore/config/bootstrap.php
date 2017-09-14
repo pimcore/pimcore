880c0814149079c748942b3e7449192d4de045c0
Bernhard Rusch <bernhard.rusch@elements.at>
2017-09-14 17:42:05 +0200

    Invalid Composer Autoload Classmap breaks Updater - fixes #1972

--- pimcore/config/bootstrap.php
+++ pimcore/config/bootstrap.php
@@ -15,9 +15,10 @@ error_reporting(E_ALL & ~E_NOTICE & ~E_STRICT);
 
 /** @var $loader \Composer\Autoload\ClassLoader */
 $loader = include PIMCORE_PROJECT_ROOT . '/vendor/autoload.php';
-Pimcore::setAutoloader($loader);
-
 require_once __DIR__ . '/constants.php';
+// needs to be included manually since the updater invalidates the classmap and therefore cannot find the class anymore during an update
+require_once PIMCORE_PATH . '/lib/Pimcore.php';
+Pimcore::setAutoloader($loader);
 require_once __DIR__ . '/autoload.php';
 
 $phpLog = PIMCORE_LOG_DIRECTORY . '/php.log';
