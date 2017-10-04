3580f27299f9f5d53f4326027a8885126d5e2b10
Christian Fasching <christian.fasching@elements.at>
2017-10-04 11:22:20 +0200

    added some quotes for db queries

--- pimcore/modules/admin/controllers/RecyclebinController.php
+++ pimcore/modules/admin/controllers/RecyclebinController.php
@@ -158,7 +158,7 @@ class Admin_RecyclebinController extends \Pimcore\Controller\Action\Admin
             $type = Element\Service::getElementType($element);
             $listClass = "\\Pimcore\\Model\\" . ucfirst($type) . "\\Listing";
             $list = new $listClass();
-            $list->setCondition((($type == "object") ? "o_" : "") . "path LIKE '" . $element->getRealFullPath() . "/%'");
+            $list->setCondition((($type == "object") ? "o_" : "") . "path LIKE " . $list->quote($element->getRealFullPath() . "/%"));
             $children = $list->getTotalCount();
 
             if ($children <= 100) {
