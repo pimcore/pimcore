c7e25d29662a316637454110866040d0114e4d93
weisswurstkanone <josef.aichhorn@elements.at>
2017-10-09 16:50:49 +0200

    initial commit

--- pimcore/models/DataObject/GridConfig/IConfigElement.php
+++ pimcore/models/DataObject/GridConfig/IConfigElement.php
@@ -18,22 +18,8 @@
 
 namespace Pimcore\Model\DataObject\GridConfig;
 
-abstract class AbstractConfigElement implements IConfigElement {
+interface IConfigElement {
 
-    protected $attribute;
-    protected $label;
-
-    protected $context;
-
-    public function __construct($config, $context = null) {
-        $this->attribute = $config->attribute;
-        $this->label = $config->label;
-
-        $this->context = $context;
-    }
-
-    public function getLabel() {
-        return $this->label;
-    }
-
+    public function getLabeledValue($object);
+    
 }
