903417f6e45b7b2239e917c72b5b052fbc97346c
markus-moser <markus.moser@elements.at>
2017-10-09 17:06:47 +0200

    [ECOMMERCE][elasticsearch] updateWhere condition bugfix

--- pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/IndexService/Worker/DefaultElasticSearch.php
+++ pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/IndexService/Worker/DefaultElasticSearch.php
@@ -546,7 +546,7 @@ class DefaultElasticSearch extends AbstractMockupCacheWorker implements IBatchPr
                     Logger::error('Failed to Index Object with Id:' . $response['index']['_id']);
                 }
 
-                $this->db->updateWhere($this->getStoreTableName(), $data, ['o_id = ?' => $response['index']['_id']]);
+                $this->db->updateWhere($this->getStoreTableName(), $data, 'o_id = ' . $this->db->quote($response['index']['_id']));
             }
         }
 
