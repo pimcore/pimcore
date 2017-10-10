bf4f91c853c33fffe390b53571cc7b935f78491d
weisswurstkanone <josef.aichhorn@elements.at>
2017-10-10 12:21:44 +0200

    added a case converter

--- pimcore/models/DataObject/GridConfig/Operator/CellFormatter.php
+++ pimcore/models/DataObject/GridConfig/Operator/CellFormatter.php
@@ -17,21 +17,24 @@
 
 namespace Pimcore\Model\DataObject\GridConfig\Operator;
 
-class CellFormatter extends AbstractOperator {
+class CellFormatter extends AbstractOperator
+{
 
 
     private $maxLength;
 
-    public function __construct($config, $context = null) {
+    public function __construct($config, $context = null)
+    {
         parent::__construct($config, $context);
 
         $this->label = $config->cssClass;
         $this->maxLength = $config->maxLength;
     }
 
-    public function getLabeledValue($object) {
+    public function getLabeledValue($object)
+    {
         $childs = $this->getChilds();
-        if($childs[0]) {
+        if ($childs[0]) {
             $result = $childs[0]->getLabeledValue($object);
             if ($this->getMaxLength() && isset($result->value) && strlen($result->value) > $this->getMaxLength()) {
                 $result->value = substr($result->value, 0, $this->getMaxLength() - 3) . "...";
@@ -56,7 +59,6 @@ class CellFormatter extends AbstractOperator {
     {
         $this->maxLength = $maxLength;
     }
-
 
 
 }
