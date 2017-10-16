9aee5bc8717ca96341fee775bd9077680a8639ff
weisswurstkanone <josef.aichhorn@elements.at>
2017-10-16 12:19:39 +0200

    Localized edit mode permission not working #2113

--- web/pimcore/static6/js/pimcore/object/tags/localizedfields.js
+++ web/pimcore/static6/js/pimcore/object/tags/localizedfields.js
@@ -250,7 +250,7 @@ pimcore.object.tags.localizedfields = Class.create(pimcore.object.tags.abstract,
                     hideMode: "offsets",
                     items: [],
                     listeners: {
-                        afterrender: function (l, runtimeContext, dataProvider, panel) {
+                        afterrender: function (l, editable, runtimeContext, dataProvider, panel) {
                             if (!panel.__tabpanel_initialized) {
                                 panel.__tabpanel_initialized = true;
                                 if (l.childs && typeof l.childs == "object") {
@@ -277,7 +277,7 @@ pimcore.object.tags.localizedfields = Class.create(pimcore.object.tags.abstract,
                                 }
 
                             }
-                        }.bind(this, panelConfig, runtimeContext, dataProvider)
+                        }.bind(this, panelConfig, editable, runtimeContext, dataProvider)
                     }
 
                 };
