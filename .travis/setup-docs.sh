1c1d49354a576d66cf2ce439553ce6159f5437ab
Mathias Geat <mathias.geat@elements.at>
2017-10-30 11:20:36 +0100

    [DOCS] Build with latest pimcore-docs release

--- .travis/setup-docs.sh
+++ .travis/setup-docs.sh
@@ -5,5 +5,14 @@ set -euv
 rm -rf tmp-docs
 git clone https://github.com/pimcore/pimcore-docs.git tmp-docs/pimcore-docs
 cd tmp-docs/pimcore-docs
+
+# check out latest pimcore-docs release
+LATEST_VERSION=$(git describe --abbrev=0)
+echo "Checking out latest pimcore-docs release ${LATEST_VERSION}"
+git checkout ${LATEST_VERSION}
+
+# install composer dependencies
 composer install  --no-interaction --optimize-autoloader
+
+# prepare docs
 bin/console prepare ../../doc
