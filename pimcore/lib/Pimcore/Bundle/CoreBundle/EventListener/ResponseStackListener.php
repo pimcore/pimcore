be697e8aed1b0ee7f7789278e7b320f3ec073c7a
Mathias Geat <mathias.geat@elements.at>
2017-11-14 13:46:53 +0100

    [AREABRICKS] Handle responses returned from an areabrick action and postRenderAction method

---
+++ pimcore/lib/Pimcore/Bundle/CoreBundle/EventListener/ResponseStackListener.php
@@ -0,0 +1,60 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Bundle\CoreBundle\EventListener;
+
+use Pimcore\Http\ResponseStack;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class ResponseStackListener implements EventSubscriberInterface
+{
+    /**
+     * @var ResponseStack
+     */
+    private $responseStack;
+
+    /**
+     * @param ResponseStack $responseStack
+     */
+    public function __construct(ResponseStack $responseStack)
+    {
+        $this->responseStack = $responseStack;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => ['onKernelResponse', 24]
+        ];
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        if ($this->responseStack->hasResponses()) {
+            $event->setResponse($this->responseStack->getLastResponse());
+        }
+    }
+}
