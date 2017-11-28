cb1fdd17562451197c00634e79d81e53789e809b
Mathias Geat <mathias.geat@elements.at>
2017-11-28 10:06:33 +0100

    [TARGETING] Rename rule scope to matching scope

--- web/pimcore/static6/js/pimcore/settings/targeting/rules/item.js
+++ web/pimcore/static6/js/pimcore/settings/targeting/rules/item.js
@@ -105,7 +105,7 @@ pimcore.settings.targeting.rules.item = Class.create({
                 value: this.data.description
             }, {
                 name: "scope",
-                fieldLabel: t("scope"),
+                fieldLabel: t("matching_scope"),
                 xtype: "combo",
                 width: 250,
                 value: this.data["scope"],
