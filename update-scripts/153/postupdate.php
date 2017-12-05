76980b5ef6e12fd7f7fc18237a1087991b037a07
Christian Fasching <christian.fasching@elements.at>
2017-12-05 10:28:25 +0100

    readded permission for gdpr data extractor

--- update-scripts/153/postupdate.php
+++ update-scripts/153/postupdate.php
@@ -4,3 +4,4 @@ $db = \Pimcore\Db::get();
 $db->insert('users_permission_definitions', ['key' => 'piwik_settings']);
 $db->insert('users_permission_definitions', ['key' => 'piwik_reports']);
 $db->insert('users_permission_definitions', ['key' => 'share_configurations']);
+$db->insert('users_permission_definitions', ['key' => 'gdpr_data_extractor']);
