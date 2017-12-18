9af42bc553bab8bd225796b16c0ae8fae2627fe1
Mathias Geat <mathias.geat@elements.at>
2017-12-18 12:21:06 +0100

    Run google site verification listener before routing - fixes #2249

--- pimcore/lib/Pimcore/Bundle/CoreBundle/EventListener/Frontend/GoogleSearchConsoleVerificationListener.php
+++ pimcore/lib/Pimcore/Bundle/CoreBundle/EventListener/Frontend/GoogleSearchConsoleVerificationListener.php
@@ -28,14 +28,14 @@ class GoogleSearchConsoleVerificationListener implements EventSubscriberInterfac
     public static function getSubscribedEvents()
     {
         return [
-            KernelEvents::REQUEST => 'onKernelRequest'
+            KernelEvents::REQUEST => ['onKernelRequest', 64]
         ];
     }
 
     /**
      * @param GetResponseEvent $event
      */
-    public function onKernelRequest(GetResponseEvent  $event)
+    public function onKernelRequest(GetResponseEvent $event)
     {
         $request = $event->getRequest();
         if (!$event->isMasterRequest()) {
@@ -47,17 +47,17 @@ class GoogleSearchConsoleVerificationListener implements EventSubscriberInterfac
         }
 
         $conf = \Pimcore\Config::getReportConfig();
+
         if (!is_null($conf->webmastertools) && isset($conf->webmastertools->sites)) {
             $sites = $conf->webmastertools->sites->toArray();
 
             if (is_array($sites)) {
                 foreach ($sites as $site) {
-                    if ($site['verification']) {
-                        $request = $event->getRequest();
-                        if ($request->getPathInfo() == ('/'.$site['verification'])) {
-                            $response = new Response('google-site-verification: ' . $site['verification'], 503);
-                            $event->setResponse($response);
-                        }
+                    if ($site['verification'] && $request->getPathInfo() === '/' . $site['verification']) {
+                        $response = new Response('google-site-verification: ' . $site['verification']);
+                        $event->setResponse($response);
+
+                        break;
                     }
                 }
             }
