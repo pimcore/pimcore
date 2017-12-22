386a137163d6269c380c0f6f35c3315f37bcb606
weisswurstkanone <josef.aichhorn@elements.at>
2017-12-22 11:25:26 +0100

    Add the same passwordgenerator in the user section #1905

--- web/pimcore/static6/js/pimcore/helpers.js
+++ web/pimcore/static6/js/pimcore/helpers.js
@@ -3140,3 +3140,34 @@ pimcore.helpers.csvExportWarning = function (callback) {
     window.show();
 };
 
+pimcore.helpers.generatePassword = function (len) {
+    var length = (len) ? (len) : (10);
+    var string = "abcdefghijklmnopqrstuvwxyz"; //to upper
+    var numeric = '0123456789';
+    var punctuation = '!@#$%^&*()_+~`|}{[]\:;?><,./-=';
+    var password = "";
+    var character = "";
+    while (password.length < length) {
+        entity1 = Math.ceil(string.length * Math.random() * Math.random());
+        entity2 = Math.ceil(numeric.length * Math.random() * Math.random());
+        entity3 = Math.ceil(punctuation.length * Math.random() * Math.random());
+        hold = string.charAt(entity1);
+        hold = (entity1 % 2 == 0) ? (hold.toUpperCase()) : (hold);
+        character += hold;
+        character += numeric.charAt(entity2);
+        character += punctuation.charAt(entity3);
+        password = character;
+    }
+    return password;
+};
+
+pimcore.helpers.isValidPassword = function (pass) {
+    var passRegExp = /^(?=.*\d)(?=.*[a-z])(?=.*[A-Z])(?=.*[^a-zA-Z0-9])(?!.*\s).{10,}$/;
+    if (!pass.match(passRegExp)) {
+        return false;
+    }
+    return true;
+};
+
+
+
