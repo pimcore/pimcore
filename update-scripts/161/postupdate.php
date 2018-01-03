c3f03152ae65fc77dc3023745a49fce3c5cc2944
Mathias Geat <mathias.geat@elements.at>
2018-01-03 09:12:31 +0100

    [TARGETING] Update update script for build 161 to rename and re-create the rules table

--- update-scripts/161/postupdate.php
+++ update-scripts/161/postupdate.php
@@ -5,9 +5,26 @@ $db->query('ALTER TABLE `quantityvalue_units` MODIFY `abbreviation` varchar(20)'
 
 // targeting
 $db->query('RENAME TABLE targeting_personas TO targeting_target_groups');
-$db->query('ALTER TABLE `targeting_rules` ADD `prio` SMALLINT UNSIGNED NOT NULL DEFAULT 0 AFTER `active`');
+
 $db->query('ALTER TABLE `targeting_target_groups` DROP `conditions`');
 $db->query('ALTER TABLE `documents_page` CHANGE `personas` `targetGroupIds` VARCHAR(255)');
+
+$db->query('RENAME TABLE targeting_rules TO PLEASE_DELETE__targeting_rules');
+$db->query(<<<'EOF'
+CREATE TABLE `targeting_rules` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(255) NOT NULL DEFAULT '',
+  `description` text,
+  `scope` varchar(50) DEFAULT NULL,
+  `active` tinyint(1) DEFAULT NULL,
+  `prio` smallint(5) unsigned NOT NULL DEFAULT '0',
+  `conditions` longtext,
+  `actions` longtext,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+EOF
+);
+
 $db->query(<<<'EOF'
 CREATE TABLE `targeting_storage` (
   `visitorId` varchar(100) NOT NULL,
--- update-scripts/161/preupdate.php
+++ update-scripts/161/preupdate.php
@@ -7,7 +7,6 @@ if (!TmpStore::get($tmpStoreId)) {
     TmpStore::add($tmpStoreId, 'true', null, 86400 * 30);
     echo '<b>You\'re going to install Pimcore 5.1</b><br />';
     echo 'This release includes breaking changes in the targeting engine and for bundles implementing admin features.<br/>';
-    echo 'Please see <a href="https://pimcore.com/docs/5.0.x/Development_Documentation/Installation_and_Upgrade/Upgrade_Notes/Within_V5.html#page_Pimcore-5-1" target="_blank">the upgrade notes</a> for details<br/>';
-    echo 'and restart the update process to continue.';
+    echo 'Please see <a href="https://pimcore.com/docs/5.0.x/Development_Documentation/Installation_and_Upgrade/Upgrade_Notes/Within_V5.html#page_Pimcore-5-1" target="_blank">the upgrade notes</a> for details and restart the update process to continue.';
     exit;
 }
