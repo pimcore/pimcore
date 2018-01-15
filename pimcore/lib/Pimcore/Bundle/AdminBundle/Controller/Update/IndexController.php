2fbdbc747c8cc03c648d15852dd8c12becc87a20
Mathias Geat <mathias.geat@elements.at>
2018-01-15 17:10:21 +0100

    [SYMFONY 3.4] Clear cache without warmup when running update via web

--- pimcore/lib/Pimcore/Bundle/AdminBundle/Controller/Update/IndexController.php
+++ pimcore/lib/Pimcore/Bundle/AdminBundle/Controller/Update/IndexController.php
@@ -134,7 +134,12 @@ class IndexController extends AdminController implements EventedControllerInterf
             Update::installData($request->get('revision'), $request->get('updateScript'));
         } elseif ($request->get('type') == 'clearcache') {
             \Pimcore\Cache::clearAll();
-            $this->get(CacheClearer::class)->clear($kernel);
+
+            $symfonyCacheClearer = $this->get(CacheClearer::class);
+            $symfonyCacheClearer->clear($kernel, [
+                // warmup will break the request as it will try to re-declare the appDevDebugProjectContainerUrlMatcher class
+                'no-warmup' => true
+            ]);
         } elseif ($request->get('type') == 'preupdate') {
             $status = Update::executeScript($request->get('revision'), 'preupdate');
         } elseif ($request->get('type') == 'postupdate') {
