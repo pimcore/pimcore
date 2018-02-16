c358e6aa4eed5b809220cda62d80bd70b3b64b8a
thureos <luis@barron.im>
2018-02-16 10:18:41 -0500

    Delete Asset Syntax error
    Steps to reproduce
    Create 1 asset folder, then another one inside the first one, then try to delete the first one.

--- pimcore/modules/admin/controllers/AssetController.php
+++ pimcore/modules/admin/controllers/AssetController.php
@@ -473,7 +473,7 @@ class Admin_AssetController extends \Pimcore\Controller\Action\Admin\Element
                 if ($hasChilds) {
                     // get amount of childs
                     $list = new Asset\Listing();
-                    $list->setCondition("path LIKE ", [$asset->getRealFullPath() . "/%"]);
+                    $list->setCondition("path LIKE ?", [$asset->getRealFullPath() . "/%"]);
                     $childs = $list->getTotalCount();
                     $totalChilds += $childs;
 
