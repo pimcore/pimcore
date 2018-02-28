9c59ca6aadf212d72963af7281af7a20e88e236e
weisswurstkanone <josef.aichhorn@elements.at>
2018-02-28 11:28:03 +0100

    [Classification Store] Values not shown in grid #2637

--- pimcore/models/DataObject/Service.php
+++ pimcore/models/DataObject/Service.php
@@ -299,7 +299,15 @@ class Service extends Model\Element\Service
                         if (method_exists($object, $getter)) {
                             /** @var $classificationStoreData Classificationstore */
                             $classificationStoreData = $object->$getter();
-                            $fielddata = $classificationStoreData->getLocalizedKeyValue($groupId, $keyid, $requestedLanguage, true, true);
+
+                            /** @var $csFieldDefinition Model\DataObject\ClassDefinition\Data\Classificationstore */
+                            $csFieldDefinition = $object->getClass()->getFieldDefinition($field);
+                            $csLanguage = $requestedLanguage;
+                            if (!$csFieldDefinition->isLocalized()) {
+                                $csLanguage = 'default';
+                            }
+
+                            $fielddata = $classificationStoreData->getLocalizedKeyValue($groupId, $keyid, $csLanguage, true, true);
 
                             $keyConfig = Model\DataObject\Classificationstore\KeyConfig::getById($keyid);
                             $type = $keyConfig->getType();
