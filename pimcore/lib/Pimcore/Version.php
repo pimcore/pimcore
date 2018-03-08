895a42226c16d4c43f6c7e875ce19202e25eeaeb
pimcore <pimcore@pimcore>
2018-03-08 12:06:39 +0100

    build 216

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -24,12 +24,12 @@ class Version
     /**
      * @var int
      */
-    public static $revision = 215;
+    public static $revision = 216;
 
     /**
      * @var string
      */
-    public static $buildDate = '2018-03-08T10:39:11+00:00';
+    public static $buildDate = '2018-03-08T11:06:38+00:00';
 
     /**
      * @return string
