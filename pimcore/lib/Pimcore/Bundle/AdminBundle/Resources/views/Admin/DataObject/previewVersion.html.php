8e41ffe6232e0509446b45033de1a67301e3588b
Divesh Pahuja <divesh.pahuja@osscube.com>
2018-04-17 16:09:25 +0530

    Add field collection rendering in version preview

--- pimcore/lib/Pimcore/Bundle/AdminBundle/Resources/views/Admin/DataObject/previewVersion.html.php
+++ pimcore/lib/Pimcore/Bundle/AdminBundle/Resources/views/Admin/DataObject/previewVersion.html.php
@@ -145,6 +145,39 @@
             }
         }
         ?>
+    <?php } else if ($definition instanceof DataObject\ClassDefinition\Data\Fieldcollections) {
+            $fields = $this->object->{"get" . ucfirst($fieldName)}();
+            if($fields) {
+                $fieldDefinitions = $fields->getItemDefinitions();
+                $fieldItems = $fields->getItems();
+            }
+
+            if (count($fieldItems)) {
+                
+                foreach ($fieldItems as $fkey => $fieldItem) {
+                    $fieldKeys = $fieldDefinitions[$fieldItem->type]->getFieldDefinitions();
+                    foreach ($fieldKeys as $fieldKey) {
+
+                    
+                       $value = $fieldItem->{"get" . ucfirst($fieldKey->name)}();
+                       // if ($fieldValue) { ?>
+                             <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
+                                <td><?= ucfirst($definition->title) . " - " . $fieldKey->title ?> (<?= $language; ?>)</td>
+                                <td><?= $fieldKey->name ?></td>
+                                <td><?= $fieldKey->getVersionPreview($value) ?></td>
+                            </tr>
+                        <?php
+                        $c++;
+                      //  }
+                    }
+                }
+            } else { ?>
+                <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
+                <td><?= ucfirst($definition->title) . $fieldKey->title ?> (<?= $language; ?>)</td>
+                <td><?= $fieldKey->name ?></td>
+                <td><?= $fieldValue ?></td>
+            <?php }
+    ?>
     <?php } else { ?>
         <tr<?php if ($c % 2) { ?> class="odd"<?php } ?>>
             <td><?= $definition->getTitle() ?></td>
