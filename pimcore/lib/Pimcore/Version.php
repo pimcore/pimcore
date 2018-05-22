7c9a43a8f86b3400208f7bb109ff641aeb644324
pimcore <pimcore@pimcore>
2018-05-22 17:47:10 +0200

    Build 237

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -24,12 +24,12 @@ class Version
     /**
      * @var int
      */
-    public static $revision = 236;
+    public static $revision = 237;
 
     /**
      * @var string
      */
-    public static $buildDate = '2018-05-22T15:36:04+00:00';
+    public static $buildDate = '2018-05-22T15:47:09+00:00';
 
     /**
      * @return string
