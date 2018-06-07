9a3a00e06d94902e911c7288e2bd68e3aa0f088e
Christian Fasching <fashxp@users.noreply.github.com>
2018-06-07 14:11:42 +0200

    Update AbstractPayment.php

--- pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/PaymentManager/Payment/AbstractPayment.php
+++ pimcore/lib/Pimcore/Bundle/EcommerceFrameworkBundle/PaymentManager/Payment/AbstractPayment.php
@@ -1,9 +1,15 @@
 <?php
 /**
- * Created by PhpStorm.
- * User: Julian Raab
- * Date: 31.01.2018
- * Time: 00:31
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
 namespace Pimcore\Bundle\EcommerceFrameworkBundle\PaymentManager\Payment;
