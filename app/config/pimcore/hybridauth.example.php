602817b3d0ac696df4657e78a0709e716d53525e
Bernhard Rusch <bernhard.rusch@elements.at>
2018-06-13 13:54:47 +0200

    hybridauth example file

--- app/config/pimcore/hybridauth.example.php
+++ app/config/pimcore/hybridauth.example.php
@@ -5,22 +5,22 @@ return [
         'Google' => [
             'enabled' => true,
             'keys'    => [
-                'id' => '655439141282-nfvap5belfqv8s3v177jg9761j879cv3.apps.googleusercontent.com',
+                'id' => '655439141282-nfvapxbelfxv8sxv177jg976xxxxxxxx.apps.googleusercontent.com',
                 'secret' => 'xGtTcy3knT3YILGRRTgVWwjp'
             ]
         ],
         'Facebook' => [
             'enabled' => true,
             'keys'    => [
-                'id' => '281810551981702',
-                'secret' => '3aec78fdcb5f02cc3e9da95d6b426de6'
+                'id' => '281810550000000',
+                'secret' => '3aec78fdcb5f02cc3e9da95d6xxxxxxx'
             ]
         ],
         'Twitter' => [
             'enabled' => true,
             'keys'    => [
-                'key' => '8CfXfFFwTFURUFUH05RZw',
-                'secret' => 'vxrqB4KLBBwRLxhKnRkJj52OTDgR2ZwZxhZ125OdL8'
+                'key' => '8CfXfFFwTFURUFxxxxxxx',
+                'secret' => 'vxrqB4KLBBwRLxhKnRkJj52OTDgR2ZwZxhZxxxxxxx'
             ]
         ]
     ]
