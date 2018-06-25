467c8afb49bc173553da5f5dfecbd171ba4db234
Anna Zavodian <zavodyanka@gmail.com>
2018-06-25 14:22:09 +0200

    issue-2915: Preview of older document version

--- web/pimcore/static6/js/pimcore/document/versions.js
+++ web/pimcore/static6/js/pimcore/document/versions.js
@@ -274,7 +274,7 @@ pimcore.document.versions = Class.create({
         var data = grid.getStore().getAt(index).data;
         var versionId = data.id;
 
-        window.open(this.document.data.path + this.document.data.key + '?v=' + versionId,'_blank');
+        window.open(this.document.data.path + this.document.data.key + '?pimcore_version=' + versionId,'_blank');
     },
 
     editVersion: function (index, grid) {
