2c71f3dec2c34f2b7a8ad0c206786119e133227d
Bernhard Rusch <bernhard.rusch@elements.at>
2018-06-27 11:21:05 +0200

    quicksearch - fix 2x Ctrl mechanism

--- web/pimcore/static6/js/pimcore/startup.js
+++ web/pimcore/static6/js/pimcore/startup.js
@@ -755,6 +755,7 @@ Ext.onReady(function () {
 
     // Quick Search
     var quickSearchTimeout = null;
+    var quickSearchCount = 0;
     var quicksearchMap = new Ext.util.KeyMap({
         target: document,
         binding: [{
@@ -765,16 +766,27 @@ Ext.onReady(function () {
         }, {
             key: Ext.event.Event.CTRL,
             fn: function () {
+                quickSearchCount++;
                 // press 2x CTRL within 200ms
-                if(quickSearchTimeout) {
-                    pimcore.helpers.showQuickSearch();
-                } else {
-                    quickSearchTimeout = window.setTimeout(function () {
-                        if (quickSearchTimeout) {
-                            window.clearTimeout(quickSearchTimeout);
-                            quickSearchTimeout = null;
+                if(quickSearchCount === 2) {
+                    window.setTimeout(function () {
+                        if (quickSearchCount == 2) {
+                            pimcore.helpers.showQuickSearch();
+                            quickSearchCount = 0;
                         }
                     }, 200);
+                } else {
+                    if(quickSearchTimeout) {
+                        window.clearTimeout(quickSearchTimeout);
+                    }
+
+                    var lastCount = quickSearchCount;
+                    quickSearchTimeout = window.setTimeout(function () {
+                        if(lastCount === quickSearchCount) {
+                            quickSearchTimeout = null;
+                            quickSearchCount = 0;
+                        }
+                    }, 1000);
                 }
             }
         }]
