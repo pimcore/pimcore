2bb4de814a35b63ae718364bb671ac0fb79001a1
pimcore <pimcore@pimcore>
2018-07-13 09:07:52 +0200

    Build 281

--- pimcore/lib/Pimcore/Version.php
+++ pimcore/lib/Pimcore/Version.php
@@ -24,12 +24,12 @@ class Version
     /**
      * @var int
      */
-    public static $revision = 280;
+    public static $revision = 281;
 
     /**
      * @var string
      */
-    public static $buildDate = '2018-07-11T16:12:26+02:00';
+    public static $buildDate = '2018-07-13T09:07:47+02:00';
 
     /**
      * @return string
