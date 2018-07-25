333cf76accb4b1508ccfd5db3154a66d4a71fe74
Christian Fasching <christian.fasching@elements.at>
2018-07-25 15:05:06 +0200

    adapt travis tests to new setup

--- .travis/setup-tests.sh
+++ .travis/setup-tests.sh
@@ -7,12 +7,11 @@ set -eu
 # Folder structure is as follows:
 # - Pimcore repository is checked out by travis to /home/travis/pimcore/pimcore
 # - Empty Pimcore skeleton is checkout out to /tmp/www
-# - Symlink /tmp/www/dev/pimcore/pimcore is created to point to Pimcore repository
-#   checked out by travis (make sure we are testing the correct Pimcore version)
-# - Apache document root points to /tmp/www/web
 # - Composer dependencies are installed to /tmp/www/vendor
-# - Additional Symlink /home/travis/vendor points to /tmp/www/vendor
+# - Via composer.local.json checked out Pimcore sources (/home/travis/pimcore/pimcore) are linked to vendor folder
+# - Additional symlinks /home/travis/vendor points to /tmp/www/vendor, /home/travis/app points to /tmp/www/app
 #   (to make relative paths in Pimcore repository work)
+# - Apache document root points to /tmp/www/web
 #
 # --- END DOC -----
 
@@ -25,9 +24,10 @@ git clone https://github.com/pimcore/skeleton.git /tmp/www
 # remove .git folder so that composer merge plugin makes symlink correctly
 rm -r -f ~/build/pimcore/pimcore/.git
 
-#mkdir -p /tmp/www/vendor/pimcore
-#ln -s ~/build/pimcore/pimcore /tmp/www/vendor/pimcore/pimcore
-#ln -s ~/build/pimcore /tmp/www/dev/pimcore
+mkdir /tmp/www/dev
+mv ~/build/pimcore /tmp/www/dev
+ln -s /tmp/www/dev/pimcore ~/build/pimcore
+
 
 # add config templates
 mkdir -p /tmp/www/var/config
@@ -39,22 +39,12 @@ cp .travis/composer.local.json /tmp/www/composer.local.json
 
 # create vendor folder and symlink vendor and app folder to home directory
 mkdir -p /tmp/www/vendor
-ln -s /tmp/www/vendor ~
-ln -s /tmp/www/app ~
+#ln -s /tmp/www/vendor ~
+#ln -s /tmp/www/app ~
 
+cat /tmp/www/var/config/extensions.php
 
 # install composer dependencies
 cd /tmp/www
-COMPOSER_DISCARD_CHANGES=true COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --optimize-autoloader -vv
+COMPOSER_DISCARD_CHANGES=true COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --optimize-autoloader
 cd ~/build/pimcore/pimcore
-
-
-echo "----list----"
-cat /tmp/www/composer.local.json
-#ls -la /tmp/www/dev/pimcore
-ls -la /tmp/www/vendor/pimcore
-ls -la ~
-#ls -la /tmp/www/dev/pimcore/pimcore
-cat ~/build/pimcore/pimcore/composer.json
-
-
