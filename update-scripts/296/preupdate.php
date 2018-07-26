2dd4c3a24a499ad2cae695295d28bd6597156121
Bernhard Rusch <bernhard.rusch@elements.at>
2018-07-26 10:54:19 +0200

    added last update script

---
+++ update-scripts/296/preupdate.php
@@ -0,0 +1,6 @@
+<?php
+
+echo '<b>You\'ve reached the end of the traditional update process!</b><br />';
+echo 'Please have a look at the <a href="https://pimcore.com/docs/5.x/Development_Documentation/Installation_and_Upgrade/Upgrade_Notes/Within_V5.html#page_Version-5-4-0">upgrade notes for version 5.4.0</a> to continue the update.';
+echo '<br><div style="text-align:center"><iframe class="giphy-embed" frameborder="0" height="150" src="https://giphy.com/embed/zoiKvXZOiRijC" width="150"></iframe></div>';
+exit;
