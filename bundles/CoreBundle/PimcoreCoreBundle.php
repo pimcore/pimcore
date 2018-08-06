9980bb808afe07bb8c41c0e4eba967e3d6186d5d
Bernhard Rusch <bernhard.rusch@elements.at>
2018-08-06 14:22:25 +0200

    [Composer] Removed config normalizer - no use anymore, since we moved to Composer based updates

--- bundles/CoreBundle/PimcoreCoreBundle.php
+++ bundles/CoreBundle/PimcoreCoreBundle.php
@@ -16,7 +16,6 @@ namespace Pimcore\Bundle\CoreBundle;
 
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\AreabrickPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\CacheCollectorPass;
-use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\ComposerConfigNormalizersPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\DebugStopwatchPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\DoctrineMigrationsParametersPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\MonologPsrLogMessageProcessorPass;
@@ -78,7 +77,6 @@ class PimcoreCoreBundle extends Bundle
         $container->addCompilerPass(new DoctrineMigrationsParametersPass());
         $container->addCompilerPass(new MonologPublicLoggerPass());
         $container->addCompilerPass(new MonologPsrLogMessageProcessorPass());
-        $container->addCompilerPass(new ComposerConfigNormalizersPass());
         $container->addCompilerPass(new DebugStopwatchPass());
     }
 }
