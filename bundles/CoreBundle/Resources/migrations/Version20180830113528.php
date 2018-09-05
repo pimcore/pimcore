4ba741df2c70c1028f2d71ecfbfb38d88b7e48df
Bernhard Rusch <bernhard.rusch@elements.at>
2018-09-05 10:09:01 +0200

    [Payment Refactoring] optimized migrations: do not display SQL warning and execute only if Ecommerce bundle is enabled, see #3287 #3295

--- bundles/CoreBundle/Resources/migrations/Version20180830113528.php
+++ bundles/CoreBundle/Resources/migrations/Version20180830113528.php
@@ -3,41 +3,49 @@
 namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PimcoreEcommerceFrameworkBundle;
 use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
 use Pimcore\Model\DataObject\Fieldcollection\Definition;
 
 class Version20180830113528 extends AbstractPimcoreMigration
 {
+    public function doesSqlMigrations(): bool
+    {
+        return false;
+    }
+
     /**
      * @param Schema $schema
      */
     public function up(Schema $schema)
     {
-        $this->writeMessage("Updating field collection definition 'paymentState' - adding additional paymentState");
+        if (PimcoreEcommerceFrameworkBundle::isEnabled()) {
+            $this->writeMessage("Updating field collection definition 'paymentState' - adding additional paymentState");
 
-        try {
-            $definition = Definition::getByKey('PaymentInfo');
-        } catch (\Exception $e) {
-        }
-
-        if ($definition) {
-            $fieldDefinition = $definition->getFieldDefinition('paymentState');
-
-            if ($fieldDefinition) {
-                $options = $fieldDefinition->getOptions();
-                $options[] = [
-                    'value' => 'abortedButResponseReceived',
-                    'key' => 'Aborted but Response Received'
-                ];
-                $fieldDefinition->setOptions($options);
-
-                $this->writeMessage(" ... saving field collection definition 'paymentState'");
-                if (!$this->isDryRun()) {
-                    $definition->save();
-                }
+            try {
+                $definition = Definition::getByKey('PaymentInfo');
+            } catch (\Exception $e) {
             }
-        } else {
-            $this->writeMessage(' ... nothing to do because field collection definition does not exist.');
+
+            if ($definition) {
+                $fieldDefinition = $definition->getFieldDefinition('paymentState');
+
+                if ($fieldDefinition) {
+                    $options = $fieldDefinition->getOptions();
+                    $options[] = [
+                        'value' => 'abortedButResponseReceived',
+                        'key' => 'Aborted but Response Received'
+                    ];
+                    $fieldDefinition->setOptions($options);
+
+                    $this->writeMessage(" ... saving field collection definition 'paymentState'");
+                    if (!$this->isDryRun()) {
+                        $definition->save();
+                    }
+                }
+            } else {
+                $this->writeMessage(' ... nothing to do because field collection definition does not exist.');
+            }
         }
     }
 
@@ -46,39 +54,41 @@ class Version20180830113528 extends AbstractPimcoreMigration
      */
     public function down(Schema $schema)
     {
-        $this->writeMessage("Updating field collection definition 'paymentState' - removing additional paymentState");
+        if (PimcoreEcommerceFrameworkBundle::isEnabled()) {
+            $this->writeMessage("Updating field collection definition 'paymentState' - removing additional paymentState");
 
-        try {
-            $definition = Definition::getByKey('PaymentInfo');
-        } catch (\Exception $e) {
-        }
+            try {
+                $definition = Definition::getByKey('PaymentInfo');
+            } catch (\Exception $e) {
+            }
 
-        if ($definition) {
-            $fieldDefinition = $definition->getFieldDefinition('paymentState');
+            if ($definition) {
+                $fieldDefinition = $definition->getFieldDefinition('paymentState');
 
-            if ($fieldDefinition) {
-                $options = $fieldDefinition->getOptions();
+                if ($fieldDefinition) {
+                    $options = $fieldDefinition->getOptions();
 
-                $indexToDelete = null;
-                foreach ($options as $index => $option) {
-                    if ($option['value'] == 'abortedButResponseReceived') {
-                        $indexToDelete = $index;
+                    $indexToDelete = null;
+                    foreach ($options as $index => $option) {
+                        if ($option['value'] == 'abortedButResponseReceived') {
+                            $indexToDelete = $index;
+                        }
+                    }
+
+                    if ($indexToDelete !== null) {
+                        unset($options[$indexToDelete]);
+                    }
+
+                    $fieldDefinition->setOptions($options);
+
+                    $this->writeMessage(" ... saving field collection definition 'paymentState'");
+                    if (!$this->isDryRun()) {
+                        $definition->save();
                     }
                 }
-
-                if ($indexToDelete !== null) {
-                    unset($options[$indexToDelete]);
-                }
-
-                $fieldDefinition->setOptions($options);
-
-                $this->writeMessage(" ... saving field collection definition 'paymentState'");
-                if (!$this->isDryRun()) {
-                    $definition->save();
-                }
+            } else {
+                $this->writeMessage(' ... nothing to do because field collection definition does not exist.');
             }
-        } else {
-            $this->writeMessage(' ... nothing to do because field collection definition does not exist.');
         }
     }
 }
