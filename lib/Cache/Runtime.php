pimcore/pimcore

commit 48c0bc37dd3ba79335669f44979721be7ff25402

[Cache] Runtime cache optimizations, reduce container calls, see #3104

lib/Cache/Runtime.php
@@ -19,6 +19,7 @@ final class Runtime extends \ArrayObject
     const SERVICE_ID = __CLASS__;
 
     protected static $tempInstance;
+    protected static $instance;
 
     /**
      * Array of indexes which are blocked from cache. If a given
@@ -39,7 +40,9 @@ final class Runtime extends \ArrayObject
      */
     public static function getInstance()
     {
-        if (\Pimcore::hasContainer()) {
+        if(self::$instance) {
+            return self::$instance;
+        } elseif (\Pimcore::hasContainer()) {
             $container = \Pimcore::getContainer();
 
             /** @var self $instance */
@@ -50,6 +53,8 @@ final class Runtime extends \ArrayObject
                 $instance = new self;
                 $container->set(self::SERVICE_ID, $instance);
             }
+
+            self::$instance = $instance;
 
             if (self::$tempInstance) {
                 // copy values from static temp. instance to the service instance

commit a48b521e7fb49b964dca12b7136f1df66f2d399d

[Cache] Runtime cache optimizations, reduce container calls - fixed mem-leak issue introduced by 48c0bc37dd3ba79335669f44979721be7ff25402, see #3104

lib/Cache/Runtime.php
@@ -205,6 +205,7 @@ final class Runtime extends \ArrayObject
      */
     public static function clear($keepItems = [])
     {
+        self::$instance = null;
         $newInstance = new self();
         $oldInstance = self::getInstance();
 