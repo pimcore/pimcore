13473af124aadd5d2b721bfa7bb5a1a67fad4a05
Alexander Scheider <alex.scheider26@gmail.com>
2018-09-14 12:06:01 +0200

    Fixed localized fields filtering on custom layout change.

--- bundles/AdminBundle/Controller/Admin/DataObjectController.php
+++ bundles/AdminBundle/Controller/Admin/DataObjectController.php
@@ -464,9 +464,6 @@ class DataObjectController extends ElementControllerBase implements EventedContr
                 $objectData['currentLayoutId'] = $currentLayoutId;
             }
 
-            $objectData = $this->filterLocalizedFields($object, $objectData);
-            DataObject\Service::enrichLayoutDefinition($objectData['layout'], $object);
-
             //Hook for modifying return value - e.g. for changing permissions based on object data
             //data need to wrapped into a container in order to pass parameter to event listeners by reference so that they can change the values
             $event = new GenericEvent($this, [
