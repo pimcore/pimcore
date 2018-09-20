e771f5091cbc23ef582841ef227e206cf0e2a963
weisswurstkanone <josef.aichhorn@elements.at>
2018-09-20 12:18:29 +0200

    [Object] Delta updates on database (Step 1) - migration script #3317

--- bundles/CoreBundle/Resources/migrations/Version20180912140913.php
+++ bundles/CoreBundle/Resources/migrations/Version20180912140913.php
@@ -4,6 +4,7 @@ namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Pimcore\Migrations\Migration\AbstractPimcoreMigration;
+use Pimcore\Model\DataObject\Objectbrick\Definition;
 
 /**
  * Class Version20180912140913
