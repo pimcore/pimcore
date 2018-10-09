d37b41a9dfa6db746fbbf79cd1db6cd3f372158d
Bernhard Rusch <bernhard.rusch@elements.at>
2018-10-09 14:06:42 +0200

    Optimized migrations for #3440

--- bundles/CoreBundle/Resources/migrations/Version20181008132414.php
+++ bundles/CoreBundle/Resources/migrations/Version20181008132414.php
@@ -18,10 +18,9 @@ class Version20181008132414 extends AbstractPimcoreMigration
      */
     public function up(Schema $schema)
     {
-        $db = Db::get();
-        $db->query('ALTER TABLE `objects` ADD COLUMN `o_versionCount` INT UNSIGNED NULL DEFAULT \'0\' AFTER `o_childrenSortBy`;');
-        $db->query('ALTER TABLE `documents`ADD COLUMN `versionCount` INT UNSIGNED NULL DEFAULT \'0\' AFTER `userModification`;');
-        $db->query('ALTER TABLE `assets` ADD COLUMN `versionCount` INT UNSIGNED NOT NULL DEFAULT \'0\' AFTER `hasMetaData`;');
+        $this->addSql('ALTER TABLE `objects` ADD COLUMN `o_versionCount` INT UNSIGNED NULL DEFAULT \'0\' AFTER `o_childrenSortBy`;');
+        $this->addSql('ALTER TABLE `documents`ADD COLUMN `versionCount` INT UNSIGNED NULL DEFAULT \'0\' AFTER `userModification`;');
+        $this->addSql('ALTER TABLE `assets` ADD COLUMN `versionCount` INT UNSIGNED NOT NULL DEFAULT \'0\' AFTER `hasMetaData`;');
     }
 
     /**
@@ -29,10 +28,8 @@ class Version20181008132414 extends AbstractPimcoreMigration
      */
     public function down(Schema $schema)
     {
-        $db = Db::get();
-        $db->query('ALTER TABLE `objects` DROP COLUMN `o_versionCount`;');
-        $db->query('ALTER TABLE `documents` DROP COLUMN `versionCount`;');
-        $db->query('ALTER TABLE `assets` DROP COLUMN `versionCount`;');
-
+        $this->addSql('ALTER TABLE `objects` DROP COLUMN `o_versionCount`;');
+        $this->addSql('ALTER TABLE `documents` DROP COLUMN `versionCount`;');
+        $this->addSql('ALTER TABLE `assets` DROP COLUMN `versionCount`;');
     }
 }
