b4d7fac241833df2b82eb9a5dc4453c503264a5e
Bernhard Rusch <bernhard.rusch@elements.at>
2019-01-04 15:28:39 +0100

    Fixed error message in post update command - see #3804

--- bundles/CoreBundle/Command/OptimizeImageThumbnailsCommand.php
+++ bundles/CoreBundle/Command/OptimizeImageThumbnailsCommand.php
@@ -20,7 +20,7 @@ use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Finder\Finder;
 
-class ThumbnailsOptimizeImagesCommand extends AbstractCommand
+class OptimizeImageThumbnailsCommand extends AbstractCommand
 {
     /**
      * @var ImageOptimizerInterface
