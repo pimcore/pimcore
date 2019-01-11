4301e2a2ddd6f3a3d0d2c0095bf7746b9fe512fe
Bernhard Rusch <bernhard.rusch@elements.at>
2019-01-11 14:44:14 +0100

    [Cache] Cache Templating Helper should be aware of WebP-Support - fixes #3826

--- lib/Templating/Helper/Cache.php
+++ lib/Templating/Helper/Cache.php
@@ -17,6 +17,7 @@ namespace Pimcore\Templating\Helper;
 use Pimcore\Cache as CacheManager;
 use Pimcore\Http\Request\Resolver\EditmodeResolver;
 use Symfony\Component\Templating\Helper\Helper;
+use Pimcore\Tool;
 
 class Cache extends Helper
 {
@@ -69,6 +70,10 @@ class Cache extends Helper
     {
         $this->key = 'pimcore_viewcache_' . $name;
         $this->force = $force;
+
+        if (Tool\Frontend::hasWebpSupport()) {
+            $this->key .= 'webp';
+        }
 
         if (!$lifetime) {
             $lifetime = null;
