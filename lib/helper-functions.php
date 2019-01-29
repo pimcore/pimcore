16df74b9de35677c0e9af9f65a281f1544bf56b9
Bernhard Rusch <bernhard.rusch@elements.at>
2019-01-29 15:15:46 +0100

    formatBytes() should use base 10 - resolves #3889

--- lib/helper-functions.php
+++ lib/helper-functions.php
@@ -288,10 +288,10 @@ function formatBytes($bytes, $precision = 2)
     $units = ['B', 'KB', 'MB', 'GB', 'TB'];
 
     $bytes = max($bytes, 0);
-    $pow = floor(($bytes ? log($bytes) : 0) / log(1024));
+    $pow = floor(($bytes ? log($bytes) : 0) / log(1000));
     $pow = min($pow, count($units) - 1);
 
-    $bytes /= pow(1024, $pow);
+    $bytes /= pow(1000, $pow);
 
     return round($bytes, $precision) . ' ' . $units[$pow];
 }
