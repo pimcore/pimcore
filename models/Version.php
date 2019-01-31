e26a736a201bedd9d5ef4ebe2bcbc604c4e5daf7
ckogler <christian.kogler@elements.at>
2019-01-31 13:54:00 +0100

    Delete version if it cant be deserialized because of a incompatible/ missing class

--- models/Version.php
+++ models/Version.php
@@ -299,6 +299,11 @@ class Version extends AbstractModel
 
         if ($this->getSerialized()) {
             $data = Serialize::unserialize($data);
+            if(get_class($data) == '__PHP_Incomplete_Class'){
+                Logger::err('Version: cannot read version data from file system becaus of incompatible class.');
+                $this->delete();
+                return;
+            }
         }
 
         if ($data instanceof Concrete) {
