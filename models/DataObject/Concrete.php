ff9b30bdf9be171cf0a1305e3028ef5f9d3d679a
weisswurstkanone <josef.aichhorn@elements.at>
2019-02-18 15:35:51 +0100

    Relations: query table not always updated when parent changes #3955

--- models/DataObject/Concrete.php
+++ models/DataObject/Concrete.php
@@ -228,7 +228,7 @@ class Concrete extends AbstractObject
 
             $newVersionCount = $this->getVersionCount();
 
-            if ($newVersionCount != $oldVersionCount + 1) {
+            if (($newVersionCount != $oldVersionCount + 1) || ($this instanceof DirtyIndicatorInterface && $this->isFieldDirty("o_parentId"))) {
                 self::disableDirtyDetection();
             }
 
