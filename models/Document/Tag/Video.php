146d81152fe1719d4282bb9dc107e5378fa650fe
Bernhard Rusch <brusch@users.noreply.github.com>
2019-02-21 14:58:43 +0100

    Update Video.php

--- models/Document/Tag/Video.php
+++ models/Document/Tag/Video.php
@@ -382,8 +382,8 @@ class Video extends Model\Document\Tag
                 if ($this->poster && ($poster = Asset::getById($this->poster))) {
                     $image = $poster->getThumbnail($imageThumbnailConf);
                 } else {
-                    if ($asset->getCustomSetting('image_thumbnail_asset')) {
-                        $image = (string) $asset->getImageThumbnail($asset->getCustomSetting('image_thumbnail_asset'));
+                    if ($asset->getCustomSetting('image_thumbnail_asset') && ($customPreviewAsset = Asset::getById($asset->getCustomSetting('image_thumbnail_asset')))) {
+                        $image = (string) $customPreviewAsset->getImageThumbnail($imageThumbnailConf);
                     } else {
                         $image = (string) $asset->getImageThumbnail($imageThumbnailConf);
                     }
