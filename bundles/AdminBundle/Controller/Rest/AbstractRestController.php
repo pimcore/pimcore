626c617d3c1ec9191bb826fc80fe1472285ca97c
weisswurstkanone <josef.aichhorn@elements.at>
2019-03-08 15:58:00 +0100

    REST Webservice: make filter condition builder available for public use

--- bundles/AdminBundle/Controller/Rest/AbstractRestController.php
+++ bundles/AdminBundle/Controller/Rest/AbstractRestController.php
@@ -312,7 +312,7 @@ abstract class AbstractRestController extends AdminController
             throw new \Exception('failed to parse filter');
         }
 
-        $condition = $this->doBuildCondition($q);
+        $condition = Helper::buildSqlCondition($q);
 
 //        var_dump($condition);
 //        die();
@@ -320,99 +320,6 @@ abstract class AbstractRestController extends AdminController
         return $condition;
     }
 
-    protected function doBuildCondition($q, $op = null, $subject = null)
-    {
-
-        //    Examples:
-        //
-        //q={"o_modificationDate" : {"$gt" : "1000"}}
-        //where ((`o_modificationDate` > '1000') )
-        //
-        //
-        //
-        //
-        //q=[{"o_modificationDate" : {"$gt" : "1000"}}, {"o_modificationDate" : {"$lt" : "9999"}}]
-        //where ( ((`o_modificationDate` > '1000') )  AND  ((`o_modificationDate` < '9999') )  )
-        //
-        //
-        //
-        //
-        //q={"o_modificationDate" : {"$gt" : "1000"}, "$or": [{"o_id": "3", "o_key": {"$like" :"%lorem-ipsum%"}}]}
-        //where ((`o_modificationDate` > '1000') AND  ((`o_id` = '3') OR  ((`o_key` LIKE '%lorem-ipsum%') )  )  )
-        //
-        // q={"$and" : [{"o_published": "0"}, {"o_modificationDate" : {"$gt" : "1000"}, "$or": [{"o_id": "3", "o_key": {"$like" :"%lorem-ipsum%"}}]}]}
-        //
-        // where ( ((`o_published` = '0') )  AND  ((`o_modificationDate` > '1000') AND  ((`o_id` = '3') OR (`o_key` LIKE '%lorem-ipsum%') )  )  )
-
-        if (!$op) {
-            $op = 'AND';
-        }
-        $mappingTable = ['$gt' => '>', '$gte' => '>=', '$lt' => '<', '$lte' => '<', '$like' => 'LIKE', '$notlike' => 'NOT LIKE', '$notnull' => 'IS NOT NULL',
-                '$not' => 'NOT'];
-        $ops = array_keys($mappingTable);
-
-        $db = Db::get();
-
-        $parts = [];
-        if (is_string($q)) {
-            return $q;
-        }
-
-        foreach ($q as $key => $value) {
-            if (array_search(strtolower($key), ['$and', '$or']) !== false) {
-                $childOp = strtolower($key) == '$and' ? 'AND' : 'OR';
-
-                if (is_array($value)) {
-                    foreach ($value as $arrItem) {
-                        $parts[] = $this->doBuildCondition($arrItem, $childOp);
-                    }
-                } else {
-                    $parts[] = $this->doBuildCondition($value, $childOp);
-                }
-            } else {
-                if (is_array($value)) {
-                    foreach ($value as $subValue) {
-                        $parts[] = $this->doBuildCondition($subValue);
-                    }
-                } elseif ($value instanceof \stdClass) {
-                    $objectVars = get_object_vars($value);
-                    foreach ($objectVars as $objectVar => $objectValue) {
-                        if (array_search(strtolower($objectVar), $ops) !== false) {
-                            $innerOp = $mappingTable[strtolower($objectVar)];
-                            if ($innerOp == 'NOT') {
-                                $parts[] = '( NOT ' . $db->quoteIdentifier($key) . ' =' . $db->quote($objectValue) . ')';
-                            } else {
-                                $parts[] = '(' . $db->quoteIdentifier($key) . ' ' . $innerOp . ' ' . $db->quote($objectValue) . ')';
-                            }
-                        } else {
-                            if ($objectValue instanceof \stdClass) {
-                                $parts[] = $this->doBuildCondition($objectValue, null, $objectVar);
-                            } else {
-                                $parts[] = '(' . $db->quoteIdentifier($objectVar) . ' = ' . $db->quote($objectValue) . ')';
-                            }
-                        }
-                    }
-                    $combinedParts = implode(' ' . $op . ' ', $parts);
-                    $parts = [$combinedParts];
-                } else {
-                    if (array_search(strtolower($key), $ops) !== false) {
-                        $innerOp = $mappingTable[strtolower($key)];
-                        if ($innerOp == 'NOT') {
-                            $parts[] = '(NOT' . $db->quoteIdentifier($subject) . ' = ' . $db->quote($value) . ')';
-                        } else {
-                            $parts[] = '(' . $db->quoteIdentifier($subject) . ' ' . $innerOp . ' ' . $db->quote($value) . ')';
-                        }
-                    } else {
-                        $parts[] = '(' . $db->quoteIdentifier($key) . ' = ' . $db->quote($value) . ')';
-                    }
-                }
-            }
-        }
-
-        $subCondition = ' (' . implode(' ' . $op . ' ', $parts) . ' ) ';
-
-        return $subCondition;
-    }
 
     /**
      * @param FilterEvent $event
