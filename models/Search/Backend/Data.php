3e65953aa04e296268774a701a01c52ee03343d6
Lars Greiss <l.greiss@mediacologne.de>
2019-04-09 18:54:27 +0200

    [BackendSearch] restart save transaction if it fails

--- models/Search/Backend/Data.php
+++ models/Search/Backend/Data.php
@@ -544,7 +544,29 @@ class Data extends \Pimcore\Model\AbstractModel
     {
         if ($this->id instanceof Data\Id) {
             \Pimcore::getEventDispatcher()->dispatch(SearchBackendEvents::PRE_SAVE, new SearchBackendEvent($this));
-            $this->getDao()->save();
+
+            $maxRetries = 5;
+            for ($retries = 0; $retries < $maxRetries; $retries++) {
+                try {
+                    $this->getDao()->save();
+                    // successfully completed, so we cancel the loop here -> no restart required
+                    break;
+                } catch (\Exception $e) {
+                    // we try to start saving $maxRetries times again (deadlocks, ...)
+                    if ($retries < ($maxRetries - 1)) {
+                        $run = $retries + 1;
+                        $waitTime = rand(1, 5) * 100000;
+                        Logger::warn('Unable to finish transaction (' . $run . ". run) because of the following reason '" . $e->getMessage() . "'. --> Retrying in " . $waitTime . ' microseconds ... (' . ($run + 1) . ' of ' . $maxRetries . ')');
+
+                        // wait specified time until we restart
+                        usleep($waitTime);
+                    } else {
+                        // if we fail after $maxRetries retries, we throw out the exception
+                        throw $e;
+                    }
+                }
+            }
+
             \Pimcore::getEventDispatcher()->dispatch(SearchBackendEvents::POST_SAVE, new SearchBackendEvent($this));
         } else {
             throw new \Exception('Search\\Backend\\Data cannot be saved - no id set!');
