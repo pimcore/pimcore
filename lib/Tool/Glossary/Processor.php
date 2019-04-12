aab70d396640a73d5c18d12221e2604a229e0b56
Bernhard Rusch <brusch@users.noreply.github.com>
2019-04-12 15:01:11 +0200

    shortened regex

--- lib/Tool/Glossary/Processor.php
+++ lib/Tool/Glossary/Processor.php
@@ -254,9 +254,9 @@ class Processor
 
             // add PCRE delimiter and modifiers
             if ($d['exactmatch']) {
-                $d['text'] = '/<a.*?(?<!\w)' . preg_quote($d['text'], '/') . '(?!\w).*?\/a>(*SKIP)(*FAIL)|(?<!\w)' . preg_quote($d['text'], '/') . '(?!\w)/';
+                $d['text'] = '/<a.*\/a>(*SKIP)(*FAIL)|(?<!\w)' . preg_quote($d['text'], '/') . '(?!\w)/';
             } else {
-                $d['text'] = '/<a.*?' . preg_quote($d['text'], '/') . '.*?\/a>(*SKIP)(*FAIL)|' . preg_quote($d['text'], '/') . '/';
+                $d['text'] = '/<a.*\/a>(*SKIP)(*FAIL)|' . preg_quote($d['text'], '/') . '/';
             }
 
             if (!$d['casesensitive']) {
