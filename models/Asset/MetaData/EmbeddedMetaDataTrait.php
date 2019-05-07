e155a904661e75245aad3d3356db9cfab45bfc0f
pbrandstaetter <patrick.brandstaetter@elements.at>
2019-05-07 12:14:58 +0200

    fixed space

--- models/Asset/MetaData/EmbeddedMetaDataTrait.php
+++ models/Asset/MetaData/EmbeddedMetaDataTrait.php
@@ -70,7 +70,7 @@ trait EmbeddedMetaDataTrait
 
         if (stream_is_local($this->getStream()) && $exiftool && $useExifTool) {
             $path = escapeshellarg($filePath);
-            if(!file_exists($path)){
+            if (!file_exists($path)) {
                 $path = escapeshellarg($this->getTemporaryFile());
             }
             $output = Tool\Console::exec($exiftool . ' -j ' . $path);
