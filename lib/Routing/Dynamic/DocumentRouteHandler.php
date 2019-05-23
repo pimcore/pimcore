pimcore/pimcore

commit f1914f4568865c40aeaccac3de123a4ef36ffe10

Remove Legacy Mode and Support for v4 Compatibility Bridge (#4405)

lib/Routing/Dynamic/DocumentRouteHandler.php
@@ -179,11 +179,6 @@ class DocumentRouteHandler implements DynamicRouteHandlerInterface
             }
         }
 
-        // check if document should be handled (not legacy)
-        if (!$this->isDocumentSupported($document)) {
-            return null;
-        }
-
         $route = new DocumentRoute($document->getFullPath());
         $route->setOption('utf8', true);
 
@@ -364,15 +359,5 @@ class DocumentRouteHandler implements DynamicRouteHandlerInterface
         }
 
         return false;
-    }
-
-    /**
-     * @param Document $document
-     *
-     * @return bool
-     */
-    private function isDocumentSupported(Document $document)
-    {
-        return !$document->doRenderWithLegacyStack();
     }
 }

bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
@@ -224,7 +224,6 @@ class DocumentController extends ElementControllerBase implements EventedControl
                     $createValues['controller'] = $docType->getController();
                     $createValues['action'] = $docType->getAction();
                     $createValues['module'] = $docType->getModule();
-                    $createValues['legacy'] = $docType->getLegacy();
                 } elseif ($request->get('translationsBaseDocument')) {
                     $translationsBaseDocument = Document::getById($request->get('translationsBaseDocument'));
                     $createValues['template'] = $translationsBaseDocument->getTemplate();

bundles/AdminBundle/Controller/Admin/IndexController.php
@@ -241,7 +241,6 @@ class IndexController extends AdminController
             'disableMinifyJs' => \Pimcore::disableMinifyJs(),
             'environment' => $kernel->getEnvironment(),
             'sessionId' => htmlentities(Session::getSessionId(), ENT_QUOTES, 'UTF-8'),
-            'isLegacyModeAvailable' => \Pimcore::isLegacyModeAvailable()
         ]);
 
         // languages

bundles/AdminBundle/Controller/ExtensionManager/ExtensionManagerController.php
@@ -17,7 +17,6 @@ namespace Pimcore\Bundle\AdminBundle\Controller\ExtensionManager;
 use ForceUTF8\Encoding;
 use Pimcore\Bundle\AdminBundle\Controller\AdminController;
 use Pimcore\Bundle\AdminBundle\HttpFoundation\JsonResponse;
-use Pimcore\Bundle\LegacyBundle\Controller\Admin\ExtensionManager\LegacyExtensionManagerController;
 use Pimcore\Cache\Symfony\CacheClearer;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Extension\Bundle\Exception\BundleNotFoundException;
@@ -37,10 +36,6 @@ use Symfony\Component\HttpKernel\KernelInterface;
 use Symfony\Component\Process\Exception\ProcessFailedException;
 use Symfony\Component\Routing\Annotation\Route;
 
-/**
- * Handles all "new" extensions as of pimcore 5 (bundles, new areabrick layout) and pipes legacy extension requests
- * to legacy controller when the legacy bundle is enabled.
- */
 class ExtensionManagerController extends AdminController implements EventedControllerInterface
 {
     /**
@@ -88,11 +83,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
             $this->getBundleList(),
             $this->getBrickList()
         );
-
-        $legacyController = $this->getLegacyController();
-        if ($legacyController) {
-            $extensions = array_merge($extensions, $legacyController->getExtensions());
-        }
 
         return $this->adminJson(['extensions' => $extensions]);
     }
@@ -161,10 +151,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
         CacheClearer $cacheClearer,
         AssetsInstaller $assetsInstaller
     ) {
-        if (null !== $response = $this->handleLegacyRequest($request, __FUNCTION__)) {
-            return $response;
-        }
-
         $type = $request->get('type');
         $id = $request->get('id');
         $enable = $request->get('method', 'enable') === 'enable' ? true : false;
@@ -248,10 +234,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
      */
     public function installAction(Request $request)
     {
-        if (null !== $response = $this->handleLegacyRequest($request, __FUNCTION__)) {
-            return $response;
-        }
-
         return $this->handleInstallation($request, true);
     }
 
@@ -264,86 +246,7 @@ class ExtensionManagerController extends AdminController implements EventedContr
      */
     public function uninstallAction(Request $request)
     {
-        if (null !== $response = $this->handleLegacyRequest($request, __FUNCTION__)) {
-            return $response;
-        }
-
         return $this->handleInstallation($request, false);
-    }
-
-    /**
-     * @Route("/admin/update", methods={"POST"})
-     *
-     * @param Request $request
-     *
-     * @return JsonResponse
-     */
-    public function updateAction(Request $request)
-    {
-        try {
-            $bundle = $this->bundleManager->getActiveBundle($request->get('id'), false);
-
-            $this->bundleManager->update($bundle);
-
-            $data = [
-                'success' => true,
-                'bundle' => $this->buildBundleInfo($bundle, true, true)
-            ];
-
-            if (!empty($message = $this->getInstallerOutput($bundle))) {
-                $data['message'] = $message;
-            }
-
-            return $this->adminJson($data);
-        } catch (BundleNotFoundException $e) {
-            return $this->adminJson([
-                'success' => false,
-                'message' => $e->getMessage()
-            ], 404);
-        } catch (\Exception $e) {
-            return $this->adminJson([
-                'success' => false,
-                'message' => $e->getMessage()
-            ], 400);
-        }
-    }
-
-    /**
-     * @return LegacyExtensionManagerController|null
-     */
-    private function getLegacyController()
-    {
-        if (\Pimcore::isLegacyModeAvailable()) {
-            if ($this->container->has('pimcore.legacy.controller.admin.extension_manager')) {
-                return $this->get('pimcore.legacy.controller.admin.extension_manager');
-            }
-        }
-    }
-
-    /**
-     * Pipe request to legacy controller
-     *
-     * @param Request $request
-     * @param $method
-     *
-     * @return JsonResponse|null
-     */
-    private function handleLegacyRequest(Request $request, $method)
-    {
-        if ($request->get('extensionType') !== 'legacy') {
-            return null;
-        }
-
-        $legacyController = $this->getLegacyController();
-        if (!$legacyController) {
-            throw new BadRequestHttpException(sprintf('Tried to call to legacy extension action %s, but legacy controller was not found', $method));
-        }
-
-        if (!method_exists($legacyController, $method)) {
-            throw new BadRequestHttpException(sprintf('Legacy extension action %s, does not exist on legacy controller', $method));
-        }
-
-        return call_user_func_array([$legacyController, $method], [$request]);
     }
 
     /**
@@ -491,7 +394,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
             'active' => $enabled,
             'installable' => false,
             'uninstallable' => false,
-            'updateable' => false,
             'installed' => $installed,
             'canChangeState' => $bm->canChangeState($bundle),
             'configuration' => $this->getIframePath($bundle),
@@ -505,7 +407,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
             $info = array_merge($info, [
                 'installable' => $bm->canBeInstalled($bundle),
                 'uninstallable' => $bm->canBeUninstalled($bundle),
-                'updateable' => $bm->canBeUpdated($bundle),
             ]);
         }
 
@@ -573,7 +474,6 @@ class ExtensionManagerController extends AdminController implements EventedContr
             'description' => $this->trans($brick->getDescription()),
             'installable' => false,
             'uninstallable' => false,
-            'updateable' => false,
             'installed' => true,
             'active' => $this->areabrickManager->isEnabled($brick->getId()),
             'version' => $brick->getVersion()

bundles/AdminBundle/Controller/Rest/InfoController.php
@@ -14,7 +14,6 @@
 
 namespace Pimcore\Bundle\AdminBundle\Controller\Rest;
 
-use Pimcore\ExtensionManager;
 use Pimcore\Tool\Console;
 use Pimcore\Version;
 use Symfony\Component\HttpFoundation\Request;
@@ -79,12 +78,10 @@ class InfoController extends AbstractRestController
         ];
 
         // TODO add new bundles here
-        $plugins = ExtensionManager::getPluginConfigs();
 
         return $this->createSuccessResponse([
             'system' => $system,
             'pimcore' => $pimcore,
-            'plugins' => $plugins
         ], false);
     }
 

bundles/AdminBundle/Resources/public/js/pimcore/document/settings_abstract.js
@@ -19,7 +19,7 @@ pimcore.document.settings_abstract = Class.create({
     },
 
     setDocumentType: function (field, newValue, oldValue) {
-        var allowedFields = ["module","controller","action","template","legacy"];
+        var allowedFields = ["module","controller","action","template"];
         var form = this.getLayout().getForm();
         var element = null;
 
@@ -262,7 +262,7 @@ pimcore.document.settings_abstract = Class.create({
                 },
                 {
                     xtype:'combo',
-                    fieldLabel: t('bundle') + "(" + t('optional') + ")",
+                    fieldLabel: t('bundle') + " (" + t('optional') + ")",
                     itemId: "bundle",
                     displayField: 'name',
                     valueField: 'name',
@@ -383,14 +383,6 @@ pimcore.document.settings_abstract = Class.create({
                     }
                 }
             ]
-        });
-
-        fieldSet.add({
-            xtype: "checkbox",
-            fieldLabel: t("legacy_mode"),
-            name: "legacy",
-            checked: this.document.data.legacy,
-            hidden: !pimcore.settings.isLegacyModeAvailable
         });
 
         return fieldSet;

bundles/AdminBundle/Resources/public/js/pimcore/extensionmanager/admin.js
@@ -52,10 +52,6 @@ pimcore.extensionmanager.admin = Class.create({
         return this.panel;
     },
 
-    isLegacyType: function (type) {
-        return Ext.Array.contains(['plugin', 'brick'], type);
-    },
-
     getExtensionId: function (record) {
         var extensionId = record.get('extensionId');
         if (extensionId) {
@@ -63,14 +59,6 @@ pimcore.extensionmanager.admin = Class.create({
         }
 
         return record.get('id');
-    },
-
-    getExtensionType: function (record) {
-        if (this.isLegacyType(record.get('type'))) {
-            return 'legacy';
-        }
-
-        return null;
     },
 
     getGrid: function () {
@@ -82,7 +70,7 @@ pimcore.extensionmanager.admin = Class.create({
                 extend: 'Ext.data.Model',
                 fields: [
                     "id", "extensionId", "type", "name", "description", "installed", "installable", "uninstallable", "active",
-                    "configuration", "updateable", "canChangeState", "version", "priority", "environments"
+                    "configuration", "canChangeState", "version", "priority", "environments"
                 ],
                 proxy: {
                     type: 'ajax',
@@ -270,8 +258,7 @@ pimcore.extensionmanager.admin = Class.create({
                             params: {
                                 method: method,
                                 id: self.getExtensionId(rec),
-                                type: rec.get("type"),
-                                extensionType: self.getExtensionType(rec)
+                                type: rec.get("type")
                             },
                             success: handleSuccess,
                             failure: handleFailure
@@ -314,50 +301,7 @@ pimcore.extensionmanager.admin = Class.create({
                             method: 'POST',
                             params: {
                                 id: self.getExtensionId(rec),
-                                type: rec.get("type"),
-                                extensionType: self.getExtensionType(rec)
-                            },
-                            success: handleSuccess,
-                            failure: handleFailure
-                        });
-                    }.bind(this)
-                }]
-            },
-            {
-                text: t('update'),
-                menuText: t('update'),
-                xtype: 'actioncolumn',
-                width: 100,
-                items: [{
-                    tooltip: t('update'),
-                    getClass: function (v, meta, rec) {
-                        if (rec.get('updateable')) {
-                            return 'pimcore_action_column pimcore_icon_add';
-                        }
-
-                        return '';
-                    },
-                    handler: function (grid, rowIndex) {
-                        var rec = grid.getStore().getAt(rowIndex);
-
-                        // legacy types can't be updated
-                        if (self.isLegacyType(rec.get('type'))) {
-                            return;
-                        }
-
-                        if (!rec.get('updateable')) {
-                            return;
-                        }
-
-                        this.panel.setLoading(true);
-
-                        Ext.Ajax.request({
-                            url: '/admin/extensionmanager/admin/update',
-                            method: 'POST',
-                            params: {
-                                id: self.getExtensionId(rec),
-                                type: rec.get("type"),
-                                extensionType: self.getExtensionType(rec)
+                                type: rec.get("type")
                             },
                             success: handleSuccess,
                             failure: handleFailure

bundles/AdminBundle/Resources/public/js/pimcore/settings/docTypes.js
@@ -56,13 +56,6 @@ pimcore.settings.document.doctypes = Class.create({
 
         this.store = pimcore.globalmanager.get("document_types_store");
 
-        var legacyCheck = new Ext.grid.column.Check({
-            text: t("legacy_mode"),
-            dataIndex: "legacy",
-            hidden: !pimcore.settings.isLegacyModeAvailable,
-            width: 90
-        });
-
         var typesColumns = [
             {
                 text: t("name"),
@@ -243,7 +236,6 @@ pimcore.settings.document.doctypes = Class.create({
                     triggerAction: "all"
                 })
             },
-            legacyCheck,
             {
                 text: t("creationDate"),
                 sortable: true,

commit 59d7d475b594420e0f83d80c2a9ac82b9c98b5f3

Fixed using undefined method 'isDocumentSupported' - caused by #4405 - fixes #4423

lib/Routing/Dynamic/DocumentRouteHandler.php
@@ -110,7 +110,7 @@ class DocumentRouteHandler implements DynamicRouteHandlerInterface
         if (preg_match('/^document_(\d+)$/', $name, $match)) {
             $document = Document::getById($match[1]);
 
-            if ($this->isDirectRouteDocument($document) && $this->isDocumentSupported($document)) {
+            if ($this->isDirectRouteDocument($document)) {
                 return $this->buildRouteForDocument($document);
             }
         }