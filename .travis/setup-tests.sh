6a04c6d1750d08be61db27479b3491c7c94fd8dd
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2019-05-24 09:48:00 +0200

    move app/bin/web files to .travis

--- .travis/setup-tests.sh
+++ .travis/setup-tests.sh
@@ -8,6 +8,10 @@ cp .travis/system.template.php var/config/system.php
 cp .travis/extensions.template.php var/config/extensions.php
 cp app/config/parameters.example.yml app/config/parameters.yml
 
+cp -r .travis/app app
+cp -r .travis/bin/console bin/console
+cp -r .travis/web web
+
 # install composer dependencies
 COMPOSER_MEMORY_LIMIT=-1 composer install --no-interaction --optimize-autoloader
 COMPOSER_MEMORY_LIMIT=-1 composer require symfony/symfony:$SYMFONY_VERSION --no-interaction --optimize-autoloader
