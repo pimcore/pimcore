4d94f025838526d9e4a33b01ee6b8f0d09d8d07c
Divesh <divesh.pahuja@pimcore.com>
2019-07-05 18:55:31 +0200

    [Document] Restrict document translation linking without language - Improved check

--- bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
+++ bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
@@ -1448,15 +1448,15 @@ class DocumentController extends ElementControllerBase implements EventedControl
         $sourceDocument = Document::getById($request->get('sourceId'));
         $targetDocument = Document::getByPath($request->get('targetPath'));
 
-        if (empty($sourceDocument->getProperty("language"))) {
-            throw new \Exception(sprintf("Source Document(ID:%s) Language(Properties) missing", $sourceDocument->getId()));
-        }
-
-        if (empty($targetDocument->getProperty("language"))) {
-            throw new \Exception(sprintf("Target Document(ID:%s) Language(Properties) missing", $sourceDocument->getId()));
-        }
-
         if ($sourceDocument && $targetDocument) {
+            if (empty($sourceDocument->getProperty("language"))) {
+                throw new \Exception(sprintf("Source Document(ID:%s) Language(Properties) missing", $sourceDocument->getId()));
+            }
+
+            if (empty($targetDocument->getProperty("language"))) {
+                throw new \Exception(sprintf("Target Document(ID:%s) Language(Properties) missing", $sourceDocument->getId()));
+            }
+
             $service = new Document\Service;
             if ($service->getTranslationSourceId($targetDocument) != $targetDocument->getId()) {
                 throw new \Exception('Target Document already linked to Source Document ID('.$service->getTranslationSourceId($targetDocument).'). Please unlink existing relation first.');
