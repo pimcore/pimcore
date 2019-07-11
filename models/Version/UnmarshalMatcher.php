138616cf94b2f52f7b58aeb61a710460a2ee5155
weisswurstkanone <josef.aichhorn@elements.at>
2019-07-11 14:44:24 +0200

    Reduce size of version files #4667

---
+++ models/Version/UnmarshalMatcher.php
@@ -0,0 +1,44 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @category   Pimcore
+ * @package    Element
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Model\Version;
+
+use DeepCopy\TypeMatcher\TypeMatcher;
+
+class UnmarshalMatcher extends TypeMatcher
+{
+    /**
+     * UnmarshalMatcher constructor.
+     */
+    public function __construct()
+    {
+    }
+
+
+    /**
+     * @param mixed $element
+     *
+     * @return boolean
+     */
+    public function matches($element)
+    {
+        if ($element instanceof ElementDescriptor) {
+            return true;
+        }
+        return false;
+    }
+}
--- tests/_support/Util/TestHelper.php
+++ tests/_support/Util/TestHelper.php
@@ -780,4 +780,18 @@ class TestHelper
 
         return $path;
     }
+
+    /**
+     * @param int $length
+     * @return string
+     */
+    public static function generateRandomString($length = 10) {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+        return $randomString;
+    }
 }
