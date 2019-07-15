c020d9e9eb3b977928281bd70e3cdd194971c8e4
Divesh <divesh.pahuja@pimcore.com>
2019-07-15 10:38:11 +0200

    [Document] Restrict document translation linking without language - Moved language filter to DataObject Service

--- bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -512,8 +512,6 @@ class DataObjectController extends ElementControllerBase implements EventedContr
             $eventDispatcher->dispatch(AdminEvents::OBJECT_GET_PRE_SEND_DATA, $event);
             $data = $event->getArgument('data');
 
-            $data = $this->filterLocalizedFields($object, $data);
-
             DataObject\Service::removeObjectFromSession($object->getId());
 
             return $this->adminJson($data);
@@ -697,55 +695,6 @@ class DataObjectController extends ElementControllerBase implements EventedContr
                 }
             }
         }
-    }
-
-    /**
-     * @param DataObject\AbstractObject $object
-     * @param $objectData
-     *
-     * @return mixed
-     */
-    protected function filterLocalizedFields(DataObject\AbstractObject $object, $objectData)
-    {
-        if (!($object instanceof DataObject\Concrete)) {
-            return $objectData;
-        }
-
-        $user = Tool\Admin::getCurrentUser();
-        if ($user->getAdmin()) {
-            return $objectData;
-        }
-
-        $fieldDefinitions = $object->getClass()->getFieldDefinitions();
-        if ($fieldDefinitions) {
-            $languageAllowedView = DataObject\Service::getLanguagePermissions($object, $user, 'lView');
-            $languageAllowedEdit = DataObject\Service::getLanguagePermissions($object, $user, 'lEdit');
-
-            foreach ($fieldDefinitions as $key => $fd) {
-                if ($fd->getFieldtype() == 'localizedfields') {
-                    foreach ($objectData['data'][$key]['data'] as $language => $languageData) {
-                        if (!is_null($languageAllowedView) && !$languageAllowedView[$language]) {
-                            unset($objectData['data'][$key]['data'][$language]);
-                        }
-                    }
-                } else if ($fd instanceof DataObject\ClassDefinition\Data\Fieldcollections
-                    || $fd instanceof DataObject\ClassDefinition\Data\Objectbricks
-                    || $fd instanceof DataObject\ClassDefinition\Data\Block) {
-                        foreach ($objectData['data'][$key] as $ckey => $collection) {
-                            if (isset($collection['data']["localizedfields"])) {
-                                foreach ($collection['data']["localizedfields"]["data"] as $language => $languageData) {
-                                    if (!is_null($languageAllowedView) && !$languageAllowedView[$language]) {
-                                        unset($objectData['data'][$key][$ckey]['data']['localizedfields']['data'][$language]);
-                                    }
-                                }
-                            }
-                        }
-                }
-            }
-            $this->setLayoutPermission($objectData['layout'], $languageAllowedView, $languageAllowedEdit);
-        }
-
-        return $objectData;
     }
 
     /**
