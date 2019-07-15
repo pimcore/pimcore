93a7f4effffd739fbde3163a6b5564be0096e0d6
jan Walther <jan.walther@blackbit.de>
2019-07-15 15:53:11 +0200

    set remote stream

--- models/Asset.php
+++ models/Asset.php
@@ -1308,7 +1308,18 @@ class Asset extends Element\AbstractElement
         if (is_resource($stream)) {
             $this->setDataChanged(true);
             $this->stream = $stream;
-            rewind($this->stream);
+
+            $isRewindable = @rewind($this->stream);
+            $tmpFile = PIMCORE_SYSTEM_TEMP_DIRECTORY . '/asset-create-tmp-file-' . uniqid() . '.' . File::getFileExtension($this->getFilename());
+            $dest = fopen($tmpFile, 'w+', false, File::getContext());
+            stream_copy_to_stream($this->stream, $dest);
+
+            if (!$isRewindable) {
+                $this->stream = $dest;
+            } else {
+                fclose($dest);
+                unlink($tmpFile);
+            }
         } elseif (is_null($stream)) {
             $this->stream = null;
         }
