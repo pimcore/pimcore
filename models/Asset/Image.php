4f9477e6b35a81fcc5a7b5308073e6d42917825d
Bernhard Rusch <bernhard.rusch@elements.at>
2019-07-19 10:54:08 +0200

    [Asset] provide getter for data uri of low quality preview image

--- models/Asset/Image.php
+++ models/Asset/Image.php
@@ -271,6 +271,19 @@ EOT;
     }
 
     /**
+     * @return string|null
+     */
+    public function getLowQualityPreviewDataUri() : ?string {
+        $file = $this->getLowQualityPreviewFileSystemPath();
+        $dataUri = null;
+        if(file_exists($file)) {
+            $dataUri = 'data:image/svg+xml;base64,' . base64_encode(file_get_contents($file));
+        }
+
+        return $dataUri;
+    }
+
+    /**
      * @inheritdoc
      */
     public function delete(bool $isNested = false)
