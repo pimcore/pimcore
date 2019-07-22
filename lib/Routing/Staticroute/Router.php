c7c620685b304b878af75fb031856f7d3228226a
Gerhard Kanzler <gerhard.kanzler@gmail.com>
2019-07-22 09:30:17 +0200

    Update Router.php

--- lib/Routing/Staticroute/Router.php
+++ lib/Routing/Staticroute/Router.php
@@ -124,7 +124,7 @@ class Router implements RouterInterface, RequestMatcherInterface, VersatileGener
     /**
      * @inheritDoc
      */
-    public function generate($name, $parameters = [], $referenceType = self::ABSOLUTE_PATH, $encode = true)
+    public function generate($name, $parameters = [], $referenceType = self::ABSOLUTE_PATH)
     {
         // when using $name = false we don't use the default route (happens when $name = null / ZF default behavior)
         // but just the query string generation using the given parameters
@@ -178,7 +178,8 @@ class Router implements RouterInterface, RequestMatcherInterface, VersatileGener
 
         if ($name && $route = Staticroute::getByName($name, $siteId)) {
             $reset = isset($parameters['reset']) ? (bool)$parameters['reset'] : false;
-            
+            $encode = isset($parameters['encode']) ? (bool)$parameters['encode'] : false;
+            unlink( $parameters['encode'] );
             // assemble the route / url in Staticroute::assemble()
             $url = $route->assemble($parameters, $reset, $encode);
 
