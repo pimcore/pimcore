52ec557a44990b99597c225c85aed2d8d9664f9c
Bernhard Rusch <bernhard.rusch@elements.at>
2019-09-03 16:25:17 +0200

    fixed mobile vs. phone value

--- lib/Tool/DeviceDetector.php
+++ lib/Tool/DeviceDetector.php
@@ -188,10 +188,14 @@ class DeviceDetector
         $type = null;
 
         // check CloudFront headers
-        foreach(['mobile', 'tablet', 'desktop'] as $cfType) {
-            $cfHeaderName = 'HTTP_CLOUDFRONT_IS_' . strtoupper($cfType). '_VIEWER';
-            if(isset($_SERVER[$cfHeaderName]) && $_SERVER[$cfHeaderName] === 'true') {
-                $type = $cfType;
+        foreach (['mobile', 'tablet', 'desktop'] as $cfType) {
+            $cfHeaderName = 'HTTP_CLOUDFRONT_IS_' . strtoupper($cfType) . '_VIEWER';
+            if (isset($_SERVER[$cfHeaderName]) && $_SERVER[$cfHeaderName] === 'true') {
+                if ($cfType === 'mobile') {
+                    $type = 'phone';
+                } else {
+                    $type = $cfType;
+                }
             }
         }
 
