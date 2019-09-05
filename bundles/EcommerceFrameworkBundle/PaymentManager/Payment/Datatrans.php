6ee3c62bf1289a65d8bbc2ef1a39177e3561c905
Peter Philipp <peter.philipp@cando-image.com>
2019-09-05 09:19:08 +0200

    Don't strip optional datatrans config properties (#4888)
    * Don't strip optional config properties
    
    It would be nice to be able to extend the Datatrans Payment Method without the need to defined new required configuration properties.
    Currently the check if all required configuration options are set: `$config = array_intersect_key($config, $required);` overwrites possibly optional options.
    
    * Naming in CamelCase

--- bundles/EcommerceFrameworkBundle/PaymentManager/Payment/Datatrans.php
+++ bundles/EcommerceFrameworkBundle/PaymentManager/Payment/Datatrans.php
@@ -184,9 +184,9 @@ class Datatrans extends AbstractPayment implements \Pimcore\Bundle\EcommerceFram
         // check params
         $required = $this->getRequiredRequestFields();
 
-        $config = array_intersect_key($config, $required);
+        $requiredConfigIntersect = array_intersect_key($config, $required);
 
-        if (count($required) != count($config)) {
+        if (count($required) != count($requiredConfigIntersect)) {
             throw new \Exception(sprintf('required fields are missing! required: %s', implode(', ', array_keys(array_diff_key($required, $config)))));
         }
 
