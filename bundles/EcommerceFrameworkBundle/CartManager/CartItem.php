7f7528684f03bd411223a6e10e5d94ece1f86af0
andreas-gruenwald <andreas.gruenwald@elements.at>
2019-09-23 08:54:52 +0200

    Ecommerce: getCart() in CartItem class must use user-defined class definition  (#4998)
    * getCart() must use user-defined class definition instead of default class.
    
    * Replace wrong $this->class statement for getting the cart via its ID.

--- bundles/EcommerceFrameworkBundle/CartManager/CartItem.php
+++ bundles/EcommerceFrameworkBundle/CartManager/CartItem.php
@@ -16,6 +16,7 @@ namespace Pimcore\Bundle\EcommerceFrameworkBundle\CartManager;
 
 use Pimcore\Cache\Runtime;
 use Pimcore\Logger;
+use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
 
 class CartItem extends AbstractCartItem implements CartItemInterface
 {
@@ -43,7 +44,8 @@ class CartItem extends AbstractCartItem implements CartItemInterface
     public function getCart()
     {
         if (empty($this->cart)) {
-            $this->cart = Cart::getById($this->cartId);
+            $cartClass = '\\'.Factory::getInstance()->getCartManager()->getCartClassName();
+            $this->cart = $cartClass::getById($this->cartId);
         }
 
         return $this->cart;
