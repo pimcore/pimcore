pimcore/pimcore

commit ccfc5c30869a18b05ba4cc415fdc32fedd8854e2

[Object] use prepared statement in ClassificationstoreController::addCollectionsAction()

bundles/AdminBundle/Controller/Admin/DataObject/ClassificationstoreController.php
@@ -929,11 +929,10 @@ class ClassificationstoreController extends AdminController implements EventedCo
 
         if ($ids) {
             $db = \Pimcore\Db::get();
-            $query = 'select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (' . implode(',', $ids)
-                . ') and g.id = c.groupId';
-
             $mappedData = [];
-            $groupsData = $db->fetchAll($query);
+            $groupsData = $db->fetchAll('select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (:ids) and g.id = c.groupId', [
+                'ids' => $ids
+            ]);
 
             foreach ($groupsData as $groupData) {
                 $mappedData[$groupData['id']] = $groupData;

commit 4d0f1f49e52952faa02a39a4ad5827a6348816c4

[DataObject] Classificationstore cannot add collections
When adding a classification collection no groups were found.
Caused by an array of IDs being injected as query parameter for SQL IN().
Fixed by converting the array to comma separated values.
Also force the IDs to integer values.
Regression introduced in commit ccfc5c30869a18b05ba4cc415fdc32fedd8854e2

bundles/AdminBundle/Controller/Admin/DataObject/ClassificationstoreController.php
@@ -931,7 +931,7 @@ class ClassificationstoreController extends AdminController implements EventedCo
             $db = \Pimcore\Db::get();
             $mappedData = [];
             $groupsData = $db->fetchAll('select * from classificationstore_groups g, classificationstore_collectionrelations c where colId IN (:ids) and g.id = c.groupId', [
-                'ids' => $ids
+                'ids' => implode(',', array_filter($ids, 'intval')),
             ]);
 
             foreach ($groupsData as $groupData) {