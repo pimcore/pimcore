e0b48faf7d29ce43a98825a0b230e88350ebcf78
Bernhard Rusch <bernhard.rusch@elements.at>
2019-10-30 17:00:19 +0100

    [Email] Email log: do not allow script/iframe execution in preview window

--- bundles/AdminBundle/Controller/Admin/EmailController.php
+++ bundles/AdminBundle/Controller/Admin/EmailController.php
@@ -133,7 +133,9 @@ class EmailController extends AdminController
 
             return new Response('<html><head><meta http-equiv="Content-Type" content="text/html; charset=utf-8" /><style>body{background-color:#fff;}</style></head><body><pre>' . $templatingEnginePhp->escape($emailLog->getTextLog()) . '</pre></body></html>');
         } elseif ($request->get('type') == 'html') {
-            return new Response($emailLog->getHtmlLog());
+            return new Response($emailLog->getHtmlLog(), 200, [
+                'Content-Security-Policy' => "default-src 'self'; style-src 'self' 'unsafe-inline'"
+            ]);
         } elseif ($request->get('type') == 'params') {
             try {
                 $params = $this->decodeJson($emailLog->getParams());
