fe4b06e067666acc33037e88585a1239f9258154
Bernhard Rusch <bernhard.rusch@elements.at>
2019-11-06 14:53:50 +0100

    [UI] Do not allow frame-embedding the admin interface - resolves #5163

---
+++ bundles/AdminBundle/EventListener/FrameOptionsHeaderListener.php
@@ -0,0 +1,68 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Bundle\AdminBundle\EventListener;
+
+use Pimcore\Bundle\CoreBundle\EventListener\Traits\PimcoreContextAwareTrait;
+use Pimcore\Http\Request\Resolver\PimcoreContextResolver;
+use Pimcore\Http\RequestHelper;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\HttpKernel\Event\FilterResponseEvent;
+use Symfony\Component\HttpKernel\KernelEvents;
+
+class FrameOptionsHeaderListener implements EventSubscriberInterface
+{
+    use PimcoreContextAwareTrait;
+
+    /**
+     * @var RequestHelper
+     */
+    protected $requestHelper;
+
+    /**
+     * @param RequestHelper $requestHelper
+     */
+    public function __construct(RequestHelper $requestHelper)
+    {
+        $this->requestHelper = $requestHelper;
+    }
+
+    /**
+     * @inheritDoc
+     */
+    public static function getSubscribedEvents()
+    {
+        return [
+            KernelEvents::RESPONSE => 'onKernelResponse'
+        ];
+    }
+
+    public function onKernelResponse(FilterResponseEvent $event)
+    {
+        if (!$event->isMasterRequest()) {
+            return;
+        }
+
+        $request = $event->getRequest();
+        $response = $event->getResponse();
+
+        if ($this->matchesPimcoreContext($request, PimcoreContextResolver::CONTEXT_ADMIN)) {
+            $response->headers->set('X-Frame-Options', 'deny', true);
+        }
+
+        if($this->requestHelper->isFrontendRequestByAdmin($request)) {
+            $response->headers->set('X-Frame-Options', 'SAMEORIGIN', true);
+        }
+    }
+}
