3f6c3a985d1c1bddbe7075a279f3836c53667d78
Bernhard Rusch <brusch@users.noreply.github.com>
2019-11-13 14:01:55 +0100

    Fixed namespace issues

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -9,9 +9,9 @@ if (file_exists(__DIR__ . '/../vendor/autoload.php')) {
 } elseif (getenv('PIMCORE_PROJECT_ROOT') != '' && file_exists(getenv('PIMCORE_PROJECT_ROOT') . '/vendor/autoload.php')) {
     include getenv('PIMCORE_PROJECT_ROOT') . '/vendor/autoload.php';
 } elseif (getenv('PIMCORE_PROJECT_ROOT') != '') {
-    throw new Exception('Invalid Pimcore project root "' . getenv('PIMCORE_PROJECT_ROOT') . '"');
+    throw new \Exception('Invalid Pimcore project root "' . getenv('PIMCORE_PROJECT_ROOT') . '"');
 } else {
-    throw new Exception('Unknown configuration! Pimcore project root not found, please set env variable PIMCORE_PROJECT_ROOT.');
+    throw new \Exception('Unknown configuration! Pimcore project root not found, please set env variable PIMCORE_PROJECT_ROOT.');
 }
 
 \Pimcore\Bootstrap::setProjectRoot();
