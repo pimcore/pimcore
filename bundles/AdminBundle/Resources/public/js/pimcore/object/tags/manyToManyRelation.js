de7de7b487c74b921d19e70fca765d35b3b97af4
Bernhard Rusch <bernhard.rusch@elements.at>
2019-11-13 11:13:55 +0100

    replaced arrow functions (IE11)

--- bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToManyRelation.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToManyRelation.js
@@ -26,7 +26,7 @@ pimcore.object.tags.manyToManyRelation = Class.create(pimcore.object.tags.abstra
 
         this.fieldConfig = fieldConfig;
 
-        this.fieldConfig.classes =  this.fieldConfig.classes.filter(x => {
+        this.fieldConfig.classes =  this.fieldConfig.classes.filter(function (x) {
             if(x.classes == 'folder') {
                 this.dataObjectFolderAllowed = true;
                 return false;
--- bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToOneRelation.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToOneRelation.js
@@ -27,7 +27,7 @@ pimcore.object.tags.manyToOneRelation = Class.create(pimcore.object.tags.abstrac
         }
         this.fieldConfig = fieldConfig;
 
-        this.fieldConfig.classes =  this.fieldConfig.classes.filter(x => {
+        this.fieldConfig.classes =  this.fieldConfig.classes.filter(function (x) {
             if(x.classes == 'folder') {
                 this.dataObjectFolderAllowed = true;
                 return false;
