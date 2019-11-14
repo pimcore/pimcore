6f21bc554ee9984af7cd0b8c07903b451179226c
jan Walther <jan.walther@blackbit.de>
2019-11-14 13:29:41 +0100

    use ClassResolver

--- models/DataObject/ClassDefinition/Helper/DefaultValueGeneratorResolver.php
+++ models/DataObject/ClassDefinition/Helper/DefaultValueGeneratorResolver.php
@@ -2,38 +2,14 @@
 
 namespace Pimcore\Model\DataObject\ClassDefinition\Helper;
 
-use Pimcore\Logger;
 use Pimcore\Model\DataObject\ClassDefinition\DefaultValueGeneratorInterface;
 
-class DefaultValueGeneratorResolver
+class DefaultValueGeneratorResolver extends ClassResolver
 {
-    public static $cache = [];
-
-    /**
-     * @param $generatorClass
-     *
-     * @return DefaultValueGeneratorInterface
-     */
     public static function resolveGenerator($generatorClass)
     {
-        if ($generatorClass) {
-            $generator = null;
-            if (substr($generatorClass, 0, 1) === '@') {
-                $serviceName = substr($generatorClass, 1);
-                try {
-                    $generator = \Pimcore::getKernel()->getContainer()->get($serviceName);
-                } catch (\Exception $e) {
-                    Logger::error($e);
-                }
-            } else {
-                $generator = new $generatorClass;
-            }
-
-            if ($generator instanceof DefaultValueGeneratorInterface) {
-                return $generator;
-            }
-        }
-
-        return null;
+        return self::resolve($generatorClass, static function($generator) {
+            return $generator instanceof DefaultValueGeneratorInterface;
+        });
     }
 }
