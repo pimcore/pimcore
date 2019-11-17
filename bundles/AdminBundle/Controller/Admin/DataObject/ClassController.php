8f5c076fff12b23e05d1efaab9d93205e5c27398
jraab <julian.raab@elements.at>
2019-11-17 17:49:40 +0100

    refactor

--- bundles/AdminBundle/Controller/Admin/DataObject/ClassController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/ClassController.php
@@ -1757,7 +1757,7 @@ class ClassController extends AdminController implements EventedControllerInterf
         $classId = $request->get('classId');
 
         $path = "/bundles/pimcoreadmin/img/data-object-icons";
-        $icons = scandir(PIMCORE_WEB_ROOT.$path);
+        $icons = scandir(PIMCORE_WEB_ROOT . $path);
 
         array_shift($icons);
         array_shift($icons);
