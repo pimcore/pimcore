eb300de9d6f826ecbdf2c3bda3b6ab3f0841cea5
jan Walther <jan.walther@blackbit.de>
2019-12-17 22:34:03 +0100

    fix typo

--- models/DataObject/Listing/Dao.php
+++ models/DataObject/Listing/Dao.php
@@ -202,7 +202,7 @@ class Dao extends Model\Listing\Dao\AbstractDao
                         if (count($indexCandidates) > 0) {
                             Logger::info(
                                 'Query execution plan for "'.$query
-                                .'" seems not to be optimal, please consider adding an index to fields '.implode(
+                                .'" seems not to be optimal, please consider adding an index to field(s) '.implode(
                                     ',', $indexCandidates
                                 ).' of data object class '.$classDefinition->getName()
                             );
