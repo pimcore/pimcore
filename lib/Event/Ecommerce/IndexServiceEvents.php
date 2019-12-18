c73de8ffefca6042c45d414695ef9abf5af3ca0f
Julian Raab <julian.raab@elements.at>
2019-12-18 12:11:52 +0100

    add pre_send_reqeust event type

--- lib/Event/Ecommerce/IndexServiceEvents.php
+++ lib/Event/Ecommerce/IndexServiceEvents.php
@@ -33,4 +33,11 @@ final class IndexServiceEvents
      * @var string
      */
     const GENERAL_PREPROCESSING_ERROR = 'pimcore.ecommerce.indexservice.generalPreProcessingError';
+    
+      /**
+     * @Event("Pimcore\Event\Model\Ecommerce\IndexService\PreSendRequestEvent")
+     *
+     * @var string
+     */
+    const PRE_SEND_REQUST_ELASTIC_SEARCH = 'pimcore.ecommerce.indexservice.productlist.pre_send_request_elastic_search';
 }
