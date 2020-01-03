ce031cbd12239fa3b4e0c2c46a5c247f4fa95b80
weisswurstkanone <josef.aichhorn@elements.at>
2020-01-03 15:57:38 +0100

    cleanup

--- tests/model/LazyLoading/AbstractLazyLoadingTest.php
+++ tests/model/LazyLoading/AbstractLazyLoadingTest.php
@@ -38,10 +38,6 @@ class AbstractLazyLoadingTest extends ModelTestCase
         $this->tester->setupObjectbrick_LazyLoadingTest();
 
         $this->tester->setupObjectbrick_LazyLoadingLocalizedTest();
-
-        $name = 'LazyLoadingLocalizedTest';
-        $file = 'lazyloading/objectbrick_LazyLoadingLocalizedTest_export.json';
-        $brick = $this->tester->setupObjectBrick($name, $file);
     }
 
     protected function createRelationObjects()
