f4bf49524d3639099a2d50f2f0a0884181219ae0
Lukas Huppertz <45906638+HuppertzL@users.noreply.github.com>
2020-01-03 09:33:52 +0100

    FIX: PayPalSmartPaymentButton (#5514)
    * FIX: PayPalSmartPaymentButton
    + transform stdClass to json
    + check if return is json
    
    * FIX: PayPalSmartPaymentButton
    + transform stdClass to json
    + check if return is json
    + throw exception if result neither stdClass nor json

--- bundles/EcommerceFrameworkBundle/PaymentManager/Payment/PayPalSmartPaymentButton.php
+++ bundles/EcommerceFrameworkBundle/PaymentManager/Payment/PayPalSmartPaymentButton.php
@@ -164,9 +164,20 @@ class PayPalSmartPaymentButton extends AbstractPayment implements \Pimcore\Bundl
      */
     public function startPayment(OrderAgentInterface $orderAgent, PriceInterface $price, AbstractRequest $config): StartPaymentResponseInterface
     {
-        $json = $this->initPayment($price, $config->asArray());
+        $result = $this->initPayment($price, $config->asArray());
 
-        return new JsonResponse($orderAgent->getOrder(), $json);
+        if ($result instanceof \stdClass) {
+            if($json = json_encode($result)) {
+                return new JsonResponse($orderAgent->getOrder(), $json);
+            }
+        }
+
+        json_decode($result);
+        if (json_last_error() == JSON_ERROR_NONE) {
+            return new JsonResponse($orderAgent->getOrder(), $result);
+        }
+
+        throw new \Exception("result of initPayment neither stdClass nor JSON");
     }
 
     /**
@@ -317,8 +328,7 @@ class PayPalSmartPaymentButton extends AbstractPayment implements \Pimcore\Bundl
             ->setDefault('user_action', 'PAY_NOW')
             ->setAllowedValues('user_action', ['CONTINUE', 'PAY_NOW'])
             ->setDefault('capture_strategy', self::CAPTURE_STRATEGY_AUTOMATIC)
-            ->setAllowedValues('capture_strategy', [self::CAPTURE_STRATEGY_AUTOMATIC, self::CAPTURE_STRATEGY_MANUAL])
-            ;
+            ->setAllowedValues('capture_strategy', [self::CAPTURE_STRATEGY_AUTOMATIC, self::CAPTURE_STRATEGY_MANUAL]);
 
         $notEmptyValidator = function ($value) {
             return !empty($value);
