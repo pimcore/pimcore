a02354475a92f435f47f7b8196e6e6a06007db92
Sebastian Blank <sebastian.bl@gmx.de>
2020-01-06 01:11:39 +0100

    [PHPSTAN] Load heidelpay-php

--- .travis/run-stan.sh
+++ .travis/run-stan.sh
@@ -2,7 +2,7 @@
 
 
 # install suggested packages first
-composer require "klarna/checkout:^3.0.0" "elasticsearch/elasticsearch:2.0.0" "paypal/paypal-checkout-sdk:^1" "mpay24/mpay24-php:^4.2" "composer/composer:*"
+composer require "heidelpay/heidelpay-php:^1.2.5.1" "klarna/checkout:^3.0.0" "elasticsearch/elasticsearch:2.0.0" "paypal/paypal-checkout-sdk:^1" "mpay24/mpay24-php:^4.2" "composer/composer:*"
 
 if [ $SYMFONY_VERSION = "^3.4" ]
 then
