5954316f2b8afdb7f4b1f08f18dcdca68635c714
weisswurstkanone <josef.aichhorn@elements.at>
2020-01-07 14:31:30 +0100

    Deprecated array_key_exists in cache Runtime.php #5534

--- lib/Cache/Runtime.php
+++ lib/Cache/Runtime.php
@@ -163,18 +163,6 @@ final class Runtime extends \ArrayObject
     }
 
     /**
-     * @param string $index
-     *
-     * @return mixed
-     *
-     * Workaround for http://bugs.php.net/bug.php?id=40442 (ZF-960).
-     */
-    public function offsetExists($index)
-    {
-        return array_key_exists($index, $this);
-    }
-
-    /**
      * Alias of self::set() to be compatible with Pimcore\Cache
      *
      * @param $data
