2181f8ac4975cb5b42e5f9114007cc0f9c683e9a
Albert Mueller <2986829+albertmueller@users.noreply.github.com>
2020-02-05 10:25:02 +0100

    use insertOrUpdate instead of insert on relation save, add index to the localized field relation data

--- models/DataObject/Traits/ContextPersistenceTrait.php
+++ models/DataObject/Traits/ContextPersistenceTrait.php
@@ -57,8 +57,8 @@ trait ContextPersistenceTrait
             $context = $object->getContext();
             if (isset($context['containerType']) && ($context['containerType'] === 'fieldcollection' || $context['containerType'] === 'objectbrick')) {
                 $fieldname = $context['fieldname'];
-                $index = $context['index'] ?? null;
-                $row['ownername'] = '/' . $context['containerType'] . '~' . $fieldname . '/' . $index . '/localizedfield~' . $row['ownername'];
+                $row['index'] = $context['index'] ?? null;
+                $row['ownername'] = '/' . $context['containerType'] . '~' . $fieldname . '/' . $row['index'] . '/localizedfield~' . $row['ownername'];
             }
 
             $row['position'] = $params['language'];
