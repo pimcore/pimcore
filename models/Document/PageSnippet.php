pimcore/pimcore

commit 9e550ead94e5b35a8d8a0d3291d531e5c7fa76e7

[Core] Refactor System Configuration - Models (#5787)
* [Core] Refactor System Configuration - Models

* [Core] Refactor system config - fix tests

* [Core] Refactor system config - reverts commit d2ff3ecb

* [Core] Refactor System Configuration - Models - use offset

* [Core] Refactor System Configuration - Models - fix typos

models/Document/PageSnippet.php
@@ -153,8 +153,9 @@ abstract class PageSnippet extends Model\Document
 
             // only create a new version if there is at least 1 allowed
             // or if saveVersion() was called directly (it's a newer version of the object)
-            if (Config::getSystemConfig()->documents->versions->steps
-                || Config::getSystemConfig()->documents->versions->days
+            $documentsConfig = \Pimcore\Config::getSystemConfiguration('documents');
+            if (!empty($documentsConfig['versions']['steps'])
+                || !empty($documentsConfig['versions']['days'])
                 || $setModificationDate) {
                 $version = $this->doSaveVersion($versionNote, $saveOnlyVersion);
             }
@@ -602,7 +603,7 @@ abstract class PageSnippet extends Model\Document
         }
 
         if (!$hostname) {
-            if (!$hostname = \Pimcore\Config::getSystemConfig()->general->domain) {
+            if (!empty($hostname = \Pimcore\Config::getSystemConfiguration('general')['domain'])) {
                 if (!$hostname = \Pimcore\Tool::getHostname()) {
                     throw new \Exception('No hostname available');
                 }

models/Asset.php
@@ -845,8 +845,9 @@ class Asset extends Element\AbstractElement
 
             // only create a new version if there is at least 1 allowed
             // or if saveVersion() was called directly (it's a newer version of the asset)
-            if (Config::getSystemConfig()->assets->versions->steps
-                || Config::getSystemConfig()->assets->versions->days
+            $assetsConfig = \Pimcore\Config::getSystemConfiguration('assets');
+            if (!empty($assetsConfig['versions']['steps'])
+                || !empty($assetsConfig['versions']['days'])
                 || $setModificationDate) {
                 $version = $this->doSaveVersion($versionNote, $saveOnlyVersion);
             }

models/DataObject/ClassDefinition/Data/Localizedfields.php
@@ -1104,10 +1104,10 @@ class Localizedfields extends Data implements CustomResourcePersistingInterface
      */
     public function checkValidity($data, $omitMandatoryCheck = false)
     {
-        $conf = \Pimcore\Config::getSystemConfig();
+        $config = \Pimcore\Config::getSystemConfiguration('general');
         $languages = [];
-        if ($conf->general->validLanguages) {
-            $languages = explode(',', $conf->general->validLanguages);
+        if (isset($config['valid_languages'])) {
+            $languages = explode(',', $config['valid_languages']);
         }
 
         $data = $this->getDataForValidity($data, $languages);

models/DataObject/Concrete.php
@@ -318,8 +318,9 @@ class Concrete extends AbstractObject implements LazyLoadedFieldsInterface
 
             // only create a new version if there is at least 1 allowed
             // or if saveVersion() was called directly (it's a newer version of the object)
-            if (Config::getSystemConfig()->objects->versions->steps
-                || Config::getSystemConfig()->objects->versions->days
+            $objectsConfig = \Pimcore\Config::getSystemConfiguration('objects');
+            if (!empty($objectsConfig['versions']['steps'])
+                || !empty($objectsConfig['versions']['days'])
                 || $setModificationDate) {
                 $version = $this->doSaveVersion($versionNote, $saveOnlyVersion);
             }

models/Document.php
@@ -895,7 +895,7 @@ class Document extends Element\AbstractElement
             }
 
             if (!$link) {
-                $config = \Pimcore\Config::getSystemConfig();
+                $config = \Pimcore\Config::getSystemConfiguration('general');
                 $request = $requestStack->getCurrentRequest();
                 $scheme = 'http://';
                 if ($request) {
@@ -915,8 +915,8 @@ class Document extends Element\AbstractElement
                     }
                 }
 
-                if (!$link && $config->general->domain) {
-                    $link = $scheme . $config->general->domain . $this->getRealFullPath();
+                if (!$link && !empty($config['domain'])) {
+                    $link = $scheme . $config['domain'] . $this->getRealFullPath();
                 }
             }
         }

models/Document/Service.php
@@ -612,10 +612,10 @@ class Service extends Model\Element\Service
 
         $url = $hostUrl . $doc->getRealFullPath();
 
-        $config = \Pimcore\Config::getSystemConfig();
-        if ($config->general->http_auth) {
-            $username = $config->general->http_auth->username;
-            $password = $config->general->http_auth->password;
+        $config = \Pimcore\Config::getSystemConfiguration('general');
+        if (isset($config['http_auth'])) {
+            $username = $config['http_auth']['username'] ?? null;
+            $password = $config['http_auth']['password'] ?? null;
             if ($username && $password) {
                 $url = str_replace('://', '://' . $username .':'. $password . '@', $url);
             }

models/Document/Traits/RedirectHelperTrait.php
@@ -25,8 +25,8 @@ trait RedirectHelperTrait
 {
     protected function createRedirectForFormerPath(string $oldPath, ?Document $oldDocument)
     {
-        $config = \Pimcore\Config::getSystemConfig();
-        if ($oldPath && $config->documents->createredirectwhenmoved && $oldPath != $this->getRealFullPath()) {
+        $documentsConfig = \Pimcore\Config::getSystemConfiguration('documents');
+        if ($oldPath && !empty($documentsConfig['create_redirect_when_moved']) && $oldPath != $this->getRealFullPath()) {
             // create redirect for old path
             $redirect = new Redirect();
             $redirect->setType(Redirect::TYPE_PATH);

models/Tool/UUID.php
@@ -78,8 +78,8 @@ class UUID extends Model\AbstractModel
      */
     public function setSystemInstanceIdentifier()
     {
-        $instanceIdentifier = \Pimcore\Config::getSystemConfig()->general->instanceIdentifier;
-        if (!$instanceIdentifier) {
+        $instanceIdentifier = \Pimcore\Config::getSystemConfiguration('general')['instance_identifier'] ?? null;
+        if (empty($instanceIdentifier)) {
             throw new \Exception('No instance identifier set in system config!');
         }
         $this->setInstanceIdentifier($instanceIdentifier);

commit 81ffb899f0cec27173f0226dec33b7c221de59d8

Fixed regression introduced by 9e550ead94e5b35a8d8a0d3291d531e5c7fa76e7 (#5787) -> getUrl() returns incomplete URL (hostname is missing)

models/Document/PageSnippet.php
@@ -602,7 +602,8 @@ abstract class PageSnippet extends Model\Document
         }
 
         if (!$hostname) {
-            if (!empty($hostname = \Pimcore\Config::getSystemConfiguration('general')['domain'])) {
+            $hostname = \Pimcore\Config::getSystemConfiguration('general')['domain'];
+            if (empty($hostname)) {
                 if (!$hostname = \Pimcore\Tool::getHostname()) {
                     throw new \Exception('No hostname available');
                 }