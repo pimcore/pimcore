pimcore/pimcore

commit 0a50951e7f0987fe082ec6d481316e3a6dab4eae

[Core] Refactor System Configuration - Admin Bundle & CoreBundle (#5745)
* [Core] Refactor system config

* [Core] Refactor system config - use ArrayAccess interface and revert get/setPimcoreConfig()

* [Core] Refactor system config - revert PimcoreConfig setter

* [Core] Refactor system config - throw exception on config modifications

* [Core] Refactor system config - AdminBundle

* [Core] Refactor system config - CoreBundle

* [Core] Refactor system config - use getSystemConfiguration for UUIDListener

* [Core] Refactor system config - fix namespace

bundles/AdminBundle/Controller/Admin/IndexController.php
@@ -67,6 +67,7 @@ class IndexController extends AdminController implements EventedControllerInterf
      * @param KernelInterface $kernel
      * @param Executor $maintenanceExecutor
      * @param CsrfProtectionListener $csrfProtectionListener
+     * @param Config $config
      *
      * @return ViewModel
      *
@@ -77,11 +78,12 @@ class IndexController extends AdminController implements EventedControllerInterf
         SiteConfigProvider $siteConfigProvider,
         KernelInterface $kernel,
         Executor $maintenanceExecutor,
-        CsrfProtectionListener $csrfProtectionListener
+        CsrfProtectionListener $csrfProtectionListener,
+        Config $config
     ) {
         $user = $this->getAdminUser();
         $view = new ViewModel([
-            'config' => Config::getSystemConfig()
+            'config' => $config
         ]);
 
         $this
@@ -255,7 +257,7 @@ class IndexController extends AdminController implements EventedControllerInterf
             'language' => $request->getLocale(),
             'websiteLanguages' => Admin::reorderWebsiteLanguages(
                 $this->getAdminUser(),
-                $config->general->validLanguages,
+                $config['general']['valid_languages'],
                 true
             )
         ]);
@@ -263,26 +265,24 @@ class IndexController extends AdminController implements EventedControllerInterf
         // flags
         $namingStrategy = $this->get('pimcore.document.tag.naming.strategy');
 
-        // config
-        $pimcoreSymfonyConfig = $this->getParameter('pimcore.config');
 
         $settings->getParameters()->add([
             'showCloseConfirmation' => true,
-            'debug_admin_translations' => (bool)$config->general->debug_admin_translations,
-            'document_generatepreviews' => (bool)$config->documents->generate_preview,
+            'debug_admin_translations' => (bool)$config['general']['debug_admin_translations'],
+            'document_generatepreviews' => (bool)$config['documents']['generate_preview'],
             'document_naming_strategy' => $namingStrategy->getName(),
-            'asset_disable_tree_preview' => (bool)$config->assets->disable_tree_preview,
+            'asset_disable_tree_preview' => (bool)$config['assets']['disable_tree_preview'],
             'htmltoimage' => \Pimcore\Image\HtmlToImage::isSupported(),
             'videoconverter' => \Pimcore\Video::isAvailable(),
-            'asset_hide_edit' => (bool)$config->assets->hide_edit_image,
-            'main_domain' => $config->general->domain,
-            'timezone' => $config->general->timezone,
-            'tile_layer_url_template' => $pimcoreSymfonyConfig['maps']['tile_layer_url_template'],
-            'geocoding_url_template' => $pimcoreSymfonyConfig['maps']['geocoding_url_template'],
-            'reverse_geocoding_url_template' => $pimcoreSymfonyConfig['maps']['reverse_geocoding_url_template'],
-            'asset_tree_paging_limit' => $pimcoreSymfonyConfig['assets']['tree_paging_limit'],
-            'document_tree_paging_limit' => $pimcoreSymfonyConfig['documents']['tree_paging_limit'],
-            'object_tree_paging_limit' => $pimcoreSymfonyConfig['objects']['tree_paging_limit'],
+            'asset_hide_edit' => (bool)$config['assets']['hide_edit_image'],
+            'main_domain' => $config['general']['domain'],
+            'timezone' => $config['general']['timezone'],
+            'tile_layer_url_template' => $config['maps']['tile_layer_url_template'],
+            'geocoding_url_template' => $config['maps']['geocoding_url_template'],
+            'reverse_geocoding_url_template' => $config['maps']['reverse_geocoding_url_template'],
+            'asset_tree_paging_limit' => $config['assets']['tree_paging_limit'],
+            'document_tree_paging_limit' => $config['documents']['tree_paging_limit'],
+            'object_tree_paging_limit' => $config['objects']['tree_paging_limit'],
             'maxmind_geoip_installed' => (bool) $this->getParameter('pimcore.geoip.db_file')
         ]);
 
@@ -386,7 +386,7 @@ class IndexController extends AdminController implements EventedControllerInterf
 
     /**
      * @param ViewModel $settings
-     * @param \stdClass $config
+     * @param Config $config
      *
      * @return $this
      */
@@ -394,20 +394,20 @@ class IndexController extends AdminController implements EventedControllerInterf
     {
         //mail settings
         $mailIncomplete = false;
-        if ($config->email) {
-            if (!$config->email->debug->emailaddresses) {
+        if (isset($config['email'])) {
+            if (!empty($config['email']['debug']['email_addresses'])) {
                 $mailIncomplete = true;
             }
-            if (!$config->email->sender->email) {
+            if (!empty($config['email']['sender']['email'])) {
                 $mailIncomplete = true;
             }
-            if ($config->email->method == 'smtp' && !$config->email->smtp->host) {
+            if (($config['email']['method'] ?? '') == 'smtp' && !empty($config['email']['smtpy']['host'])) {
                 $mailIncomplete = true;
             }
         }
 
         $settings->mail = !$mailIncomplete;
-        $settings->mailDefaultAddress = $config->email->sender->email ?: null;
+        $settings->mailDefaultAddress = $config['email']['sender']['email'] ?? null;
 
         return $this;
     }

bundles/AdminBundle/Controller/Admin/AssetController.php
@@ -17,6 +17,7 @@ namespace Pimcore\Bundle\AdminBundle\Controller\Admin;
 use Pimcore\Bundle\AdminBundle\Controller\Traits\AdminStyleTrait;
 use Pimcore\Bundle\AdminBundle\Controller\Traits\ApplySchedulerDataTrait;
 use Pimcore\Bundle\AdminBundle\Helper\GridHelperService;
+use Pimcore\Config;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Controller\EventedControllerInterface;
 use Pimcore\Controller\Traits\ElementEditLockHelperTrait;
@@ -288,12 +289,13 @@ class AssetController extends ElementControllerBase implements EventedController
      * @Route("/add-asset", methods={"POST"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return JsonResponse
      */
-    public function addAssetAction(Request $request)
+    public function addAssetAction(Request $request, Config $config)
     {
-        $res = $this->addAsset($request);
+        $res = $this->addAsset($request, $config);
 
         $response = [
             'success' => $res['success'],
@@ -314,13 +316,14 @@ class AssetController extends ElementControllerBase implements EventedController
      * @Route("/add-asset-compatibility", methods={"POST"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return JsonResponse
      */
-    public function addAssetCompatibilityAction(Request $request)
+    public function addAssetCompatibilityAction(Request $request, Config $config)
     {
         // this is a special action for the compatibility mode upload (without flash)
-        $res = $this->addAsset($request);
+        $res = $this->addAsset($request, $config);
 
         $response = $this->adminJson([
             'success' => $res['success'],
@@ -336,15 +339,16 @@ class AssetController extends ElementControllerBase implements EventedController
 
     /**
      * @param Request $request
+     * @param Config $config
      *
      * @return array
      *
      * @throws \Exception
      */
-    protected function addAsset(Request $request)
+    protected function addAsset(Request $request, Config $config)
     {
         $success = false;
-        $defaultUploadPath = $this->getParameter('pimcore.config')['assets']['default_upload_path'];
+        $defaultUploadPath = $config['assets']['default_upload_path'] ?? "/";
 
         if (array_key_exists('Filedata', $_FILES)) {
             $filename = $_FILES['Filedata']['name'];

bundles/AdminBundle/Controller/Admin/DataObject/DataObjectHelperController.php
@@ -308,10 +308,11 @@ class DataObjectHelperController extends AdminController
      * @Route("/grid-delete-column-config", methods={"DELETE"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return JsonResponse
      */
-    public function gridDeleteColumnConfigAction(Request $request)
+    public function gridDeleteColumnConfigAction(Request $request, Config $config)
     {
         $gridConfigId = $request->get('gridConfigId');
         $gridConfig = null;
@@ -329,7 +330,7 @@ class DataObjectHelperController extends AdminController
             $success = true;
         }
 
-        $newGridConfig = $this->doGetGridColumnConfig($request, true);
+        $newGridConfig = $this->doGetGridColumnConfig($request, $config, true);
         $newGridConfig['deleteSuccess'] = $success;
 
         return $this->adminJson($newGridConfig);
@@ -339,23 +340,25 @@ class DataObjectHelperController extends AdminController
      * @Route("/grid-get-column-config", methods={"GET"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return JsonResponse
      */
-    public function gridGetColumnConfigAction(Request $request)
+    public function gridGetColumnConfigAction(Request $request, Config $config)
     {
-        $result = $this->doGetGridColumnConfig($request);
+        $result = $this->doGetGridColumnConfig($request, $config);
 
         return $this->adminJson($result);
     }
 
     /**
      * @param Request $request
+     * @param Config $config
      * @param bool $isDelete
      *
      * @return array
      */
-    public function doGetGridColumnConfig(Request $request, $isDelete = false)
+    public function doGetGridColumnConfig(Request $request, Config $config, $isDelete = false)
     {
         $class = null;
         $fields = null;
@@ -607,8 +610,7 @@ class DataObjectHelperController extends AdminController
             return ($a['position'] < $b['position']) ? -1 : 1;
         });
 
-        $config = \Pimcore\Config::getSystemConfig();
-        $frontendLanguages = Tool\Admin::reorderWebsiteLanguages(\Pimcore\Tool\Admin::getCurrentUser(), $config->general->validLanguages);
+        $frontendLanguages = Tool\Admin::reorderWebsiteLanguages(\Pimcore\Tool\Admin::getCurrentUser(), $config['general']['valid_languages']);
         if ($frontendLanguages) {
             $language = explode(',', $frontendLanguages)[0];
         } else {

bundles/AdminBundle/Controller/Admin/Document/DocumentController.php
@@ -1038,6 +1038,7 @@ class DocumentController extends ElementControllerBase implements EventedControl
     {
         $site = null;
         $childDocument = $element;
+        $config = $this->get(Config::class);
 
         $tmpDocument = [
             'id' => $childDocument->getId(),
@@ -1087,7 +1088,7 @@ class DocumentController extends ElementControllerBase implements EventedControl
         $this->addAdminStyle($childDocument, ElementAdminStyleEvent::CONTEXT_TREE, $tmpDocument);
 
         // PREVIEWS temporary disabled, need's to be optimized some time
-        if ($childDocument instanceof Document\Page && Config::getSystemConfig()->documents->generatepreview) {
+        if ($childDocument instanceof Document\Page && isset($config['documents']['generate_preview'])) {
             $thumbnailFile = $childDocument->getPreviewImageFilesystemPath();
             // only if the thumbnail exists and isn't out of time
             if (file_exists($thumbnailFile) && filemtime($thumbnailFile) > ($childDocument->getModificationDate() - 20)) {

bundles/AdminBundle/Controller/Admin/Document/PrintpageControllerBase.php
@@ -244,12 +244,13 @@ class PrintpageControllerBase extends DocumentControllerBase
      * @Route("/start-pdf-generation", methods={"POST"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return JsonResponse
      *
      * @throws \Exception
      */
-    public function startPdfGenerationAction(Request $request)
+    public function startPdfGenerationAction(Request $request, Config $config)
     {
         $allParams = json_decode($request->getContent(), true);
 
@@ -258,9 +259,7 @@ class PrintpageControllerBase extends DocumentControllerBase
             throw new \Exception('Document with id ' . $allParams['id'] . ' not found.');
         }
 
-        if (\Pimcore\Config::getSystemConfig()->general->domain) {
-            $allParams['hostName'] = \Pimcore\Config::getSystemConfig()->general->domain;
-        } else {
+        if (empty($allParams['hostName'] = $config['general']['domain'])) {
             $allParams['hostName'] = $_SERVER['HTTP_HOST'];
         }
 

bundles/AdminBundle/Controller/Admin/LoginController.php
@@ -68,7 +68,7 @@ class LoginController extends AdminController implements BruteforceProtectedCont
      *
      * @TemplatePhp()
      */
-    public function loginAction(Request $request, CsrfProtectionListener $csrfProtectionListener)
+    public function loginAction(Request $request, CsrfProtectionListener $csrfProtectionListener, Config $config)
     {
         if ($request->get('_route') === 'pimcore_admin_login_fallback') {
             return $this->redirectToRoute('pimcore_admin_login', $request->query->all(), Response::HTTP_MOVED_PERMANENTLY);
@@ -81,7 +81,7 @@ class LoginController extends AdminController implements BruteforceProtectedCont
             return $this->redirectToRoute('pimcore_admin_index');
         }
 
-        $view = $this->buildLoginPageViewModel();
+        $view = $this->buildLoginPageViewModel($config);
 
         if ($request->get('auth_failed')) {
             $view->error = 'error_auth_failed';
@@ -118,9 +118,9 @@ class LoginController extends AdminController implements BruteforceProtectedCont
      * @Route("/login/lostpassword")
      * @TemplatePhp()
      */
-    public function lostpasswordAction(Request $request, BruteforceProtectionHandler $bruteforceProtectionHandler, CsrfProtectionListener $csrfProtectionListener)
+    public function lostpasswordAction(Request $request, BruteforceProtectionHandler $bruteforceProtectionHandler, CsrfProtectionListener $csrfProtectionListener, Config $config)
     {
-        $view = $this->buildLoginPageViewModel();
+        $view = $this->buildLoginPageViewModel($config);
         $error = null;
 
         if ($request->getMethod() === 'POST' && $username = $request->get('username')) {
@@ -216,12 +216,12 @@ class LoginController extends AdminController implements BruteforceProtectedCont
     /**
      * @return ViewModel
      */
-    protected function buildLoginPageViewModel()
+    protected function buildLoginPageViewModel(Config $config)
     {
         $bundleManager = $this->get('pimcore.extension.bundle_manager');
 
         $view = new ViewModel([
-            'config' => Config::getSystemConfig(),
+            'config' => $config,
             'pluginCssPaths' => $bundleManager->getCssPaths()
         ]);
 
@@ -232,9 +232,9 @@ class LoginController extends AdminController implements BruteforceProtectedCont
      * @Route("/login/2fa", name="pimcore_admin_2fa")
      * @TemplatePhp()
      */
-    public function twoFactorAuthenticationAction(Request $request, BruteforceProtectionHandler $bruteforceProtectionHandler)
+    public function twoFactorAuthenticationAction(Request $request, BruteforceProtectionHandler $bruteforceProtectionHandler, Config $config)
     {
-        $view = $this->buildLoginPageViewModel();
+        $view = $this->buildLoginPageViewModel($config);
 
         if ($request->hasSession()) {
 

bundles/AdminBundle/Controller/Admin/MiscController.php
@@ -15,6 +15,7 @@
 namespace Pimcore\Bundle\AdminBundle\Controller\Admin;
 
 use Pimcore\Bundle\AdminBundle\Controller\AdminController;
+use Pimcore\Config;
 use Pimcore\Controller\Config\ControllerDataProvider;
 use Pimcore\Controller\Configuration\TemplatePhp;
 use Pimcore\Db;
@@ -229,15 +230,16 @@ class MiscController extends AdminController
      * @Route("/admin-css", methods={"GET"})
      *
      * @param Request $request
+     * @param Config $config
      *
      * @return Response
      */
-    public function adminCssAction(Request $request)
+    public function adminCssAction(Request $request, Config $config)
     {
         // customviews config
         $cvData = Tool::getCustomViewConfig();
 
-        $response = $this->render('PimcoreAdminBundle:Admin/Misc:admin-css.html.php', ['customviews' => $cvData]);
+        $response = $this->render('PimcoreAdminBundle:Admin/Misc:admin-css.html.php', ['customviews' => $cvData, 'config' => $config]);
         $response->headers->set('Content-Type', 'text/css; charset=UTF-8');
 
         return $response;

bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -335,6 +335,7 @@ class SettingsController extends AdminController
     {
         $this->checkPermission('system_settings');
 
+        //TODO use Pimcore\Config service when legacy mapping is removed
         $values = Config::getSystemConfig();
 
         $timezones = \DateTimeZone::listIdentifiers();

commit 393058a6cc82b5cece787c2d4ed300bd57bea30a

[System-Config]: fix email settings detection
still one regression left from https://github.com/pimcore/pimcore/pull/5745

email settings detection was false positiv

bundles/AdminBundle/Controller/Admin/IndexController.php
@@ -394,13 +394,13 @@ class IndexController extends AdminController implements EventedControllerInterf
         //mail settings
         $mailIncomplete = false;
         if (isset($config['email'])) {
-            if (!empty($config['email']['debug']['email_addresses'])) {
+            if (empty($config['email']['debug']['email_addresses'])) {
                 $mailIncomplete = true;
             }
-            if (!empty($config['email']['sender']['email'])) {
+            if (empty($config['email']['sender']['email'])) {
                 $mailIncomplete = true;
             }
-            if (($config['email']['method'] ?? '') == 'smtp' && !empty($config['email']['smtp']['host'])) {
+            if (($config['email']['method'] ?? '') == 'smtp' && empty($config['email']['smtp']['host'])) {
                 $mailIncomplete = true;
             }
         }