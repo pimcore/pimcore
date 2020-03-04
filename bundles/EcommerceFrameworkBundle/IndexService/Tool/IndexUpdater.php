aa5b6dc5ceaf15e5833cefa3ddf75f0a4c90b3bc
andreas-gruenwald <andreas.gruenwald@elements.at>
2020-03-04 13:53:22 +0100

    Output actual name of Product class on indexing.
    Output the actual (short) name of the class instead of "product" as other entities might get indexed as well.

--- bundles/EcommerceFrameworkBundle/IndexService/Tool/IndexUpdater.php
+++ bundles/EcommerceFrameworkBundle/IndexService/Tool/IndexUpdater.php
@@ -88,7 +88,7 @@ class IndexUpdater
             self::log($loggername, '=========================');
 
             foreach ($products as $p) {
-                self::log($loggername, 'Updating product ' . $p->getId());
+                self::log($loggername, 'Updating '.array_pop(explode('\\', get_class($p))).' '.$p->getId());
                 $updater->updateIndex($p);
             }
             $page++;
