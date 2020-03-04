24976dfd91fdd558810bf78a4a9e32878bf3612c
jan Walther <jan.walther@blackbit.de>
2020-03-04 13:52:49 +0100

    Revert "Document::hasElement() should return true for inherited elements"
    This reverts commit 8c6557ea

--- models/Document/PageSnippet.php
+++ models/Document/PageSnippet.php
@@ -387,7 +387,7 @@ abstract class PageSnippet extends Model\Document
      */
     public function removeElement($name)
     {
-        if (isset($this->elements[$name])) {
+        if ($this->hasElement($name)) {
             unset($this->elements[$name]);
         }
 
