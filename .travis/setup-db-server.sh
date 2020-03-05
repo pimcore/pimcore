b2a18e404dd362b91c41940f57380aecf2b6c4c9
Bernhard Rusch <bernhard.rusch@elements.at>
2020-03-05 10:31:39 +0100

    fixes

--- .travis/setup-db-server.sh
+++ .travis/setup-db-server.sh
@@ -2,14 +2,22 @@
 
 echo "Setting up database server ..."
 
-if [ $DATABASE_SERVER == "mariadb-10.1" ]
+
+if [ $DATABASE_SERVER ]
 then
     sudo rm -rf /var/lib/mysql
+    sudo rm -rf /etc/mysql
     sudo systemctl stop mysql
     sudo apt-get install software-properties-common
     sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
-    sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://mirrors.piconets.webwerks.in/mariadb-mirror/repo/10.1/ubuntu xenial main'
+fi
+
+if [ $DATABASE_SERVER == "mariadb-10.1" ]
+then
+
+    sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
 
     sudo apt update
     sudo apt install mariadb-server-10.1
+    sudo systemctl start mysql
 fi
