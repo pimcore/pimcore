7920c7a2298294dd22ee19bab7a93b5f2231e9e8
Bernhard Rusch <bernhard.rusch@elements.at>
2020-03-05 12:03:36 +0100

    added more mariadb versions and mysql 5.6

--- .travis/setup-db-server.sh
+++ .travis/setup-db-server.sh
@@ -12,15 +12,48 @@ then
 
     sudo apt-get install software-properties-common
     sudo apt-key adv --fetch-keys 'https://mariadb.org/mariadb_release_signing_key.asc'
+
+    # Oracle / MySQL Key
+    sudo apt-key adv --keyserver keys.gnupg.net --recv-keys 5072E1F5
 fi
 
 if [ $DATABASE_SERVER == "mariadb-10.1" ]
 then
-
     sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.1/ubuntu xenial main'
-
     sudo apt-get update
     sudo apt-get install -y mariadb-server-10.1
     sudo systemctl start mysql
     mysql -e "SET GLOBAL innodb_large_prefix=1;"
+fi
+
+if [ $DATABASE_SERVER == "mariadb-10.2" ]
+then
+    sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.2/ubuntu xenial main'
+    sudo apt-get update
+    sudo apt-get install -y mariadb-server-10.2
+    sudo systemctl start mysql
+fi
+
+if [ $DATABASE_SERVER == "mariadb-10.3" ]
+then
+    sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.3/ubuntu xenial main'
+    sudo apt-get update
+    sudo apt-get install -y mariadb-server-10.3
+    sudo systemctl start mysql
+fi
+
+if [ $DATABASE_SERVER == "mariadb-10.4" ]
+then
+    sudo add-apt-repository 'deb [arch=amd64,arm64,i386,ppc64el] http://nyc2.mirrors.digitalocean.com/mariadb/repo/10.4/ubuntu xenial main'
+    sudo apt-get update
+    sudo apt-get install -y mariadb-server-10.4
+    sudo systemctl start mysql
+fi
+
+if [ $DATABASE_SERVER == "mysql-5.6" ]
+then
+    sudo add-apt-repository 'deb http://repo.mysql.com/apt/ubuntu/ xenial mysql-5.6'
+    sudo apt-get update
+    sudo apt-get install -y mysql-server-5.6
+    sudo systemctl start mysql
 fi
