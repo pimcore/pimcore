2aa9984a2e2e8286b1109cb78a3a37364fc2a8fc
Sebastian Blank <sebastian.bl@gmx.de>
2020-03-22 18:35:46 +0100

    [PHPStan] Use new generate baseline option

--- .travis/run-stan.sh
+++ .travis/run-stan.sh
@@ -2,8 +2,6 @@
 
 set -e
 
-if [ $PHPSTAN_BASELINE == 0 ]; then sed -e "s?- phpstan-baseline.neon?#- phpstan-baseline.neon?g" -i phpstan.neon; fi
-
 if [ $SYMFONY_VERSION = "^3.4" ]
 then
     config=".travis/phpstan.travis.neon"
@@ -13,7 +11,7 @@ fi
 
 cmd="vendor/bin/phpstan analyse -c $config bundles/ lib/ models/ -l $PHPSTAN_LEVEL --memory-limit=-1"
 
-if [ $PHPSTAN_BASELINE_GENERATE == 1 ]; then cmd+=" --error-format baselineNeon > phpstan-baseline.neon"; fi
+if [ $PHPSTAN_BASELINE_GENERATE == 1 ]; then cmd+=" --generate-baseline"; fi
 
 echo $cmd
 eval $cmd
