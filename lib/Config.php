d87d1b3627732304288bafb1bb5e0080dc98a7c8
weisswurstkanone <josef.aichhorn@elements.at>
2020-04-07 09:04:33 +0200

    [Ecommerce][FilterService] Category filter is resetting after page reload (2nd alternative approach)

--- lib/Config.php
+++ lib/Config.php
@@ -14,8 +14,11 @@
 
 namespace Pimcore;
 
+use Pimcore\Cache\Runtime;
 use Pimcore\Config\EnvironmentConfig;
 use Pimcore\Config\EnvironmentConfigInterface;
+use Pimcore\Model\Element\AbstractElement;
+use Pimcore\Model\Element\Service;
 use Pimcore\Model\User\UserRole;
 use Pimcore\Model\WebsiteSetting;
 use Symfony\Cmf\Bundle\RoutingBundle\Routing\DynamicRouter;
@@ -241,7 +244,9 @@ class Config implements \ArrayAccess
                 $cacheKey = $cacheKey . '_site_' . $siteId;
             }
 
-            if (!$config = Cache::load($cacheKey)) {
+            /** @var \Pimcore\Config\Config $config */
+            $config = Cache::load($cacheKey);
+            if (!$config) {
                 $settingsArray = [];
                 $cacheTags = ['website_config', 'system', 'config', 'output'];
 
@@ -298,6 +303,15 @@ class Config implements \ArrayAccess
                 $config = new \Pimcore\Config\Config($settingsArray, true);
 
                 Cache::save($config, $cacheKey, $cacheTags, null, 998);
+            } else {
+                $data = $config->toArray();
+                foreach ($data as $key => $setting) {
+                    if ($setting instanceof AbstractElement) {
+                        $type = Service::getType($setting);
+                        $elementCacheKey = $type . "_" . $setting->getId();
+                        Runtime::set($elementCacheKey, $setting);
+                    }
+                }
             }
 
             self::setWebsiteConfig($config, $language);
