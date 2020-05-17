b96a4745aa0869af51d61fb4fec0e3e7a6c105e8
roland4432 <roland.arvanitopoulos@seitwerk.de>
2020-05-17 23:59:05 +0200

    Update configuration.js

--- bundles/AdminBundle/Resources/public/js/pimcore/element/tag/configuration.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/element/tag/configuration.js
@@ -14,6 +14,9 @@
 pimcore.registerNS("pimcore.element.tag.configuration");
 pimcore.element.tag.configuration = Class.create({
 
+    dataUrl: '/admin/tags/get-translations-for-tag/',
+    updateUrl: '/admin/tags/update-translation-for-tag',
+
     initialize: function() {
         var tabPanel = Ext.getCmp("pimcore_panel_tabs");
         tabPanel.add(this.getLayout());
@@ -37,18 +40,121 @@ pimcore.element.tag.configuration = Class.create({
 
             var tree = new pimcore.element.tag.tree();
 
+            treeLayout = tree.getLayout();
+            treeLayout.addListener({
+                itemclick: function(tree, record){
+                    this.fillPanelRow(record);
+                }.bind(this),
+            })
+
             this.layout = new Ext.Panel({
                 id: "tag_configuration",
                 title: t('element_tag_configuration'),
                 iconCls: "pimcore_icon_element_tags",
-                items: [tree.getLayout()],
+                items: [
+                    treeLayout,
+                    this.getEditTableLayout(),
+                ],
                 layout: "border",
-                closable: true
+                closable: true,
             });
 
-            tree.setFilterFieldWidth(340);
+            tree.setFilterFieldWidth(250);
         }
 
         return this.layout;
-    }
+    },
+
+    fillPanelRow: function(record){
+
+
+        this.store = pimcore.helpers.grid.buildDefaultStore(
+            this.dataUrl + record.id,
+            this.columnConfig.map(function (item) {
+                return item.dataIndex;
+            }),
+            pimcore.helpers.grid.getDefaultPageSize(-1)
+        );
+        this.store.load();
+
+        this.gridPanel.setStore(this.store);
+    },
+
+    getEditTableLayout: function(){
+        if(!this.editPanel){
+            this.cellEditing = Ext.create('Ext.grid.plugin.CellEditing', {
+                clicksToEdit: 1,
+                listeners: {
+                    validateedit: function(editor, context) {
+                        editor.editors.each(function (e) {
+                            Ext.Ajax.request({
+                                url: this.updateUrl,
+                                method: 'POST',
+                                params: {
+                                    id: context.record.id,
+                                    language: context.field,
+                                    translation: e.getValue(),
+                                },
+                                success: function(){
+                                    this.store.reload();
+                                }.bind(this)
+                            });
+                        }.bind(this));
+
+                        editor.editors.clear();
+                    }.bind(this)
+                }
+            });
+
+            var languages = pimcore.settings.websiteLanguages;
+            var dateRenderer = function (d) {
+                return Ext.Date.format(new Date(d * 1000), "Y-m-d H:i:s");
+            };
+
+            this.columnConfig = [{text: 'id', dataIndex: 'id', hidden: true,}];
+            for(var i = 0; i < languages.length; i++){
+                this.columnConfig.push({
+                    cls: 'x-column-header_' + languages[i].toLowerCase(),
+                    text: pimcore.available_languages[languages[i]],
+                    dataIndex: '_' + languages[i],
+                    sortable: false,
+                    getEditor: function(){ return new Ext.form.TextField({}); },
+                });
+            }
+
+            this.columnConfig.push({
+                text: t('creationDate'), dataIndex: 'creationDate', editable: false, renderer: dateRenderer, sortable: false,
+            });
+            this.columnConfig.push({
+                text: t('modificationDate'), dataIndex: 'modificationDate', editable: false, renderer: dateRenderer, sortable: false,
+            });
+
+            this.gridPanel = new Ext.grid.Panel({
+                frame: false,
+                bodyCls: "pimcore_editable_grid",
+                autoScroll: true,
+                columnLines: true,
+                plugins: [
+                    this.cellEditing,
+                ],
+                columns: {
+                    items: this.columnConfig,
+                    defaults: {
+                        flex: 1,
+                    }
+                }
+            });
+
+            this.editPanel = new Ext.Panel({
+                region: 'east',
+                width: '75%',
+                split: true,
+                items: [
+                    this.gridPanel,
+                ]
+            });
+        }
+
+        return this.editPanel;
+    },
 });
