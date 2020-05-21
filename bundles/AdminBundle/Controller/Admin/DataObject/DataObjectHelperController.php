3a76a116eb8e7117a15dcc1b274aae69256648a5
Michael LOOCK <michael.loock@gmail.com>
2020-05-21 20:32:58 +0200

    Fix uncorrect CSV/XLS export for 'Operator Merge' values

--- bundles/AdminBundle/Controller/Admin/DataObject/DataObjectHelperController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/DataObjectHelperController.php
@@ -2073,7 +2073,13 @@ class DataObjectHelperController extends AdminController
                 // check for objects bricks and localized fields
                 if (DataObject\Service::isHelperGridColumnConfig($field)) {
                     if ($helperDefinitions[$field]) {
-                        return DataObject\Service::calculateCellValue($object, $helperDefinitions, $field, ['language' => $requestedLanguage]);
+                        $cellValue = DataObject\Service::calculateCellValue($object, $helperDefinitions, $field, ['language' => $requestedLanguage]);
+
+                        //'Operator Merge' management
+                        if (is_array($cellValue)) {
+                            $cellValue = implode(',', $cellValue);
+                        }
+                        return $cellValue;
                     }
                 } elseif (substr($field, 0, 1) == '~') {
                     $type = $fieldParts[1];
