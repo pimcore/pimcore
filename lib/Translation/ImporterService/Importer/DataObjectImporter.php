896ae17ecd0fbb1e58d2af3d18ce8854646f012a
Michael Spahn <michaels@polargold.de>
2020-06-15 13:58:48 +0200

    Fix xliff import overriding source values

--- lib/Translation/ImporterService/Importer/DataObjectImporter.php
+++ lib/Translation/ImporterService/Importer/DataObjectImporter.php
@@ -19,6 +19,8 @@ use Pimcore\Model\Element;
 use Pimcore\Translation\AttributeSet\Attribute;
 use Pimcore\Translation\ExportDataExtractorService\DataExtractor\DataObjectDataExtractor;
 
+use function explode;
+
 class DataObjectImporter extends AbstractElementImporter
 {
     /**
@@ -70,12 +72,10 @@ class DataObjectImporter extends AbstractElementImporter
             $blockData = $element->{'get' . $blockName}($targetLanguage);
             $blockItem = !empty($blockData) && $blockData[$blockIndex] ? $blockData[$blockIndex] : $originalBlockItem;
 
-            // ensure correct target language
-            /* @var $blockItemField DataObject\Data\BlockElement */
-            $blockItemField = $blockItem[$fieldname];
-            $blockItemField->setValue('_language', $targetLanguage);
-
             $blockItemData = !empty($blockData) ? $blockItem[$fieldname] : clone $originalBlockItemData;
+
+            /* @var $blockItemData DataObject\Data\BlockElement */
+            $blockItemData->setLanguage($targetLanguage);
 
             $blockItemData->setData($attribute->getContent());
             $blockItem[$fieldname] = $blockItemData;
