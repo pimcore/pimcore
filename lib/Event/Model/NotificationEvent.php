fa6339cf82d48bf7f58a586a7668253838aabe25
Michael Loock <michael.loock@gmail.com>
2020-06-24 14:32:56 +0200

    Notification events for add/delete (#6600)
    * Notification events for add/update/delete
    
    * Renaming setObject to setNotification for consistency
    
    * Removing the pre/post update event on notifications
    
    * Notification events : rename pre/post add to pre/post save

---
+++ lib/Event/Model/NotificationEvent.php
@@ -0,0 +1,65 @@
+<?php
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Event\Model;
+
+use Pimcore\Event\Traits\ArgumentsAwareTrait;
+use Pimcore\Model\Notification;
+use Symfony\Component\EventDispatcher\Event;
+
+class NotificationEvent extends Event implements ElementEventInterface
+{
+    use ArgumentsAwareTrait;
+
+    /**
+     * @var Notification
+     */
+    protected $notification;
+
+    /**
+     * DataObjectEvent constructor.
+     *
+     * @param Notification $notification
+     * @param array $arguments
+     */
+    public function __construct(Notification $notification, array $arguments = [])
+    {
+        $this->notification = $notification;
+        $this->arguments = $arguments;
+    }
+
+    /**
+     * @return Notification
+     */
+    public function getNotification()
+    {
+        return $this->notification;
+    }
+
+    /**
+     * @param Notification $notification
+     */
+    public function setNotification($notification)
+    {
+        $this->notification = $notification;
+    }
+
+    /**
+     * @return Notification
+     */
+    public function getElement()
+    {
+        return $this->getNotification();
+    }
+}
