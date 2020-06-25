799b109596a8f3d985086f1eae78250da9a5c99a
Divesh <divesh.pahuja@pimcore.com>
2020-06-25 12:20:47 +0200

    [Documents] Link Editable - support data & aria custom attributes - #6621

--- models/Document/Tag/Link.php
+++ models/Document/Tag/Link.php
@@ -161,7 +161,10 @@ class Link extends Model\Document\Tag
             // add attributes to link
             $attribs = [];
             foreach ($availableAttribs as $key => $value) {
-                if ((is_string($value) || is_numeric($value)) && in_array($key, $allowedAttributes)) {
+                if ((is_string($value) || is_numeric($value))
+                    && (strpos($key, 'data-') === 0 ||
+                        strpos($key, 'aria-') === 0 ||
+                        in_array($key, $allowedAttributes))) {
                     if (!empty($this->data[$key]) && !empty($this->options[$key])) {
                         $attribs[] = $key.'="'. $this->data[$key] .' '. $this->options[$key] .'"';
                     } elseif (!empty($value)) {
