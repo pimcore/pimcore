647341bcc6f138f2eaa6c9c1fd39f879cfda78a3
Christian Fasching <fashxp@users.noreply.github.com>
2020-06-29 13:25:12 +0200

    Update NumberRange.php

--- bundles/EcommerceFrameworkBundle/FilterService/FilterType/FactFinder/NumberRange.php
+++ bundles/EcommerceFrameworkBundle/FilterService/FilterType/FactFinder/NumberRange.php
@@ -21,7 +21,7 @@ use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 /**
- * @deprecated version 6.7.0 and will be removed in 7.0.0.
+ * @deprecated since version 6.7.0 and will be removed in 7.0.0.
  *
  */
 class NumberRange extends \Pimcore\Bundle\EcommerceFrameworkBundle\FilterService\FilterType\NumberRange
