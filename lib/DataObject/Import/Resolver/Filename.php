17fc2280260878ffade3557355ffa879c43d6f9d
weisswurstkanone <josef.aichhorn@elements.at>
2020-07-01 12:28:24 +0200

    [Data Objects] CSV Import - New Objects Based On Key #5303

--- lib/DataObject/Import/Resolver/Filename.php
+++ lib/DataObject/Import/Resolver/Filename.php
@@ -17,6 +17,8 @@
 
 namespace Pimcore\DataObject\Import\Resolver;
 
+use Pimcore\File;
+use Pimcore\Model\Element\Service;
 use const FILTER_VALIDATE_BOOLEAN;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\AbstractObject;
@@ -56,21 +58,33 @@ class Filename extends AbstractResolver
             throw new ImportErrorException('not allowed to import into folder ' . $parent->getFullPath());
         }
 
-        if ($overwrite) {
-            $objectKey = $rowData[$this->getIdColumn($config)];
-        } else {
-            $objectKey = $prefix;
-        }
+        $object = null;
 
         $classId = $config->classId;
         $classDefinition = ClassDefinition::getById($classId);
         $className = 'Pimcore\\Model\\DataObject\\' . ucfirst($classDefinition->getName());
 
-        $intendedPath = $parent->getRealFullPath() . '/' . $objectKey;
-        $object = null;
+        if ($overwrite) {
+            $objectKey = $rowData[$this->getIdColumn($config)];
+            $intendedPath = $parent->getRealFullPath() . '/' . $objectKey;
+        } else {
+            $objectKey = $rowData[$this->getIdColumn($config)];
+            $objectKey = Service::getValidKey($objectKey, "object");
+            $intendedPath = $parent->getRealFullPath() . '/' . $objectKey;
 
-        if ($object = DataObject::getByPath($intendedPath) && $skipIfExists) {
-            throw new ImportWarningException('skipped filename exists: ' . $parent->getFullPath() . '/' . $objectKey);
+            if (AbstractObject::getByPath($intendedPath)) {
+                $objectKey = $prefix;
+            } else {
+                $object = $this->modelFactory->build($className);
+                $object->setParent($parent);
+                $object->setKey($objectKey);
+            }
+        }
+
+        if (!$object) {
+            if ($object = DataObject::getByPath($intendedPath) && $skipIfExists) {
+                throw new ImportWarningException('skipped filename exists: ' . $parent->getFullPath() . '/' . $objectKey);
+            }
         }
 
         if ($overwrite && !$service) {
@@ -111,7 +125,9 @@ class Filename extends AbstractResolver
                     'classname' => $className,
                 ]);
             } else {
-                $object = $this->getAlternativeObject($prefix, $intendedPath, $parent, $className);
+                if (!$object) {
+                    $object = $this->getAlternativeObject($prefix, $intendedPath, $parent, $className);
+                }
             }
         }
 
