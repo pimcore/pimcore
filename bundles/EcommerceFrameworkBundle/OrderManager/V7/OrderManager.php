be684d7eff35fae7a9f9f4c8ba0b32f0d34f0af8
Christian Fasching <fashxp@users.noreply.github.com>
2020-07-30 17:06:09 +0200

    added additional check to satisfy phpstan

--- bundles/EcommerceFrameworkBundle/OrderManager/V7/OrderManager.php
+++ bundles/EcommerceFrameworkBundle/OrderManager/V7/OrderManager.php
@@ -21,6 +21,7 @@ use Pimcore\Bundle\EcommerceFrameworkBundle\Exception\OrderUpdateNotPossibleExce
 use Pimcore\Bundle\EcommerceFrameworkBundle\Exception\UnsupportedException;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Model\AbstractOrder;
 use Pimcore\Bundle\EcommerceFrameworkBundle\OrderManager\OrderAgentFactoryInterface;
+use Pimcore\Bundle\EcommerceFrameworkBundle\PriceSystem\ModificatedPrice;
 use Pimcore\Bundle\EcommerceFrameworkBundle\VoucherService\VoucherServiceInterface;
 use Pimcore\Event\Ecommerce\OrderManagerEvents;
 use Pimcore\Event\Model\Ecommerce\OrderManagerEvent;
