a2081ebc0d88f73c72dc5b401ecc7def46a7752b
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2020-08-06 14:13:11 +0200

    Add a dummy test

---
+++ tests/Unit/Bundles/InstallBundle/InstallerTest.php
@@ -0,0 +1,16 @@
+<?php
+
+namespace Pimcore\Tests\Unit\Bundles\InstallBundle;
+
+use Pimcore\Tests\Test\TestCase;
+
+class InstallerTest extends TestCase
+{
+    /**
+     * @test
+     */
+    public function the_truth(): void
+    {
+        $this->assertTrue(true);
+    }
+}
