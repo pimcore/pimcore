a4c4287bf894d4f426f69d9e76a77a91490e7639
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2020-08-06 15:44:24 +0200

    WIP

--- tests/Unit/Bundles/InstallBundle/InstallerTest.php
+++ tests/Unit/Bundles/InstallBundle/InstallerTest.php
@@ -2,7 +2,10 @@
 
 namespace Pimcore\Tests\Unit\Bundles\InstallBundle;
 
+use Monolog\Logger;
+use Pimcore\Bundle\InstallBundle\Installer;
 use Pimcore\Tests\Test\TestCase;
+use Symfony\Component\EventDispatcher\EventDispatcher;
 
 class InstallerTest extends TestCase
 {
@@ -11,6 +14,7 @@ class InstallerTest extends TestCase
      */
     public function the_truth(): void
     {
-        $this->assertTrue(true);
+        $installer = new Installer(new Logger('test'), new EventDispatcher());
+        $this->assertNotNull($installer);
     }
 }
