88351dcbe177ec11ff2b759deb04867057ab11ca
roland4432 <roland.arvanitopoulos@seitwerk.de>
2020-08-13 17:03:26 +0200

    Update bundles/AdminBundle/Resources/public/js/pimcore/element/note_details.js
    oki dokey :)
    
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>

--- bundles/AdminBundle/Resources/public/js/pimcore/element/note_details.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/element/note_details.js
@@ -38,7 +38,7 @@ pimcore.element.note_details = Class.create({
                 buttons: [
                     {
                         text: t('close'),
-                        icon: '/bundles/pimcoreadmin/img/flat-white-icons/checkmark.svg',
+                        iconCls: "pimcore_icon_cancel",
                         handler: function(){
                             this.detailWindow.hide();
                             this.detailWindow.destroy();
