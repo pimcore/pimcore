dea4d5e48f11dc99f2eccddff2ceab5f8d0984e8
Iaroslav Sheptykin <i.sheptykin@neusta.de>
2020-08-18 14:47:03 +0200

    Move notification serivce to its home

--- tests/Service/Notification/Service/NotificationServiceTest.php
+++ tests/Service/Notification/Service/NotificationServiceTest.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  * @license    http://www.pimcore.org/license     GPLv3 and PEL
  */
 
-namespace Pimcore\Tests\Unit\Notification\Service;
+namespace Pimcore\Tests\Service\Notification\Service;
 
 use Pimcore\Model\Notification\Service\NotificationService;
 use Pimcore\Model\User;
