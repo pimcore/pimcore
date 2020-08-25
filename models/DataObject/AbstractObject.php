pimcore/pimcore

commit ccbaffccdcc2e2bccf49a09e8c56e1eeaf4fbd61

[DataObject] Fix deleting folders - #6945

models/DataObject/AbstractObject.php
@@ -355,7 +355,7 @@ class AbstractObject extends Model\Element\AbstractElement
     {
         $className = DataObject::class;
         // get classname
-        if (!in_array(static::class, [__CLASS__, Concrete::class], true)) {
+        if (!in_array(static::class, [__CLASS__, Concrete::class, Folder::class], true)) {
             /** @var Concrete $tmpObject */
             $tmpObject = new static();
             $className = 'Pimcore\\Model\\DataObject\\' . ucfirst($tmpObject->getClassName());

commit 054418995d9f3572fda855f00ad673442f2e8814

[DataObject] Fix deleting folders with class override
Regression from #6947 and #6945

as discussed external bundles with class override on DataObject\Folder class will still break.
Fix it with explicity checking if class is instanceof Concrete before constructing DataObject class and listing

models/DataObject/AbstractObject.php
@@ -358,7 +358,9 @@ class AbstractObject extends Model\Element\AbstractElement
         if (!in_array(static::class, [__CLASS__, Concrete::class, Folder::class], true)) {
             /** @var Concrete $tmpObject */
             $tmpObject = new static();
-            $className = 'Pimcore\\Model\\DataObject\\' . ucfirst($tmpObject->getClassName());
+            if ($tmpObject instanceof Concrete) {
+                $className = 'Pimcore\\Model\\DataObject\\' . ucfirst($tmpObject->getClassName());
+            }
         }
 
         if (is_array($config)) {