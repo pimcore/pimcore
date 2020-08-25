f0874d1db9f301e46300dbc7a805b740b69fd4a1
Divesh <divesh.pahuja@pimcore.com>
2020-08-25 16:34:31 +0200

    [Tests] Dependencies - Add tests for element dependencies

--- tests/Model/Element/DependenciesTest.php
+++ tests/Model/Element/DependenciesTest.php
@@ -3,7 +3,12 @@
 namespace Pimcore\Tests\Model\Element;
 
 use Pimcore\Db;
+use Pimcore\Model\Asset;
+use Pimcore\Model\DataObject;
+use Pimcore\Model\DataObject\AbstractObject;
 use Pimcore\Model\DataObject\Unittest;
+use Pimcore\Model\Document;
+use Pimcore\Model\Property;
 use Pimcore\Tests\Test\ModelTestCase;
 use Pimcore\Tests\Util\TestHelper;
 
@@ -61,5 +66,106 @@ class DependenciesTest extends ModelTestCase
         $source->delete();
         $count = $db->fetchOne("SELECT count(*) from dependencies WHERE sourceType = 'object' AND sourceID = " . $sourceId);
         $this->assertEquals(0, $count);
+    }
+
+    /**
+     * Verifies that an object requires and requiredBy dependencies are stored and fetched
+     *
+     */
+    public function testObjectDependencies()
+    {
+        /** @var DataObject $source */
+        $source = TestHelper::createEmptyObject();
+
+        /** @var Unittest[] $targets */
+        $targets = TestHelper::createEmptyObjects('', true, 3);
+        $this->saveElementDependencies($source, $targets);
+
+        //Reload source object
+        $source = AbstractObject::getById($source->getId(), true);
+
+        //get dependencies
+        $dependencies = $source->getDependencies();
+        $requires = $dependencies->getRequires();
+        $requiredBy = $dependencies->getRequiredBy();
+
+        $this->assertEquals(3, count($requires), 'DataObject: requires dependencies not saved or loaded properly');
+        $this->assertEquals($targets[0]->getId(), $requires[0]["id"], 'DataObject: requires dependency not saved or loaded properly');
+        $this->assertEquals($targets[2]->getId(), $requiredBy[0]["id"], 'DataObject: requiredBy dependency not saved or loaded properly');
+    }
+
+    /**
+     * Verifies that a document requires and requiredBy dependencies are stored and fetched
+     *
+     */
+    public function testDocumentDependencies()
+    {
+        /** @var Document $source */
+        $source = TestHelper::createEmptyDocumentPage();
+        /** @var Unittest[] $targets */
+        $targets = TestHelper::createEmptyObjects('', true, 3);
+        $this->saveElementDependencies($source, $targets);
+
+        //Reload source document
+        $source = Document::getById($source->getId(), true);
+
+        //get dependencies
+        $dependencies = $source->getDependencies();
+        $requires = $dependencies->getRequires();
+        $requiredBy = $dependencies->getRequiredBy();
+
+        $this->assertEquals(3, count($requires), 'Document: requires dependencies not saved or loaded properly');
+        $this->assertEquals($targets[0]->getId(), $requires[0]["id"], 'Document: requires dependency not saved or loaded properly');
+        $this->assertEquals($targets[2]->getId(), $requiredBy[0]["id"], 'Document: requiredBy dependency not saved or loaded properly');
+    }
+
+    /**
+     * Verifies that an asset requires and requiredBy dependencies are stored and fetched
+     *
+     */
+    public function testAssetDependencies()
+    {
+        /** @var Asset $source */
+        $source = TestHelper::createImageAsset();
+        /** @var Unittest[] $targets */
+        $targets = TestHelper::createEmptyObjects('', true, 3);
+        $this->saveElementDependencies($source, $targets);
+
+        //Reload source asset
+        $source = Asset::getById($source->getId(), true);
+
+        //get dependencies
+        $dependencies = $source->getDependencies();
+        $requires = $dependencies->getRequires();
+        $requiredBy = $dependencies->getRequiredBy();
+
+        $this->assertEquals(3, count($requires), 'Asset: requires dependencies not saved or loaded properly');
+        $this->assertEquals($targets[0]->getId(), $requires[0]["id"], 'Asset: requires dependency not saved or loaded properly');
+        $this->assertEquals($targets[2]->getId(), $requiredBy[0]["id"], 'Asset: requiredBy dependency not saved or loaded properly');
+    }
+
+    /**
+     * @param $source
+     * @param $targets
+     *
+     */
+    private function saveElementDependencies($source, $targets)
+    {
+        $properties = [];
+        foreach ($targets as $idx => $target) {
+            $propertyName = 'prop_' . $idx;
+            $property = new Property();
+            $property->setType('object');
+            $property->setName($propertyName);
+            $property->setCtype('object');
+            $property->setDataFromEditmode($target);
+            $properties[$propertyName] = $property;
+        }
+
+        $source->setProperties($properties);
+        $source->save();
+
+        $targets[2]->setMultihref([$source]);
+        $targets[2]->save();
     }
 }
