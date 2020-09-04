f2ef405eb2e6ab0dc1722d241c308ef67cc99edd
weisswurstkanone <josef.aichhorn@elements.at>
2020-09-04 13:11:54 +0200

    Corehandler: Collaboration approach

--- models/DataObject/Data/BlockElement.php
+++ models/DataObject/Data/BlockElement.php
@@ -18,17 +18,20 @@
 namespace Pimcore\Model\DataObject\Data;
 
 use DeepCopy\DeepCopy;
+use Pimcore\Cache\Core\CacheMarshallerInterface;
 use Pimcore\Cache\Runtime;
 use Pimcore\Model\AbstractModel;
 use Pimcore\Model\DataObject\OwnerAwareFieldInterface;
 use Pimcore\Model\DataObject\Traits\OwnerAwareFieldTrait;
 use Pimcore\Model\Element\ElementDescriptor;
+use Pimcore\Model\Element\ElementDumpStateInterface;
 use Pimcore\Model\Element\ElementInterface;
 use Pimcore\Model\Element\Service;
 use Pimcore\Model\Version\MarshalMatcher;
+use Pimcore\Model\Version\SetDumpStateFilter;
 use Pimcore\Model\Version\UnmarshalMatcher;
 
-class BlockElement extends AbstractModel implements OwnerAwareFieldInterface
+class BlockElement extends AbstractModel implements OwnerAwareFieldInterface, CacheMarshallerInterface
 {
     use OwnerAwareFieldTrait;
 
@@ -172,34 +175,6 @@ class BlockElement extends AbstractModel implements OwnerAwareFieldInterface
         }
     }
 
-    /**
-     * @return array
-     */
-    public function __sleep()
-    {
-        $copier = new DeepCopy();
-        $copier->skipUncloneable(true);
-        $copier->addTypeFilter(
-            new \DeepCopy\TypeFilter\ReplaceFilter(
-                function ($currentValue) {
-                    if ($currentValue instanceof ElementInterface) {
-                        $elementType = Service::getType($currentValue);
-                        $descriptor = new ElementDescriptor($elementType, $currentValue->getId());
-
-                        return $descriptor;
-                    }
-
-                    return $currentValue;
-                }
-            ),
-            new MarshalMatcher(null, null)
-        );
-
-        $this->needsRenewReferences = true;
-        $this->data = $copier->copy($this->data);
-
-        return parent::__sleep();
-    }
 
     /**
      * @return bool
@@ -224,4 +199,36 @@ class BlockElement extends AbstractModel implements OwnerAwareFieldInterface
     {
         $this->_language = $language;
     }
+
+    public function marshalForCache()
+    {
+        $this->needsRenewReferences = true;
+
+        $context = [
+            'source' => __METHOD__,
+            'conversion' => false
+        ];
+        $copier = Service::getDeepCopyInstance($this, $context);
+        $copier->addFilter(new SetDumpStateFilter(false), new \DeepCopy\Matcher\PropertyMatcher(ElementDumpStateInterface::class, ElementDumpStateInterface::DUMP_STATE_PROPERTY_NAME));
+
+        $copier->addTypeFilter(
+            new \DeepCopy\TypeFilter\ReplaceFilter(
+                function ($currentValue) {
+                    if ($currentValue instanceof ElementInterface) {
+                        $elementType = Service::getType($currentValue);
+                        $descriptor = new ElementDescriptor($elementType, $currentValue->getId());
+
+                        return $descriptor;
+                    }
+
+                    return $currentValue;
+                }
+            ),
+            new \Pimcore\Model\Element\DeepCopy\MarshalMatcher(null, null)
+        );
+        $data = $copier->copy($this);
+        return $data;
+    }
+
+
 }
