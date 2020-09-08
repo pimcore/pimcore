4daf010ec07e456a29f500906e07208e537d29e1
BlackbitNeueMedien <jan.walther@blackbit.de>
2020-09-08 15:04:15 +0200

    [Ghostscript] Use version_compare to compare ghostscript versions (#7063)
    * use version_compare to compare ghostscript versions
    
    * use version_compare to compare ghostscript versions
    
    * use version_compare to compare ghostscript versions
    
    * use version_compare to compare ghostscript versions

--- lib/Document/Adapter/Ghostscript.php
+++ lib/Document/Adapter/Ghostscript.php
@@ -27,7 +27,7 @@ class Ghostscript extends Adapter
     protected $path;
 
     /**
-     * @var float|null
+     * @var string|null
      */
     private $version = null;
 
@@ -166,7 +166,7 @@ class Ghostscript extends Adapter
         $command = self::getGhostscriptCli() . ' -dNODISPLAY -q';
 
         // Adding permit-file-read flag to prevent issue with Ghostscript's SAFER mode which is enabled by default as of version 9.50.
-        if ($this->getVersion() >= 9.50) {
+        if (version_compare($this->getVersion(), '9.50', '>=')) {
             $command .= " --permit-file-read='" . $this->path . "'";
         }
 
@@ -178,14 +178,14 @@ class Ghostscript extends Adapter
     /**
      * Get the version of the installed Ghostscript CLI.
      *
-     * @return float
+     * @return string
      *
      * @throws \Exception
      */
     protected function getVersion()
     {
         if (is_null($this->version)) {
-            $this->version = (float) Console::exec(self::getGhostscriptCli() . ' --version');
+            $this->version = Console::exec(self::getGhostscriptCli() . ' --version');
         }
 
         return $this->version;
