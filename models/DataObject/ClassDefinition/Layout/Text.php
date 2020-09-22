78b80cbf67c57155c07aa8ecdf67867b9fb367fd
weisswurstkanone <josef.aichhorn@elements.at>
2020-09-22 14:53:23 +0200

    [Object Grid] Missing field name and values when edit related fields from/in grid view table popup #7140

--- models/DataObject/ClassDefinition/Layout/Text.php
+++ models/DataObject/ClassDefinition/Layout/Text.php
@@ -137,7 +137,7 @@ class Text extends Model\DataObject\ClassDefinition\Layout
             @trigger_error('Using a text renderer class which does not implement ' . DynamicTextLabelInterface::class.' is deprecated', \E_USER_DEPRECATED);
         }
 
-        if (method_exists($renderer, 'renderLayoutText')) {
+        if (method_exists($renderer, 'renderLayoutText') && $object) {
             $context['fieldname'] = $this->getName();
             $context['layout'] = $this;
             $result = call_user_func([$renderer, 'renderLayoutText'], $this->renderingData, $object, $context);
