5dbba5c85be552e70f45dba7c48747f10e443f93
brusch <bernhard.rusch@elements.at>
2020-10-05 11:42:08 +0200

    add compiler pass

--- bundles/CoreBundle/PimcoreCoreBundle.php
+++ bundles/CoreBundle/PimcoreCoreBundle.php
@@ -19,6 +19,7 @@ use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\CacheCollectorPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\DebugStopwatchPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\DoctrineMigrationsParametersPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\LegacyTemplatePass;
+use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\LockStoragePass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\LongRunningHelperPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\MonologPsrLogMessageProcessorPass;
 use Pimcore\Bundle\CoreBundle\DependencyInjection\Compiler\MonologPublicLoggerPass;
@@ -92,5 +93,6 @@ class PimcoreCoreBundle extends Bundle
         $container->addCompilerPass(new RoutingLoaderPass());
         $container->addCompilerPass(new ProfilerAliasPass());
         $container->addCompilerPass(new LegacyTemplatePass());
+        $container->addCompilerPass(new LockStoragePass());
     }
 }
