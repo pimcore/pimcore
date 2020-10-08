f6d168fa5213b61ec9b509967d3db78d8beac4f9
brusch <bernhard.rusch@elements.at>
2020-10-08 15:33:14 +0200

    Removed HybridAuth Integration - resolves #6933

--- bundles/CoreBundle/Controller/PublicServicesController.php
+++ bundles/CoreBundle/Controller/PublicServicesController.php
@@ -228,16 +228,6 @@ class PublicServicesController extends Controller
     /**
      * @param Request $request
      *
-     * @deprecated
-     */
-    public function hybridauthAction(Request $request)
-    {
-        \Pimcore\Tool\HybridAuth::process();
-    }
-
-    /**
-     * @param Request $request
-     *
      * @return \Symfony\Component\HttpFoundation\RedirectResponse
      */
     public function customAdminEntryPointAction(Request $request)
