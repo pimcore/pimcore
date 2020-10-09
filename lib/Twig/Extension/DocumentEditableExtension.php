d0f7dc17d465e7e7a492c3c14e90c8c96eea8e86
brusch <bernhard.rusch@elements.at>
2020-10-09 14:28:43 +0200

    [Document] Editables: removed BC layer - follow up to #7278

--- lib/Twig/Extension/DocumentEditableExtension.php
+++ lib/Twig/Extension/DocumentEditableExtension.php
@@ -79,21 +79,6 @@ class DocumentEditableExtension extends AbstractExtension
      * @param array $options
      *
      * @return \Pimcore\Model\Document\Editable|string
-     *
-     * @deprecated since v6.8 and will be removed in 7. use renderEditable instead.
-     */
-    public function renderTag($context, $name, $inputName, array $options = [])
-    {
-        return $this->renderEditable($context, $name, $inputName, $options);
-    }
-
-    /**
-     * @param array $context
-     * @param string $name
-     * @param string $inputName
-     * @param array $options
-     *
-     * @return \Pimcore\Model\Document\Editable|string
      */
     public function renderEditable($context, $name, $inputName, array $options = [])
     {
