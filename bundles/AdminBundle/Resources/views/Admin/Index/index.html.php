bb4ed32e8fc01522af371e3409514d06a6d8d0b8
brusch <bernhard.rusch@elements.at>
2020-10-14 12:20:23 +0200

    Removed Tag & Snippet Management - resolves #7212

--- bundles/AdminBundle/Resources/views/Admin/Index/index.html.php
+++ bundles/AdminBundle/Resources/views/Admin/Index/index.html.php
@@ -693,9 +693,6 @@ $scripts = array(
     "pimcore/report/custom/definitions/analytics.js",
     "pimcore/report/custom/toolbarenricher.js",
 
-    "pimcore/settings/tagmanagement/panel.js",
-    "pimcore/settings/tagmanagement/item.js",
-
     // extension manager
     "pimcore/extensionmanager/admin.js",
 
