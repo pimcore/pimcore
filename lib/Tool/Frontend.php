3fbe28a09d62750aecc49ad8ac2b363086b61cbc
Niklas <niklas.brunberg@ateles.se>
2020-10-20 10:09:57 +0200

    Updates Frontend.php with webp_auto_support Config changes

--- lib/Tool/Frontend.php
+++ lib/Tool/Frontend.php
@@ -150,8 +150,9 @@ class Frontend
      */
     public static function hasWebpSupport()
     {
-        $config = \Pimcore::getContainer()->getParameter('pimcore.config')['assets']['image']['thumbnails']['webp_auto_support'];
-        if ($config) {
+        $config = \Pimcore\Config::getSystemConfiguration();
+        $isWebPAutoSupport = $config['assets']['image']['thumbnails']['webp_auto_support'] ?? false;
+        if ($isWebPAutoSupport) {
             if (self::hasClientWebpSupport() && self::hasDocumentWebpSupport()) {
                 return true;
             }
