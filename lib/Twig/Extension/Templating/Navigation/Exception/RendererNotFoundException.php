e3aba66ec91c8213f2d3b64566c58dc99bb24831
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2020-11-04 15:54:29 +0100

    [Templating] ease migration with template helpers by adding them to 6.8 already and alias them to php template helpers. this also makes it possible to make bundles compatible with 6.8 and 7.0

---
+++ lib/Twig/Extension/Templating/Navigation/Exception/RendererNotFoundException.php
@@ -0,0 +1,28 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Enterprise License (PEL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ * @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ * @license    http://www.pimcore.org/license     GPLv3 and PEL
+ */
+
+namespace Pimcore\Twig\Extension\Templating\Navigation\Exception;
+
+class RendererNotFoundException extends \InvalidArgumentException
+{
+    public static function create(string $name): self
+    {
+        return new static(sprintf('The navigation renderer "%s" was not found', $name));
+    }
+}
+
+class_alias(RendererNotFoundException::class, 'Pimcore\Templating\Helper\Navigation\Exception\RendererNotFoundException');
