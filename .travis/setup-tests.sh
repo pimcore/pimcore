7c9ad9b4396d539acc542614a003e91a3c55fea6
Bernhard Rusch <brusch@users.noreply.github.com>
2020-11-05 16:02:39 +0100

    Update .travis/setup-tests.sh
    Co-authored-by: Sebastian Blank <blank@data-factory.net>

--- .travis/setup-tests.sh
+++ .travis/setup-tests.sh
@@ -13,6 +13,5 @@ cp app/config/parameters.example.yml app/config/parameters.yml
 
 # install composer dependencies
 composer self-update --2
-composer config preferred-install source
 composer require symfony/symfony:$SYMFONY_VERSION --no-interaction --no-update --no-scripts
 composer install --no-interaction --optimize-autoloader
