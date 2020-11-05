5066f6f5637fad4a041fe13860f9436af735c396
Sebastian Blank <blank@data-factory.net>
2020-11-05 11:12:53 +0100

    Fix catched undefined error

--- models/User/AbstractUser/Dao.php
+++ models/User/AbstractUser/Dao.php
@@ -42,7 +42,7 @@ class Dao extends Model\Dao\AbstractDao
             $data = $this->db->fetchRow('SELECT * FROM users WHERE `id` = ?', $id);
         }
 
-        if (is_numeric($data['id'])) {
+        if (!empty($data['id'])) {
             $this->assignVariablesToModel($data);
         } else {
             throw new \Exception("user doesn't exist");
