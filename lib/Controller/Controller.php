8d83b3e944530be9945c901513a099ca515f696b
dvesh3 <divesh.pahuja@pimcore.com>
2020-11-13 10:17:16 +0100

    [Core] Add Rendering support for Templating Engine

--- lib/Controller/Controller.php
+++ lib/Controller/Controller.php
@@ -15,11 +15,81 @@
 namespace Pimcore\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\StreamedResponse;
+use Symfony\Component\Templating\EngineInterface;
 
 abstract class Controller extends AbstractController
 {
+    /**
+     * @inheritdoc
+     *
+     */
+    protected function render(string $view, array $parameters = [], Response $response = null): Response
+    {
+        $templatingEngine = $this->container->get('pimcore.templating');
+        if ($templatingEngine->isDelegate()) {
+            $content = $templatingEngine->render($view, $parameters);
+            if (null === $response) {
+                $response = new Response();
+            }
+
+            $response->setContent($content);
+
+            return $response;
+        }
+
+        return parent::render($view, $parameters, $response);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     */
+    protected function stream(string $view, array $parameters = [], StreamedResponse $response = null): StreamedResponse
+    {
+        $templatingEngine = $this->container->get('pimcore.templating');
+        if ($templatingEngine->isDelegate()) {
+
+            $callback = function () use ($templatingEngine, $view, $parameters) {
+                $templatingEngine->stream($view, $parameters);
+            };
+
+            if (null === $response) {
+                return new StreamedResponse($callback);
+            }
+
+            $response->setCallback($callback);
+
+            return $response;
+        }
+
+        return parent::stream($view, $parameters, $response);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     */
+    protected function renderView(string $view, array $parameters = []): string
+    {
+        $templatingEngine = $this->container->get('pimcore.templating');
+        if ($templatingEngine->isDelegate()) {
+            return $templatingEngine->render($view, $parameters);
+        }
+
+        return parent::renderView($view, $parameters);
+    }
+
+    /**
+     * @inheritdoc
+     *
+     */
     public static function getSubscribedServices()
     {
-        return parent::getSubscribedServices();
+        $services = parent::getSubscribedServices();
+        $services['pimcore.templating'] = '?'.EngineInterface::class;
+
+        return $services;
     }
 }
