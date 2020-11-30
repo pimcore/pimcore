d7860ef69b85ff75715c25675e50bbf0ac2c2eb1
Sebastian Blank <blank@data-factory.net>
2020-11-30 12:03:07 +0100

    PHPDoc: Duplicate types (#7625)

--- models/DataObject/ClassDefinition/Data.php
+++ models/DataObject/ClassDefinition/Data.php
@@ -1688,9 +1688,9 @@ abstract class Data
     }
 
     /**
-     * @param DataObject\Listing            $listing
-     * @param string|int|float|float|array $data comparison data, can be scalar or array (if operator is e.g. "IN (?)")
-     * @param string                        $operator SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
+     * @param DataObject\Listing $listing
+     * @param string|int|float|array $data comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+     * @param string $operator SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
      *
      * @return DataObject\Listing
      */
--- models/DataObject/Listing.php
+++ models/DataObject/Listing.php
@@ -205,7 +205,7 @@ class Listing extends Model\Listing\AbstractListing implements AdapterInterface,
      *
      * @param string $field database column to use for WHERE condition
      * @param string $operator SQL comparison operator, e.g. =, <, >= etc. You can use "?" as placeholder, e.g. "IN (?)"
-     * @param string|int|float|float|array $data comparison data, can be scalar or array (if operator is e.g. "IN (?)")
+     * @param string|int|float|array $data comparison data, can be scalar or array (if operator is e.g. "IN (?)")
      *
      * @return static
      */
