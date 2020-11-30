86037a22595cff4d63c55609e1dc3942299606f7
Jacob Dreesen <j.dreesen@neusta.de>
2020-11-30 10:42:38 +0100

    Remove unused variable

--- bundles/AdminBundle/Controller/Admin/TagsController.php
+++ bundles/AdminBundle/Controller/Admin/TagsController.php
@@ -135,7 +135,7 @@ class TagsController extends AdminController
             $filterTagList = new Tag\Listing();
             $filterTagList->setCondition('LOWER(`name`) LIKE ?', ['%' . $filterTagList->escapeLike(mb_strtolower($request->get('filter'))) . '%']);
             foreach ($filterTagList->load() as $filterTag) {
-                if ($parentId = $filterTag->getParentId() == 0) {
+                if ($filterTag->getParentId() == 0) {
                     $filterIds[] = $filterTag->getId();
                 } else {
                     $ids = explode('/', $filterTag->getIdPath());
