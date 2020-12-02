125f4c25d12d0d7ccbd038f17bceb63c8f6f144c
Bernhard Rusch <brusch@users.noreply.github.com>
2020-12-02 14:33:24 +0100

    Update models/User/AbstractUser/Dao.php

--- models/User/AbstractUser/Dao.php
+++ models/User/AbstractUser/Dao.php
@@ -61,7 +61,7 @@ class Dao extends Model\Dao\AbstractDao
         if (!empty($data['id'])) {
             $this->assignVariablesToModel($data);
         } else {
-            throw new Model\Exception\NotFoundException("user doesn't exist");
+            throw new Model\Exception\NotFoundException(sprintf('User with name "%s" does not exist', $name));
         }
     }
 
