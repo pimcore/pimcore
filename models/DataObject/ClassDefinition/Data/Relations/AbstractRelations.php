ea4dd07173d4a8c0cb842b164fb16c57e107de82
Josef Aichhorn <josef.aichhorn@elements.at>
2020-12-16 11:52:02 +0100

    Update models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>

--- models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
+++ models/DataObject/ClassDefinition/Data/Relations/AbstractRelations.php
@@ -444,7 +444,7 @@ abstract class AbstractRelations extends Data implements
                     }
 
                     if ($elementHash === null) {
-                        throw new \Exception('Passing relations without ID or type not allowed anymore!);
+                        throw new \Exception('Passing relations without ID or type not allowed anymore!');
                     } elseif (!isset($relationItems[$elementHash])) {
                         $relationItems[$elementHash] = $item;
                     } else {
