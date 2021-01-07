5444e692bd9c68c6f1ed544ae92fed7e0d03c396
Bernhard Rusch <bernhard.rusch@elements.at>
2021-01-07 11:43:34 +0100

    added migration script

--- bundles/CoreBundle/Migrations/Version20210107103923.php
+++ bundles/CoreBundle/Migrations/Version20210107103923.php
@@ -6,6 +6,7 @@ namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Pimcore\Model\DataObject;
 
 /**
  * Auto-generated Migration: Please modify to your needs!
@@ -19,13 +20,31 @@ final class Version20210107103923 extends AbstractMigration
 
     public function up(Schema $schema) : void
     {
-        // this up() migration is auto-generated, please modify it to your needs
+        $list = new DataObject\ClassDefinition\Listing();
+        $list = $list->load();
 
+        foreach ($list as $class) {
+            $this->write(sprintf('Saving class: %s', $class->getName()));
+            $class->save();
+        }
+
+        $list = new DataObject\Objectbrick\Definition\Listing();
+        $list = $list->load();
+        foreach ($list as $brickDefinition) {
+            $this->write(sprintf('Saving object brick: %s', $brickDefinition->getKey()));
+            $brickDefinition->save();
+        }
+
+        $list = new DataObject\Fieldcollection\Definition\Listing();
+        $list = $list->load();
+        foreach ($list as $fc) {
+            $this->write(sprintf('Saving field collection: %s', $fc->getKey()));
+            $fc->save();
+        }
     }
 
     public function down(Schema $schema) : void
     {
-        // this down() migration is auto-generated, please modify it to your needs
-
+        $this->write(sprintf('Please restore your class definition files in %s and run bin/console pimcore:deployment:classes-rebuild manually.', PIMCORE_CLASS_DIRECTORY));
     }
 }
