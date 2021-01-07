98292928386a2c0a9cd360b6b01b1102c3fb0a0a
Bernhard Rusch <bernhard.rusch@elements.at>
2021-01-07 11:54:53 +0100

    fixed PHPStan

--- lib/Maintenance/Tasks/ScheduledTasksTask.php
+++ lib/Maintenance/Tasks/ScheduledTasksTask.php
@@ -98,7 +98,7 @@ final class ScheduledTasksTask implements TaskInterface
                 } elseif ($task->getCtype() === 'object') {
                     $object = DataObject::getById($task->getCid());
 
-                    if ($object instanceof DataObject) {
+                    if ($object instanceof DataObject\Concrete) {
                         if ($task->getAction() === 'publish-version' && $task->getVersion() && $object->isAllowed('publish', $taskUser) && $object->isAllowed('versions', $taskUser)) {
                             if ($version = Version::getById($task->getVersion())) {
                                 $object = $version->getData();
