f7717e30031b8d9fe76cc9233dd0093024b7951e
Bernhard Rusch <bernhard.rusch@elements.at>
2021-01-12 17:04:45 +0100

    Asset tree preview for folders: performance improvement + reduced amount of requests

--- models/Asset/Folder.php
+++ models/Asset/Folder.php
@@ -95,4 +95,72 @@ class Folder extends Model\Asset
 
         return $this->getDao()->hasChildren();
     }
+
+    /**
+     * @internal
+     */
+    public function getPreviewImage(bool $hdpi = false): ?string
+    {
+        $filesystemPath = PIMCORE_TEMPORARY_DIRECTORY . '/image-thumbnails' . $this->getRealFullPath() . '/folder-preview' . ($hdpi ? '-hdpi' : '') . '.jpg';
+        $tileThumbnailConfig = Asset\Image\Thumbnail\Config::getPreviewConfig($hdpi);
+
+        $limit = 42;
+        $db = \Pimcore\Db::get();
+        $condition = "path LIKE :path AND type IN ('image', 'video', 'document')";
+        $conditionParams = [
+            'path' => $db->escapeLike($this->getRealFullPath()) . '/%',
+        ];
+
+        if(file_exists($filesystemPath)) {
+            $lastUpdate = $db->fetchOne('SELECT MAX(modificationDate) FROM assets WHERE ' . $condition . ' ORDER BY filename ASC LIMIT ' . $limit, $conditionParams);
+            if($lastUpdate < filemtime($filesystemPath)) {
+                return $filesystemPath;
+            }
+        }
+
+        $list = new Asset\Listing();
+        $list->setCondition($condition, $conditionParams);
+        $list->setOrderKey('filename');
+        $list->setOrder('asc');
+        $list->setLimit($limit);
+
+        $totalImages = $list->getTotalCount();
+        $count = 0;
+        $gutter = 5;
+        $squareDimension = 130;
+        $offsetTop = 0;
+        $colums = 3;
+
+        if($totalImages) {
+            $collage = imagecreatetruecolor(($squareDimension * $colums) + ($gutter * ($colums-1)), ceil(($totalImages / $colums)) * ($squareDimension + $gutter));
+            $background = imagecolorallocate($collage, 12, 15, 18);
+            imagefill($collage, 0, 0, $background);
+
+            foreach ($list as $asset) {
+                $offsetLeft = ($squareDimension + $gutter) * ($count % $colums);
+                $tileThumb = null;
+                if ($asset instanceof Image) {
+                    $tileThumb = $asset->getThumbnail($tileThumbnailConfig);
+                } elseif ($asset instanceof Document || $asset instanceof Video) {
+                    $tileThumb = $asset->getImageThumbnail($tileThumbnailConfig);
+                }
+
+                if ($tileThumb && preg_match('/\.jpg$/', $tileThumb->getFileSystemPath())) {
+                    $tile = imagecreatefromjpeg($tileThumb->getFileSystemPath());
+                    imagecopyresampled($collage, $tile, $offsetLeft, $offsetTop, 0, 0, $squareDimension, $squareDimension, $tileThumb->getWidth(), $tileThumb->getHeight());
+
+                    $count++;
+                    if($count % $colums === 0) {
+                        $offsetTop += ($squareDimension + $gutter);
+                    }
+                }
+            }
+
+            imagejpeg($collage, $filesystemPath, 60);
+
+            return $filesystemPath;
+        }
+
+        return null;
+    }
 }
