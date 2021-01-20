69020cc457160f97dedf17f5e019a101a75c4442
Divesh Pahuja <divesh.pahuja@pimcore.com>
2021-01-20 16:03:45 +0100

    Use KnpPaginatorBundle for listing pagination (#7913)
    * Use KnpPaginatorBundle for listing pagination - #7743
    
    * Use KnpPaginatorBundle for listing pagination - #7743
    
    * Introduces new generic interface, replaced Zend interfaces
    
    * Use KnpPaginatorBundle for listing pagination - #7743
    
    * Use KnpPaginatorBundle for listing pagination - docs #7743
    
    Co-authored-by: Bernhard Rusch <bernhard.rusch@elements.at>

---
+++ lib/Model/Paginator/PaginateListingInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace Pimcore\Model\Paginator;
+
+use Zend\Paginator\Adapter\AdapterInterface;
+use Zend\Paginator\AdapterAggregateInterface;
+
+/**
+ * @TODO: Pimcore 10 : Remove AdapterInterface and AdapterAggregateInterface
+ */
+interface PaginateListingInterface extends \Countable, \Iterator, AdapterInterface, AdapterAggregateInterface
+{
+    /**
+     * Returns a collection of items for a page.
+     *
+     * @param  int $offset Page offset
+     * @param  int $itemCountPerPage Number of items per page
+     * @return array
+     */
+    public function getItems($offset, $itemCountPerPage);
+}
