716d21b473d7f5b5a78ff2b55c234e23d1d1ef1d
weisswurstkanone <josef.aichhorn@elements.at>
2021-01-26 16:07:26 +0100

    [Tests] Tests for all data object field types #7475

--- bundles/EcommerceFrameworkBundle/PimcoreEcommerceFrameworkBundle.php
+++ bundles/EcommerceFrameworkBundle/PimcoreEcommerceFrameworkBundle.php
@@ -49,7 +49,7 @@ class PimcoreEcommerceFrameworkBundle extends AbstractPimcoreBundle
     {
         return [
             '/bundles/pimcoreecommerceframework/css/backend.css',
-            '/bundles/pimcoreecommerceframework/css/pricing.css',
+            '/bundles/pimcoreecommerceframework/css/pricing.css'
         ];
     }
 
@@ -71,15 +71,12 @@ class PimcoreEcommerceFrameworkBundle extends AbstractPimcoreBundle
             '/bundles/pimcoreecommerceframework/js/pricing/config/objects.js',
             '/bundles/pimcoreecommerceframework/js/voucherservice/VoucherSeriesTab.js',
             '/bundles/pimcoreecommerceframework/js/order/OrderTab.js',
-            '/admin/ecommerceframework/config/js-config',
+            '/admin/ecommerceframework/config/js-config'
         ];
     }
 
     public function boot()
     {
-        var_dump("isInstalled " . $this->getInstaller()->isInstalled());
-
-        die("boot done");
         $container = $this->container;
 
         if ($container->hasParameter('pimcore_ecommerce.decimal_scale')) {
