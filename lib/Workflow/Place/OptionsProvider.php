6a536c8931b475d9d58d455791604730834ce2a5
weisswurstkanone <josef.aichhorn@elements.at>
2021-02-11 12:47:16 +0100

    Phpstan test

--- lib/Workflow/Place/OptionsProvider.php
+++ lib/Workflow/Place/OptionsProvider.php
@@ -80,6 +80,9 @@ class OptionsProvider implements SelectOptionsProviderInterface
 
     protected function generatePlaceLabel(PlaceConfig $placeConfig): string
     {
+        if (!method_exists($this->translator, 'getLocale')) {
+            return '';
+        }
         // do not translate or format options when not in admin context
         if (empty($this->translator->getLocale())) {
             return $placeConfig->getLabel();
