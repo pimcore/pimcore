eee34da04b8cbb34e8606cc92e1932cc80a3714c
Josef Aichhorn <josef.aichhorn@elements.at>
2021-02-11 09:42:15 +0100

    Update lib/Targeting/Storage/SessionStorage.php
    Co-authored-by: Jacob Dreesen <j.dreesen@neusta.de>

--- lib/Targeting/Storage/SessionStorage.php
+++ lib/Targeting/Storage/SessionStorage.php
@@ -184,8 +184,9 @@ class SessionStorage implements TargetingStorageInterface
                 ));
         }
 
-        if (!$bag instanceof NamespacedAttributeBag)
+        if (!$bag instanceof NamespacedAttributeBag) {
             throw new \Exception("wrong type");
+        }
 
         return $bag;
     }
