3103a08eff11e4b3adf51e67c483e3531da7f242
weisswurstkanone <josef.aichhorn@elements.at>
2021-02-22 12:00:16 +0100

    Phpstan test

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -8,7 +8,7 @@ cp -r .github/ci/files/config/. config
 cp -r .github/ci/files/templates templates
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/src src
-cp -r .github/ci/files/models models
+cp -rv .github/ci/files/models models
 cp -r .github/ci/files/public public
 
 cp .github/ci/files/extensions.template.php var/config/extensions.php
