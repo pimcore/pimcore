9dee249746c73f8c2f6eb757cb79632e338767f4
weisswurstkanone <josef.aichhorn@elements.at>
2021-02-22 12:34:43 +0100

    Phpstan test

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -8,9 +8,6 @@ cp -r .github/ci/files/config/. config
 cp -r .github/ci/files/templates templates
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/src src
-cp -rv .github/ci/files/models models
-cp -r .github/ci/files/public public
-
 cp .github/ci/files/extensions.template.php var/config/extensions.php
 
 # temp. until elasticsearch/elasticsearch 7.11 is released
