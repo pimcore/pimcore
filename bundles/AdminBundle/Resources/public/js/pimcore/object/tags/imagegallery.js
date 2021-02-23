55a3fe889435056546e2fca081bd1023290f626c
patrick.rupp <patrick.rupp@gal-digital.de>
2021-02-23 13:02:01 +0100

    Fix getDefaultConfig of imagegallery.js overwriting previously set configuration

--- bundles/AdminBundle/Resources/public/js/pimcore/object/tags/imagegallery.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/tags/imagegallery.js
@@ -103,13 +103,13 @@ pimcore.object.tags.imageGallery = Class.create(pimcore.object.tags.abstract, {
     getDefaultFieldConfig: function () {
         var itemWidth = this.fieldConfig.width ? this.fieldConfig.width : 150;
         var itemHeight = this.fieldConfig.height ? this.fieldConfig.height : 150;
-
-        var fieldConfig = {
+    
+        var fieldConfig = Object.assign({}, this.fieldConfig, {
             width: itemWidth,
             height: itemHeight,
-            uploadPath: this.fieldConfig.uploadPath,
-        };
-
+            uploadPath: this.fieldConfig.uploadPath
+        });
+    
         return fieldConfig;
     },
 
