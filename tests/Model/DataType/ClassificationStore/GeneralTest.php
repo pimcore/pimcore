2bfffa1e037868825df5e87fb036850df0a5c302
Divesh Pahuja <divesh.pahuja@pimcore.com>
2021-03-02 11:53:06 +0100

    Fix Classification Store doesn't allow null (#8278)
    * Fix Classification Store doesn't allow null - #8277
    
    * Fix Classification Store doesn't allow null - Add test #8277
    
    * Fix Classification Store doesn't allow null - Add test #8277
    
    * Fix Classification Store doesn't allow null - optimize test #8277

--- tests/Model/DataType/ClassificationStore/GeneralTest.php
+++ tests/Model/DataType/ClassificationStore/GeneralTest.php
@@ -90,7 +90,7 @@ class GeneralTest extends AbstractClassificationStoreTest
             }
         }
 
-        // now check if inheritance is correctly implented
+        // now check if inheritance is correctly implemented
         $keyConfig = \Pimcore\Model\DataObject\Classificationstore\KeyConfig::getByName('key1', $store->getId());
 
         Cache::clearAll();
@@ -107,6 +107,67 @@ class GeneralTest extends AbstractClassificationStoreTest
         $csField = $o->getCsstore();
         $value = $csField->getLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId(), 'en');
         $this->assertEquals('defaultValue', $value);
+
+        Cache::enable();
+    }
+
+    public function testClassificationStoreQuantityValue()
+    {
+        $store = Classificationstore\StoreConfig::getByName('teststore');
+        $this->configureStoreWithQuantityValueField($store);
+
+        $o = new \Pimcore\Model\DataObject\Csstore();
+        $o->setParentId(1);
+        $o->setKey('testobject');
+        $o->setPublished(1);
+        $o->save();
+
+        Cache::disable();
+        Cache::clearAll();
+
+        /** @var \Pimcore\Model\DataObject\Classificationstore $csField */
+        $csField = $o->getCsstore();
+        $groupConfig = Classificationstore\GroupConfig::getByName('testgroupQvalue');
+        $keyConfig = \Pimcore\Model\DataObject\Classificationstore\KeyConfig::getByName('qValue', $store->getId());
+        $value = new \Pimcore\Model\DataObject\Data\QuantityValue(123, 1);
+        $csField->setLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId(), $value);
+        $o->save();
+
+        Cache::clearAll();
+        Cache\Runtime::clear();
+
+        $o = \Pimcore\Model\DataObject\Csstore::getById($o->getId());
+        /** @var \Pimcore\Model\DataObject\Data\QuantityValue $value1 */
+        $value1 = $o->getCsstore()->getLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId());
+        $this->assertEquals($value->getValue(), $value1->getValue());
+        $this->assertEquals($value->getUnit(), $value1->getUnit());
+
+        //clear value
+        $value = new \Pimcore\Model\DataObject\Data\QuantityValue(null, 1);
+        $o->getCsstore()->setLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId(), $value);
+        $o->save();
+
+        Cache::clearAll();
+        Cache\Runtime::clear();
+
+        $o = \Pimcore\Model\DataObject\Csstore::getById($o->getId());
+        /** @var \Pimcore\Model\DataObject\Data\QuantityValue $value1 */
+        $value1 = $o->getCsstore()->getLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId());
+        $this->assertNull($value1->getValue());
+
+        //clear value+unit (nullify field)
+        $value = new \Pimcore\Model\DataObject\Data\QuantityValue(null, null);
+        $o->getCsstore()->setLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId(), $value);
+        $o->save();
+
+        Cache::clearAll();
+        Cache\Runtime::clear();
+
+        $o = \Pimcore\Model\DataObject\Csstore::getById($o->getId());
+        /** @var \Pimcore\Model\DataObject\Data\QuantityValue $value1 */
+        $value1 = $o->getCsstore()->getLocalizedKeyValue($groupConfig->getId(), $keyConfig->getId());
+        $this->assertNull($value1);
+
 
         Cache::enable();
     }
