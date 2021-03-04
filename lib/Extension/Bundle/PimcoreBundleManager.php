f093a1893b83495bc864ea5d89d25a42b15e8a72
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-04 16:42:28 +0100

    Removed Debug Code

--- lib/Extension/Bundle/PimcoreBundleManager.php
+++ lib/Extension/Bundle/PimcoreBundleManager.php
@@ -550,7 +550,6 @@ class PimcoreBundleManager
      */
     public function isInstalled(PimcoreBundleInterface $bundle): bool
     {
-        return true;
         if (null === $installer = $bundle->getInstaller()) {
             // bundle has no dedicated installer, so we can treat it as installed
             return true;
