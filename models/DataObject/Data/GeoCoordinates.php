8dc808f7745f6bd479b4880e03678f343669c7ed
Josef Aichhorn <josef.aichhorn@elements.at>
2021-03-04 09:49:41 +0100

    Remove BC layer for Geopoint latitude/longitude constructor (#8317)
    * Remove BC layer for Geopoint latitude/longitude constructor #8307
    
    * Remove BC layer for Geopoint latitude/longitude constructor #8307

--- models/DataObject/Data/GeoCoordinates.php
+++ models/DataObject/Data/GeoCoordinates.php
@@ -17,15 +17,88 @@
 
 namespace Pimcore\Model\DataObject\Data;
 
-class GeoCoordinates extends Geopoint
+use Pimcore\Model\DataObject\OwnerAwareFieldInterface;
+use Pimcore\Model\DataObject\Traits\OwnerAwareFieldTrait;
+
+class GeoCoordinates implements OwnerAwareFieldInterface
 {
+
+    use OwnerAwareFieldTrait;
+
+    /**
+     * @var float
+     */
+    protected $longitude;
+
+    /**
+     * @var float
+     */
+    protected $latitude;
+
     /**
      * @param float|null $latitude
      * @param float|null $longitude
      */
     public function __construct($latitude = null, $longitude = null)
     {
-        parent::__construct($longitude, $latitude);
+        if ($latitude !== null) {
+            $this->setLatitude($latitude);
+        }
+
+        if ($longitude !== null) {
+            $this->setLongitude($longitude);
+        }
+
+        $this->markMeDirty();
+    }
+
+    /**
+     * @return float
+     */
+    public function getLongitude()
+    {
+        return $this->longitude;
+    }
+
+    /**
+     * @param float $longitude
+     *
+     * @return $this
+     */
+    public function setLongitude($longitude)
+    {
+        $longitude = (float)$longitude;
+
+        if ($this->longitude != $longitude) {
+            $this->longitude = $longitude;
+            $this->markMeDirty();
+        }
+
+        return $this;
+    }
+
+    /**
+     * @return float
+     */
+    public function getLatitude()
+    {
+        return $this->latitude;
+    }
+
+    /**
+     * @param float $latitude
+     *
+     * @return $this
+     */
+    public function setLatitude($latitude)
+    {
+        $latitude = (float)$latitude;
+        if ($this->latitude != $latitude) {
+            $this->latitude = $latitude;
+            $this->markMeDirty();
+        }
+
+        return $this;
     }
 
     /**
