fd569cff13dfc9406d98249a307fbab0ae02f905
Jan Walther <jan.walther@blackbit.de>
2021-03-07 08:56:47 +0100

    add phpdoc

--- lib/Helper/TemporaryFileHelperTrait.php
+++ lib/Helper/TemporaryFileHelperTrait.php
@@ -5,7 +5,12 @@ use Pimcore\File;
 
 trait TemporaryFileHelperTrait
 {
-    private function getLocalFile($path) {
+    /**
+     * Get local file path of the given file or URL
+     * @param string $path file path or URL
+     * @return string path to local file
+     */
+    private function getLocalFile($path): string {
         if (!stream_is_local($path)) {
             $tmpFilename = 'tmpfile_'.md5($path).'.'.File::getFileExtension($path);
             $tmpFilePath = PIMCORE_SYSTEM_TEMP_DIRECTORY.'/'.$tmpFilename;
