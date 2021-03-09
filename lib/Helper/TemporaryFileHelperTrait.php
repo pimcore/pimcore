dca5320a598c2f24e5abaa2b2478dbb1acbd7024
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-09 16:23:05 +0100

    Fixed Asset::getTemporaryFile() (regression of #8335) & removed debug code 😉

--- lib/Helper/TemporaryFileHelperTrait.php
+++ lib/Helper/TemporaryFileHelperTrait.php
@@ -3,6 +3,9 @@ namespace Pimcore\Helper;
 
 use Pimcore\File;
 
+/**
+ * @internal
+ */
 trait TemporaryFileHelperTrait
 {
     /**
@@ -12,35 +15,8 @@ trait TemporaryFileHelperTrait
      * @throws \Exception
      */
     private function getLocalFile($stream): string {
-        if (!stream_is_local($stream) || true) {
-            if(is_string($stream)) {
-                $src = fopen($stream, 'rb');
-                $fileExtension = File::getFileExtension($stream);
-            } else {
-                $src = $stream;
-                $streamMeta = stream_get_meta_data($src);
-                $fileExtension = File::getFileExtension($streamMeta['uri']);
-            }
-
-            $tmpFilePath = sprintf('%s/temp-file-%s.%s',
-                PIMCORE_SYSTEM_TEMP_DIRECTORY,
-                uniqid() . '-' .  bin2hex(random_bytes(15)),
-                $fileExtension
-            );
-
-            $dest = fopen($tmpFilePath, 'wb', false, File::getContext());
-            if(!$dest) {
-                throw new \Exception(sprintf('Unable to create temporary file in %s', $tmpFilePath));
-            }
-
-            stream_copy_to_stream($src, $dest);
-            fclose($dest);
-
-            register_shutdown_function(static function() use ($tmpFilePath) {
-                @unlink($tmpFilePath);
-            });
-
-            $stream = $tmpFilePath;
+        if (!stream_is_local($stream)) {
+            $stream = $this->getTemporaryFileFromStream($stream);
         }
 
         if(is_resource($stream)) {
@@ -49,5 +25,42 @@ trait TemporaryFileHelperTrait
         }
 
         return $stream;
+    }
+
+    /**
+     * @param resource|string $stream
+     * @return string
+     * @throws \Exception
+     */
+    private function getTemporaryFileFromStream($stream): string
+    {
+        if(is_string($stream)) {
+            $src = fopen($stream, 'rb');
+            $fileExtension = File::getFileExtension($stream);
+        } else {
+            $src = $stream;
+            $streamMeta = stream_get_meta_data($src);
+            $fileExtension = File::getFileExtension($streamMeta['uri']);
+        }
+
+        $tmpFilePath = sprintf('%s/temp-file-%s.%s',
+            PIMCORE_SYSTEM_TEMP_DIRECTORY,
+            uniqid() . '-' .  bin2hex(random_bytes(15)),
+            $fileExtension
+        );
+
+        $dest = fopen($tmpFilePath, 'wb', false, File::getContext());
+        if(!$dest) {
+            throw new \Exception(sprintf('Unable to create temporary file in %s', $tmpFilePath));
+        }
+
+        stream_copy_to_stream($src, $dest);
+        fclose($dest);
+
+        register_shutdown_function(static function() use ($tmpFilePath) {
+            @unlink($tmpFilePath);
+        });
+
+        return $tmpFilePath;
     }
 }
