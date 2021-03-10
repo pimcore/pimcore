98b4e8b72c0c0d75c62c0eb1cf869f360b2fa530
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-10 11:50:20 +0100

    Migration: consider #8389 for Pimcore X

--- bundles/CoreBundle/Migrations/Version20201008101817.php
+++ bundles/CoreBundle/Migrations/Version20201008101817.php
@@ -12,6 +12,8 @@ class Version20201008101817 extends AbstractMigration
      */
     public function up(Schema $schema): void
     {
+        $this->addSql('DROP VIEW IF EXISTS documents_editables;');
+
         if ($schema->hasTable('documents_elements')) {
             $this->addSql('RENAME TABLE documents_elements TO documents_editables;');
         }
@@ -23,5 +25,6 @@ class Version20201008101817 extends AbstractMigration
     public function down(Schema $schema): void
     {
         $this->addSql('RENAME TABLE documents_editables TO documents_elements;');
+        $this->addSql('CREATE OR REPLACE VIEW documents_editables AS SELECT * FROM documents_elements;');
     }
 }
