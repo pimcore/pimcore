166508f7264ece4d59baa7477f9b2ac0517fa640
Bernhard Rusch <brusch@users.noreply.github.com>
2021-03-18 10:30:45 +0100

    [Document] Fixed Symfony 5 compatibility / template reference for areabricks (#8500)
    * [Document] Fixed Symfony 5 compatibility / template reference for areabricks
    
    * Update lib/Document/Editable/EditableHandler.php
    
    Co-authored-by: Aaron <aarongerig@users.noreply.github.com>
    
    Co-authored-by: Aaron <aarongerig@users.noreply.github.com>

--- lib/Document/Editable/EditableHandler.php
+++ lib/Document/Editable/EditableHandler.php
@@ -342,13 +342,17 @@ class EditableHandler implements LoggerAwareInterface
     {
         if ($brick->getTemplateLocation() === TemplateAreabrickInterface::TEMPLATE_LOCATION_BUNDLE) {
             $bundle = $this->bundleLocator->getBundle($brick);
+            $bundleName = $bundle->getName();
+            if (str_ends_with($bundleName, 'Bundle')) {
+                $bundleName = substr($bundleName, 0, -6);
+            }
 
             $templateReference = '';
 
             foreach (['areas', 'Areas'] as $folderName) {
                 $templateReference = sprintf(
-                    '%s:%s/%s:%s.%s',
-                    $bundle->getName(),
+                    '@%s/%s/%s/%s.%s',
+                    $bundleName,
                     $folderName,
                     $brick->getId(),
                     $type,
