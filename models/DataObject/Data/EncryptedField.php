pimcore/pimcore

commit 3f37e671b5d63a55fee30dc4928932470a8af4e9

[DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save (#8502)
* [DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save - resolves #8501

* [DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save - fix Geo Types

* [DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save - resolves #8501

* [DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save - resolves #8501

* [DataObjects] Fix Encrypted fields with Ownerawareinterface leads to recursion on save - resolves #8501

models/DataObject/Data/EncryptedField.php
@@ -101,7 +101,12 @@ class EncryptedField implements OwnerAwareFieldInterface
             try {
                 $key = \Pimcore::getContainer()->getParameter('pimcore.encryption.secret');
                 $key = Key::loadFromAsciiSafeString($key);
-                $data = Serialize::serialize($this->plain);
+                $data = $this->plain;
+                //clear owner to avoid recursion
+                if ($data instanceof OwnerAwareFieldInterface) {
+                    $data->setOwner(null, '');
+                }
+                $data = Serialize::serialize($data);
 
                 $data = Crypto::encrypt($data, $key, true);
                 $this->encrypted = $data;
@@ -110,7 +115,7 @@ class EncryptedField implements OwnerAwareFieldInterface
                 throw new \Exception('could not load key');
             }
 
-            return ['encrypted'];
+            return ['encrypted', '_owner'];
         }
 
         return [];
@@ -129,6 +134,11 @@ class EncryptedField implements OwnerAwareFieldInterface
                 $data = Crypto::decrypt($this->encrypted, $key, true);
 
                 $data = Serialize::unserialize($data);
+
+                if ($data instanceof OwnerAwareFieldInterface) {
+                    $data->setOwner($this->_owner, '_owner');
+                }
+
                 $this->plain = $data;
             } catch (\Exception $e) {
                 Logger::error($e);

bundles/AdminBundle/Resources/public/js/pimcore/object/classes/data/geo/abstract.js
@@ -70,14 +70,14 @@ pimcore.object.classes.data.geo.abstract = Class.create(pimcore.object.classes.d
                 xtype: "numberfield",
                 fieldLabel: t("width"),
                 name: "width",
-                value: this.datax.width
+                value: datax.width
             },
             {
                 xtype: "numberfield",
                 fieldLabel: t("height"),
                 name: "height",
                 minValue: 180,
-                value: this.datax.height
+                value: datax.height
             }
         ];
     },

commit 0011e7f1691b3dd7a1c9564c491c274b07751d5c

Fixed merging issue caused by #8502

models/DataObject/Data/EncryptedField.php
@@ -104,7 +104,8 @@ class EncryptedField implements OwnerAwareFieldInterface
                 $data = $this->plain;
                 //clear owner to avoid recursion
                 if ($data instanceof OwnerAwareFieldInterface) {
-                    $data->setOwner(null, '');
+                    $data->_setOwner(null);
+                    $data->_setOwnerFieldname('');
                 }
                 $data = Serialize::serialize($data);
 
@@ -136,7 +137,8 @@ class EncryptedField implements OwnerAwareFieldInterface
                 $data = Serialize::unserialize($data);
 
                 if ($data instanceof OwnerAwareFieldInterface) {
-                    $data->setOwner($this->_owner, '_owner');
+                    $data->_setOwner($this->_owner);
+                    $data->_setOwnerFieldname('_owner');
                 }
 
                 $this->plain = $data;