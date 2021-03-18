b0c1695fc335190b1119d60da6619eeec1748efe
alexander.heidrich <alexander.heidrich@basilicom.de>
2021-03-18 13:32:24 +0100

    [Data Object] unpublishing-publish-permission-fix
    The saveAction will throw errors when unpublishing/publishing not allowed.
    Otherwise the dataObject will get striked, while no change in visibility is done.
    This might be caused due to the creation of a new version, which is in general unpublished.

--- bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -1236,10 +1236,15 @@ class DataObjectController extends ElementControllerBase implements KernelContro
         $this->assignPropertiesFromEditmode($request, $object);
         $this->applySchedulerDataToElement($request, $object);
 
-        if ($request->get('task') == 'unpublish' && $object->isAllowed('unpublish')) {
+        if (($request->get('task') === 'unpublish' && !$object->isAllowed('unpublish')) || ($request->get('task') === 'publish' && !$object->isAllowed('publish'))) {
+            throw $this->createAccessDeniedHttpException();
+        }
+
+        if ($request->get('task') == 'unpublish') {
             $object->setPublished(false);
         }
-        if ($request->get('task') == 'publish' && $object->isAllowed('publish')) {
+
+        if ($request->get('task') == 'publish') {
             $object->setPublished(true);
         }
 
