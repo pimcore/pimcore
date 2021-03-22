c5eb83bbe5d81883f2a65ebff355711577a2f914
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-22 10:10:26 +0100

    [Documents] cannot unset $site->rootDocument - fixes #8517

--- bundles/AdminBundle/Controller/Traits/DocumentTreeConfigTrait.php
+++ bundles/AdminBundle/Controller/Traits/DocumentTreeConfigTrait.php
@@ -80,7 +80,7 @@ trait DocumentTreeConfigTrait
 
             // test for a site
             if ($site = Site::getByRootId($childDocument->getId())) {
-                unset($site->rootDocument);
+                $site->setRootDocument(null);
                 $tmpDocument['site'] = $site;
             }
         } elseif ($childDocument->getType() == 'folder' || $childDocument->getType() == 'link' || $childDocument->getType() == 'hardlink') {
