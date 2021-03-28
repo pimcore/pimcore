31989d7b07e6d0b498544dd622e5eb01db22c2a2
Bernhard Rusch <bernhard.rusch@elements.at>
2021-03-28 14:38:32 +0200

    Tests: include /local configs

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -5,6 +5,7 @@ set -eu -o xtrace
 mkdir -p var/config
 
 cp -r .github/ci/files/config/. config
+mkdir -p config/local/
 cp -r .github/ci/files/templates templates
 cp -r .github/ci/files/bin/console bin/console
 cp -r .github/ci/files/src src
@@ -13,7 +14,6 @@ cp .github/ci/files/extensions.template.php var/config/extensions.php
 cp .github/ci/files/.env ./
 
 if [ $PIMCORE_STORAGE = "minio" ]; then
-    mkdir -p config/local/
     cp .github/ci/files/minio-flysystem.yml config/local/
     composer require -n --no-update league/flysystem-aws-s3-v3
 fi
