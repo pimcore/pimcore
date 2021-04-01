6a50a9dd9c3ec112ff4ef816e21d084164b87dcf
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-01 16:17:58 +0200

    fixed merging issues

--- bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
+++ bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
@@ -73,7 +73,9 @@ final class PimcoreCoreExtension extends ConfigurableExtension implements Prepen
         // unauthenticated routes do not double-check for authentication
         $container->setParameter('pimcore.admin.unauthenticated_routes', $config['admin']['unauthenticated_routes']);
 
-        $container->setParameter('pimcore.encryption.secret', $config['encryption']['secret']);
+        if(!$container->hasParameter('pimcore.encryption.secret')) {
+            $container->setParameter('pimcore.encryption.secret', $config['encryption']['secret']);
+        }
 
         $container->setParameter('pimcore.admin.session.attribute_bags', $config['admin']['session']['attribute_bags']);
         $container->setParameter('pimcore.admin.translations.path', $config['admin']['translations']['path']);
