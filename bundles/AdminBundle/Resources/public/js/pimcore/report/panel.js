054213d4ae82d4aeeef3334bafe72bc0a4b1f592
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-02 13:24:25 +0200

    Use material icon for report tab in elements

--- bundles/AdminBundle/Resources/public/js/pimcore/report/panel.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/report/panel.js
@@ -150,7 +150,7 @@ pimcore.report.panel = Class.create({
                 border: false,
                 layout: "border",
                 items: [this.tree,this.reportContainer],
-                iconCls: "pimcore_icon_reports"
+                iconCls: "pimcore_material_icon_reports pimcore_material_icon"
             };
 
             // register an id for the standalone version
@@ -158,6 +158,7 @@ pimcore.report.panel = Class.create({
                 layoutConfig.id = "pimcore_reports";
                 layoutConfig.closable = true;
                 layoutConfig["title"] = t('reports');
+                layoutConfig["iconCls"] = 'pimcore_icon_reports';
             }
 
             this.layout = new Ext.Panel(layoutConfig);
