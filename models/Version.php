41a3a3398a1eb854371f1ebc2f03b120dca01684
weisswurstkanone <josef.aichhorn@elements.at>
2021-04-02 12:58:25 +0200

    BC layer for version unserialization of geopoint

--- models/Version.php
+++ models/Version.php
@@ -24,6 +24,7 @@ use Pimcore\File;
 use Pimcore\Logger;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 use Pimcore\Model\DataObject\Concrete;
+use Pimcore\Model\DataObject\Data\GeoCoordinates;
 use Pimcore\Model\Element\DeepCopy\PimcoreClassDefinitionMatcher;
 use Pimcore\Model\Element\DeepCopy\PimcoreClassDefinitionReplaceFilter;
 use Pimcore\Model\Element\ElementDumpStateInterface;
@@ -361,7 +362,7 @@ class Version extends AbstractModel
 
         if ($this->getSerialized()) {
             class_alias(GeoCoordinates::class, 'Pimcore\Model\DataObject\Data\Geopoint');
-            
+
             $data = Serialize::unserialize($data);
             //clear runtime cache to avoid dealing with marshalled data
             Runtime::clear();
