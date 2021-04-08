04cbc46787328c65d82e1116626589953a960a41
ckogler <christian.kogler@elements.at>
2021-04-08 13:11:09 +0200

    Bugfix

--- bundles/CoreBundle/Migrations/Version20210408153226.php
+++ bundles/CoreBundle/Migrations/Version20210408153226.php
@@ -1,14 +1,17 @@
 <?php
 
+declare(strict_types=1);
+
 namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
+
 /**
  * Auto-generated Migration: Please modify to your needs!
  */
-class Version20190102153226 extends AbstractPimcoreMigration
+class Version20190102153226 extends AbstractMigration
 {
     /**
      * @param Schema $schema
