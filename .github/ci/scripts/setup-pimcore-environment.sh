7724e5e49097b04ddd6bec325635abad484771dc
Sebastian Blank <blank@data-factory.net>
2021-04-08 12:05:10 +0200

    Simplify local phpstan usage (#8623)
    * Simplify local phpstan usage
    
    * Support usage in project
    
    * Remove temp. minimum-stability "dev"

--- .github/ci/scripts/setup-pimcore-environment.sh
+++ .github/ci/scripts/setup-pimcore-environment.sh
@@ -13,11 +13,7 @@ cp -r .github/ci/files/public public
 cp .github/ci/files/extensions.template.php var/config/extensions.php
 cp .github/ci/files/.env ./
 
-if [ $PIMCORE_STORAGE = "minio" ]; then
+if [ ${PIMCORE_STORAGE:-local} = "minio" ]; then
     cp .github/ci/files/minio-flysystem.yml config/local/
     composer require -n --no-update league/flysystem-aws-s3-v3
 fi
-
-# temp. until elasticsearch/elasticsearch 7.11 is released
-composer config minimum-stability "dev"
-composer config prefer-stable true
