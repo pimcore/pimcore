45fad2c0003e47f2f650c62d9b89bfb5a7ddec84
Sebastian Blank <blank@data-factory.net>
2021-04-12 14:48:49 +0200

    Placeholder/Twig functionality is missing with PdfReactor

--- lib/Web2Print/Processor/PdfReactor.php
+++ lib/Web2Print/Processor/PdfReactor.php
@@ -142,7 +142,7 @@ class PdfReactor extends Processor
         $pdfreactor = $this->getClient();
 
         $reactorConfig = $this->getConfig($config);
-        $reactorConfig['document'] = $html;
+        $reactorConfig['document'] = $this->processHtml($html, $params);
 
         $event = new PrintConfigEvent($this, ['config' => $config, 'reactorConfig' => $reactorConfig, 'document' => $document]);
         \Pimcore::getEventDispatcher()->dispatch(DocumentEvents::PRINT_MODIFY_PROCESSING_CONFIG, $event);
