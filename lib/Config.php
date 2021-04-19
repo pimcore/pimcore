794114c0f5f98018a72167c404c8af1a07e03b71
Josef Aichhorn <josef.aichhorn@elements.at>
2021-04-19 11:36:23 +0200

    Config::setSystemConfig sets it to the runtime but getSystemConfig never gets it from there (#8830)
    * Config::setSystemConfig sets it to the runtime but getSystemConfig never takes it from there #8825
    
    * Config::setSystemConfig sets it to the runtime but getSystemConfig never takes it from there #8825

--- lib/Config.php
+++ lib/Config.php
@@ -136,6 +136,23 @@ class Config implements \ArrayAccess
     }
 
     /**
+     * @internal ONLY FOR TESTING PURPOSES IF NEEDED FOR SPECIFIC TEST CASES
+     *
+     * @param null|array $configuration
+     * @param null|mixed $offset
+     */
+    public static function setSystemConfiguration($configuration, $offset = null)
+    {
+        if (null !== $offset) {
+            self::getSystemConfiguration();
+            static::$systemConfig[$offset] = $configuration;
+        } else {
+            static::$systemConfig = $configuration;
+        }
+    }
+
+
+    /**
      * @internal
      *
      * @param null|mixed $offset
