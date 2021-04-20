89daae2bd4d93ae6de058ee86a73a57dd661738a
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-20 17:58:39 +0200

    added versionCount condition to getLatestVersion()

--- models/Element/Traits/VersionDaoTrait.php
+++ models/Element/Traits/VersionDaoTrait.php
@@ -17,11 +17,12 @@ trait VersionDaoTrait
      */
     public function getLatestVersion($userId = null)
     {
-        $versionId = $this->db->fetchOne("SELECT id FROM versions WHERE cid = :cid AND ctype = :ctype AND `date` > :mdate AND ((draft = 1 AND userId = :userId) OR draft = 0) ORDER BY `versionCount` DESC LIMIT 1", [
+        $versionId = $this->db->fetchOne("SELECT id FROM versions WHERE cid = :cid AND ctype = :ctype AND (`date` > :mdate OR versionCount > :versionCount) AND ((draft = 1 AND userId = :userId) OR draft = 0) ORDER BY `versionCount` DESC LIMIT 1", [
             'cid' => $this->model->getId(),
             'ctype' => Element\Service::getType($this->model),
             'userId' => $userId,
             'mdate' => $this->model->getModificationDate(),
+            'versionCount' => $this->model->getVersionCount(),
         ]);
 
         if ($versionId) {
