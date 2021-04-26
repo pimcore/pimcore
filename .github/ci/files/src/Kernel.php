4b2c56033b914ce30d08b24b3d64279c8000e10a
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-26 16:54:38 +0200

    Renamed remaining PEL references

--- .github/ci/files/src/Kernel.php
+++ .github/ci/files/src/Kernel.php
@@ -1,10 +1,11 @@
 <?php
+
 /**
  * Pimcore
  *
  * This source file is available under two different licenses:
  * - GNU General Public License version 3 (GPLv3)
- * - Pimcore Enterprise License (PEL)
+ * - Pimcore Commercial License (PCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
  *
