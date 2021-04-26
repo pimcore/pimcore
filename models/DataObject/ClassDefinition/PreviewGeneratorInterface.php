83c94c23c655f9c6e2a9c215439aaee5416500e2
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-26 12:02:54 +0200

    updated PhpDoc to be more specific

--- models/DataObject/ClassDefinition/PreviewGeneratorInterface.php
+++ models/DataObject/ClassDefinition/PreviewGeneratorInterface.php
@@ -4,19 +4,30 @@
 namespace Pimcore\Model\DataObject\ClassDefinition;
 
 
+use Pimcore\Model\DataObject\Concrete;
+
 interface PreviewGeneratorInterface
 {
 
     /**
-     * @param \Pimcore\Model\DataObject\Concrete $object
+     * @param Concrete $object
      * @param array $params
      * @return string
      */
-    public function generatePreviewUrl(\Pimcore\Model\DataObject\Concrete $object, array $params): string;
+    public function generatePreviewUrl(Concrete $object, array $params): string;
 
 
     /**
-     * @return array
+     * @return array[
+     *  [
+     *  'name' => string,
+     *  'label' => string,
+     *  'values' => [
+     *     string => string,
+     *  ]
+     *  ]
+     * ]
+     *
      */
-    public function getPreviewConfig(\Pimcore\Model\DataObject\Concrete $object): array;
+    public function getPreviewConfig(Concrete $object): array;
 }
