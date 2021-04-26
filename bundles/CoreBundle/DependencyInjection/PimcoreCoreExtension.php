6e8ee8d09a8900e1fe17d9df3dd27378c1e1964d
Josef Aichhorn <josef.aichhorn@elements.at>
2021-04-26 10:08:54 +0200

    Query Table + Fallback languages #7721

--- bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
+++ bundles/CoreBundle/DependencyInjection/PimcoreCoreExtension.php
@@ -65,7 +65,7 @@ final class PimcoreCoreExtension extends ConfigurableExtension implements Prepen
         if (!$container->hasParameter('container.dumper.inline_class_loader')) {
             $container->setParameter('container.dumper.inline_class_loader', true);
         }
-        $container->setParameter("pimcore.objects.ignore_localized_query_fallback", $config["objects"]["ignore_localized_query_fallback"]);
+        $container->setParameter("pimcore.objects.ignore_localized_query_fallback", $config["objects"]["ignore_localized_query_fallback"] ?? false);
 
         // bundle manager/locator config
         $container->setParameter('pimcore.extensions.bundles.search_paths', $config['bundles']['search_paths']);
