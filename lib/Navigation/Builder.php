2089a0a1552f90271290303ecf8ae24523c4839c
Niklas <niklas.brunberg@ateles.se>
2021-04-28 11:04:59 +0200

    Adds cachetags to navigation

--- lib/Navigation/Builder.php
+++ lib/Navigation/Builder.php
@@ -44,6 +44,8 @@ class Builder
      */
     private $currentLevel = 0;
 
+    private $navCacheTags = [];
+
     /**
      * @param RequestHelper $requestHelper
      * @param string|null $pageClass
@@ -106,6 +108,8 @@ class Builder
         if (!$navigation || !$cacheEnabled) {
             $navigation = new \Pimcore\Navigation\Container();
 
+            $this->navCacheTags = ['output', 'navigation'];
+
             if ($navigationRootDocument->hasChildren()) {
                 $this->currentLevel = 0;
                 $rootPage = $this->buildNextLevel($navigationRootDocument, true, $pageCallback, [], $maxDepth);
@@ -115,7 +119,7 @@ class Builder
             // we need to force caching here, otherwise the active classes and other settings will be set and later
             // also written into cache (pass-by-reference) ... when serializing the data directly here, we don't have this problem
             if ($cacheEnabled) {
-                CacheManager::save($navigation, $cacheKey, ['output', 'navigation'], $cacheLifetime, 999, true);
+                CacheManager::save($navigation, $cacheKey, $this->navCacheTags, $cacheLifetime, 999, true);
             }
         }
 
@@ -341,6 +345,8 @@ class Builder
                 if ($pageCallback instanceof \Closure) {
                     $pageCallback($page, $child);
                 }
+
+                $this->navCacheTags[] = $page->getDocument()->getCacheTag();
 
                 $pages[] = $page;
             }
