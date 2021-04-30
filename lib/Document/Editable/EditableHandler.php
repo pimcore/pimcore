0b98d4f348b01c981489e16750ccb004d4361ef1
Bernhard Rusch <bernhard.rusch@elements.at>
2021-04-30 10:57:18 +0200

    [Document] Renderlet: fixed rendering on CLI

--- lib/Document/Editable/EditableHandler.php
+++ lib/Document/Editable/EditableHandler.php
@@ -33,7 +33,9 @@ use Pimcore\Templating\Renderer\ActionRenderer;
 use Psr\Log\LoggerAwareInterface;
 use Psr\Log\LoggerAwareTrait;
 use Symfony\Bundle\FrameworkBundle\Templating\EngineInterface;
+use Symfony\Component\HttpFoundation\RequestStack;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpKernel\Fragment\FragmentRendererInterface;
 use Symfony\Contracts\Translation\TranslatorInterface;
 
 class EditableHandler implements EditableHandlerInterface, LoggerAwareInterface
@@ -85,6 +87,16 @@ class EditableHandler implements EditableHandlerInterface, LoggerAwareInterface
      */
     protected $brickTemplateCache = [];
 
+    /**
+     * @var FragmentRendererInterface
+     */
+    protected $fragmentRenderer;
+
+    /**
+     * @var RequestStack
+     */
+    protected $requestStack;
+
     public const ATTRIBUTE_AREABRICK_INFO = '_pimcore_areabrick_info';
 
     /**
@@ -115,6 +127,26 @@ class EditableHandler implements EditableHandlerInterface, LoggerAwareInterface
         $this->requestHelper = $requestHelper;
         $this->translator = $translator;
         $this->responseStack = $responseStack;
+    }
+
+    /**
+     * @internal
+     * @required
+     * @param FragmentRendererInterface $fragmentRenderer
+     */
+    public function setFragmentRenderer(FragmentRendererInterface $fragmentRenderer): void
+    {
+        $this->fragmentRenderer = $fragmentRenderer;
+    }
+
+    /**
+     * @internal
+     * @required
+     * @param RequestStack $requestStack
+     */
+    public function setRequestStack(RequestStack $requestStack): void
+    {
+        $this->requestStack = $requestStack;
     }
 
     /**
@@ -401,7 +433,17 @@ class EditableHandler implements EditableHandlerInterface, LoggerAwareInterface
             $query
         );
 
-        return $this->actionRenderer->render($uri, $options);
+        if($this->requestHelper->hasCurrentRequest()) {
+            return $this->actionRenderer->render($uri, $options);
+        } else {
+            // this case could happen when rendering on CLI, e.g. search-reindex ...
+            $request = $this->requestHelper->createRequestWithContext();
+            $this->requestStack->push($request);
+            $response = $this->actionRenderer->render($uri, $options);
+            $this->requestStack->pop();
+
+            return $response;
+        }
     }
 }
 
