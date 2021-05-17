8c7e98311f830ee88b3c8519e5f72ec47de6a3f9
dvesh3 <divesh.pahuja@pimcore.com>
2021-05-17 12:19:34 +0200

    [Admin] Fix Custom Login Background Image is not saved - resolves #9125

--- bundles/AdminBundle/Controller/Admin/SettingsController.php
+++ bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -470,7 +470,7 @@ class SettingsController extends AdminController
                     'login_screen_invert_colors' => $values['branding.login_screen_invert_colors'],
                     'color_login_screen' => $values['branding.color_login_screen'],
                     'color_admin_interface' => $values['branding.color_admin_interface'],
-                    'login_screen_custom_image' => $values['general.loginscreencustomimage'],
+                    'login_screen_custom_image' => $values['branding.login_screen_custom_image'],
                 ],
         ];
 
