cea992e2d4d941938961d628a4891769316ca4d0
Niklas <niklas@lynks.se>
2021-07-05 18:25:59 +0200

    Update bundles/CoreBundle/Migrations/Version20210702102100.php
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>

--- bundles/CoreBundle/Migrations/Version20210702102100.php
+++ bundles/CoreBundle/Migrations/Version20210702102100.php
@@ -40,7 +40,7 @@ final class Version20210702102100 extends AbstractMigration
 
     public function down(Schema $schema): void
     {
-        $this->rebuildClassesCommand();
+        $this->regenerateClasses();
     }
 
     /**
