b61c4f7bbaf1dd3f1ab7c82ed10de3ec66728ab0
Bernhard Rusch <bernhard.rusch@elements.at>
2021-07-06 17:01:49 +0200

    POEditor Action

--- .github/ci/scripts/poeditor.php
+++ .github/ci/scripts/poeditor.php
@@ -2,52 +2,61 @@
 
 $token = getenv('POEDITOR_TOKEN');
 $languagesString = getenv('POEDITOR_LANGUAGES');
-
-$projects = [
-    38068 => [
-        'zh-CN',
-        'cs',
-        'nl',
-        'fr',
-        'de',
-        'hu',
-        'it',
-        'ja',
-        'fa',
-        'pl',
-        'pt-br',
-        'ru',
-        'sk',
-        'es',
-        'sv',
-        'sv-fi',
-        'th',
-        'tr',
-        'uk',
-    ],
-    197253 => [
-        'cs',
-        'nl',
-        'de',
-        'hu',
-        'it',
-        'pl',
-        'sk',
-        'es',
-        'th',
-    ]
-];
+$projects = [38068, 197253];
 
 $allowedLanguages = [];
 if(!empty($languagesString)) {
     $allowedLanguages = array_map('trim', explode(',', $languagesString));
 }
 
+$getPostValues = function ($url, array $params) {
+    $ch = curl_init();
+    curl_setopt($ch, CURLOPT_URL,$url);
+    curl_setopt($ch, CURLOPT_POST, 1);
+    curl_setopt($ch, CURLOPT_POSTFIELDS, http_build_query($params));
+    curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+
+    $response = curl_exec($ch);
+    curl_close($ch);
+
+    return @json_decode($response, true);
+};
+
+$getFile = function ($projectId, $language) {
+    return sprintf('bundles/CoreBundle/Resources/translations/%s.%sjson',
+        $language,
+        ($projectId === 197253) ? 'extended.' : ''
+    );
+};
+
 echo "Allowed languages: \n\n";
 var_dump($allowedLanguages);
 echo "\n\n";
 
 foreach($projects as $projectId => $languages) {
+
+    $responseJson = $getPostValues('https://api.poeditor.com/v2/languages/list', [
+        'api_token' => $token,
+        'id' => $projectId,
+    ]);
+
+    $languages = [];
+    if($responseJson && isset($responseJson['result']) && isset($responseJson['result']['languages'])) {
+        foreach($responseJson['result']['languages'] as $language) {
+            if($language['percentage'] > 70) {
+                // add language to be updated or added
+                $languages[] = $language['code'];
+            } else {
+                // language isn't over 70%, skip it and delete existing
+                $file = $getFile($projectId, $language['code']);
+                if(file_exists($file)) {
+                    echo sprintf("Deleted %s because it's not over 70 percent translated.\n", $file);
+                    unlink($file);
+                }
+            }
+        }
+    }
+
     foreach($languages as $language) {
 
         if(!empty($allowedLanguages) && !in_array($language, $allowedLanguages)) {
@@ -55,32 +64,22 @@ foreach($projects as $projectId => $languages) {
             continue;
         }
 
-        $file = sprintf('bundles/CoreBundle/Resources/translations/%s.%sjson',
-            $language,
-            ($projectId === 197253) ? 'extended.' : ''
-        );
-
+        $file = $getFile($projectId, $language['code']);
         echo $file . "\n-------------------------------------\n";
 
-        $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL,"https://api.poeditor.com/v2/projects/export");
-        curl_setopt($ch, CURLOPT_POST, 1);
-        curl_setopt($ch, CURLOPT_POSTFIELDS,
-            sprintf("api_token=%s&id=%s&language=%s&type=key_value_json", $token, $projectId, $language));
+        $responseJson = $getPostValues('https://api.poeditor.com/v2/projects/export', [
+            'api_token' => $token,
+            'id' => $projectId,
+            'language' => $language,
+            'type' => 'key_value_json',
+        ]);
 
-        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
-
-        $response = curl_exec($ch);
-        curl_close($ch);
-
-        $responseJson = @json_decode($response, true);
         if($responseJson && isset($responseJson['result']) && isset($responseJson['result']['url'])) {
             $contents = file_get_contents($responseJson['result']['url']);
             //echo $contents;
             //echo "\n-----------------------------------------------------------\n\n\n";
             //file_put_contents($file, $contents);
         } else {
-            var_dump($response);
             var_dump($responseJson);
             exit(1);
         }
