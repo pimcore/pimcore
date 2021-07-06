a83c37eb62bd4a5450919b9cd25d8e0fc90e889c
Bernhard Rusch <bernhard.rusch@elements.at>
2021-07-06 16:06:41 +0200

    POEditor Action

--- .github/ci/scripts/poeditor.php
+++ .github/ci/scripts/poeditor.php
@@ -1,7 +1,54 @@
 <?php
 
+$token = getenv('POEDITOR_TOKEN');
 
-echo getenv('FOOBAR');
+$projects = [
+    38068 => [
+        'zh_Hans',
+        'cs',
+        'nl',
+        'fr',
+        'de',
+        'hu',
+        'it',
+        'ja',
+        'fa',
+        'pl',
+        'pt_BR',
+        'ru',
+        'sk',
+        'es',
+        'sv',
+        'sv_FI',
+        'th',
+        'tr',
+        'uk',
+    ],
+    197253 => [
+        'cs',
+        'nl',
+        'de',
+        'hu',
+        'it',
+        'pl',
+        'sk',
+        'es',
+        'th',
+    ]
+];
 
+foreach($projects as $projectId => $languages) {
+    foreach($languages as $language) {
+        $file = sprintf('bundles/CoreBundle/Resources/translations/%s.%sjson',
+            $language,
+            ($projectId === 197253) ? '.extended' : ''
+        );
 
-
+        echo $file . "\n";
+        @exec('curl -X POST https://api.poeditor.com/v2/projects/export -d api_token="' . $token . '" -d id="' . $projectId . '" -d language="de" -d type="key_value_json" -o ' . $file, $output, $returnCode);
+        if($returnCode !== 0) {
+            echo sprintf('Unable to retrieve translations for project %s with language %s', $projectId, $language);
+            exit($returnCode);
+        }
+    }
+}
