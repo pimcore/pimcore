eed98a084d6e8852788cbbcaf8f2b67fe60a1ba6
Sebastian Blank <blank@data-factory.net>
2021-07-20 09:45:31 +0200

    Consistent method name getMimeType/setMimeType (#9637)
    * Consistent method name getMimeType
    
    * Consistent method name setMimeType

--- bundles/AdminBundle/Controller/Admin/Asset/AssetController.php
+++ bundles/AdminBundle/Controller/Admin/Asset/AssetController.php
@@ -1101,7 +1101,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
         return new StreamedResponse(function () use ($stream) {
             fpassthru($stream);
         }, 200, [
-            'Content-Type' => $asset->getMimetype(),
+            'Content-Type' => $asset->getMimeType(),
             'Content-Disposition' => sprintf('attachment; filename="%s"', $asset->getFilename()),
             'Content-Length' => $asset->getFileSize(),
         ]);
@@ -1253,7 +1253,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
         $response = new StreamedResponse(function () use ($stream) {
             fpassthru($stream);
         }, 200, [
-            'Content-Type' => $image->getMimetype(),
+            'Content-Type' => $image->getMimeType(),
             'Access-Control-Allow-Origin' => '*',
         ]);
         $this->addThumbnailCacheHeaders($response);
@@ -1556,7 +1556,7 @@ class AssetController extends ElementControllerBase implements KernelControllerE
     {
         $stream = null;
 
-        if ($asset->getMimetype() == 'application/pdf') {
+        if ($asset->getMimeType() == 'application/pdf') {
             $stream = $asset->getStream();
         }
 
