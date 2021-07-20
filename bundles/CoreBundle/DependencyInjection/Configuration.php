2d72d26dfa3f8e12b3c88dc8001b8a2d2f4e2c3a
Sebastian Blank <blank@data-factory.net>
2021-07-20 16:54:59 +0200

    NodeDefinition::setDeprecated() - Fix deprecation warnings

--- bundles/CoreBundle/DependencyInjection/Configuration.php
+++ bundles/CoreBundle/DependencyInjection/Configuration.php
@@ -73,7 +73,11 @@ final class Configuration implements ConfigurationInterface
                             ->end()
                         ->end()
                     ->end()
-                    ->setDeprecated('The "%node%" option is deprecated since Pimcore 10.1, it will be removed in Pimcore 11.')
+                    ->setDeprecated(
+                        'pimcore/pimcore',
+                        '10.1',
+                        'The "%node%" option is deprecated since Pimcore 10.1, it will be removed in Pimcore 11.'
+                    )
                 ->end()
                 ->arrayNode('bundles')
                     ->addDefaultsIfNotSet()
