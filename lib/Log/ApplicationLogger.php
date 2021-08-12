e76184855302bad0af6bfac3a83e4e6b84ce14f9
Julian Raab <julian.raab@elements.at>
2021-08-12 15:44:02 +0200

    Avoid var_dump-ing exceptions (#9968)
    * remove var_dump exceptions
    
    * check for previous existence
    
    * apply includeStackTrace for previous
    
    * Update ApplicationLogger.php
    
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>

--- lib/Log/ApplicationLogger.php
+++ lib/Log/ApplicationLogger.php
@@ -399,17 +399,29 @@ class ApplicationLogger implements LoggerInterface
         ], $context));
     }
 
-    private static function createExceptionFileObject(\Throwable $exceptionObject)
+    private static function exceptionToString(\Throwable $exceptionObject, bool $includeStackTrace, bool $includePrevious = false): string
     {
-        //workaround to prevent "nesting level to deep" errors when used var_export()
-        ob_start();
-        var_dump($exceptionObject);
-        $dataDump = ob_get_clean();
+        $data = [
+            $exceptionObject->getMessage(),
+            "File: " . $exceptionObject->getFile(),
+            "Line: " . $exceptionObject->getLine(),
+            "Code: " . $exceptionObject->getCode(),
+        ];
 
-        if (!$dataDump) {
-            $dataDump = $exceptionObject->getMessage();
+        if ($includeStackTrace) {
+            $data[] = "Trace:\n" . $exceptionObject->getTraceAsString();
         }
 
-        return new FileObject($dataDump);
+        if ($includePrevious && $exceptionObject->getPrevious()) {
+            $data[] = "\nPrevious:\n" . self::exceptionToString($exceptionObject->getPrevious(), $includeStackTrace);
+        }
+
+        return implode("\n", $data);
+    }
+    
+    private static function createExceptionFileObject(\Throwable $exceptionObject)
+    {
+        $data = self::exceptionToString($exceptionObject, true, true);
+        return new FileObject($data);
     }
 }
