c1796b01e8c1991962c6570d3761258ad8cfce13
Jan Walther <jan.walther@blackbit.de>
2021-09-06 15:36:20 +0200

    throw exceptions when select field option array items have no "key" or "value" keys

--- models/DataObject/ClassDefinition/Data/Select.php
+++ models/DataObject/ClassDefinition/Data/Select.php
@@ -15,6 +15,7 @@
 
 namespace Pimcore\Model\DataObject\ClassDefinition\Data;
 
+use InvalidArgumentException;
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
@@ -195,9 +196,16 @@ class Select extends Data implements
      *
      * @return $this
      */
-    public function setOptions($options)
+    public function setOptions(array $options)
     {
-        $this->options = $options;
+        $this->options = [];
+        foreach($options as $option) {
+            if(!array_key_exists('key', $option) || !array_key_exists('value', $option)) {
+                throw new InvalidArgumentException('Please provide select options as associative array with fields "key" and "value"');
+            }
+
+            $this->options[] = $options;
+        }
 
         return $this;
     }
