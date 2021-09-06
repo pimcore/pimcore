969b2a9fb395d64c7412aaa93cfcc056863377e0
Iida Halmela <iida.halmela@loiki.fi>
2021-09-06 19:16:27 +0300

    display image at natural size, if smaller than max dimensions

--- bundles/AdminBundle/Resources/public/js/pimcore/asset/image.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/asset/image.js
@@ -438,8 +438,11 @@ pimcore.asset.image = Class.create(pimcore.asset.asset, {
                     this.initPreviewImage();
                     var area = this.displayPanel.getEl().down('img');
                     if(area) {
+                        area.setStyle('width', this.data.imageInfo.dimensions.width + "px");
+                        area.setStyle('height', this.data.imageInfo.dimensions.height + "px");
                         area.setStyle('max-width', (width - 340) + "px");
                         area.setStyle('max-height', (height - 40) + "px");
+                        area.setStyle('object-fit', 'contain');
                     }
                 }
             }.bind(this));
