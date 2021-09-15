81e3f9cd6483af338a23904b8d53ca52478fe16a
Niklas <niklas.brunberg@cag.se>
2021-09-15 15:04:59 +0200

    UrlSlug table name constant and cache fix (#10293)
    * UrlSlug db operation fixes
    
    * Adds UrlSlug::TABLE_NAME to Concrete.php
    
    * Use UrlSlug::TABLE_NAME in GridHelperService.php
    
    * Use UrlSlug::TABLE_NAME in UrlSlug classdefinition

--- models/DataObject/Concrete.php
+++ models/DataObject/Concrete.php
@@ -858,7 +858,7 @@ class Concrete extends DataObject implements LazyLoadedFieldsInterface
     {
         $descriptor['objectId'] = $this->getId();
 
-        return $this->doRetrieveData($descriptor, 'object_url_slugs');
+        return $this->doRetrieveData($descriptor, DataObject\Data\UrlSlug::TABLE_NAME);
     }
 
     /**
