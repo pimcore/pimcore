3faddb9a7203a37f4d5181dc31ae3f669ef9b18e
weisswurstkanone <weisswurstkanone@users.noreply.github.com>
2021-09-21 10:20:51 +0000

    Apply php-cs-fixer changes

--- bundles/AdminBundle/Controller/Admin/SettingsController.php
+++ bundles/AdminBundle/Controller/Admin/SettingsController.php
@@ -238,8 +238,8 @@ class SettingsController extends AdminController
         $list = Metadata\Predefined\Listing::getByTargetType($type, [$subType]);
         $result = [];
         foreach ($list as $item) {
-            $itemGroup = $item->getGroup() ?? "";
-            if ($group === "default" || $group === $itemGroup) {
+            $itemGroup = $item->getGroup() ?? '';
+            if ($group === 'default' || $group === $itemGroup) {
                 $item->expand();
                 $result[] = $item->getObjectVars();
             }
