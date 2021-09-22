1e64f9bf6390be3d6f6bff1c3d3955b602176e00
BabovicT <80684610+BabovicT@users.noreply.github.com>
2021-09-22 10:22:59 +0200

    FIX vimeo and dailymotion video editables (#10370)
    * FIX dailymotion video editable
    
    * FIX vimeo video editable

--- models/Document/Editable/Video.php
+++ models/Document/Editable/Video.php
@@ -637,13 +637,13 @@ class Video extends Model\Document\Editable
             $additional_params = '';
 
             $clipConfig = [];
-            if (is_array($config['config']['clip'])) {
+            if (isset($config['config']['clip']) && is_array($config['config']['clip'])) {
                 $clipConfig = $config['config']['clip'];
             }
 
             // this is to be backward compatible to <= v 1.4.7
             $configurations = $clipConfig;
-            if (is_array($config['vimeo'])) {
+            if (isset($config['vimeo']) && is_array($config['vimeo'])) {
                 $configurations = array_merge($clipConfig, $config['vimeo']);
             }
 
@@ -707,11 +707,11 @@ class Video extends Model\Document\Editable
 
             $additional_params = '';
 
-            $clipConfig = is_array($config['config']['clip']) ? $config['config']['clip'] : [];
+            $clipConfig = isset($config['config']['clip']) && is_array($config['config']['clip']) ? $config['config']['clip'] : [];
 
             // this is to be backward compatible to <= v 1.4.7
             $configurations = $clipConfig;
-            if (is_array($config['dailymotion'])) {
+            if (isset($config['dailymotion']) && is_array($config['dailymotion'])) {
                 $configurations = array_merge($clipConfig, $config['dailymotion']);
             }
 
