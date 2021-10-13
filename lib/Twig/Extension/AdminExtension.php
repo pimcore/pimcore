af1aa4bf5d118507eec081623a4b010b2cc70ef7
Bernhard Rusch <bernhard.rusch@elements.at>
2021-10-13 14:16:13 +0200

    [Admin] Minimize bundle JS files in production

--- lib/Twig/Extension/AdminExtension.php
+++ lib/Twig/Extension/AdminExtension.php
@@ -18,6 +18,7 @@ declare(strict_types=1);
 namespace Pimcore\Twig\Extension;
 
 use Pimcore\Tool\Admin;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Twig\Extension\AbstractExtension;
 use Twig\TwigFunction;
 
@@ -26,23 +27,48 @@ use Twig\TwigFunction;
  */
 class AdminExtension extends AbstractExtension
 {
+    public function __construct(private UrlGeneratorInterface $generator)
+    {
+    }
+
     public function getFunctions()
     {
         return [
             new TwigFunction('pimcore_minimize_scripts', [$this, 'minimize']),
+            new TwigFunction('pimcore_script_exists', [$this, 'exists']),
         ];
     }
 
-    public function minimize(array $paths): array
+    public function minimize(array $paths): string
     {
+        $returnHtml = '';
         $scriptContents = '';
         foreach ($paths as $path) {
-            $fullPath = PIMCORE_WEB_ROOT . '/bundles/pimcoreadmin/js/' . $path;
-            if (file_exists($fullPath)) {
-                $scriptContents .= file_get_contents($fullPath) . "\n\n\n";
+            $found = false;
+            foreach([PIMCORE_WEB_ROOT . '/bundles/pimcoreadmin/js/' . $path,
+                        PIMCORE_WEB_ROOT . $path,
+                    ] as $fullPath) {
+                if (file_exists($fullPath)) {
+                    $scriptContents .= file_get_contents($fullPath) . "\n\n\n";
+                    $found = true;
+                }
+            }
+
+            if(!$found) {
+                $returnHtml .= $this->getScriptTag($path);
             }
         }
 
-        return Admin::getMinimizedScriptPath($scriptContents);
+        $parameters = Admin::getMinimizedScriptPath($scriptContents);
+        $url = $this->generator->generate('pimcore_admin_misc_scriptproxy', $parameters, UrlGeneratorInterface::ABSOLUTE_PATH);
+
+        $returnHtml .= $this->getScriptTag($url);
+
+        return $returnHtml;
+    }
+
+    private function getScriptTag($url): string
+    {
+        return '<script src="' . $url . '"></script>' . "\n";
     }
 }
