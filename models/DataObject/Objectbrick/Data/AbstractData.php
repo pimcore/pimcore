3fdb55677692ee7bf147642350e6da45ccacc156
Divesh <divesh.pahuja@pimcore.com>
2021-10-18 17:05:25 +0200

    [DataObject] Fix dataobject versions using Objectbricks created on Pimcore 6.9 or earlier - resolves #10620

--- models/DataObject/Objectbrick/Data/AbstractData.php
+++ models/DataObject/Objectbrick/Data/AbstractData.php
@@ -47,7 +47,7 @@ abstract class AbstractData extends Model\AbstractModel implements Model\DataObj
     /**
      * @var bool
      */
-    protected bool $doDelete = false;
+    protected $doDelete = false;
 
     /**
      * @var Concrete|null
@@ -113,7 +113,7 @@ abstract class AbstractData extends Model\AbstractModel implements Model\DataObj
     public function setDoDelete($doDelete)
     {
         $this->flushContainer();
-        $this->doDelete = $doDelete;
+        $this->doDelete = (bool)$doDelete;
 
         return $this;
     }
