f48d52ee4564cacd8b5dfe58414de626babbd419
BlackbitNeueMedien <jan.walther@blackbit.de>
2021-10-20 11:39:43 +0200

    Add foreign keys to data object tables (#10486)
    * add foreign keys to data store tables
    
    * add foreign keys to data store tables
    
    * add foreign keys to query table
    
    * add foreign keys to data object tables
    
    * add foreign keys to data object tables
    
    * add foreign keys to data object tables
    
    * delete object-related data in other tables via foreign keys
    
    * alter referencing object id columns to be INT(11) UNSIGNED
    
    * shorten foreign key name if longer than 64 characters
    
    * shorten foreign key name if longer than 64 characters
    
    * Update bundles/CoreBundle/Migrations/Version20211018104331.php
    
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>
    
    * Update bundles/CoreBundle/Migrations/Version20211018104331.php
    
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>
    
    * Update bundles/CoreBundle/Migrations/Version20211018104331.php
    
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>
    
    * add foreign keys to data object tables
    
    * add foreign keys to data object tables
    
    * Update lib/Model/Dao/AbstractDao.php
    
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>

--- models/DataObject/Data/ObjectMetadata/Dao.php
+++ models/DataObject/Data/ObjectMetadata/Dao.php
@@ -117,7 +117,7 @@ class Dao extends Model\Dao\AbstractDao
         $table = 'object_metadata_' . $classId;
 
         $this->db->query('CREATE TABLE IF NOT EXISTS `' . $table . "` (
-              `o_id` int(11) NOT NULL default '0',
+              `o_id` int(11) UNSIGNED NOT NULL default '0',
               `dest_id` int(11) NOT NULL default '0',
 	          `type` VARCHAR(50) NOT NULL DEFAULT '',
               `fieldname` varchar(71) NOT NULL,
@@ -134,7 +134,8 @@ class Dao extends Model\Dao\AbstractDao
               INDEX `ownertype` (`ownertype`),
               INDEX `ownername` (`ownername`),
               INDEX `position` (`position`),
-              INDEX `index` (`index`)
+              INDEX `index` (`index`),
+              CONSTRAINT `".self::getForeignKeyName($table, 'o_id')."` FOREIGN KEY (`o_id`) REFERENCES objects (`o_id`) ON DELETE CASCADE
 		) DEFAULT CHARSET=utf8mb4;");
 
         $this->handleEncryption($class, [$table]);
