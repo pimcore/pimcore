pimcore/pimcore

commit 72e61b1024fefe2dde370e0136fec078cc7d66ac

[Ecommerce] VoucherService - Fix undefined notices

bundles/EcommerceFrameworkBundle/VoucherService/Token/Listing.php
@@ -79,21 +79,21 @@ class Listing extends \Pimcore\Model\Listing\AbstractListing implements AdapterI
                 $this->addConditionParam('length = ?', $filter['length']);
             }
 
-            if ($filter['creation_from']) {
+            if (isset($filter['creation_from'])) {
                 $this->addConditionParam("DATE(timestamp) >= STR_TO_DATE(?,'%Y-%m-%d')", $filter['creation_from']);
             }
 
-            if ($filter['creation_to']) {
+            if (isset($filter['creation_to'])) {
                 $this->addConditionParam("DATE(timestamp) <= STR_TO_DATE(?,'%Y-%m-%d')", $filter['creation_to']);
             }
 
-            if ($this->isValidOrderKey($filter['sort_criteria'])) {
+            if ($this->isValidOrderKey($filter['sort_criteria'] ?? '')) {
                 $this->setOrderKey($filter['sort_criteria']);
             } else {
                 $this->setOrderKey('timestamp');
             }
 
-            if ($filter['sort_order'] == 'ASC') {
+            if (($filter['sort_order'] ?? false) == 'ASC') {
                 $this->setOrder('ASC');
             } else {
                 $this->setOrder('DESC');
@@ -157,13 +157,13 @@ class Listing extends \Pimcore\Model\Listing\AbstractListing implements AdapterI
         }
 
         $tmp = new self();
-        if ($tmp->isValidOrderKey($params['sort_criteria'])) {
+        if ($tmp->isValidOrderKey($params['sort_criteria'] ?? '')) {
             $query .= ' ORDER BY ' . $params['sort_criteria'];
         } else {
             $query .= ' ORDER BY timestamp';
         }
 
-        if ($params['sort_order'] == 'ASC') {
+        if (($params['sort_order'] ?? false) == 'ASC') {
             $query .= ' ASC';
         } else {
             $query .= ' DESC';

bundles/EcommerceFrameworkBundle/VoucherService/TokenManager/Pattern.php
@@ -535,20 +535,20 @@ class Pattern extends AbstractTokenManager implements ExportableTokenManagerInte
         try {
             $tokens->setFilterConditions($params['id'], $params);
         } catch (\Exception $e) {
-            $this->template = 'PimcoreEcommerceFrameworkBundle:Voucher:voucherCodeTabError.html.php';
+            $this->template = 'PimcoreEcommerceFrameworkBundle:voucher:voucher_code_tab_pattern.html.twig';
             $viewParamsBag['errors'][] = $e->getMessage() . ' | Error-Code: ' . $e->getCode();
         }
 
         if ($tokens) {
             $paginator = new Paginator($tokens);
 
-            if ($params['tokensPerPage']) {
+            if ($params['tokensPerPage'] ?? false) {
                 $paginator->setItemCountPerPage((int)$params['tokensPerPage']);
             } else {
                 $paginator->setItemCountPerPage(25);
             }
 
-            $paginator->setCurrentPageNumber($params['page']);
+            $paginator->setCurrentPageNumber($params['page'] ?? 1);
 
             $viewParamsBag['paginator'] = $paginator;
             $viewParamsBag['count'] = count($tokens);
@@ -556,8 +556,8 @@ class Pattern extends AbstractTokenManager implements ExportableTokenManagerInte
             $viewParamsBag['msg']['result'] = 'bundle_ecommerce_voucherservice_msg-error-token-noresult';
         }
 
-        $viewParamsBag['msg']['error'] = $params['error'];
-        $viewParamsBag['msg']['success'] = $params['success'];
+        $viewParamsBag['msg']['error'] = $params['error'] ?? "";
+        $viewParamsBag['msg']['success'] = $params['success'] ?? "";
 
         // Settings parsed via foreach in view -> key is translation
         $viewParamsBag['settings'] = [

commit 5ec527d5c076c1f4fd723e4c141b1bca670d91fc

[eCommerceFramework] Fixed token filtering in VoucherSeries details tab
If you try to filter for tokens in the details tab on a OnlineShopVoucherSeries, you won't get any results unless you provide from- and to-dates.
This bug was introduced with https://github.com/pimcore/pimcore/pull/7482/commits/72e61b1024fefe2dde370e0136fec078cc7d66ac and is fixed with this patch

bundles/EcommerceFrameworkBundle/VoucherService/Token/Listing.php
@@ -67,11 +67,11 @@ class Listing extends \Pimcore\Model\Listing\AbstractListing implements Paginate
                 $this->addConditionParam('length = ?', $filter['length']);
             }
 
-            if (isset($filter['creation_from'])) {
+            if (isset($filter['creation_from']) && $filter['creation_from'] !== '') {
                 $this->addConditionParam("DATE(timestamp) >= STR_TO_DATE(?,'%Y-%m-%d')", $filter['creation_from']);
             }
 
-            if (isset($filter['creation_to'])) {
+            if (isset($filter['creation_to']) && $filter['creation_to'] !== '') {
                 $this->addConditionParam("DATE(timestamp) <= STR_TO_DATE(?,'%Y-%m-%d')", $filter['creation_to']);
             }
 