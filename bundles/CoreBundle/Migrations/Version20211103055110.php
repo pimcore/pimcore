1addbaab2f3982ed370bdc1e78ceb500a481991a
BabovicT <tomislav.babovic@factory.hr>
2021-11-04 08:00:07 +0100

    Namespace fix, strict type declaration, added pimcore copyright and license info

--- bundles/CoreBundle/Migrations/Version20211103055110.php
+++ bundles/CoreBundle/Migrations/Version20211103055110.php
@@ -1,15 +1,30 @@
 <?php
 
-namespace App\Migrations\SixNine;
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+/**
+ * @internal
+ */
+namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
 class Version20211103055110 extends AbstractMigration
 {
-    /**
-     * @param Schema $schema
-     */
     public function up(Schema $schema): void
     {
         $db = \Pimcore\Db::get();
@@ -28,9 +43,6 @@ class Version20211103055110 extends AbstractMigration
         }
     }
 
-    /**
-     * @param Schema $schema
-     */
     public function down(Schema $schema): void
     {
         $db = \Pimcore\Db::get();
