9cc1623a23df290c1df2519b101fd55d98b18e46
Peter Philipp <peter.philipp@cando-image.com>
2021-11-10 10:51:55 +0100

    Cache get has to apply the same conditions as cache set.
    Fundamental rule for caching should be to apply the same rule set / cache key for setting and getting cache items.
    This was violated by having the $this->sessionStatus->isDisabledBySession($request) check only present when setting the cache.

--- bundles/CoreBundle/EventListener/Frontend/FullPageCacheListener.php
+++ bundles/CoreBundle/EventListener/Frontend/FullPageCacheListener.php
@@ -228,6 +228,12 @@ class FullPageCacheListener
                     }
                 }
 
+                if ($this->sessionStatus->isDisabledBySession($request)) {
+                    $this->disable('Session in use');
+
+                    return;
+                }
+                
                 // output-cache is always disabled when logged in at the admin ui
                 if (null !== $pimcoreUser = Tool\Authentication::authenticateSession($request)) {
                     $this->disable('backend user is logged in');
