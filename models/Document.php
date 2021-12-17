3903318bfc39d14df33189fa1408cdce32800525
Jacob Dreesen <j.dreesen@neusta.de>
2021-12-17 12:06:31 +0100

    Remove Document from runtime path cache after it was deleted (#11016)
    * Remove Document from runtime path cache after it was deleted
    
    * Extract `getPathCacheKey()` method
    
    * Remove unused imports
    
    * Remove Document and its children from runtime path cache after it was saved with a new path
    
    * Fix clearing path cache on update
    
    The old path must be cleared, as this is the one that may already be cached.

--- models/Document.php
+++ models/Document.php
@@ -20,7 +20,6 @@ use Pimcore\Event\DocumentEvents;
 use Pimcore\Event\FrontendEvents;
 use Pimcore\Event\Model\DocumentEvent;
 use Pimcore\Logger;
-use Pimcore\Model\Document\Hardlink;
 use Pimcore\Model\Document\Hardlink\Wrapper\WrapperInterface;
 use Pimcore\Model\Document\Listing;
 use Pimcore\Model\Element\ElementInterface;
@@ -209,6 +208,18 @@ class Document extends Element\AbstractElement
     }
 
     /**
+     * @internal
+     *
+     * @param string $path
+     *
+     * @return string
+     */
+    protected static function getPathCacheKey(string $path): string
+    {
+        return 'document_path_' . md5($path);
+    }
+
+    /**
      * @param string $path
      * @param bool $force
      *
@@ -218,7 +229,7 @@ class Document extends Element\AbstractElement
     {
         $path = Element\Service::correctPath($path);
 
-        $cacheKey = 'document_path_' . md5($path);
+        $cacheKey = self::getPathCacheKey($path);
 
         if (!$force && \Pimcore\Cache\Runtime::isRegistered($cacheKey)) {
             return \Pimcore\Cache\Runtime::get($cacheKey);
@@ -420,10 +431,17 @@ class Document extends Element\AbstractElement
 
                     // if the old path is different from the new path, update all children
                     $updatedChildren = [];
-                    if ($oldPath && $oldPath != $this->getRealFullPath()) {
+                    if ($oldPath && $oldPath !== $newPath = $this->getRealFullPath()) {
                         $differentOldPath = $oldPath;
                         $this->getDao()->updateWorkspaces();
-                        $updatedChildren = $this->getDao()->updateChildPaths($oldPath);
+                        $updatedChildren = array_map(
+                            static function (array $doc) use ($oldPath, $newPath): array {
+                                $doc['oldPath'] = substr_replace($doc['path'], $oldPath, 0, strlen($newPath));
+
+                                return $doc;
+                            },
+                            $this->getDao()->updateChildPaths($oldPath),
+                        );
                     }
 
                     $this->commit();
@@ -453,12 +471,13 @@ class Document extends Element\AbstractElement
 
             $additionalTags = [];
             if (isset($updatedChildren) && is_array($updatedChildren)) {
-                foreach ($updatedChildren as $documentId) {
-                    $tag = 'document_' . $documentId;
+                foreach ($updatedChildren as $updatedDocument) {
+                    $tag = self::getCacheKey($updatedDocument['id']);
                     $additionalTags[] = $tag;
 
-                    // remove the child also from registry (internal cache) to avoid path inconsistencies during long running scripts, such as CLI
+                    // remove the child also from registry (internal cache) to avoid path inconsistencies during long-running scripts, such as CLI
                     \Pimcore\Cache\Runtime::set($tag, null);
+                    \Pimcore\Cache\Runtime::set(self::getPathCacheKey($updatedDocument['oldPath']), null);
                 }
             }
             $this->clearDependentCache($additionalTags);
@@ -825,6 +844,7 @@ class Document extends Element\AbstractElement
 
         //clear document from registry
         \Pimcore\Cache\Runtime::set(self::getCacheKey($this->getId()), null);
+        \Pimcore\Cache\Runtime::set(self::getPathCacheKey($this->getRealFullPath()), null);
 
         \Pimcore::getEventDispatcher()->dispatch(new DocumentEvent($this), DocumentEvents::POST_DELETE);
     }
