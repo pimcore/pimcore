aa1bfe42a8bfa21d94876823e38f36c02553eb03
Divesh Pahuja <divesh.pahuja@pimcore.com>
2021-12-23 09:41:20 +0100

    [Thumbnails] Support Asset Image for overlay transformations (#11017)
    * [Thumbnails] Support Asset Images for overlay transformations - resolves #10649
    
    * [Thumbnails] Support Asset Images for overlay transformations - resolves #10649
    
    * Apply suggestions from code review
    
    Co-authored-by: mcop1 <89011527+mcop1@users.noreply.github.com>
    
    Co-authored-by: mcop1 <89011527+mcop1@users.noreply.github.com>

--- lib/Image/Adapter/Imagick.php
+++ lib/Image/Adapter/Imagick.php
@@ -20,6 +20,7 @@ use Pimcore\Config;
 use Pimcore\File;
 use Pimcore\Image\Adapter;
 use Pimcore\Logger;
+use Pimcore\Model\Asset;
 
 class Imagick extends Adapter
 {
@@ -780,8 +781,19 @@ class Imagick extends Adapter
         $newImage = null;
 
         if (is_string($image)) {
-            $image = ltrim($image, '/');
-            $image = PIMCORE_PROJECT_ROOT . '/' . $image;
+            $asset = Asset\Image::getByPath($image);
+            if ($asset instanceof Asset\Image) {
+                $image = $asset->getTemporaryFile();
+            } else {
+                trigger_deprecation(
+                    'pimcore/pimcore',
+                    '10.3',
+                    'Using relative path for Image Thumbnail overlay is deprecated, use Asset Image path.'
+                );
+
+                $image = ltrim($image, '/');
+                $image = PIMCORE_PROJECT_ROOT . '/' . $image;
+            }
 
             $newImage = new \Imagick();
             $newImage->readimage($image);
@@ -816,8 +828,19 @@ class Imagick extends Adapter
      */
     public function addOverlayFit($image, $composite = 'COMPOSITE_DEFAULT')
     {
-        $image = ltrim($image, '/');
-        $image = PIMCORE_PROJECT_ROOT . '/' . $image;
+        $asset = Asset\Image::getByPath($image);
+        if ($asset instanceof Asset\Image) {
+            $image = $asset->getTemporaryFile();
+        } else {
+            trigger_deprecation(
+                'pimcore/pimcore',
+                '10.3',
+                'Using relative path for Image Thumbnail overlay is deprecated, use Asset Image path.'
+            );
+
+            $image = ltrim($image, '/');
+            $image = PIMCORE_PROJECT_ROOT . '/' . $image;
+        }
 
         $newImage = new \Imagick();
         $newImage->readimage($image);
