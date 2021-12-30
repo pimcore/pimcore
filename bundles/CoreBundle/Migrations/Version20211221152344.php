4f0d40b2fc8c6b63060c56572c1cb3cc66ad7d39
mcop1 <mcop1@users.noreply.github.com>
2021-12-30 07:25:50 +0000

    Apply php-cs-fixer changes

--- bundles/CoreBundle/Migrations/Version20211221152344.php
+++ bundles/CoreBundle/Migrations/Version20211221152344.php
@@ -2,6 +2,19 @@
 
 declare(strict_types=1);
 
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
 namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
@@ -23,7 +36,6 @@ final class Version20211221152344 extends AbstractMigration
             REFERENCES `assets` (`id`)
             ON UPDATE NO ACTION
             ON DELETE CASCADE;');
-
     }
 
     public function down(Schema $schema): void
