02355aaf71ecdf3b6df566177d368aae20bcf55b
brusch <brusch@users.noreply.github.com>
2022-01-14 11:03:51 +0000

    Apply php-cs-fixer changes

--- models/DataObject/ClassDefinition/Data/ImageGallery.php
+++ models/DataObject/ClassDefinition/Data/ImageGallery.php
@@ -278,7 +278,7 @@ class ImageGallery extends Data implements ResourcePersistenceAwareInterface, Qu
             ];
 
             $itemResult = $fd->getDataFromResource($itemData, $object, $params);
-            if($itemResult instanceof DataObject\Data\Hotspotimage) {
+            if ($itemResult instanceof DataObject\Data\Hotspotimage) {
                 $resultItems[] = $itemResult;
             }
         }
