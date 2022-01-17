43c80c0107695a1af3ed1e6f75961ef3e2d725c7
Sebastian Blank <blank@data-factory.net>
2022-01-17 12:14:25 +0100

    Fix: Wrong website settings in multi site environment (#11137)

--- phpstan-bootstrap.php
+++ phpstan-bootstrap.php
@@ -5,7 +5,11 @@ if (file_exists(__DIR__ . '/vendor/autoload.php')) {
 } elseif (file_exists(__DIR__ . '/../../../vendor/autoload.php')) {
     define('PIMCORE_PROJECT_ROOT', __DIR__ . '/../../..');
 } elseif (getenv('PIMCORE_PROJECT_ROOT')) {
-    define('PIMCORE_PROJECT_ROOT', getenv('PIMCORE_PROJECT_ROOT'));
+    if (file_exists(getenv('PIMCORE_PROJECT_ROOT') . '/vendor/autoload.php')) {
+        define('PIMCORE_PROJECT_ROOT', getenv('PIMCORE_PROJECT_ROOT'));
+    } else {
+        throw new \Exception('Invalid Pimcore project root "' . getenv('PIMCORE_PROJECT_ROOT') . '"');
+    }
 } else {
     throw new \Exception('Unknown configuration! Pimcore project root not found, please set env variable PIMCORE_PROJECT_ROOT.');
 }
