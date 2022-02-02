0bff02bcb18fd06544fbe81369c2ba4eb97c908f
Andreas Wroblewski <awroblewski@users.noreply.github.com>
2022-02-02 11:59:20 +0100

    Update lib/Cache/RuntimeCacheTrait.php
    Co-authored-by: Bernhard Rusch <brusch@users.noreply.github.com>

--- lib/Cache/RuntimeCacheTrait.php
+++ lib/Cache/RuntimeCacheTrait.php
@@ -1,12 +1,15 @@
 <?php
 /**
+ * Pimcore
  *
- * Created by PhpStorm.
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
  *
- * @date: 02.02.2022
- *
- * @author: Andreas Wroblewski <andreas.wroblewski@codafish.net>
- * @copyright: codafish><> GmbH, https://codafish.net
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
 namespace Pimcore\Cache;
