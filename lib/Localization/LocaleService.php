3da77eb862832c58c8fc228c44d182851f1f6018
Axel Tietje <xlttj@pm.me>
2022-02-02 17:18:24 +0100

    Deprecation: Passing null to parameter #1 ($bundle) of type string is deprecated
    ResourceBundle::getLocales(): Passing null to parameter #1 ($bundle) of type string is deprecated in PHP 8.1

--- lib/Localization/LocaleService.php
+++ lib/Localization/LocaleService.php
@@ -96,7 +96,7 @@ class LocaleService implements LocaleServiceInterface
      */
     public function getLocaleList()
     {
-        $locales = \ResourceBundle::getLocales(null);
+        $locales = \ResourceBundle::getLocales();
 
         return $locales;
     }
