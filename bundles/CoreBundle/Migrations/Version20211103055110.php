2e8bd03c339fe6601408537be01f34bfea6a8cbc
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-02-07 15:04:14 +0100

    Update bundles/CoreBundle/Migrations/Version20211103055110.php
    Co-authored-by: Sebastian Blank <sebastian.bl@gmx.de>

--- bundles/CoreBundle/Migrations/Version20211103055110.php
+++ bundles/CoreBundle/Migrations/Version20211103055110.php
@@ -36,7 +36,7 @@ class Version20211103055110 extends AbstractMigration
 
             if ($schema->hasTable($objectDatastoreTableRelation)) {
                 $this->addSql(
-                    "UPDATE $objectDatastoreTableRelation SET `type` = NULL WHERE `type` = 'object'"
+                    "UPDATE $objectDatastoreTableRelation SET `type` = 'object' WHERE `type` = NULL"
                 );
                 $this->addSql(
                     "ALTER TABLE $objectDatastoreTableRelation CHANGE COLUMN
