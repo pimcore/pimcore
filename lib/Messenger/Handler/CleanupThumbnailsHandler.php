168fc133553758f91fb78d57d9ca3da4d00e00a7
Bernhard Rusch <brusch@users.noreply.github.com>
2022-02-11 16:10:27 +0100

    [Messenger] Process certain messages in batches to speed things up (#11399)
    * [Messenger] Process certain messages in batches to speed things up
    
    * muzzle for PhpStan
    
    * muzzle for PhpStan
    
    * Filter for unique messages within a batch
    
    * Stan

--- lib/Messenger/Handler/CleanupThumbnailsHandler.php
+++ lib/Messenger/Handler/CleanupThumbnailsHandler.php
@@ -17,18 +17,42 @@ namespace Pimcore\Messenger\Handler;
 
 use Pimcore\Messenger\CleanupThumbnailsMessage;
 use Pimcore\Model\Asset;
+use Symfony\Component\Messenger\Handler\Acknowledger;
+use Symfony\Component\Messenger\Handler\BatchHandlerInterface;
+use Symfony\Component\Messenger\Handler\BatchHandlerTrait;
 
 /**
  * @internal
  */
-class CleanupThumbnailsHandler
+class CleanupThumbnailsHandler implements BatchHandlerInterface
 {
-    public function __invoke(CleanupThumbnailsMessage $message)
+    use BatchHandlerTrait;
+    use HandlerHelperTrait;
+
+    public function __invoke(CleanupThumbnailsMessage $message, Acknowledger $ack = null)
     {
-        $configClass = 'Pimcore\Model\Asset\\' . ucfirst($message->getType()) . '\Thumbnail\Config';
-        /** @var Asset\Image\Thumbnail\Config|Asset\Video\Thumbnail\Config|null $thumbConfig */
-        $thumbConfig = new $configClass();
-        $thumbConfig->setName($message->getName());
-        $thumbConfig->clearTempFiles();
+        return $this->handle($message, $ack);
+    }
+
+    private function process(array $jobs): void
+    {
+        $jobs = $this->filterUnique($jobs, static function (CleanupThumbnailsMessage $message) {
+            return $message->getType() . '-' . $message->getName();
+        });
+
+        foreach ($jobs as [$message, $ack]) {
+            try {
+
+                $configClass = 'Pimcore\Model\Asset\\' . ucfirst($message->getType()) . '\Thumbnail\Config';
+                /** @var Asset\Image\Thumbnail\Config|Asset\Video\Thumbnail\Config|null $thumbConfig */
+                $thumbConfig = new $configClass();
+                $thumbConfig->setName($message->getName());
+                $thumbConfig->clearTempFiles();
+
+                $ack->ack($message);
+            } catch (\Throwable $e) {
+                $ack->nack($e);
+            }
+        }
     }
 }
