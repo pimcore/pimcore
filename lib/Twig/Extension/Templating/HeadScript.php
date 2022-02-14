f9db5f89f2073420bd3c1b6099eb00783d325bb9
dpahuja <divesh.pahuja@pimcore.com>
2022-02-14 11:54:52 +0100

    [Admin] Security - Add handler to enable Content Security Policy

--- lib/Twig/Extension/Templating/HeadScript.php
+++ lib/Twig/Extension/Templating/HeadScript.php
@@ -483,7 +483,7 @@ class HeadScript extends CacheBusterAware implements RuntimeExtensionInterface
             }
         }
 
-        /** @var ContentSecurityPolicyHandler $contentSecurityPolicyHandler */
+        /** @var ContentSecurityPolicyHandler $cspHandler */
         $cspHandler = \Pimcore::getContainer()->get(ContentSecurityPolicyHandler::class);
         $attrString .= $cspHandler->getNonce();
         $addScriptEscape = !(isset($item->attributes['noescape']) && filter_var($item->attributes['noescape'], FILTER_VALIDATE_BOOLEAN));
