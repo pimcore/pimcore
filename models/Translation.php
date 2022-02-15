fa6159044ad19e5406ba92af9f317623fe78ddb4
Jan Walther <jan.walther@blackbit.de>
2022-02-15 09:52:10 +0100

    return null if translation key does not exist and $returnIdIfEmpty == false and $create == false

--- models/Translation.php
+++ models/Translation.php
@@ -273,6 +273,10 @@ final class Translation extends AbstractModel
         try {
             $translation->getDao()->getByKey($id);
         } catch (\Exception $e) {
+            if(!$create && !$returnIdIfEmpty) {
+                return null;
+            }
+
             $translation->setKey($id);
             $translation->setCreationDate(time());
             $translation->setModificationDate(time());
