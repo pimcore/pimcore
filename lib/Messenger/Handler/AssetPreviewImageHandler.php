66adf21331b116481ec2cd415ce8c2444dae8700
Bernhard Rusch <bernhard.rusch@elements.at>
2022-02-24 11:16:11 +0100

    Fixed folder previews and handle messages in batches

--- lib/Messenger/Handler/AssetPreviewImageHandler.php
+++ lib/Messenger/Handler/AssetPreviewImageHandler.php
@@ -19,26 +19,54 @@ use Pimcore\Logger;
 use Pimcore\Messenger\AssetPreviewImageMessage;
 use Pimcore\Model\Asset;
 use Psr\Log\LoggerInterface;
+use Symfony\Component\Messenger\Handler\Acknowledger;
+use Symfony\Component\Messenger\Handler\BatchHandlerInterface;
+use Symfony\Component\Messenger\Handler\BatchHandlerTrait;
 
 /**
  * @internal
  */
-class AssetPreviewImageHandler
+class AssetPreviewImageHandler implements BatchHandlerInterface
 {
+    use BatchHandlerTrait;
+
     public function __construct(protected LoggerInterface $logger)
     {
     }
 
-    public function __invoke(AssetPreviewImageMessage $message)
+    public function __invoke(AssetPreviewImageMessage $message, Acknowledger $ack = null)
     {
-        $asset = Asset::getById($message->getId());
+        return $this->handle($message, $ack);
+    }
 
-        if ($asset instanceof Asset\Image) {
-            $asset->getThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
-        } elseif ($asset instanceof Asset\Document) {
-            $asset->getImageThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
-        } elseif ($asset instanceof Asset\Video) {
-            $asset->getImageThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
+    // @phpstan-ignore-next-line
+    private function process(array $jobs): void
+    {
+        foreach ($jobs as [$message, $ack]) {
+            try {
+                $asset = Asset::getById($message->getId());
+
+                if ($asset instanceof Asset\Image) {
+                    $asset->getThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
+                } elseif ($asset instanceof Asset\Document) {
+                    $asset->getImageThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
+                } elseif ($asset instanceof Asset\Video) {
+                    $asset->getImageThumbnail(Asset\Image\Thumbnail\Config::getPreviewConfig())->generate(false);
+                } elseif ($asset instanceof Asset\Folder) {
+                    $asset->getPreviewImage(true);
+                }
+
+                $ack->ack($message);
+            } catch (\Throwable $e) {
+                $ack->nack($e);
+            }
         }
+    }
+
+
+    // @phpstan-ignore-next-line
+    private function shouldFlush(): bool
+    {
+        return 5 <= \count($this->jobs);
     }
 }
