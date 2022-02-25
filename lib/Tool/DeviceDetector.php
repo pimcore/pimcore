72c3e5393c5a96ac986a42cb86a5192fa773cc84
Bernhard Rusch <bernhard.rusch@elements.at>
2022-02-25 09:23:27 +0100

    [Actions] Added Psalm Static Analysis

--- lib/Tool/DeviceDetector.php
+++ lib/Tool/DeviceDetector.php
@@ -250,6 +250,7 @@ class DeviceDetector
 
                 // we don't set a cookie if we're in preview mode, or if a cookie is set already
                 if (!isset($_COOKIE['forceDeviceType']) && !isset($_REQUEST['pimcore_preview'])) {
+                    /** @psalm-suppress TaintedCookie */
                     setcookie('forceDeviceType', $type);
                 }
             }
