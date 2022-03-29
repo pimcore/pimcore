f05750b9283f5ac4581b43188bfe5326c0264972
Bernhard Rusch <brusch@users.noreply.github.com>
2022-03-29 09:19:35 +0200

    Apply suggestions from code review
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

--- models/DataObject/ClassDefinition/Data/Extension/Text.php
+++ models/DataObject/ClassDefinition/Data/Extension/Text.php
@@ -36,7 +36,7 @@ trait Text
      */
     public function isEmpty($data)
     {
-        return empty($data) || strlen($data) < 1;
+        return !is_string($data) || strlen($data) < 1;
     }
 
     /**
