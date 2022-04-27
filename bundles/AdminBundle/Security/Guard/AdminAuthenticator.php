0882bf8ced707480d840459686e0fca5cd61718e
aryaantony92 <arya.antony@pimcore.com>
2022-04-27 10:50:05 +0200

    Fixed Deprecated error message in admin login

--- bundles/AdminBundle/Security/Guard/AdminAuthenticator.php
+++ bundles/AdminBundle/Security/Guard/AdminAuthenticator.php
@@ -134,7 +134,10 @@ class AdminAuthenticator extends AbstractGuardAuthenticator implements LoggerAwa
             return $response;
         }
 
-        $event = new LoginRedirectEvent('pimcore_admin_login', ['perspective' => strip_tags($request->get('perspective'))]);
+        $perspective = $request->get('perspective');
+        $perspective = ($perspective !== null)? strip_tags($perspective): [];
+
+        $event = new LoginRedirectEvent('pimcore_admin_login', ['perspective' => $perspective ]);
         $this->dispatcher->dispatch($event, AdminEvents::LOGIN_REDIRECT);
 
         $url = $this->router->generate($event->getRouteName(), $event->getRouteParams());
