617a5bf6087d03fbf88113b0943e4defd917e6dd
Sebastian Blank <blank@data-factory.net>
2022-05-06 11:01:59 +0200

    [PhpStan] Fix AdminExceptionListener:recursiveAddValidationExceptionSubItems() (#11980)
    * [PhpStan] ValidationException should only accept ValidationExceptions as sub item
    
    * [PhpStan] Fix AdminExceptionListener:recursiveAddValidationExceptionSubItems()
    
    * [PhpStan] Fix AdminExceptionListener::addContext() expects ValidationException

--- bundles/AdminBundle/EventListener/AdminExceptionListener.php
+++ bundles/AdminBundle/EventListener/AdminExceptionListener.php
@@ -119,7 +119,7 @@ class AdminExceptionListener implements EventSubscriberInterface
     }
 
     /**
-     * @param ValidationException[] $items
+     * @param \Exception[] $items
      * @param string $message
      * @param string $detailedInfo
      */
@@ -131,7 +131,9 @@ class AdminExceptionListener implements EventSubscriberInterface
         foreach ($items as $e) {
             if ($e->getMessage()) {
                 $message .= '<b>' . $e->getMessage() . '</b>';
-                $this->addContext($e, $message);
+                if ($e instanceof ValidationException) {
+                    $this->addContext($e, $message);
+                }
                 $message .= '<br>';
 
                 $detailedInfo .= '<br><b>Message:</b><br>';
@@ -141,7 +143,9 @@ class AdminExceptionListener implements EventSubscriberInterface
                 $detailedInfo .= '<br><b>Trace:</b> ' . $inner->getTraceAsString() . '<br>';
             }
 
-            $this->recursiveAddValidationExceptionSubItems($e->getSubItems(), $message, $detailedInfo);
+            if ($e instanceof ValidationException) {
+                $this->recursiveAddValidationExceptionSubItems($e->getSubItems(), $message, $detailedInfo);
+            }
         }
     }
 
