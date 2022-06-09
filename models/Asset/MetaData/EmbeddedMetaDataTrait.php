1e8d05f8400ad3ad9f7858a556236755a3235601
Aashan Ghimire <ashanghimire10@gmail.com>
2022-06-09 15:22:57 +0545

    Added @param and @returns to docblocks for models/Asset (#12306)

--- models/Asset/MetaData/EmbeddedMetaDataTrait.php
+++ models/Asset/MetaData/EmbeddedMetaDataTrait.php
@@ -142,6 +142,11 @@ trait EmbeddedMetaDataTrait
         return $data;
     }
 
+    /**
+     * @param string|null $filePath
+     * @return array
+     * @throws \Exception
+     */
     public function getXMPData(?string $filePath = null)
     {
         if (!$filePath) {
