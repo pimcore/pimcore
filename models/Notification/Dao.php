d5fc096db4e64dd39a4622f2bf1b23bb68a3e53f
aryaantony92 <arya.antony@pimcore.com>
2022-06-17 11:53:49 +0200

    Removed ternary check to avoid php stan error

--- models/Notification/Dao.php
+++ models/Notification/Dao.php
@@ -147,7 +147,7 @@ class Dao extends AbstractDao
             'creationDate' => $model->getCreationDate(),
             'modificationDate' => $model->getModificationDate(),
             'sender' => $model->getSender() ? $model->getSender()->getId() : null,
-            'recipient' => $model->getRecipient() ? $model->getRecipient()->getId() : null,
+            'recipient' => $model->getRecipient(),
             'title' => $model->getTitle(),
             'message' => $model->getMessage(),
             'linkedElement' => $model->getLinkedElement() ? $model->getLinkedElement()->getId() : null,
