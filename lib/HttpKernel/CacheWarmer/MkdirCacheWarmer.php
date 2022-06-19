e85c2286fdf83cd876a2b1a842715d5355088d0e
Jia Jia Ji <kingjia90@gmail.com>
2022-06-19 20:24:05 +0200

    removing PIMCORE_LOG_FILEOBJECT_DIRECTORY since it can be created in the FileObject constructor https://github.com/pimcore/pimcore/blob/cae32ab5dea2e5237a798cdfb6b8f18266d9a72b/lib/Log/FileObject.php#L39

--- lib/HttpKernel/CacheWarmer/MkdirCacheWarmer.php
+++ lib/HttpKernel/CacheWarmer/MkdirCacheWarmer.php
@@ -56,9 +56,7 @@ class MkdirCacheWarmer implements CacheWarmerInterface
             PIMCORE_CLASS_DIRECTORY,
             PIMCORE_CLASS_DEFINITION_DIRECTORY,
             PIMCORE_CONFIGURATION_DIRECTORY,
-            PIMCORE_CUSTOMLAYOUT_DIRECTORY,
             PIMCORE_LOG_DIRECTORY,
-            PIMCORE_LOG_FILEOBJECT_DIRECTORY,
             PIMCORE_SYSTEM_TEMP_DIRECTORY,
         ];
 
