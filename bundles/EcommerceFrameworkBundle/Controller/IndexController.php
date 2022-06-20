4dd4d38ec8b3b61f379e8d411f4b70eb680fea96
Johanna <77202738+Johanna-Maria@users.noreply.github.com>
2022-06-20 14:03:07 +0200

    [Ecommerce] Add Event to GetFields-Method of IndexController (#12312)
    * Add Event for getFilters (IndexController)
    
    Sometimes, it would be useful to exclude/change some of the fields returned by the getFieldsAction method of the IndexController. I propose dispatching an event (like it already happens within the getValuesForFilterFieldAction method).
    
    * add constant to AdminEvents
    
    * change constant name
    
    * Apply suggestions from code review
    
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>

--- bundles/EcommerceFrameworkBundle/Controller/IndexController.php
+++ bundles/EcommerceFrameworkBundle/Controller/IndexController.php
@@ -112,9 +112,11 @@ class IndexController extends AdminController
      *
      * @param Request $request
      *
+     * @param EventDispatcherInterface $eventDispatcher
+     *
      * @return \Pimcore\Bundle\AdminBundle\HttpFoundation\JsonResponse
      */
-    public function getFieldsAction(Request $request)
+    public function getFieldsAction(Request $request, EventDispatcherInterface $eventDispatcher)
     {
         $indexService = Factory::getInstance()->getIndexService();
 
@@ -156,7 +158,10 @@ class IndexController extends AdminController
 
         ksort($fields);
 
-        return $this->adminJson(['data' => array_values($fields)]);
+        $event = new GenericEvent(null, ['data' => $fields]);
+        $eventDispatcher->dispatch($event, AdminEvents::GET_INDEX_FIELD_NAMES_PRE_SEND_DATA);
+        $data = $event->getArgument('data');
+        return $this->adminJson(['data' => array_values($data)]);
     }
 
     /**
