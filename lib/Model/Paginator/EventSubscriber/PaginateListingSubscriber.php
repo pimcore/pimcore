3f87912e2e763837c5936cf0ea8e011ffb79d124
Johanna <77202738+Johanna-Maria@users.noreply.github.com>
2022-06-27 16:15:29 +0200

    Update PaginateListingSubscriber.php

--- lib/Model/Paginator/EventSubscriber/PaginateListingSubscriber.php
+++ lib/Model/Paginator/EventSubscriber/PaginateListingSubscriber.php
@@ -26,8 +26,8 @@ class PaginateListingSubscriber implements EventSubscriberInterface
         $paginationAdapter = $event->target;
 
         if ($paginationAdapter instanceof PaginateListingInterface) {
-            $items = $paginationAdapter->getItems($event->getOffset(), $event->getLimit());
             $event->count = $paginationAdapter->count();
+            $items = $paginationAdapter->getItems($event->getOffset(), $event->getLimit());
             $event->items = $items;
             $event->stopPropagation();
         }
