c39eec664a66b71e37db25196a54aba95edbdef8
dvesh3 <dvesh3@users.noreply.github.com>
2022-07-19 13:31:40 +0000

    Apply php-cs-fixer changes

--- lib/Messenger/Handler/AssetUpdateTasksHandler.php
+++ lib/Messenger/Handler/AssetUpdateTasksHandler.php
@@ -73,7 +73,8 @@ class AssetUpdateTasksHandler
             $asset->setCustomSetting('duration', $asset->getDurationFromBackend());
         } catch (\Exception $e) {
             Logger::err('Unable to get duration of video: ' . $asset->getId());
-            throw new UnrecoverableMessageHandlingException($e->getMessage (), 0, $e);
+
+            throw new UnrecoverableMessageHandlingException($e->getMessage(), 0, $e);
         }
 
         try {
@@ -87,7 +88,8 @@ class AssetUpdateTasksHandler
             }
         } catch (\Exception $e) {
             Logger::err('Unable to get dimensions of video: ' . $asset->getId());
-            throw new UnrecoverableMessageHandlingException($e->getMessage (), 0, $e);
+
+            throw new UnrecoverableMessageHandlingException($e->getMessage(), 0, $e);
         }
 
         $asset->handleEmbeddedMetaData(true);
