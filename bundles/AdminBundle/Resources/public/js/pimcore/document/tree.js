09b3dc4401df3c9c2ea617c8e0649b9e9c8eaf7c
Niklas <niklas.brunberg@cag.se>
2022-07-20 17:31:46 +0200

    Make sure document_types are sorted the same way as Priority is shown

--- bundles/AdminBundle/Resources/public/js/pimcore/document/tree.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/document/tree.js
@@ -1034,7 +1034,7 @@ pimcore.document.tree = Class.create({
         };
 
         document_types.sort([
-            {property: 'priority', direction: 'DESC'},
+            {property: 'priority', direction: 'ASC'},
             {property: 'translatedGroup', direction: 'ASC'},
             {property: 'translatedName', direction: 'ASC'}
         ]);
