e6d130f1c5061afb7e456e404e263025dd0f2766
Sebastian Blank <blank@data-factory.net>
2022-07-21 17:33:19 +0200

    AssetHelperController::downloadXlsxFileAction(): Use loadSpreadsheetFromString() to avoid the extra temp file

--- bundles/AdminBundle/Controller/Admin/Asset/AssetHelperController.php
+++ bundles/AdminBundle/Controller/Admin/Asset/AssetHelperController.php
@@ -926,18 +926,11 @@ class AssetHelperController extends AdminController
         $csvFile = $this->getCsvFile($fileHandle);
 
         try {
-            $csvStream= $storage->readStream($csvFile);
-
             $csvReader = new Csv();
             $csvReader->setDelimiter(';');
             $csvReader->setSheetIndex(0);
 
-            $temp = tmpfile();
-            stream_copy_to_stream($csvStream, $temp, null, 0);
-            $tempMetaData = stream_get_meta_data($temp);
-            //TODO: use this method and storage->read() to avoid the extra temp file, is not available in the current version. See: https://github.com/PHPOffice/PhpSpreadsheet/pull/2792
-            //$spreadsheet = $csvReader->loadSpreadsheetFromString($csvData);
-            $spreadsheet = $csvReader->load($tempMetaData['uri']);
+            $spreadsheet = $csvReader->loadSpreadsheetFromString($storage->read($csvFile));
             $writer = new Xlsx($spreadsheet);
             $xlsxFilename = PIMCORE_SYSTEM_TEMP_DIRECTORY. '/' .$fileHandle. '.xlsx';
             $writer->save($xlsxFilename);
