1ace468ab862f4fd4c98df474826ba507d4528c8
storbahn <s.torbahn@neusta.de>
2022-08-26 12:49:15 +0200

    Update bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToManyRelation.js
    Co-authored-by: robertSt7 <104770750+robertSt7@users.noreply.github.com>

--- bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToManyRelation.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/tags/manyToManyRelation.js
@@ -469,7 +469,7 @@ pimcore.object.tags.manyToManyRelation = Class.create(pimcore.object.tags.abstra
             }
         }.bind(this), 
         function (res) {
-            var response = Ext.decode(res.response.responseText);
+            const response = Ext.decode(res.response.responseText);
             if (response && response.success === false) {
                 pimcore.helpers.showNotification(t("error"), response.message, "error",
                     res.response.responseText);
