25334afa03e40659735056a922e713d634e162e2
aryaantony92 <97134765+aryaantony92@users.noreply.github.com>
2022-09-02 14:42:58 +0200

    Update models/Document.php
    Co-authored-by: Sebastian Blank <blank@data-factory.net>

--- models/Document.php
+++ models/Document.php
@@ -176,7 +176,7 @@ class Document extends Element\AbstractElement
      *
      * @return static|null
      */
-    public static function getByPath($path, $params = [])
+    public static function getByPath($path, array $params = [])
     {
         if (!$path) {
             return null;
