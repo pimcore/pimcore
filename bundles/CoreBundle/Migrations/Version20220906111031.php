099cab523877e5e44083da9d9ad38a885895f060
mcop1 <mcop1@users.noreply.github.com>
2022-09-14 06:37:57 +0000

    Apply php-cs-fixer changes

--- bundles/CoreBundle/Migrations/Version20220906111031.php
+++ bundles/CoreBundle/Migrations/Version20220906111031.php
@@ -29,7 +29,7 @@ final class Version20220906111031 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("ALTER TABLE `recyclebin` ADD INDEX `recyclebin_date` (`date`)");
+        $this->addSql('ALTER TABLE `recyclebin` ADD INDEX `recyclebin_date` (`date`)');
     }
 
     public function down(Schema $schema): void
