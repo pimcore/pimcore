b039e3ad0e00428a22dfdc53ab1593d3a29604c0
Aashan Ghimire <ashanghimire10@gmail.com>
2022-10-05 18:40:00 +0545

    Removed unnecessary comments

--- bundles/CoreBundle/Migrations/Version20221005122653.php
+++ bundles/CoreBundle/Migrations/Version20221005122653.php
@@ -7,9 +7,6 @@ namespace Pimcore\Bundle\CoreBundle\Migrations;
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
 
-/**
- * Auto-generated Migration: Please modify to your needs!
- */
 final class Version20221005122653 extends AbstractMigration
 {
     public function getDescription(): string
