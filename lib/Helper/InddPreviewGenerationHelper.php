412c60624a39a922c93fd77dccf423bd665b5f6a
Jörg Neugebauer <neugebauer@studio1.de>
2022-10-06 13:54:47 +0200

    Adding functionality to create preview images on InDesign Files

--- lib/Helper/InddPreviewGenerationHelper.php
+++ lib/Helper/InddPreviewGenerationHelper.php
@@ -26,28 +26,36 @@ class InddPreviewGenerationHelper
      */
     public static function generatePreviewImage(Asset $asset, string $storagePath): void
     {
-        $exiftool = \Pimcore\Tool\Console::getExecutable('exiftool');
-        $filePath = $asset->getLocalFile();
-        $process = new Process([$exiftool, '-b', '-json', $filePath]);
-        $process->run();
-        $output = $process->getOutput();
-        $embeddedMetaData = json_decode($output);
-        $pageImage = reset($embeddedMetaData)->PageImage;
+        try {
+            $exiftool = \Pimcore\Tool\Console::getExecutable('exiftool');
+            $filePath = $asset->getLocalFile();
+            $process = new Process([$exiftool, '-b', '-json', $filePath]);
+            $process->run();
+            $output = $process->getOutput();
+            $embeddedMetaData = json_decode($output);
+            $pageImage = reset($embeddedMetaData)->PageImage;
 
-        $tmpPath = sprintf('%s/var/tmp/indd_%s.jpg', PIMCORE_WEB_ROOT, $asset->getFilename());
-        $tmpPathPng = sprintf('%s/var/tmp/indd_%s.png', PIMCORE_WEB_ROOT, $asset->getFilename());
-        if (file_exists($tmpPath)) {
+            $tmpPath = sprintf('%s/var/tmp/indd_%s.jpg', PIMCORE_WEB_ROOT, $asset->getFilename());
+            $tmpPathPng = sprintf('%s/var/tmp/indd_%s.png', PIMCORE_WEB_ROOT, $asset->getFilename());
+            if (file_exists($tmpPath)) {
+                unlink($tmpPath);
+            }
+
+            if (is_array($pageImage)) {
+                $pageImage = reset($pageImage);
+            }
+
+            $pageImage = str_replace('base64:', '', $pageImage);
+            file_put_contents($tmpPath, base64_decode($pageImage));
+
+            $storage = Storage::get('thumbnail');
+            imagepng(imagecreatefromstring(file_get_contents($tmpPath)), $tmpPathPng);
+            $storage->write($storagePath, file_get_contents($tmpPathPng));
+
             unlink($tmpPath);
+            unlink($tmpPathPng);
+        } catch (Exception $e) {
+
         }
-
-        $image = str_replace('base64:', '', reset($pageImage));
-        file_put_contents($tmpPath, base64_decode($image));
-
-        $storage = Storage::get('thumbnail');
-        imagepng(imagecreatefromstring(file_get_contents($tmpPath)), $tmpPathPng);
-        $storage->write($storagePath, file_get_contents($tmpPathPng));
-
-        unlink($tmpPath);
-        unlink($tmpPathPng);
     }
 }
