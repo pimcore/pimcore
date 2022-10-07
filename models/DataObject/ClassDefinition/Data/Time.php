70e6953ca4e9871730c48cdc48e9e817acf815fa
sdrewergutland <s.drewer-gutland@edjufy.com>
2022-10-07 17:52:00 +0200

    add `is_string()` before `strlen()` on possible `null` values in ``\Pimcore\Model\DataObject\ClassDefinition\Data\Time`

--- models/DataObject/ClassDefinition/Data/Time.php
+++ models/DataObject/ClassDefinition/Data/Time.php
@@ -71,7 +71,7 @@ class Time extends Model\DataObject\ClassDefinition\Data\Input
      */
     public function setMinValue($minValue)
     {
-        if (strlen($minValue)) {
+        if (is_string($minValue) && strlen($minValue)) {
             $this->minValue = $this->toTime($minValue);
         } else {
             $this->minValue = null;
@@ -91,7 +91,7 @@ class Time extends Model\DataObject\ClassDefinition\Data\Input
      */
     public function setMaxValue($maxValue)
     {
-        if (strlen($maxValue)) {
+        if (is_string($maxValue) && strlen($maxValue)) {
             $this->maxValue = $this->toTime($maxValue);
         } else {
             $this->maxValue = null;
@@ -113,16 +113,16 @@ class Time extends Model\DataObject\ClassDefinition\Data\Input
             throw new Model\Element\ValidationException('Wrong time format given must be a 5 digit string (eg: 06:49) [ '.$this->getName().' ]');
         }
 
-        if (!$omitMandatoryCheck && strlen($data)) {
+        if (!$omitMandatoryCheck && is_string($data) && strlen($data)) {
             if (!$this->toTime($data)) {
                 throw new Model\Element\ValidationException('Wrong time format given must be a 5 digit string (eg: 06:49) [ '.$this->getName().' ]');
             }
 
-            if (strlen($this->getMinValue()) && $this->isEarlier($this->getMinValue(), $data)) {
+            if (is_string($this->getMinValue()) && strlen($this->getMinValue()) && $this->isEarlier($this->getMinValue(), $data)) {
                 throw new Model\Element\ValidationException('Value in field [ '.$this->getName().' ] is not at least ' . $this->getMinValue());
             }
 
-            if (strlen($this->getMaxValue()) && $this->isLater($this->getMaxValue(), $data)) {
+            if (is_string($this->getMaxValue()) && strlen($this->getMaxValue()) && $this->isLater($this->getMaxValue(), $data)) {
                 throw new Model\Element\ValidationException('Value in field [ ' . $this->getName() . ' ] is bigger than ' . $this->getMaxValue());
             }
         }
@@ -143,7 +143,7 @@ class Time extends Model\DataObject\ClassDefinition\Data\Input
      */
     public function isEmpty($data)
     {
-        return strlen($data) !== 5;
+        return is_string($data) && strlen($data) !== 5;
     }
 
     /**
