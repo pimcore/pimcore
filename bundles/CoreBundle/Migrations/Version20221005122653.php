82aa2f83f5991e2b907432a44014fef33758e39a
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-12 08:49:23 +0545

    Changed data type for fullpath to be able to full text index

--- bundles/CoreBundle/Migrations/Version20221005122653.php
+++ bundles/CoreBundle/Migrations/Version20221005122653.php
@@ -16,14 +16,18 @@ final class Version20221005122653 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql("ALTER TABLE search_backend_data DROP INDEX fulltext;");
+        $this->addSql("ALTER TABLE search_backend_data DROP INDEX `fulltext`;");
+        // Change the collation and character set of the column to be able to add fulltext search index
+        $this->addSql("ALTER TABLE search_backend_data MODIFY COLUMN fullpath text;");
         $this->addSql("ALTER TABLE search_backend_data ADD FULLTEXT(`data`,`properties`, `fullpath`)");
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql("ALTER TABLE search_backend_data DROP INDEX fulltext;");
-        $this->addSql("ALTER TABLE search_backend_data ADD FULLTEXT(`data`,`properties`)");
+        $this->addSql("ALTER TABLE search_backend_data DROP INDEX `fulltext`;");
+        // Revert back the charset and collation info for the fullpath column
+        $this->addSql("ALTER TABLE search_backend_data MODIFY COLUMN fullpath varchar(765);");
+        $this->addSql("ALTER TABLE search_backend_data ADD FULLTEXT INDEX fulltext(`data`,`properties`)");
 
     }
 }
