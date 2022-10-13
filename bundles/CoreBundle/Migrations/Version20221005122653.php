31c95c0f42aa4d8aadad80b193acf7e15d21d412
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-13 13:49:00 +0545

    Updated charset and collation of the column

--- bundles/CoreBundle/Migrations/Version20221005122653.php
+++ bundles/CoreBundle/Migrations/Version20221005122653.php
@@ -18,6 +18,7 @@ final class Version20221005122653 extends AbstractMigration
     {
         $this->addSql('ALTER TABLE search_backend_data DROP INDEX `fullpath`;');
         $this->addSql('ALTER TABLE search_backend_data DROP INDEX `fulltext`;');
+        $this->addSql('ALTER TABLE search_backend_data MODIFY fullpath varchar(765) CARACTER SET utf8mb4 COLLATE utf8mb4_general_ci;');
         $this->addSql('ALTER TABLE search_backend_data ADD FULLTEXT INDEX `fulltext`(`data`, `properties`, `fullpath`);');
     }
 
@@ -25,6 +26,7 @@ final class Version20221005122653 extends AbstractMigration
     {
         $this->addSql('ALTER TABLE search_backend_data ADD INDEX `fullpath`(`fullpath`);');
         $this->addSql('ALTER TABLE search_backend_data DROP INDEX `fulltext`;');
+        $this->addSql('ALTER TABLE search_backend_data MODIFY fullpath varchar(765) CARACTER SET utf8 COLLATE utf8_general_ci;');
         $this->addSql('ALTER TABLE search_backend_data ADD INDEX `fulltext`(`data`, `properties`);');
     }
 }
