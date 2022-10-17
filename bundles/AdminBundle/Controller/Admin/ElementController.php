3e44979a5fc35df26cfbedd9a5d5a677dc538574
Matthias Hamacher <m.hamacher@mhamacher.de>
2022-10-17 14:29:19 +0200

    Remove unused import

--- bundles/AdminBundle/Controller/Admin/ElementController.php
+++ bundles/AdminBundle/Controller/Admin/ElementController.php
@@ -31,7 +31,6 @@ use Symfony\Component\HttpFoundation\JsonResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
-use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 
 /**
  *
