8c91e9631e15a45f05a64781a627a08721e242ac
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-18 15:05:34 +0545

    fixed typo

--- bundles/InstallBundle/Resources/install.sql
+++ bundles/InstallBundle/Resources/install.sql
@@ -464,7 +464,7 @@ CREATE TABLE `search_backend_data` (
   `id` int(11) NOT NULL,
   `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin default '',
   `index` int(11) unsigned DEFAULT '0',
-  `fullpath` varchar(765) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL, /* path in utf8 (3-byte) using the full key length of 3072 bytes */,
+  `fullpath` varchar(765) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL, /* path in utf8 (3-byte) using the full key length of 3072 bytes */
   `maintype` varchar(8) NOT NULL DEFAULT '',
   `type` varchar(20) DEFAULT NULL,
   `subtype` varchar(190) DEFAULT NULL,
