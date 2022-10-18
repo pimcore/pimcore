9110ce317e2a5d48c969ba9576e07fb0a6df2930
Ashan Ghimire <ashanghimire10@gmail.com>
2022-10-18 14:53:58 +0545

    Removed migrations as they are unnecessary

--- bundles/InstallBundle/Resources/install.sql
+++ bundles/InstallBundle/Resources/install.sql
@@ -464,7 +464,7 @@ CREATE TABLE `search_backend_data` (
   `id` int(11) NOT NULL,
   `key` varchar(255) CHARACTER SET utf8 COLLATE utf8_bin default '',
   `index` int(11) unsigned DEFAULT '0',
-  `fullpath` varchar(765) DEFAULT NULL,
+  `fullpath` varchar(765) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL, /* path in utf8 (3-byte) using the full key length of 3072 bytes */,
   `maintype` varchar(8) NOT NULL DEFAULT '',
   `type` varchar(20) DEFAULT NULL,
   `subtype` varchar(190) DEFAULT NULL,
@@ -478,11 +478,12 @@ CREATE TABLE `search_backend_data` (
   PRIMARY KEY (`id`,`maintype`),
   KEY `key` (`key`),
   KEY `index` (`index`),
+  KEY `fullpath` (`fullpath`),
   KEY `maintype` (`maintype`),
   KEY `type` (`type`),
   KEY `subtype` (`subtype`),
   KEY `published` (`published`),
-  FULLTEXT KEY `fulltext` (`data`,`properties`, `fullpath`)
+  FULLTEXT KEY `fulltext` (`data`,`properties`)
 ) DEFAULT CHARSET=utf8mb4 ROW_FORMAT=DYNAMIC;
 
 DROP TABLE IF EXISTS `sites`;
