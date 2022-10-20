48b43870517cde1eb9bc404c84cd4dd0605051cf
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-10-20 15:06:01 +0200

    Update bundles/AdminBundle/src/EventListener/TwoFactorListener.php
    Co-authored-by: JiaJia Ji <kingjia90@gmail.com>

--- bundles/AdminBundle/src/EventListener/TwoFactorListener.php
+++ bundles/AdminBundle/src/EventListener/TwoFactorListener.php
@@ -49,7 +49,7 @@ class TwoFactorListener
 
     public function onAuthenticationComplete(TwoFactorAuthenticationEvent $event)
     {
-        // this session flag is set in \Pimcore\Bundle\AdminBundle\Security\AdminAuthenticator
+        // this session flag is set in \Pimcore\Bundle\AdminBundle\Security\Authenticator\AdminAbstractAuthenticator
         // @TODO: check if there's a nicer way of doing this, actually it feels a bit like a hack :)
         Session::useSession(function (AttributeBagInterface $adminSession) {
             $adminSession->set('2fa_required', false);
