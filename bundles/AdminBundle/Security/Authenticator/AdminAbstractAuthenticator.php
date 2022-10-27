c07e8ab75a89c859e4ea1bbbb9a6174bd9cd6fc9
kingjia90 <kingjia90@users.noreply.github.com>
2022-10-27 11:51:50 +0000

    Apply php-cs-fixer changes

--- bundles/AdminBundle/Security/Authenticator/AdminAbstractAuthenticator.php
+++ bundles/AdminBundle/Security/Authenticator/AdminAbstractAuthenticator.php
@@ -30,7 +30,6 @@ use Symfony\Component\HttpFoundation\RedirectResponse;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\HttpFoundation\Session\Attribute\AttributeBagInterface;
-use Symfony\Component\HttpKernel\Exception\AccessDeniedHttpException;
 use Symfony\Component\Routing\RouterInterface;
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Exception\AuthenticationException;
