22fcfe66147cc22b57ff8cfd5dcf670a35602e88
Cameron Jenkins <cameron.jenkins@torqit.ca>
2022-11-10 08:57:32 -0400

    make window shrink when too large for window

--- bundles/AdminBundle/Resources/public/js/pimcore/element/helpers/gridConfigDialog.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/element/helpers/gridConfigDialog.js
@@ -123,6 +123,32 @@ pimcore.element.helpers.gridConfigDialog = Class.create({
 
         this.window.show();
         this.updatePreview();
+
+        Ext.on('resize', function() {
+            var winW = 0, winH = 0;
+            if (document.body && document.body.offsetWidth) {
+                winW = document.body.offsetWidth;
+                winH = document.body.offsetHeight;
+            }
+            if (document.compatMode=='CSS1Compat' && document.documentElement && document.documentElement.offsetWidth ) {
+                winW = document.documentElement.offsetWidth;
+                winH = document.documentElement.offsetHeight;
+            }
+            if (window.innerWidth && window.innerHeight) {
+                winW = window.innerWidth;
+                winH = window.innerHeight;
+            }   
+            var size = { width: winW, height: winH };
+
+            if (size.height < this.window.height) {
+                this.window.setHeight(size.height);
+            }
+            if (size.width < this.window.width) {
+                this.window.setWidth(size.width);
+            }
+            this.window.center();
+            // console.log(this.getHeight() + "hello" + this.getWidth());
+        }, this);
     },
 
     getConfigPanel: function() {
