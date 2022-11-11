e399e6b128e1879b670bdaa830c903876b40de95
mattamon <matthias.schuhmayer@gmx.at>
2022-11-11 14:17:48 +0100

    [Fix] moving "enableTextSelection" option out of class context scope

--- bundles/AdminBundle/Resources/public/js/pimcore/object/classes/data/manyToManyObjectRelation.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/classes/data/manyToManyObjectRelation.js
@@ -192,18 +192,19 @@ pimcore.object.classes.data.manyToManyObjectRelation = Class.create(pimcore.obje
 
         this.specificPanel.add({
             xtype: "checkbox",
+            boxLabel: t("enable_text_selection"),
+            name: "enableTextSelection",
+            value: this.datax.enableTextSelection
+        });
+
+        this.specificPanel.add({
+            xtype: "checkbox",
             boxLabel: t("allow_to_create_new_object"),
             name: "allowToCreateNewObject",
             value: this.datax.allowToCreateNewObject
         });
 
         if(this.context == 'class') {
-            this.specificPanel.add({
-                xtype: "checkbox",
-                boxLabel: t("enable_text_selection"),
-                name: "enableTextSelection",
-                value: this.datax.enableTextSelection
-            });
             this.specificPanel.add({
                 xtype: "checkbox",
                 boxLabel: t("enable_admin_async_load"),
