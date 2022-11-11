3df92cc6d543857b1d31124022111b6c03d9f02a
aryaantony92 <arya.antony@pimcore.com>
2022-11-11 09:36:10 +0100

    Update Namespace for Support

--- tests/_bootstrap.php
+++ tests/_bootstrap.php
@@ -48,7 +48,7 @@ include PIMCORE_PROJECT_ROOT . '/vendor/autoload.php';
 
 Autoloader::addNamespace('Pimcore\Model\DataObject', __DIR__ . '/_output/var/classes/DataObject');
 Autoloader::addNamespace('Pimcore\Tests', __DIR__);
-Autoloader::addNamespace('Pimcore\Tests', __DIR__ . '/Support');
+Autoloader::addNamespace('Pimcore\Tests\Support', __DIR__ . '/Support');
 
 if (!defined('PIMCORE_TEST')) {
     define('PIMCORE_TEST', true);
