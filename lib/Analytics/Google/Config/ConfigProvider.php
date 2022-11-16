1ae3775280a1ade0bf0a87389d5fed9e073bd622
Divesh Pahuja <divesh.pahuja@pimcore.com>
2022-11-16 12:37:45 +0100

    Fix error undefined index 'analytics' - related to #12911

--- lib/Analytics/Google/Config/ConfigProvider.php
+++ lib/Analytics/Google/Config/ConfigProvider.php
@@ -59,11 +59,6 @@ class ConfigProvider
     {
         $reportConfig = \Pimcore\Config::getReportConfig();
 
-        $config = $reportConfig['analytics'];
-        if (!is_array($config)) {
-            $config = [];
-        }
-
-        return $config;
+        return $reportConfig['analytics'] ?? [];
     }
 }
