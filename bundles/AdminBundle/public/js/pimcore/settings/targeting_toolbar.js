53a0cc3a98a1706661c1efe32c1ab234fbfe59c0
Corepex <christian.feldkirchner@pimcore.com>
2022-12-06 09:47:09 +0100

    updated some ancient links

--- bundles/AdminBundle/public/js/pimcore/settings/targeting_toolbar.js
+++ bundles/AdminBundle/public/js/pimcore/settings/targeting_toolbar.js
@@ -49,7 +49,7 @@ pimcore.settings.targetingToolbar = Class.create({
                 bodyStyle: "padding: 20px; font-size: 14px;",
                 html: t("targeting_toolbar_browser_note", null,
                     {
-                            targetingLink: 'https://pimcore.com/docs/6.x/Development_Documentation/Tools_and_Features/Targeting_and_Personalization/index.html#page_Debugging-Targeting-Data'
+                            targetingLink: 'https://pimcore.com/docs/pimcore/11.0/Development_Documentation/Tools_and_Features/Targeting_and_Personalization/index.html#page_Debugging-Targeting-Data'
                     })
             }],
             buttons: buttons
