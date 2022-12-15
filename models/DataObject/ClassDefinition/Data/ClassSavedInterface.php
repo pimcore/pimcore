86b5330a5cb7f96aa9344d3f288e301be626257a
Corepex <christian.feldkirchner@pimcore.com>
2022-12-15 12:16:39 +0100

    added type

--- models/DataObject/ClassDefinition/Data/ClassSavedInterface.php
+++ models/DataObject/ClassDefinition/Data/ClassSavedInterface.php
@@ -7,7 +7,7 @@ use Pimcore\Model\DataObject;
 interface ClassSavedInterface
 {
     /**
-     * @param $class
+     * @param DataObject\ClassDefinition $class
      * @param array $params
      */
     public function classSaved($class/**, $params = [] **/);
