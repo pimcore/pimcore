ba1f3076ad5f18d075ff048c1d4fa409f59bc1ec
mattamon <matthias.schuhmayer@pimcore.com>
2022-12-15 11:40:03 +0100

    Move GlossaryTest back to tests

--- tests/Unit/Document/Glossary/GlossaryTest.php
+++ tests/Unit/Document/Glossary/GlossaryTest.php
@@ -15,7 +15,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\GlossaryBundle\Tests\Glossary;
+namespace Pimcore\Tests\Unit\Document\Glossary;
 
 use Pimcore\Bundle\GlossaryBundle\Model\Glossary;
 use Pimcore\Bundle\GlossaryBundle\Tool\Processor;
