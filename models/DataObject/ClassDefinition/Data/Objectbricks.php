3ff72c476dfd034e4e0af952fbdd5e73e9dac692
Corepex <christian.feldkirchner@pimcore.com>
2022-12-16 11:58:39 +0100

    changed condition order

--- models/DataObject/ClassDefinition/Data/Objectbricks.php
+++ models/DataObject/ClassDefinition/Data/Objectbricks.php
@@ -903,16 +903,14 @@ class Objectbricks extends Data implements CustomResourcePersistingInterface, Ty
 
                     foreach ($fieldDefinition as $fd) {
                         //TODO Pimcore 11 remove method_exists call
-                        if ($fd instanceof ClassSavedInterface || method_exists($fd, 'classSaved')) {
+                        if (!$fd instanceof DataContainerAwareInterface && ($fd instanceof ClassSavedInterface || method_exists($fd, 'classSaved'))) {
                             if (!$fd instanceof ClassSavedInterface) {
                                 trigger_deprecation('pimcore/pimcore', '10.6',
                                     sprintf('Usage of method_exists is deprecated since version 10.6 and will be removed in Pimcore 11.' .
                                     'Implement the %s interface instead.', ClassSavedInterface::class));
                             }
                             // defer creation
-                            if (!$fd instanceof DataContainerAwareInterface) {
-                                $fd->classSaved($class, $params);
-                            }
+                            $fd->classSaved($class, $params);
                         }
                     }
 
