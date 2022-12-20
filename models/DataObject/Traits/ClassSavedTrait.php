1dccd93c925c961e49a21a6fef8753f6ee743e0a
Corepex <christian.feldkirchner@pimcore.com>
2022-12-20 16:57:07 +0100

    Revert "pass $params to classSaved method in postSave trait"
    This reverts commit 6a9879ce4db518a44c73d7589a92a5a91fc4cdf8.

--- models/DataObject/Traits/ClassSavedTrait.php
+++ models/DataObject/Traits/ClassSavedTrait.php
@@ -31,7 +31,7 @@ trait ClassSavedTrait
     public function postSave(mixed $containerDefinition, array $params = []): void
     {
         if ($containerDefinition instanceof ClassDefinition) {
-            $this->classSaved($containerDefinition, $params);
+            $this->classSaved($containerDefinition);
         }
     }
 }
