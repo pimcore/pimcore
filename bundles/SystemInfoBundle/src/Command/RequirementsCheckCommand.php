0e859a94857e09b49c68b739e1de4eccd379c890
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-01-10 15:08:27 +0100

    move pimcore:system:requirements command

--- bundles/SystemInfoBundle/src/Command/RequirementsCheckCommand.php
+++ bundles/SystemInfoBundle/src/Command/RequirementsCheckCommand.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\CoreBundle\Command;
+namespace Pimcore\Bundle\SystemInfoBundle\Command;
 
 use Pimcore\Console\AbstractCommand;
 use Pimcore\Db;
