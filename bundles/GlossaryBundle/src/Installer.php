45ba539516b19e455b8622015de5316be81ba4ec
mattamon <matthias.schuhmayer@pimcore.com>
2023-01-11 13:09:43 +0100

    Add user permissions category for glossary and seo

--- bundles/GlossaryBundle/src/Installer.php
+++ bundles/GlossaryBundle/src/Installer.php
@@ -23,6 +23,7 @@ use Pimcore\Extension\Bundle\Installer\SettingsStoreAwareInstaller;
  */
 class Installer extends SettingsStoreAwareInstaller
 {
+    protected const USER_PERMISSIONS_CATEGORY = 'Pimcore Glossary Bundle';
     protected const USER_PERMISSIONS = [
         'glossary',
     ];
@@ -48,6 +49,7 @@ class Installer extends SettingsStoreAwareInstaller
         foreach (self::USER_PERMISSIONS as $permission) {
             $db->insert('users_permission_definitions', [
                 $db->quoteIdentifier('key') => $permission,
+                $db->quoteIdentifier('category') => self::USER_PERMISSIONS_CATEGORY,
             ]);
         }
     }
