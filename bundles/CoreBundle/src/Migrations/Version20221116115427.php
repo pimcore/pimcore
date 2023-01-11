325f18c6ea3a63ab8ff4b72ab0dc336a2da31222
Jan Walther <jan.walther@blackbit.de>
2023-01-11 13:26:55 +0100

    fix migration

--- bundles/CoreBundle/src/Migrations/Version20221116115427.php
+++ bundles/CoreBundle/src/Migrations/Version20221116115427.php
@@ -21,12 +21,12 @@ final class Version20221116115427 extends AbstractMigration
     {
         $this->addSql("INSERT INTO `users_permission_definitions` (`key`, `category`) VALUES ('objectbricks', '')");
 
-        $this->addSql('UPDATE `users` SET `permissions`=CONCAT(`permissions`, \',objectbricks\') WHERE `permissions` REGEXP \'(?:^|,)classes(?:^|,)\'');
+        $this->addSql('UPDATE `users` SET `permissions`=CONCAT(`permissions`, \',objectbricks\') WHERE `permissions` REGEXP \'(?:^|,)classes(?:$|,)\'');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('UPDATE `users` SET `permissions`=REGEXP_REPLACE(`permissions`, \'(?:^|,)objectbricks(?:^|,)\', \'\') WHERE `permissions` REGEXP \'(?:^|,)objectbricks(?:^|,)\'');
+        $this->addSql('UPDATE `users` SET `permissions`=REGEXP_REPLACE(`permissions`, \'(?:^|,)objectbricks(?:^|,)\', \'\') WHERE `permissions` REGEXP \'(?:^|,)objectbricks(?:$|,)\'');
 
         $this->addSql("DELETE FROM `users_permission_definitions` WHERE `key` = 'objectbricks'");
     }
