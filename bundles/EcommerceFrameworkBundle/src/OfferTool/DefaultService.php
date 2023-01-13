d006c76c90d7c63d034a7b937228d290aff5cfc3
fashxp <fashxp@users.noreply.github.com>
2023-01-13 08:07:50 +0000

    Apply php-cs-fixer changes

--- bundles/EcommerceFrameworkBundle/src/OfferTool/DefaultService.php
+++ bundles/EcommerceFrameworkBundle/src/OfferTool/DefaultService.php
@@ -21,7 +21,6 @@ use Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\CartInterface;
 use Pimcore\Bundle\EcommerceFrameworkBundle\CartManager\CartItemInterface;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Factory;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Model\CheckoutableInterface;
-use Pimcore\Bundle\EcommerceFrameworkBundle\Model\ProductInterface;
 use Pimcore\Bundle\EcommerceFrameworkBundle\Type\Decimal;
 use Pimcore\Model\DataObject\AbstractObject;
 use Pimcore\Model\DataObject\Folder;
