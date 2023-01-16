82b56d5e41e16ccecba2dc929c6e43623cd68b1a
mattamon <matthias.schuhmayer@pimcore.com>
2023-01-16 11:59:58 +0100

    Remove console.log

--- bundles/SeoBundle/public/js/robotstxt.js
+++ bundles/SeoBundle/public/js/robotstxt.js
@@ -55,7 +55,6 @@ pimcore.bundle.seo.robotstxt = Class.create({
         var items = [];
 
         pimcore.globalmanager.get("sites").load(function(records) {
-            console.log(records);
             Ext.each(records, function(record) {
                 items.push(this.getEditPanel(record))
             }.bind(this));
