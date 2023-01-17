bd1a27022caeb429667741e82e98b209a2313fe0
mattamon <matthias.schuhmayer@pimcore.com>
2023-01-17 11:16:42 +0100

    Add return type

--- bundles/WordExportBundle/src/DependencyInjection/PimcoreWordExportExtension.php
+++ bundles/WordExportBundle/src/DependencyInjection/PimcoreWordExportExtension.php
@@ -21,7 +21,7 @@ use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 final class PimcoreWordExportExtension extends Extension
 {
-    public function load(array $configs, ContainerBuilder $container)
+    public function load(array $configs, ContainerBuilder $container): void
     {
         // on container build the shutdown handler shouldn't be called
         // for details please see https://github.com/pimcore/pimcore/issues/4709
