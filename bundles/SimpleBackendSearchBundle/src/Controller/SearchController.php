524fa1efda5f8c4a3511e40d19c035c4620f4311
Corepex <christian.feldkirchner@pimcore.com>
2023-01-18 12:58:49 +0100

    reordered controller actions

--- bundles/SimpleBackendSearchBundle/src/Controller/SearchController.php
+++ bundles/SimpleBackendSearchBundle/src/Controller/SearchController.php
@@ -45,133 +45,6 @@ class SearchController extends AdminController
     use AdminStyleTrait;
 
     /**
-     * @Route("/quicksearch-get-by-id", name="pimcore_bundle_search_search_quicksearch_by_id", methods={"GET"})
-     *
-     * @param Request $request
-     * @param Config $config
-     *
-     * @return JsonResponse
-     */
-    public function quickSearchByIdAction(Request $request, Config $config): JsonResponse
-    {
-        $type = $request->get('type');
-        $id = $request->get('id');
-        $db = \Pimcore\Db::get();
-        $searcherList = new Data\Listing();
-
-        $searcherList->addConditionParam('id = :id', ['id' => $id]);
-        $searcherList->addConditionParam('maintype = :type', ['type' => $type]);
-        $searcherList->setLimit(1);
-
-        $hits = $searcherList->load();
-
-        //There will always be one result in hits but load returns array.
-        $data = [];
-        foreach ($hits as $hit) {
-            $element = Element\Service::getElementById($hit->getId()->getType(), $hit->getId()->getId());
-            if ($element->isAllowed('list')) {
-                $data = [
-                    'id' => $element->getId(),
-                    'type' => $hit->getId()->getType(),
-                    'subtype' => $element->getType(),
-                    'className' => ($element instanceof DataObject\Concrete) ? $element->getClassName() : '',
-                    'fullpath' => htmlspecialchars($element->getRealFullPath()),
-                    'fullpathList' => htmlspecialchars($this->shortenPath($element->getRealFullPath())),
-                    'iconCls' => 'pimcore_icon_asset_default',
-                ];
-
-                $this->addAdminStyle($element, ElementAdminStyleEvent::CONTEXT_SEARCH, $data);
-
-                $validLanguages = \Pimcore\Tool::getValidLanguages();
-
-                $data['preview'] = $this->renderView(
-                    '@PimcoreAdmin/searchadmin/search/quicksearch/' . $hit->getId()->getType() . '.html.twig', [
-                        'element' => $element,
-                        'iconCls' => $data['iconCls'],
-                        'config' => $config,
-                        'validLanguages' => $validLanguages,
-                    ]
-                );
-            }
-        }
-
-        return $this->adminJson($data);
-    }
-
-    /**
-     * @Route("/quicksearch", name="pimcore_bundle_search_search_quicksearch", methods={"GET"})
-     *
-     * @param Request $request
-     * @param EventDispatcherInterface $eventDispatcher
-     *
-     * @return JsonResponse
-     */
-    public function quickSearchAction(Request $request, EventDispatcherInterface $eventDispatcher): JsonResponse
-    {
-        $query = $this->filterQueryParam($request->get('query', ''));
-        if (!preg_match('/[\+\-\*"]/', $query)) {
-            // check for a boolean operator (which was not filtered by filterQueryParam()),
-            // if present, do not add asterisk at the end of the query
-            $query = $query . '*';
-        }
-
-        $db = \Pimcore\Db::get();
-        $searcherList = new Data\Listing();
-
-        $conditionParts = [];
-
-        $conditionParts[] = $this->getPermittedPaths();
-
-        $matchCondition = '( MATCH (`data`,`properties`) AGAINST (' . $db->quote($query) . ' IN BOOLEAN MODE) )';
-        $conditionParts[] = '(' . $matchCondition . " AND `type` != 'folder') ";
-
-        $queryCondition = implode(' AND ', $conditionParts);
-
-        $searcherList->setCondition($queryCondition);
-        $searcherList->setLimit(50);
-        $searcherList->setOrderKey($matchCondition, false);
-        $searcherList->setOrder('DESC');
-
-        $beforeListLoadEvent = new GenericEvent($this, [
-            'list' => $searcherList,
-            'query' => $query,
-        ]);
-        $eventDispatcher->dispatch($beforeListLoadEvent, AdminEvents::QUICKSEARCH_LIST_BEFORE_LIST_LOAD);
-        $searcherList = $beforeListLoadEvent->getArgument('list');
-
-        $hits = $searcherList->load();
-
-        $elements = [];
-        foreach ($hits as $hit) {
-            $element = Element\Service::getElementById($hit->getId()->getType(), $hit->getId()->getId());
-            if ($element->isAllowed('list')) {
-                $data = [
-                    'id' => $element->getId(),
-                    'type' => $hit->getId()->getType(),
-                    'subtype' => $element->getType(),
-                    'className' => ($element instanceof DataObject\Concrete) ? $element->getClassName() : '',
-                    'fullpathList' => htmlspecialchars($this->shortenPath($element->getRealFullPath())),
-                ];
-
-                $this->addAdminStyle($element, ElementAdminStyleEvent::CONTEXT_SEARCH, $data);
-
-                $elements[] = $data;
-            }
-        }
-
-        $afterListLoadEvent = new GenericEvent($this, [
-            'list' => $elements,
-            'context' => $query,
-        ]);
-        $eventDispatcher->dispatch($afterListLoadEvent, AdminEvents::QUICKSEARCH_LIST_AFTER_LIST_LOAD);
-        $elements = $afterListLoadEvent->getArgument('list');
-
-        $result = ['data' => $elements, 'success' => true];
-
-        return $this->adminJson($result);
-    }
-
-    /**
      * @Route("/find", name="pimcore_bundle_search_search_find", methods={"GET", "POST"})
      * @todo: $conditionTypeParts could be undefined
      * @todo: $conditionSubtypeParts could be undefined
@@ -558,6 +431,133 @@ class SearchController extends AdminController
         $query = rtrim($query, '+- ');
 
         return $query;
+    }
+
+    /**
+     * @Route("/quicksearch", name="pimcore_bundle_search_search_quicksearch", methods={"GET"})
+     *
+     * @param Request $request
+     * @param EventDispatcherInterface $eventDispatcher
+     *
+     * @return JsonResponse
+     */
+    public function quickSearchAction(Request $request, EventDispatcherInterface $eventDispatcher): JsonResponse
+    {
+        $query = $this->filterQueryParam($request->get('query', ''));
+        if (!preg_match('/[\+\-\*"]/', $query)) {
+            // check for a boolean operator (which was not filtered by filterQueryParam()),
+            // if present, do not add asterisk at the end of the query
+            $query = $query . '*';
+        }
+
+        $db = \Pimcore\Db::get();
+        $searcherList = new Data\Listing();
+
+        $conditionParts = [];
+
+        $conditionParts[] = $this->getPermittedPaths();
+
+        $matchCondition = '( MATCH (`data`,`properties`) AGAINST (' . $db->quote($query) . ' IN BOOLEAN MODE) )';
+        $conditionParts[] = '(' . $matchCondition . " AND `type` != 'folder') ";
+
+        $queryCondition = implode(' AND ', $conditionParts);
+
+        $searcherList->setCondition($queryCondition);
+        $searcherList->setLimit(50);
+        $searcherList->setOrderKey($matchCondition, false);
+        $searcherList->setOrder('DESC');
+
+        $beforeListLoadEvent = new GenericEvent($this, [
+            'list' => $searcherList,
+            'query' => $query,
+        ]);
+        $eventDispatcher->dispatch($beforeListLoadEvent, AdminEvents::QUICKSEARCH_LIST_BEFORE_LIST_LOAD);
+        $searcherList = $beforeListLoadEvent->getArgument('list');
+
+        $hits = $searcherList->load();
+
+        $elements = [];
+        foreach ($hits as $hit) {
+            $element = Element\Service::getElementById($hit->getId()->getType(), $hit->getId()->getId());
+            if ($element->isAllowed('list')) {
+                $data = [
+                    'id' => $element->getId(),
+                    'type' => $hit->getId()->getType(),
+                    'subtype' => $element->getType(),
+                    'className' => ($element instanceof DataObject\Concrete) ? $element->getClassName() : '',
+                    'fullpathList' => htmlspecialchars($this->shortenPath($element->getRealFullPath())),
+                ];
+
+                $this->addAdminStyle($element, ElementAdminStyleEvent::CONTEXT_SEARCH, $data);
+
+                $elements[] = $data;
+            }
+        }
+
+        $afterListLoadEvent = new GenericEvent($this, [
+            'list' => $elements,
+            'context' => $query,
+        ]);
+        $eventDispatcher->dispatch($afterListLoadEvent, AdminEvents::QUICKSEARCH_LIST_AFTER_LIST_LOAD);
+        $elements = $afterListLoadEvent->getArgument('list');
+
+        $result = ['data' => $elements, 'success' => true];
+
+        return $this->adminJson($result);
+    }
+
+    /**
+     * @Route("/quicksearch-get-by-id", name="pimcore_bundle_search_search_quicksearch_by_id", methods={"GET"})
+     *
+     * @param Request $request
+     * @param Config $config
+     *
+     * @return JsonResponse
+     */
+    public function quickSearchByIdAction(Request $request, Config $config): JsonResponse
+    {
+        $type = $request->get('type');
+        $id = $request->get('id');
+        $db = \Pimcore\Db::get();
+        $searcherList = new Data\Listing();
+
+        $searcherList->addConditionParam('id = :id', ['id' => $id]);
+        $searcherList->addConditionParam('maintype = :type', ['type' => $type]);
+        $searcherList->setLimit(1);
+
+        $hits = $searcherList->load();
+
+        //There will always be one result in hits but load returns array.
+        $data = [];
+        foreach ($hits as $hit) {
+            $element = Element\Service::getElementById($hit->getId()->getType(), $hit->getId()->getId());
+            if ($element->isAllowed('list')) {
+                $data = [
+                    'id' => $element->getId(),
+                    'type' => $hit->getId()->getType(),
+                    'subtype' => $element->getType(),
+                    'className' => ($element instanceof DataObject\Concrete) ? $element->getClassName() : '',
+                    'fullpath' => htmlspecialchars($element->getRealFullPath()),
+                    'fullpathList' => htmlspecialchars($this->shortenPath($element->getRealFullPath())),
+                    'iconCls' => 'pimcore_icon_asset_default',
+                ];
+
+                $this->addAdminStyle($element, ElementAdminStyleEvent::CONTEXT_SEARCH, $data);
+
+                $validLanguages = \Pimcore\Tool::getValidLanguages();
+
+                $data['preview'] = $this->renderView(
+                    '@PimcoreAdmin/searchadmin/search/quicksearch/' . $hit->getId()->getType() . '.html.twig', [
+                        'element' => $element,
+                        'iconCls' => $data['iconCls'],
+                        'config' => $config,
+                        'validLanguages' => $validLanguages,
+                    ]
+                );
+            }
+        }
+
+        return $this->adminJson($data);
     }
 
     protected function shortenPath(string $path): string
