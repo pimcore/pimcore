20b94e4887ed80eea2ff6182555935f72d3f8fd0
JiaJiaJi <kingjia90@gmail.com>
2023-01-26 21:41:04 +0100

    Task: adapt the menu to new implementation

--- bundles/EcommerceFrameworkBundle/src/Resources/public/js/bundle.js
+++ bundles/EcommerceFrameworkBundle/src/Resources/public/js/bundle.js
@@ -11,95 +11,82 @@
  * @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
+
+
+pimcore.registerNS("pimcore.bundle.ecommerce.startup");
+
 /**
  * @private
  */
-document.addEventListener(pimcore.events.pimcoreReady, (e) => {
-    const perspectiveCfg = pimcore.globalmanager.get("perspective");
+pimcore.bundle.ecommerce.startup = Class.create({
+    initialize: function () {
+        document.addEventListener(pimcore.events.preMenuBuild, this.preMenuBuild.bind(this));
+    },
 
-    if (!perspectiveCfg.inToolbar("ecommerce")) {
-        return
-    }
+    preMenuBuild: function (e) {
+        const perspectiveCfg = pimcore.globalmanager.get("perspective");
+        if (!perspectiveCfg.inToolbar("ecommerce")) {
+            return
+        }
+        const user = pimcore.globalmanager.get("user");
+        let menu = e.detail.menu;
 
-    const toolbar = pimcore.globalmanager.get("layout_toolbar");
+        let items = [];
 
-    // init
-    let menuItems = toolbar.ecommerceMenu;
-    if (!menuItems) {
-        menuItems = new Ext.menu.Menu({
-            cls: "pimcore_navigation_flyout",
-            listeners: {
-                "show": function (e) {
-                    Ext.get('pimcore_menu_ecommerce').addCls('active');
-                },
-                "hide": function (e) {
-                    Ext.get('pimcore_menu_ecommerce').removeCls('active');
-                }
-            }
-        });
-        toolbar.ecommerceMenu = menuItems;
-    }
-
-    const user = pimcore.globalmanager.get("user");
-
-    let config = pimcore.bundle.EcommerceFramework.config;
-
-    // pricing rules
-    if (perspectiveCfg.inToolbar("ecommerce.rules") && user.isAllowed("bundle_ecommerce_pricing_rules") && (!config.menu || config.menu.pricing_rules.enabled)) {
-        // add pricing rules to menu
-        // create item
-        const pricingPanelId = "bundle_ecommerce_pricing_config";
-        const item = {
-            text: t("bundle_ecommerce_pricing_rules"),
-            iconCls: "pimcore_nav_icon_commerce_pricing_rules",
-            handler: function () {
-                try {
-                    pimcore.globalmanager.get(pricingPanelId).activate();
-                }
-                catch (e) {
-                    pimcore.globalmanager.add(pricingPanelId, new pimcore.bundle.EcommerceFramework.pricing.config.panel(pricingPanelId));
-                }
-            }
+        menu.ecommerce = {
+            label: t('bundle_ecommerce_mainmenu'),
+            iconCls: 'bundle_ecommerce_nav_icon_shopping_cart',
+            priority: 42,
+            items: items,
+            shadow: false,
+            cls: "pimcore_navigation_flyout"
         };
 
-        // add to menu
-        menuItems.add(item);
-    }
+        let config = pimcore.bundle.EcommerceFramework.config;
 
-    // order backend
-    if (perspectiveCfg.inToolbar("ecommerce.orderbackend") && user.isAllowed("bundle_ecommerce_back-office_order") && (!config.menu || config.menu.order_list.enabled)) {
-        // create item
-        const orderPanelId = "bundle_ecommerce_back-office_order";
-        const item = {
-            text: t("bundle_ecommerce_back-office_order"),
-            iconCls: "pimcore_nav_icon_commerce_backoffice",
-            handler: function () {
-                try {
-                    pimcore.globalmanager.get(orderPanelId).activate();
+        // pricing rules
+        if (perspectiveCfg.inToolbar("ecommerce.rules") && user.isAllowed("bundle_ecommerce_pricing_rules") && (!config.menu || config.menu.pricing_rules.enabled)) {
+            // add pricing rules to menu
+            const pricingPanelId = "bundle_ecommerce_pricing_config";
+            const item = {
+                text: t("bundle_ecommerce_pricing_rules"),
+                iconCls: "pimcore_nav_icon_commerce_pricing_rules",
+                priority: 10,
+                handler: function () {
+                    try {
+                        pimcore.globalmanager.get(pricingPanelId).activate();
+                    }
+                    catch (e) {
+                        pimcore.globalmanager.add(pricingPanelId, new pimcore.bundle.EcommerceFramework.pricing.config.panel(pricingPanelId));
+                    }
                 }
-                catch (e) {
-                    pimcore.globalmanager.add(orderPanelId, new pimcore.tool.genericiframewindow(orderPanelId, config.menu.order_list.route, "bundle_ecommerce_back-office_order", t('bundle_ecommerce_back-office_order')));
+            };
+
+            items.push(item);
+        }
+
+        // order backend
+        if (perspectiveCfg.inToolbar("ecommerce.orderbackend") && user.isAllowed("bundle_ecommerce_back-office_order") && (!config.menu || config.menu.order_list.enabled)) {
+            const orderPanelId = "bundle_ecommerce_back-office_order";
+            const item = {
+                text: t("bundle_ecommerce_back-office_order"),
+                iconCls: "pimcore_nav_icon_commerce_backoffice",
+                priority: 20,
+                handler: function () {
+                    try {
+                        pimcore.globalmanager.get(orderPanelId).activate();
+                    }
+                    catch (e) {
+                        pimcore.globalmanager.add(orderPanelId, new pimcore.tool.genericiframewindow(orderPanelId, config.menu.order_list.route, "bundle_ecommerce_back-office_order", t('bundle_ecommerce_back-office_order')));
+                    }
                 }
-            }
-        };
+            };
 
-        // add to menu
-        menuItems.add(item);
-    }
-
-    if (menuItems.items.length > 0) {
-        initializeMenu(toolbar, menuItems);
+            items.push(item);
+        }
+        menu.ecommerce.items = items;
     }
 });
-
-function initializeMenu(toolbar, menuItems) {
-    // add e-commerce framework main menu
-   //let navEl = Ext.get('pimcore_menu_ecommerce');
-   //navEl.show();
-   //navEl.on("mousedown", toolbar.showSubMenu.bind(menuItems));
-
-    pimcore.helpers.initMenuTooltips();
-}
 
 document.addEventListener(pimcore.events.postOpenObject, (e) => {
     if (pimcore.globalmanager.get("user").isAllowed("bundle_ecommerce_pricing_rules")) {
@@ -123,3 +110,6 @@ document.addEventListener(pimcore.events.postOpenObject, (e) => {
         }
     }
 });
+
+
+const pimcoreBundleEcommerce = new pimcore.bundle.ecommerce.startup();
