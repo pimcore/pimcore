e5ea7586b8c183336c5df8057650c1f89bf3a941
mattamon <matthias.schuhmayer@pimcore.com>
2023-01-27 08:43:21 +0100

    fix: using reset to get the first element of the validlayouts instead of using fixed index 0

--- bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/DataObjectController.php
@@ -513,7 +513,7 @@ class DataObjectController extends ElementControllerBase implements KernelContro
             }
 
             if ($currentLayoutId === null && count($validLayouts) > 0) {
-                $currentLayoutId = $validLayouts[0]->getId();
+                $currentLayoutId = reset($validLayouts)->getId();
             }
 
             if (!empty($validLayouts)) {
