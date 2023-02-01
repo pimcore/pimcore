bca002eaeb2c1a5a6cbf4b1a737cd6d2216b8055
aryaantony92 <arya.antony@pimcore.com>
2023-02-01 16:15:58 +0100

    Typo fixes

--- bundles/PersonalizationBundle/src/Targeting/EventListener/Frontend/TargetingDocumentRendererListener.php
+++ bundles/PersonalizationBundle/src/Targeting/EventListener/Frontend/TargetingDocumentRendererListener.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\PersonalizationBundle\Tageting\EventListener\Frontend;
+namespace Pimcore\Bundle\PersonalizationBundle\Targeting\EventListener\Frontend;
 
 use Pimcore\Bundle\PersonalizationBundle\Targeting\Document\DocumentTargetingConfigurator;
 use Pimcore\Event\DocumentEvents;
