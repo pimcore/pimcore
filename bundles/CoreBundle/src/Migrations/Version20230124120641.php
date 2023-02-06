5c07dc21ccafbd7c6dddda5096dd4a48036132af
Corepex <Corepex@users.noreply.github.com>
2023-02-06 08:59:10 +0000

    Apply php-cs-fixer changes

--- bundles/CoreBundle/src/Migrations/Version20230124120641.php
+++ bundles/CoreBundle/src/Migrations/Version20230124120641.php
@@ -30,38 +30,38 @@ final class Version20230124120641 extends AbstractMigration
     public function up(Schema $schema): void
     {
         if ($schema->getTable('classificationstore_keys')->hasColumn('definition') === true) {
-            $this->addSql("ALTER TABLE `classificationstore_keys` MODIFY COLUMN `definition` json;");
+            $this->addSql('ALTER TABLE `classificationstore_keys` MODIFY COLUMN `definition` json;');
         }
         if ($schema->getTable('users')->hasColumn('keyBindings') === true) {
-            $this->addSql("ALTER TABLE `users` MODIFY COLUMN `keyBindings` json;");
+            $this->addSql('ALTER TABLE `users` MODIFY COLUMN `keyBindings` json;');
         }
         if ($schema->getTable('gridconfigs')->hasColumn('config') === true) {
-            $this->addSql("ALTER TABLE `gridconfigs` MODIFY COLUMN `config` json;");
+            $this->addSql('ALTER TABLE `gridconfigs` MODIFY COLUMN `config` json;');
         }
         if ($schema->getTable('importconfigs')->hasColumn('config') === true) {
-            $this->addSql("ALTER TABLE `importconfigs` MODIFY COLUMN `config` json;");
+            $this->addSql('ALTER TABLE `importconfigs` MODIFY COLUMN `config` json;');
         }
         if ($schema->getTable('targeting_storage')->hasColumn('value') === true) {
-            $this->addSql("ALTER TABLE `targeting_storage` MODIFY COLUMN `value` json;");
+            $this->addSql('ALTER TABLE `targeting_storage` MODIFY COLUMN `value` json;');
         }
     }
 
     public function down(Schema $schema): void
     {
         if ($schema->getTable('classificationstore_keys')->hasColumn('definition') === true) {
-            $this->addSql("ALTER TABLE `classificationstore_keys` MODIFY COLUMN `definition` LONGTEXT;");
+            $this->addSql('ALTER TABLE `classificationstore_keys` MODIFY COLUMN `definition` LONGTEXT;');
         }
         if ($schema->getTable('users')->hasColumn('keyBindings') === true) {
-            $this->addSql("ALTER TABLE `users` MODIFY COLUMN `keyBindings` LONGTEXT;");
+            $this->addSql('ALTER TABLE `users` MODIFY COLUMN `keyBindings` LONGTEXT;');
         }
         if ($schema->getTable('gridconfigs')->hasColumn('config') === true) {
-            $this->addSql("ALTER TABLE `gridconfigs` MODIFY COLUMN `config` LONGTEXT;");
+            $this->addSql('ALTER TABLE `gridconfigs` MODIFY COLUMN `config` LONGTEXT;');
         }
         if ($schema->getTable('importconfigs')->hasColumn('config') === true) {
-            $this->addSql("ALTER TABLE `importconfigs` MODIFY COLUMN `config` LONGTEXT;");
+            $this->addSql('ALTER TABLE `importconfigs` MODIFY COLUMN `config` LONGTEXT;');
         }
         if ($schema->getTable('targeting_storage')->hasColumn('value') === true) {
-            $this->addSql("ALTER TABLE `targeting_storage` MODIFY COLUMN `value` LONGTEXT;");
+            $this->addSql('ALTER TABLE `targeting_storage` MODIFY COLUMN `value` LONGTEXT;');
         }
     }
 }
