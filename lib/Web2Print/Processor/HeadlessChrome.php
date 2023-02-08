bd01aefdf68d4408a2cba0a4b9865c4266ca016e
mattamon <mattamon@users.noreply.github.com>
2023-02-08 14:23:19 +0000

    Apply php-cs-fixer changes

--- lib/Web2Print/Processor/HeadlessChrome.php
+++ lib/Web2Print/Processor/HeadlessChrome.php
@@ -102,7 +102,7 @@ class HeadlessChrome extends Processor
 
         ['html' => $html, 'params' => $params] = $event->getArguments();
 
-        if(!class_exists(StringInput::class)) {
+        if (!class_exists(StringInput::class)) {
             throw new \UnexpectedValueException('Please install spiritix/php-chrome-html2pdf via Composer to use Headless Chrome PDF conversion');
         }
 
