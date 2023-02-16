6147606f08887d289d53f49560629a30263278ed
aryaantony92 <arya.antony@pimcore.com>
2023-02-16 17:45:33 +0100

    Add migration to mark bundle as installed

--- bundles/CoreBundle/src/Migrations/Version20230125164101.php
+++ bundles/CoreBundle/src/Migrations/Version20230125164101.php
@@ -19,23 +19,31 @@ namespace Pimcore\Bundle\CoreBundle\Migrations;
 
 use Doctrine\DBAL\Schema\Schema;
 use Doctrine\Migrations\AbstractMigration;
+use Pimcore\Bundle\ApplicationLoggerBundle\PimcoreApplicationLoggerBundle;
+use Pimcore\Db;
+use Pimcore\Model\Tool\SettingsStore;
 
 final class Version20230125164101 extends AbstractMigration
 {
     public function getDescription(): string
     {
-        return '';
+        return 'Mark the PersonalizationBundle as installed';
     }
 
     public function up(Schema $schema): void
     {
-        $this->addSql ("DELETE FROM `users_permission_definitions` WHERE `key` = 'targeting'");
+        if(!SettingsStore::get('BUNDLE_INSTALLED__Pimcore\\Bundle\\PersonalizationBundle\\PimcorePersonalizationBundle', 'pimcore')) {
+            SettingsStore::set('BUNDLE_INSTALLED__Pimcore\\Bundle\\PersonalizationBundle\\PimcorePersonalizationBundle', true, 'bool', 'pimcore');
+        }
 
+        $this->warnIf(
+            null !== SettingsStore::get('BUNDLE_INSTALLED__Pimcore\\Bundle\\PersonalizationBundle\\PimcorePersonalizationBundle', 'pimcore'),
+            'Please make sure to enable the Pimcore\\Bundle\\PersonalizationBundle\\PimcorePersonalizationBundle manually in config/bundles.php'
+        );
     }
 
     public function down(Schema $schema): void
     {
-
 
     }
 }
