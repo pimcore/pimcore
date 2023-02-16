a9f5803bf44f34be8aa59902c5e3e1ee29a149e5
Jan Walther <jan.walther@blackbit.de>
2023-02-16 08:52:59 +0100

    support language-specific transliteration

--- lib/Tool/Transliteration.php
+++ lib/Tool/Transliteration.php
@@ -31,7 +31,7 @@ class Transliteration
     public static function toASCII($value, $language = null)
     {
         $transliterationId = '';
-        if(in_array($language.'-ASCII', transliterator_list_ids())) {
+        if($language !== null && in_array($language.'-ASCII', transliterator_list_ids())) {
             $transliterationId = $language.'-ASCII; ';
         }
         $value = transliterator_transliterate($transliterationId.'Any-Latin; Latin-ASCII; [^\u001F-\u007f] remove', $value);
