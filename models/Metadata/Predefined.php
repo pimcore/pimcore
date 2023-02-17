ba908a7d32a2c5c4fd170b6a935597af5560bfdb
Sebastian Blank <blank@data-factory.net>
2023-02-17 11:15:57 +0100

    Make attributes private (class is final), Remove unused attributes

--- models/Metadata/Predefined.php
+++ models/Metadata/Predefined.php
@@ -31,37 +31,27 @@ use Pimcore\Model;
  */
 final class Predefined extends Model\AbstractModel
 {
-    protected ?string $id = null;
+    private ?string $id = null;
 
-    protected ?string $name = null;
+    private ?string $name = null;
 
-    protected ?string $description = null;
+    private ?string $description = null;
 
-    /**
-     * @TODO if required?
-     */
-    protected ?string $key = null;
+    private ?string $type = null;
 
-    protected ?string $type = null;
+    private ?string $targetSubtype = null;
 
-    protected ?string $targetSubtype = null;
+    private mixed $data = null;
 
-    protected mixed $data = null;
+    private ?string $config = null;
 
-    protected ?string $config = null;
+    private ?string $language = null;
 
-    /**
-     * @TODO if required?
-     */
-    protected ?string $ctype = null;
+    private ?string $group = null;
 
-    protected ?string $language = null;
+    private ?int $creationDate = null;
 
-    protected ?string $group = null;
-
-    protected ?int $creationDate = null;
-
-    protected ?int $modificationDate = null;
+    private ?int $modificationDate = null;
 
     public static function getById(string $id): ?Predefined
     {
