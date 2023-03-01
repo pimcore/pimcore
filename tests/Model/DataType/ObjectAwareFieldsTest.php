6f26a7c920d931bac61ef8eb53c638f8c8df3738
lukmzig <lukmzig@users.noreply.github.com>
2023-03-01 14:39:30 +0000

    Apply php-cs-fixer changes

--- tests/Model/DataType/ObjectAwareFieldsTest.php
+++ tests/Model/DataType/ObjectAwareFieldsTest.php
@@ -18,7 +18,6 @@ namespace Pimcore\Tests\Model\DataType;
 use Pimcore\Model\DataObject\AbstractObject;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Model\DataObject\Fieldcollection;
-use Pimcore\Model\DataObject\LazyLoading;
 use Pimcore\Model\DataObject\Objectbrick\Data\LazyLoadingLocalizedTest;
 use Pimcore\Model\DataObject\Unittest;
 use Pimcore\Tests\Model\LazyLoading\AbstractLazyLoadingTest;
