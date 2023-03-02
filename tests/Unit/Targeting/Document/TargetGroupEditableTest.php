98d3c6430ccc10d9f1db86b6ab4623bbde609a99
Sebastian Blank <blank@data-factory.net>
2023-03-02 10:33:47 +0100

    Fix Codeception test

--- tests/Unit/Targeting/Document/TargetGroupEditableTest.php
+++ tests/Unit/Targeting/Document/TargetGroupEditableTest.php
@@ -18,13 +18,13 @@ namespace Pimcore\Tests\Model\Document;
 use Pimcore\Model\Document\Page;
 use Pimcore\Model\Tool\Targeting\TargetGroup;
 use Pimcore\Tests\Helper\Document\TestDataHelper;
-use Pimcore\Tests\Test\ModelTestCase;
+use Pimcore\Tests\Support\Test\TestCase;
 use Pimcore\Tests\Util\TestHelper;
 
 /**
  * @group model.document.target.document
  */
-class TargetGroupEditableTest extends ModelTestCase
+class TargetGroupEditableTest extends TestCase
 {
     protected int $seed = 1;
 
