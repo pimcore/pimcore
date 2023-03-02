03d2bfd512dc1af4f5e4564a613d4f293c343f3d
aryaantony92 <arya.antony@pimcore.com>
2023-03-02 18:06:13 +0100

    Make Default value of targeting as disabled

--- bundles/PersonalizationBundle/src/DependencyInjection/Configuration.php
+++ bundles/PersonalizationBundle/src/DependencyInjection/Configuration.php
@@ -36,9 +36,10 @@ class Configuration implements ConfigurationInterface
         $rootNode
             ->children()
                 ->arrayNode('targeting')
-                    ->canBeEnabled()
-                    ->addDefaultsIfNotSet()
                     ->children()
+                        ->booleanNode('enabled')
+                            ->defaultFalse()
+                        ->end()
                         ->scalarNode('storage_id')
                             ->info('Service ID of the targeting storage which should be used. This ID will be aliased to ' . TargetingStorageInterface::class)
                             ->defaultValue(CookieStorage::class)
