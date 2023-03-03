c2b3068ce831517038151715965d48a19e275232
Sebastian Blank <blank@data-factory.net>
2023-03-03 10:30:55 +0100

    [10.6] Marked AbstractUser and UserRole class abstract (#14519)
    * [10.6] Make AbstractUser and UserRole class abstrac
    
    * Apply suggestions from code review
    
    Co-authored-by: Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>
    
    ---------
    
    Co-authored-by: Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>

--- models/User/AbstractUser.php
+++ models/User/AbstractUser.php
@@ -23,6 +23,8 @@ use Pimcore\Model;
 /**
  * @method \Pimcore\Model\User\AbstractUser\Dao getDao()
  * @method void setLastLoginDate()
+ *
+ * @abstract Will be natively abstract in Pimcore 11
  */
 class AbstractUser extends Model\AbstractModel
 {
