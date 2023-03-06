59f5d132257f74d1a2a0ea031dff3752c1acca44
aryaantony92 <arya.antony@pimcore.com>
2023-03-06 20:04:03 +0100

    Cast to string before passing to stripos() method

--- bundles/AdminBundle/src/Controller/Admin/SettingsController.php
+++ bundles/AdminBundle/src/Controller/Admin/SettingsController.php
@@ -221,7 +221,7 @@ class SettingsController extends AdminController
             if ($filter = $request->get('filter')) {
                 $list->setFilter(function (Metadata\Predefined $predefined) use ($filter) {
                     foreach ($predefined->getObjectVars() as $value) {
-                        if (stripos($value, $filter) !== false) {
+                        if (stripos((string)$value, $filter) !== false) {
                             return true;
                         }
                     }
@@ -341,7 +341,7 @@ class SettingsController extends AdminController
                             $cellValues = is_array($value) ? $value : [$value];
 
                             foreach ($cellValues as $cellValue) {
-                                if (stripos($cellValue, $filter) !== false) {
+                                if (stripos((string)$cellValue, $filter) !== false) {
                                     return true;
                                 }
                             }
