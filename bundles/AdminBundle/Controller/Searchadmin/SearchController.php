84f735b2cec73db648a48df1cd7e09aeab8bdd94
ChristianFeldkirchne <christian.feldkirchner@pimcore.com>
2023-03-06 12:48:20 +0100

    Revert "optimized findAction in SearchController"
    This reverts commit 838c8d23

--- bundles/AdminBundle/Controller/Searchadmin/SearchController.php
+++ bundles/AdminBundle/Controller/Searchadmin/SearchController.php
@@ -77,9 +77,9 @@ class SearchController extends AdminController
 
         $query = $this->filterQueryParam($allParams['query'] ?? '');
 
-        $types = explode(',', preg_replace('/[^a-zA-Z,]/', '', $allParams['type'] ?? ''));
-        $subtypes = explode(',', preg_replace('/[^a-zA-Z,]/', '', $allParams['subtype'] ?? ''));
-        $classnames = explode(',', preg_replace('/[^a-zA-Z,]/', '', $allParams['class'] ?? ''));
+        $types = explode(',', $allParams['type'] ?? '');
+        $subtypes = explode(',', $allParams['subtype'] ?? '');
+        $classnames = explode(',', $allParams['class'] ?? '');
 
         $offset = (int)$allParams['start'];
         $limit = (int)$allParams['limit'];
