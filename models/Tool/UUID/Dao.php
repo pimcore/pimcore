7a4f5f603e795461d9f66dc5e52a593e1a884ed8
lukmzig <lukas.mzigot@gmail.com>
2023-03-13 13:07:49 +0100

    fix: unify uuid queries

--- models/Tool/UUID/Dao.php
+++ models/Tool/UUID/Dao.php
@@ -80,7 +80,7 @@ class Dao extends Model\Dao\AbstractDao
      */
     public function getByUuid($uuid)
     {
-        $data = $this->db->fetchAssociative('SELECT * FROM ' . self::TABLE_NAME ." where uuid='" . $uuid . "'");
+        $data = $this->db->fetchAssociative('SELECT * FROM ' . self::TABLE_NAME . ' where uuid= ?', [$uuid]);
         $model = new Model\Tool\UUID();
         $model->setValues($data);
 
