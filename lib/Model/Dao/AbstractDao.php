a02a0becfd75f00a465ca48f018f99dd96a3bb90
Marco Perberschlager <marco.perberschlager@pimcore.com>
2023-03-14 14:15:01 +0100

    Added `getPrimaryKey` method in `AbstractDao`

--- lib/Model/Dao/AbstractDao.php
+++ lib/Model/Dao/AbstractDao.php
@@ -56,33 +56,47 @@ abstract class AbstractDao implements DaoInterface
     }
 
     /**
+     * @return string[]
+     */
+    public function getPrimaryKey(string $table, bool $cache = true): array
+    {
+        return $this->getValidTableColumns($table, $cache, true);
+    }
+
+    /**
      * @param string $table
      * @param bool $cache
      *
      * @return array
      */
-    public function getValidTableColumns($table, $cache = true)
+    public function getValidTableColumns($table, $cache = true, $primaryKeyColumnsOnly = false)
     {
         $cacheKey = self::CACHEKEY . $table;
 
         if (RuntimeCache::isRegistered($cacheKey)) {
-            $columns = RuntimeCache::get($cacheKey);
+            $allColumns = RuntimeCache::get($cacheKey);
         } else {
-            $columns = Cache::load($cacheKey);
+            $allColumns = Cache::load($cacheKey);
 
-            if (!$columns || !$cache) {
+            if (!$allColumns || !$cache) {
                 $columns = [];
+                $primaryKeyColumns = [];
                 $data = $this->db->fetchAllAssociative('SHOW COLUMNS FROM ' . $table);
                 foreach ($data as $d) {
-                    $columns[] = $d['Field'];
+                    $fieldName = $d['Field'];
+                    $columns[] = $fieldName;
+                    if ($d['Key'] === 'PRI') {
+                        $primaryKeyColumns[] = $fieldName;
+                    }
                 }
-                Cache::save($columns, $cacheKey, ['system', 'resource'], null, 997);
+                $allColumns = ['columns' => $columns,  'primaryKeyColumns' => $primaryKeyColumns];
+                Cache::save($allColumns, $cacheKey, ['system', 'resource'], null, 997);
             }
 
-            RuntimeCache::set($cacheKey, $columns);
+            RuntimeCache::set($cacheKey, $allColumns);
         }
 
-        return $columns;
+        return $primaryKeyColumnsOnly ? $allColumns['primaryKeyColumns'] : $allColumns['columns'];
     }
 
     /**
