c981536e70bb5cdaeb00560e33ec2dabe30015c2
Christian F <christian.feldkirchner@pimcore.com>
2023-03-21 13:34:47 +0100

    cherry-picked PimcoreBundleAdminSupportInterface changes

---
+++ bundles/AdminBundle/src/Support/BundleAdminSupportTrait.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Pimcore\Bundle\AdminBundle\Support;
+
+trait BundleAdminSupportTrait
+{
+    public function getJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getCssPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeJsPaths(): array
+    {
+        return [];
+    }
+
+    public function getEditmodeCssPaths(): array
+    {
+        return [];
+    }
+}
