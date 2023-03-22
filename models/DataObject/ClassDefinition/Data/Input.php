d4d577dec5487130cb2c5bd62993080df7438267
lukmzig <lukmzig@users.noreply.github.com>
2023-03-22 14:23:54 +0000

    Apply php-cs-fixer changes

--- models/DataObject/ClassDefinition/Data/Input.php
+++ models/DataObject/ClassDefinition/Data/Input.php
@@ -328,19 +328,18 @@ class Input extends Data implements
     {
         if (!$omitMandatoryCheck && $this->getRegex() && strlen($data) > 0) {
             $throwException = false;
-            if(in_array('g', $this->getRegexFlags())){
+            if (in_array('g', $this->getRegexFlags())) {
                 $flags = str_replace('g', '', implode('', $this->getRegexFlags()));
                 if (!preg_match_all('#' . $this->getRegex() . '#' . $flags, $data)) {
                     $throwException = true;
                 }
-            }
-            else{
+            } else {
                 if (!preg_match('#' . $this->getRegex() . '#' . implode('', $this->getRegexFlags()), $data)) {
                     $throwException = true;
                 }
             }
 
-            if($throwException) {
+            if ($throwException) {
                 throw new Model\Element\ValidationException('Value in field [ ' . $this->getName() . " ] doesn't match input validation '" . $this->getRegex() . "'");
             }
         }
