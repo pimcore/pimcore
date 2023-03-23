019c9723aa6f0ffbdc1c3d4b755096bdd174140d
rliebi <remo@liebi.net>
2023-03-23 08:38:25 +0100

    removed return type for 10.5, phpstan issue fix

--- bundles/EcommerceFrameworkBundle/OrderManager/V7/OrderAgent.php
+++ bundles/EcommerceFrameworkBundle/OrderManager/V7/OrderAgent.php
@@ -257,9 +257,9 @@ class OrderAgent implements OrderAgentInterface
     /**
      * @return Currency
      */
-    public function getCurrency(): Currency
+    public function getCurrency()
     {
-        return $this->getOrder()?->getCurrency() !== null ? new Currency($this->order->getCurrency()) : $this->environment->getDefaultCurrency();
+        return $this->getOrder()->getCurrency() !== null ? new Currency($this->order->getCurrency()) : $this->environment->getDefaultCurrency();
     }
 
     /**
