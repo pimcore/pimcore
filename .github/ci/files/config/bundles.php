e4edce68024691d18ab71d0408a189de94c819e5
Bernhard Rusch <brusch@users.noreply.github.com>
2023-03-28 15:56:44 +0200

    Removed EcommerceFrameworkBundle -> moved to pimcore/ecommerce-framework-bundle (#14776)
    * Removed EcommerceFrameworkBundle -> moved to pimcore/ecommerce-framework-bundle
    
    * Removed obsolete migration
    
    * deleted remaining file

--- .github/ci/files/config/bundles.php
+++ .github/ci/files/config/bundles.php
@@ -2,9 +2,6 @@
 declare(strict_types=1);
 
 return [
-    "Pimcore\\Bundle\\EcommerceFrameworkBundle\\PimcoreEcommerceFrameworkBundle" => [
-        "all" => TRUE
-    ],
     "Pimcore\\Bundle\\GlossaryBundle\\PimcoreGlossaryBundle" => [
         "all" => TRUE
     ],
