bf5c58740ef3e525502e36a78dd5a3ee27e3cb5c
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-03-30 18:06:03 +0200

    Move back ImageChart.php

--- lib/Helper/ImageChart.php
+++ lib/Helper/ImageChart.php
@@ -14,7 +14,7 @@ declare(strict_types=1);
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
 
-namespace Pimcore\Bundle\GoogleMarketingBundle\Helper;
+namespace Pimcore\Helper;
 
 /**
  * @internal
