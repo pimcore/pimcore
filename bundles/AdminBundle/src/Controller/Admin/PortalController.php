162dfdbe1d4770d791cd836ec4fba846e1d19594
Marco Perberschlager <marco.perberschlager@pimcore.com>
2023-04-05 12:40:26 +0200

    Removed #

--- bundles/AdminBundle/src/Controller/Admin/PortalController.php
+++ bundles/AdminBundle/src/Controller/Admin/PortalController.php
@@ -9,7 +9,7 @@ declare(strict_types=1);
  * - Pimcore Commercial License (PCL)
  * Full copyright and license information is available in
  * LICENSE.md which is distributed with this source code.
- *#
+ *
  *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
  *  @license    http://www.pimcore.org/license     GPLv3 and PCL
  */
