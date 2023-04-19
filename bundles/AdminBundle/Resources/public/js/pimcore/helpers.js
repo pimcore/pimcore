19ba7096fbe942f6c369f0e37db1fa2cf83f72c3
ChristianFeldkirchne <christian.feldkirchner@pimcore.com>
2023-04-19 08:11:01 +0200

    fixed double encode issue

--- bundles/AdminBundle/Resources/public/js/pimcore/helpers.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/helpers.js
@@ -25,8 +25,9 @@ pimcore.helpers.sanitizeUrlSlug = function (slug) {
 pimcore.helpers.htmlEncodeTextField = function (textField) {
     if(textField.getValue()) {
         textField.suspendEvent('change');
+        const decodedValue = Ext.util.Format.htmlDecode(textField.getValue());
         textField.setValue(
-            Ext.util.Format.htmlEncode(textField.getValue())
+            Ext.util.Format.htmlEncode(decodedValue)
         );
         textField.resumeEvent('change');
     }
