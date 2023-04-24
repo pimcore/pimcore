f25061ee25cb6b341a94b4bbefa1ffb3e94d778d
Felix Flor <f.flor@twocream.com>
2023-04-24 12:26:58 +0200

    fix CSV Export breakes file at more than 20 rows

--- bundles/AdminBundle/Controller/Admin/DataObject/DataObjectHelperController.php
+++ bundles/AdminBundle/Controller/Admin/DataObject/DataObjectHelperController.php
@@ -1429,7 +1429,7 @@ class DataObjectHelperController extends AdminController
             } else {
                 fwrite($temp, implode($delimiter, array_map([$this, 'encodeFunc'], $line)));
             }
-            if ($i < $lineCount - 1) {
+            if ($i <= $lineCount - 1) {
                 fwrite($temp, "\r\n");
             }
         }
