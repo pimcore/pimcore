d9ea62dab4d68a3221183a74798727f0cdb6c239
Dominik Pfaffenbauer <dominik@pfaffenbauer.at>
2023-04-28 13:31:15 +0200

    add auto increment primary key to notes_data table

---
+++ bundles/CoreBundle/Migrations/Version20230428112302.php
@@ -0,0 +1,42 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Pimcore\Bundle\CoreBundle\Migrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+final class Version20230428112302 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return 'Migrate notes table schema to have a auto increment column';
+    }
+
+    public function up(Schema $schema): void
+    {
+        $notesData = $schema->getTable('notes_data');
+
+        if (!$notesData->hasColumn('auto_id')) {
+            $notesData->addColumn('auto_id', 'integer', [
+                'autoincrement' => true,
+            ]);
+
+            $notesData->dropPrimaryKey();
+            $notesData->setPrimaryKey(['auto_id']);
+            $notesData->addUniqueIndex(['id', 'name']);
+        }
+    }
+
+    public function down(Schema $schema): void
+    {
+        $notesData = $schema->getTable('notes_data');
+
+        if ($notesData->hasColumn('auto_id')) {
+            $notesData->dropPrimaryKey();
+            $notesData->dropColumn('auto_id');
+            $notesData->setPrimaryKey(['id', 'name']);
+        }
+    }
+}
