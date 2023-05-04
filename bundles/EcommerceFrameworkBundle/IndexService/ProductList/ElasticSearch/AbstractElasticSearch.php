01d7469d09563c5d484d697441dea1a6255e1553
Julian Raab <julian.raab@elements.at>
2023-05-04 15:29:56 +0200

    remove type in elastic search _search requests

--- bundles/EcommerceFrameworkBundle/IndexService/ProductList/ElasticSearch/AbstractElasticSearch.php
+++ bundles/EcommerceFrameworkBundle/IndexService/ProductList/ElasticSearch/AbstractElasticSearch.php
@@ -570,7 +570,6 @@ abstract class AbstractElasticSearch implements ProductListInterface
 
         $params = [];
         $params['index'] = $this->getIndexName();
-        $params['type'] = $this->getTenantConfig()->getElasticSearchClientParams()['indexType'];
         $params['track_total_hits'] = true;
         $params['rest_total_hits_as_int'] = true;
 
