3c13ad9c1ce7f6fff554978c3d14474db5afcdcc
Christian F <christian.feldkirchner@pimcore.com>
2023-05-25 14:35:40 +0200

    [Task] Improved validation of url slug (#15145)
    * improved validation of url slug
    
    * improved url slug check
    
    * improved url slug check v2
    
    * Update urlSlug.js
    
    removed TODO
    
    * Update urlSlug.js
    
    * Update UrlSlug.php
    
    * encoded slug urls, rewrite slug matcher
    
    * removed urlencode in database
    
    * Update UrlSlug.php
    
    * removed `filtter_var`
    
    * adjust docs, fixed url decode for `+` signs
    
    * thow exception on reserved chars
    
    * redefined reserved chars

--- bundles/AdminBundle/Resources/public/js/pimcore/object/tags/urlSlug.js
+++ bundles/AdminBundle/Resources/public/js/pimcore/object/tags/urlSlug.js
@@ -145,22 +145,16 @@ pimcore.object.tags.urlSlug = Class.create(pimcore.object.tags.abstract, {
                     value = value.substring(1);
                     value = value.replace(/\/$/, "");
 
-                    var parts = value.split('/');
+                    const parts = value.split('/');
                     for (let i = 0; i < parts.length; i++) {
                         let part = parts[i];
                         if  (part.length == 0) {
                             return false;
                         }
-
-                        sanitizedPart = part.replace(/[#\?\*\:\\\\<\>\|"%&@=;]/g, '-');
-                        if (sanitizedPart != part) {
-                            return t('url-slug-invalid-chars');
-                        }
                     }
                 }
 
                 return true;
-
             }
         };
         if (this.fieldConfig.width) {
