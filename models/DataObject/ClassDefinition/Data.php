25d249f85fc45107345efbb636387437c8ce0787
Matthew Fedak <135224620+mfedak-notebooksbillger@users.noreply.github.com>
2023-06-06 14:41:43 +0200

    Update models/DataObject/ClassDefinition/Data.php
    Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

--- models/DataObject/ClassDefinition/Data.php
+++ models/DataObject/ClassDefinition/Data.php
@@ -405,12 +405,7 @@ abstract class Data implements DataObject\ClassDefinition\Data\TypeDeclarationSu
         }
 
         if ($operator === 'in') {
-            $formattedValues = implode(
-                ',',
-                array_map(function ($value) {
-                    return (float)$value;
-                }, explode(',', $value))
-            );
+            $formattedValues = implode(',', array_map('floatval', explode(',', $value)));
 
             return $key . ' ' . $operator . ' (' . $formattedValues . ')';
         }
