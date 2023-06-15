a5940282204749622b309ff813dedbfda10b492e
Martin Eiber <martin.eiber@pimcore.com>
2023-06-15 13:55:18 +0200

    Update tests. Additionally, use escapeRecord methode.

--- tests/Unit/Helper/CsvFormulaTest.php
+++ tests/Unit/Helper/CsvFormulaTest.php
@@ -25,12 +25,26 @@ class CsvFormulaTest extends TestCase
     {
         $formatter = new CsvFormulaFormatter("'", ['=', '-', '+', '@']);
 
-        $this->assertEquals("=1+1", $formatter->unEscapeField("'=1+1"));
-        $this->assertEquals("-1+1", $formatter->unEscapeField("'-1+1"));
-        $this->assertEquals("+1+1", $formatter->unEscapeField("'+1+1"));
-        $this->assertEquals("@1+1", $formatter->unEscapeField("'@1+1"));
+        $escapedRow = $formatter->escapeRecord(["=1+1"]);
+        $this->assertEquals("'=1+1", $escapedRow[0]);
+        $this->assertEquals("=1+1", $formatter->unEscapeField($escapedRow[0]));
+
+
+        $escapedRow = $formatter->escapeRecord(["-1+1"]);
+        $this->assertEquals("'-1+1", $escapedRow[0]);
+        $this->assertEquals("-1+1", $formatter->unEscapeField($escapedRow[0]));
+
+        $escapedRow = $formatter->escapeRecord(["+1+1"]);
+        $this->assertEquals("'+1+1", $escapedRow[0]);
+        $this->assertEquals("+1+1", $formatter->unEscapeField($escapedRow[0]));
+
+        $escapedRow = $formatter->escapeRecord(["@1+1"]);
+        $this->assertEquals("'@1+1", $escapedRow[0]);
+        $this->assertEquals("@1+1", $formatter->unEscapeField($escapedRow[0]));
 
         // There should be no escape. So the string should be returned as is.
-        $this->assertEquals("test", $formatter->unEscapeField("test"));
+        $escapedRow = $formatter->escapeRecord(["test"]);
+        $this->assertEquals("test", $escapedRow[0]);
+        $this->assertEquals("test", $formatter->unEscapeField($escapedRow[0]));
     }
 }
