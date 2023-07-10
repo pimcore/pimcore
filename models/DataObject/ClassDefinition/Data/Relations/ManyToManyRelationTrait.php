7fb9144523811465e7265581ab84c7b726958437
JiaJiaJi <kingjia90@gmail.com>
2023-07-10 12:28:17 +0200

    fix else again

--- models/DataObject/ClassDefinition/Data/Relations/ManyToManyRelationTrait.php
+++ models/DataObject/ClassDefinition/Data/Relations/ManyToManyRelationTrait.php
@@ -33,11 +33,10 @@ trait ManyToManyRelationTrait
 
         if ($forceSave === false) {
             if (!DataObject::isDirtyDetectionDisabled() && $object instanceof DirtyIndicatorInterface) {
-                if ($object instanceof DataObject\Localizedfield
-                    && $object->getObject() instanceof DirtyIndicatorInterface
-                    && !$object->hasDirtyFields()
-                ) {
-                    return true;
+                if ($object instanceof DataObject\Localizedfield){
+                    if ($object->getObject() instanceof DirtyIndicatorInterface && !$object->hasDirtyFields()) {
+                        return true;
+                    }
                 } elseif ($this->supportsDirtyDetection()) {
                     if (!$object->isFieldDirty($this->getName())) {
                         return true;
