9bae90c635b7dfab3a2f7049ffbb9059087be84c
Herbert Roth <herbert.roth@pimcore.com>
2023-07-10 10:06:27 +0200

    Add @method for phpStan checks.

--- models/User/AbstractUser.php
+++ models/User/AbstractUser.php
@@ -23,6 +23,7 @@ use Pimcore\Event\UserRoleEvents;
 use Pimcore\Model;
 
 /**
+ * @method \Pimcore\Model\User\AbstractUser\Dao getDao()
  * @method void setLastLoginDate()
  */
 abstract class AbstractUser extends Model\AbstractModel implements AbstractUserInterface
