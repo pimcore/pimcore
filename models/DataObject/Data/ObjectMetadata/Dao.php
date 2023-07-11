64bcb410e4321618832a8681f12ff24e2244e12f
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2023-07-11 15:39:06 +0200

    [Bug] - Deadlock found when trying to get lock on DataObject import (#15391)
    * Fix: deadlock during import
    
    * Update models/DataObject/Data/ObjectMetadata/Dao.php
    
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>
    
    * Improve: change 'auto_id' to 'id'
    
    * Fix: tests
    
    ---------
    
    Co-authored-by: Divesh Pahuja <divesh.pahuja@pimcore.com>

--- models/DataObject/Data/ObjectMetadata/Dao.php
+++ models/DataObject/Data/ObjectMetadata/Dao.php
@@ -118,6 +118,7 @@ class Dao extends Model\Dao\AbstractDao
         $table = 'object_metadata_' . $classId;
 
         $this->db->executeQuery('CREATE TABLE IF NOT EXISTS `' . $table . "` (
+              `id` int(11) UNSIGNED NOT NULL AUTO_INCREMENT,
               `o_id` int(11) UNSIGNED NOT NULL default '0',
               `dest_id` int(11) NOT NULL default '0',
 	          `type` VARCHAR(50) NOT NULL DEFAULT '',
@@ -128,7 +129,10 @@ class Dao extends Model\Dao\AbstractDao
               `ownername` VARCHAR(70) NOT NULL DEFAULT '',
               `position` VARCHAR(70) NOT NULL DEFAULT '0',
               `index` int(11) unsigned NOT NULL DEFAULT '0',
-              PRIMARY KEY (`o_id`, `dest_id`, `type`, `fieldname`, `column`, `ownertype`, `ownername`, `position`, `index`),
+              PRIMARY KEY (`id`),
+              UNIQUE KEY `metadata_un` (
+                `o_id`, `dest_id`, `type`, `fieldname`, `column`, `ownertype`, `ownername`, `position`, `index`
+              ),
               INDEX `dest_id` (`dest_id`),
               INDEX `fieldname` (`fieldname`),
               INDEX `column` (`column`),
@@ -136,7 +140,8 @@ class Dao extends Model\Dao\AbstractDao
               INDEX `ownername` (`ownername`),
               INDEX `position` (`position`),
               INDEX `index` (`index`),
-              CONSTRAINT `".self::getForeignKeyName($table, 'o_id').'` FOREIGN KEY (`o_id`) REFERENCES objects (`o_id`) ON DELETE CASCADE
+              CONSTRAINT `".self::getForeignKeyName($table, 'o_id').'` FOREIGN KEY (`o_id`)
+              REFERENCES objects (`o_id`) ON DELETE CASCADE
 		) DEFAULT CHARSET=utf8mb4;');
 
         $this->handleEncryption($class, [$table]);
