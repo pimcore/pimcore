83b230e0dd6ba0d2c64858dd1eced40f3b82f60e
JiaJia Ji <kingjia90@gmail.com>
2023-07-25 14:14:01 +0200

    change back to rely on fallback

--- lib/Tool/Requirements.php
+++ lib/Tool/Requirements.php
@@ -608,19 +608,13 @@ final class Requirements
                 'message' => "It's recommended to have the GNU C Library locale data installed (eg. apt-get install locales-all).",
             ]);
         }
-
-        $utfLocaleState = Check::STATE_ERROR;
-        if (
-            setlocale(LC_ALL, ['en.utf8', 'en_US.utf8', 'en_GB.utf8'])
-            || setlocale(LC_ALL, ['en.UTF-8', 'en_US.UTF-8', 'en_GB.UTF-8']) // MacOS format
-        ) {
-            $utfLocaleState = Check::STATE_OK;
-        }
         
         $checks[] = new Check([
             'name' => 'locales-utf8',
             'link' => 'https://packages.debian.org/en/stable/locales-all',
-            'state' => $utfLocaleState,
+            'state' => setlocale(LC_ALL, ['en.utf8', 'en.UTF-8', 'en_US.utf8', 'en_US.UTF-8', 'en_GB.utf8', 'en_GB.UTF-8']) === false 
+                      ? Check::STATE_ERROR 
+                      : Check::STATE_OK,
             'message' => 'It is recommended to install UTF-8 locale, otherwise all CLI calls which use escapeshellarg() will strip multibyte characters',
         ]);
 
