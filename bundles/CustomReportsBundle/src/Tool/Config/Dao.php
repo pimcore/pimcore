c6bf2c13f2f73d8c2577a1cc231cfbda9b072ea0
robertSt7 <robert.steinkellner@pimcore.com>
2023-08-02 11:58:53 +0200

    Fix: write custom reports

--- bundles/CustomReportsBundle/src/Tool/Config/Dao.php
+++ bundles/CustomReportsBundle/src/Tool/Config/Dao.php
@@ -108,11 +108,9 @@ class Dao extends Model\Dao\PimcoreLocationAwareConfigDao
     protected function prepareDataStructureForYaml(string $id, mixed $data): mixed
     {
         return [
-            'pimcore' => [
-                'custom_report' => [
-                    'definitions' => [
-                        $id => $data,
-                    ],
+            'pimcore_custom_reports' => [
+                'definitions' => [
+                    $id => $data,
                 ],
             ],
         ];
