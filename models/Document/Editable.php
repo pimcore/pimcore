ee7e13e1185b9dc41cd1ffb589830c183740dca0
Lennart Fries <mail@lennartsauter.de>
2023-08-08 16:19:50 +0200

    Allow realName to be null in Editable

--- models/Document/Editable.php
+++ models/Document/Editable.php
@@ -54,7 +54,7 @@ abstract class Editable extends Model\AbstractModel implements Model\Document\Ed
      *
      * @internal
      */
-    protected string $realName = '';
+    protected ?string $realName = '';
 
     /**
      * Contains parent hierarchy names (used when building elements inside a block/areablock hierarchy)
@@ -325,10 +325,6 @@ abstract class Editable extends Model\AbstractModel implements Model\Document\Ed
      */
     public function addConfig(string $name, mixed $value): static
     {
-        if (!is_array($this->config)) {
-            $this->config = [];
-        }
-
         $this->config[$name] = $value;
 
         return $this;
@@ -336,7 +332,7 @@ abstract class Editable extends Model\AbstractModel implements Model\Document\Ed
 
     public function getRealName(): string
     {
-        return $this->realName;
+        return $this->realName ?? '';
     }
 
     public function setRealName(string $realName): void
