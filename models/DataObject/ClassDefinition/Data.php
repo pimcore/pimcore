6fa788ff121ea9c78005324af0b15c01beac249c
Sebastian Blank <blank@data-factory.net>
2023-08-18 12:22:24 +0200

    Deprecate null parameter

--- models/DataObject/ClassDefinition/Data.php
+++ models/DataObject/ClassDefinition/Data.php
@@ -258,7 +258,15 @@ abstract class Data implements DataObject\ClassDefinition\Data\TypeDeclarationSu
      */
     public function setIndex(?bool $index): static
     {
-        $this->index = $index ?? false;
+        if (null === $index) {
+            trigger_deprecation(
+                'pimcore/pimcore',
+                '11.0.7',
+                sprintf('Passing null to method %s is deprecated', __METHOD__)
+            );
+            $index = false;
+        }
+        $this->index = $index;
 
         return $this;
     }
