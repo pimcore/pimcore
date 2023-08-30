d957ec5a3ab1815048dfc4b0501fe01d4d8f7ae6
Stefan Hagspiel <info@solverat.com>
2023-08-30 16:13:52 +0200

    fix hardlink child resolver

--- lib/Navigation/Builder.php
+++ lib/Navigation/Builder.php
@@ -332,6 +332,10 @@ class Builder
     {
         // the intention of this function is mainly to be overridden in order to customize the behavior of the navigation
         // e.g. for custom filtering and other very specific use-cases
+        if ($parentDocument instanceof Document\Hardlink || $parentDocument instanceof Document\Hardlink\Wrapper\WrapperInterface) {
+            return $parentDocument->getChildren()->getData();
+        }
+
         return $parentDocument->getChildren()->load();
     }
 
