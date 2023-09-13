33173df4b983fe81035e44e8b7dadf956f49f5cc
Peter Philipp <peter.philipp@cando-image.com>
2023-09-13 12:29:08 +0200

    Respect sort order in getSiblings() like in getChildren() (#15462)
    * Respect sort order in getSiblings() like in getChildren()
    * refactoring, adding changelog
    
    ---------
    
    Co-authored-by: jiajia <kingjia90@gmail.com>

--- models/DataObject/AbstractObject.php
+++ models/DataObject/AbstractObject.php
@@ -397,16 +397,17 @@ abstract class AbstractObject extends Model\Element\AbstractElement
         $cacheKey = $this->getListingCacheKey(func_get_args());
 
         if (!isset($this->siblings[$cacheKey])) {
-            if ($this->getParentId()) {
+            $parentElement = $this->getParent();
+            if ($parentElement) {
                 $list = new Listing();
                 $list->setUnpublished($includingUnpublished);
-                $list->addConditionParam('parentId = ?', $this->getParentId());
+                $list->addConditionParam('parentId = ?', $parentElement->getId());
                 if ($this->getId()) {
                     $list->addConditionParam('id != ?', $this->getId());
                 }
-                $list->setOrderKey('key');
                 $list->setObjectTypes($objectTypes);
-                $list->setOrder('asc');
+                $list->setOrderKey($parentElement->getChildrenSortBy());
+                $list->setOrder($parentElement->getChildrenSortOrder());
                 $this->siblings[$cacheKey] = $list;
             } else {
                 $list = new Listing();
