76a27e48c33b088187099eff5beada85c0bcc3c3
jiajia <kingjia90@gmail.com>
2023-09-20 13:42:05 +0200

    test with sanitizeFor body?

--- models/Document/Editable/Wysiwyg.php
+++ models/Document/Editable/Wysiwyg.php
@@ -90,7 +90,7 @@ class Wysiwyg extends Model\Document\Editable implements IdRewriterInterface, Ed
     public function setDataFromResource(mixed $data): static
     {
         $helper = self::getWysiwygSanitizer();
-        $this->text = html_entity_decode($helper->sanitizeFor('textarea', $data));
+        $this->text = html_entity_decode($helper->sanitizeFor('body', $data));
 
         return $this;
     }
