30a4cf23965ec6dd5fcfcd8d2f8bcb55322d1e1d
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-09-25 13:35:13 +0200

    Fix < encoding issue before passing to the sanitizer

--- models/DataObject/ClassDefinition/Data/Wysiwyg.php
+++ models/DataObject/ClassDefinition/Data/Wysiwyg.php
@@ -114,7 +114,8 @@ class Wysiwyg extends Data implements ResourcePersistenceAwareInterface, QueryRe
     public function getDataFromResource(mixed $data, DataObject\Concrete $object = null, array $params = []): ?string
     {
         if (is_string($data)) {
-            $data = htmlentities($data);
+            //fix for https://github.com/pimcore/pimcore/issues/15740
+            $data = preg_replace('/(?!<[a-zA-Z=\"\':; ]*[^ ]>|<\\/[a-zA-Z="\':; ]*>)(<)/', "&lt;", $data);
             $data = html_entity_decode(self::getWysiwygSanitizer()->sanitizeFor('body', $data));
         }
 
--- models/Document/Editable/Wysiwyg.php
+++ models/Document/Editable/Wysiwyg.php
@@ -90,7 +90,8 @@ class Wysiwyg extends Model\Document\Editable implements IdRewriterInterface, Ed
     public function setDataFromResource(mixed $data): static
     {
         $helper = self::getWysiwygSanitizer();
-        $data = htmlentities($data);
+        //fix for https://github.com/pimcore/pimcore/issues/15740
+        $data = preg_replace('/(?!<[a-zA-Z=\"\':; ]*[^ ]>|<\\/[a-zA-Z="\':; ]*>)(<)/', "&lt;", $data);
         $this->text = html_entity_decode($helper->sanitizeFor('body', $data));
 
         return $this;
@@ -102,7 +103,8 @@ class Wysiwyg extends Model\Document\Editable implements IdRewriterInterface, Ed
     public function setDataFromEditmode(mixed $data): static
     {
         $helper = self::getWysiwygSanitizer();
-        $data = htmlentities($data);
+        //fix for https://github.com/pimcore/pimcore/issues/15740
+        $data = preg_replace('/(?!<[a-zA-Z=\"\':; ]*[^ ]>|<\\/[a-zA-Z="\':; ]*>)(<)/', "&lt;", $data);
         $this->text = html_entity_decode($helper->sanitizeFor('body', $data));
 
         return $this;
