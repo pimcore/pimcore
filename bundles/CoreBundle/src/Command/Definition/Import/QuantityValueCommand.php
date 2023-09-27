280c9b93a306593f86a5524743f91651d2a72577
purshottam nepal <prunepal333@gmail.com>
2023-09-27 07:02:32 +0545

    #[FEATURE]: Import/export quantity value unit definitions

---
+++ bundles/CoreBundle/src/Command/Definition/Import/QuantityValueCommand.php
@@ -0,0 +1,108 @@
+<?php
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Bundle\CoreBundle\Command\Definition\Import;
+
+use Pimcore\Console\AbstractCommand;
+use Pimcore\Console\Traits\DryRun;
+use Pimcore\Model\DataObject\QuantityValue\Service;
+use Symfony\Component\Console\Input\InputArgument;
+use Symfony\Component\Console\Input\InputInterface;
+use Symfony\Component\Console\Input\InputOption;
+use Symfony\Component\Console\Output\OutputInterface;
+
+class QuantityValueCommand extends AbstractCommand
+{
+    use DryRun;
+
+    public function configure(): void
+    {
+        $this
+            ->setName('pimcore:definition:import:units')
+            ->setAliases(['definition:import:units'])
+            ->setDescription('Import quantity value units from a JSON export')
+            ->addArgument(
+                'path',
+                InputArgument::REQUIRED,
+                'Path to quantity value unit JSON export file')
+            ->addOption(
+                'override',
+                'o',
+                InputOption::VALUE_NEGATABLE,
+                'Override the existing unit definition'
+            );
+
+        $this->configureDryRunOption();
+    }
+
+    /**
+     * Validate and return path to JSON file
+     *
+     * @return string
+     */
+    protected function getPath(): string
+    {
+        $path = $this->input->getArgument('path');
+        if (!file_exists($path) || !is_readable($path)) {
+            throw new \InvalidArgumentException('File does not exist');
+        }
+
+        return $path;
+    }
+
+    /**
+     * Load JSON data from file
+     */
+    protected function getJson(string $path): string
+    {
+        $content = file_get_contents($path);
+
+        // try to decode json here as we want to fail early if file is no valid JSON
+        $json = json_decode($content);
+        if (null === $json) {
+            throw new \InvalidArgumentException('JSON could not be decoded');
+        }
+
+        return $content;
+    }
+
+    /**
+     * {@inheritdoc}
+     */
+    protected function execute(InputInterface $input, OutputInterface $output): int
+    {
+        $path = $this->getPath();
+        $json = $this->getJson($path);
+        $override = $this->input->getOption('override') ?? false;
+        $result = false;
+        if ($this->isDryRun()) {
+            $this->output->writeln($this->prefixDryRun(sprintf('Skipping the unit definition import from %s', $path)));
+            $result = true;
+        } else {
+            $this->output->writeln(sprintf('Importing quantity value unit definitions from %s', $path));
+            $result = Service::importDefinitionFromJson($json, $override);
+        }
+
+        if ($result) {
+            $this->output->writeln('Successfully imported definitions');
+
+            return 0;
+        } else {
+            $this->output->writeln('<error>ERROR:</error> Failed to import definitions');
+
+            return 1;
+        }
+    }
+}
