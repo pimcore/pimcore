93d9046fcde42350b968f47ee4987a9e85b0d374
Divesh Pahuja <divesh.pahuja@pimcore.com>
2023-09-28 15:12:22 +0200

    [Assets] Calculate Spherical MetaData in AssetUpdateTasksHandler - resolves #6820

--- lib/Messenger/Handler/AssetUpdateTasksHandler.php
+++ lib/Messenger/Handler/AssetUpdateTasksHandler.php
@@ -92,7 +92,14 @@ class AssetUpdateTasksHandler
             $asset->removeCustomSetting('videoHeight');
         }
 
-        $asset->handleEmbeddedMetaData(true);
+        $sphericalMetaData = $asset->getSphericalMetaDataFromBackend();
+        if (!empty($sphericalMetaData)) {
+            $asset->setCustomSetting('SphericalMetaData', $sphericalMetaData);
+        } else {
+            $asset->removeCustomSetting('SphericalMetaData');
+        }
+
+        $asset->handleEmbeddedMetaData();
         $this->saveAsset($asset);
 
         if ($asset->getCustomSetting('videoWidth') && $asset->getCustomSetting('videoHeight')) {
