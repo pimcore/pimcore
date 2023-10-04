aaca819a67a7560d36ca50a77b36d9823bea80b3
Bram <bgerritsen@emico.nl>
2023-10-04 09:04:06 +0200

    fix: change collation to match install

--- bundles/SimpleBackendSearchBundle/src/Migrations/Version20230914085710.php
+++ bundles/SimpleBackendSearchBundle/src/Migrations/Version20230914085710.php
@@ -29,14 +29,14 @@ final class Version20230914085710 extends AbstractMigration
 
     public function up(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE `search_backend_data` MODIFY `fullpath` varchar(765) CHARACTER SET ascii COLLATE ascii_general_ci NOT NULL');
+        $this->addSql('ALTER TABLE `search_backend_data` MODIFY `fullpath` varchar(765) CHARACTER SET utf8 COLLATE utf8_general_ci NOT NULL');
         $this->addSql('ALTER TABLE `search_backend_data` MODIFY `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL');
         $this->addSql('ALTER TABLE `search_backend_data` MODIFY `subtype` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci NOT NULL');
     }
 
     public function down(Schema $schema): void
     {
-        $this->addSql('ALTER TABLE `search_backend_data` MODIFY `fullpath` varchar(765) CHARACTER SET ascii COLLATE ascii_general_ci DEFAULT NULL');
+        $this->addSql('ALTER TABLE `search_backend_data` MODIFY `fullpath` varchar(765) CHARACTER SET utf8 COLLATE utf8_general_ci DEFAULT NULL');
         $this->addSql('ALTER TABLE `search_backend_data` MODIFY `type` varchar(20) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL');
         $this->addSql('ALTER TABLE `search_backend_data` MODIFY `subtype` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_general_ci DEFAULT NULL');
     }
