8fbf2783721edff2e1525a40c52d5870845620ff
m.binerbay <murat.binerbay@alfatraining.de>
2024-01-30 11:44:18 +0100

    Revert "Default init function parameter lifetime to 0 and always set it"
    This reverts commit 08d1397c13aa5ee0aaf09953032f588f5e1053f2.

--- lib/Twig/Extension/CacheExtension.php
+++ lib/Twig/Extension/CacheExtension.php
@@ -55,10 +55,15 @@ class CacheExtension extends AbstractExtension
      *
      * @return $this
      */
-    public function init(string $name, int $lifetime = 0, bool $force = false): static
+    public function init(string $name, int $lifetime = null, bool $force = false): static
     {
         $this->key = 'pimcore_viewcache_' . $name;
         $this->force = $force;
+
+        if (!$lifetime) {
+            $lifetime = null;
+        }
+
         $this->lifetime = $lifetime;
 
         return $this;
