pimcore/pimcore

commit b8627f4171c302378b0150ca0c053d6c55c298a1

[Improvement]: Define default value on Multiselect Object Type (#15871)
* Add default value for Multiselect object type

* Adjust readme for select types

* Remove multi select provider interface where possible

* Make multi select interface optional

* trigger deprecation if multi select provide interface is given

* Update models/DataObject/ClassDefinition/Helper/OptionsProviderResolver.php

Change version 12.0 to 11.1

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Remove getDefaultValue method from MultiSelectOptionsProviderInterface

* Add trigger for deprecation

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Revert removed Multiselect condition

* Resolve multiple default values

* Update release version

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Update release version

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Refactor getDataForResource()

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Fix typo

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Use multiple return types instead of  'mixed' type

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* Update docs

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>

* apply suggestions to doc

* add changelog

* limit the deprecation trigger to multiselectoption

* remove not neccessary null coalesce

* fix service only covering multiselect

---------

Co-authored-by: Jacob Dreesen <jacob@hdreesen.de>
Co-authored-by: JiaJia Ji <kingjia90@gmail.com>

models/DataObject/ClassDefinition/Data/Multiselect.php
@@ -20,6 +20,8 @@ use Pimcore\Db\Helper;
 use Pimcore\Model;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
+use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\MultiSelectOptionsProviderInterface;
+use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\SelectOptionsProviderInterface;
 use Pimcore\Model\DataObject\ClassDefinition\Service;
 use Pimcore\Model\DataObject\Concrete;
 use Pimcore\Normalizer\NormalizerInterface;
@@ -42,6 +44,7 @@ class Multiselect extends Data implements
     use DataObject\ClassDefinition\DynamicOptionsProvider\SelectionProviderTrait;
     use DataObject\Traits\DataHeightTrait;
     use DataObject\Traits\DataWidthTrait;
+    use DataObject\Traits\DefaultValueTrait;
     use OptionsProviderTrait;
 
     /**
@@ -68,6 +71,11 @@ class Multiselect extends Data implements
      * @internal
      */
     public bool $dynamicOptions = false;
+
+    /**
+     * @internal
+     */
+    public ?array $defaultValue = null;
 
     public function getOptions(): ?array
     {
@@ -114,6 +122,18 @@ class Multiselect extends Data implements
         return $this->renderType;
     }
 
+    public function setDefaultValue(array $defaultValue): static
+    {
+        $this->defaultValue = $defaultValue;
+
+        return $this;
+    }
+
+    public function getDefaultValue(): ?array
+    {
+        return $this->defaultValue;
+    }
+
     /**
      * @see ResourcePersistenceAwareInterface::getDataForResource
      *
@@ -121,11 +141,17 @@ class Multiselect extends Data implements
      */
     public function getDataForResource(mixed $data, DataObject\Concrete $object = null, array $params = []): ?string
     {
-        if (is_array($data)) {
+        if (!$this->isEmpty($data) && is_array($data)) {
             return implode(',', $data);
         }
 
-        return null;
+        $defaultValue = $this->handleDefaultValue($data, $object, $params);
+
+        if (is_array($defaultValue)) {
+            return implode(',', array_map(fn ($v) => $v['value'] ?? $v, $defaultValue));
+        }
+
+        return $defaultValue;
     }
 
     /**
@@ -447,7 +473,6 @@ class Multiselect extends Data implements
      */
     public function preSave(mixed $containerDefinition, array $params = []): void
     {
-        /** @var DataObject\ClassDefinition\DynamicOptionsProvider\MultiSelectOptionsProviderInterface|null $optionsProvider */
         $optionsProvider = DataObject\ClassDefinition\Helper\OptionsProviderResolver::resolveProvider(
             $this->getOptionsProviderClass(),
             DataObject\ClassDefinition\Helper\OptionsProviderResolver::MODE_MULTISELECT
@@ -509,5 +534,27 @@ class Multiselect extends Data implements
     public function getFieldType(): string
     {
         return 'multiselect';
+    }
+
+    protected function doGetDefaultValue(Concrete $object, array $context = []): mixed
+    {
+        /** @var SelectOptionsProviderInterface|MultiSelectOptionsProviderInterface|null $optionsProvider */
+        $optionsProvider = DataObject\ClassDefinition\Helper\OptionsProviderResolver::resolveProvider(
+            $this->getOptionsProviderClass(),
+            DataObject\ClassDefinition\Helper\OptionsProviderResolver::MODE_MULTISELECT
+        );
+        if ($optionsProvider instanceof SelectOptionsProviderInterface) {
+            $context['object'] = $object;
+            $context['class'] = $object->getClass();
+
+            $context['fieldname'] = $this->getName();
+            if (!isset($context['purpose'])) {
+                $context['purpose'] = 'layout';
+            }
+
+            return $optionsProvider->getDefaultValue($context, $this);
+        }
+
+        return $this->getDefaultValue();
     }
 }

models/DataObject/ClassDefinition/DynamicOptionsProvider/SelectOptionsProviderInterface.php
@@ -18,7 +18,22 @@ namespace Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider;
 
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 
-interface SelectOptionsProviderInterface extends MultiSelectOptionsProviderInterface
+interface SelectOptionsProviderInterface
 {
-    public function getDefaultValue(array $context, Data $fieldDefinition): ?string;
+    public function getOptions(array $context, Data $fieldDefinition): array;
+
+    /**
+     * Whether options are depending on the object context (i.e. different options for different objects) or not.
+     * This is especially important for exposing options in the object grid. For options depending on object-context
+     * there will be no batch assignment mode, and filtering can only be done through a text field instead of the
+     * options list.
+     *
+     *
+     */
+    public function hasStaticOptions(array $context, Data $fieldDefinition): bool;
+
+    /**
+     * @return string|array<string|array{value: string}>|null
+     */
+    public function getDefaultValue(array $context, Data $fieldDefinition): string|array|null;
 }

models/DataObject/ClassDefinition/DynamicOptionsProvider/MultiSelectOptionsProviderInterface.php
@@ -18,6 +18,11 @@ namespace Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider;
 
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 
+trigger_deprecation('pimcore/pimcore', '11.2', '%s is deprecated. Use %s instead.', MultiSelectOptionsProviderInterface::class, SelectOptionsProviderInterface::class);
+
+/**
+ * @deprecated use SelectOptionsProviderInterface instead
+ */
 interface MultiSelectOptionsProviderInterface
 {
     public function getOptions(array $context, Data $fieldDefinition): array;

models/DataObject/ClassDefinition/Helper/OptionsProviderResolver.php
@@ -33,8 +33,20 @@ class OptionsProviderResolver extends ClassResolver
     public static function resolveProvider(?string $providerClass, int $mode): ?object
     {
         return self::resolve($providerClass, function ($provider) use ($mode) {
+
+            if ($provider instanceof MultiSelectOptionsProviderInterface){
+                trigger_deprecation(
+                    'pimcore/pimcore',
+                    '11.2',
+                    'Implementing %s is deprecated, use %s instead',
+                    MultiSelectOptionsProviderInterface::class,
+                    SelectOptionsProviderInterface::class,
+                );
+            }
+
             return ($mode == self::MODE_SELECT && ($provider instanceof SelectOptionsProviderInterface))
-                || ($mode == self::MODE_MULTISELECT && ($provider instanceof MultiSelectOptionsProviderInterface));
+                || ($mode == self::MODE_MULTISELECT && ($provider instanceof MultiSelectOptionsProviderInterface))
+                || ($mode == self::MODE_MULTISELECT && ($provider instanceof SelectOptionsProviderInterface));
         });
     }
 }

models/DataObject/Service.php
@@ -32,6 +32,7 @@ use Pimcore\Model;
 use Pimcore\Model\DataObject;
 use Pimcore\Model\DataObject\ClassDefinition\Data\IdRewriterInterface;
 use Pimcore\Model\DataObject\ClassDefinition\Data\LayoutDefinitionEnrichmentInterface;
+use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\SelectOptionsProviderInterface;
 use Pimcore\Model\Element;
 use Pimcore\Model\Element\DirtyIndicatorInterface;
 use Pimcore\Model\Element\ElementInterface;
@@ -799,7 +800,7 @@ class Service extends Model\Element\Service
                     DataObject\ClassDefinition\Helper\OptionsProviderResolver::MODE_MULTISELECT
                 );
 
-                if (!$definition->useConfiguredOptions() && $optionsProvider instanceof DataObject\ClassDefinition\DynamicOptionsProvider\MultiSelectOptionsProviderInterface) {
+                if (!$definition->useConfiguredOptions() && $optionsProvider instanceof SelectOptionsProviderInterface) {
                     $_options = $optionsProvider->getOptions(['fieldname' => $definition->getName()], $definition);
                 } else {
                     $_options = $definition->getOptions();

commit e4fea5a022c1ecfbb2b07ae2409ad476bc6da9e1

[Bug]: Fix BC-Break: Default value on multiselect object type introduced by #15871 (#16779)
* remove parent interface is bc break

* fix bc-break reported by Niklas

* Update models/DataObject/ClassDefinition/Data/Multiselect.php

Co-authored-by: Sebastian Blank <sebastian.bl@gmx.de>

* Update models/DataObject/ClassDefinition/Data/Multiselect.php

Co-authored-by: Sebastian Blank <sebastian.bl@gmx.de>

* missing default handling for editmode

* missing default handling for editmode

* enrich definition

* enrich definition

* enrich definition

* Update models/DataObject/ClassDefinition/Data/Multiselect.php

Co-authored-by: Sebastian Blank <sebastian.bl@gmx.de>

* Fix SelectionProviderTrait (#16782)

* Fix SelectionProviderTrait

* Change unneeded getOptions() call by string

* fix getDataForQueryResource

---------

Co-authored-by: Sebastian Blank <sebastian.bl@gmx.de>
Co-authored-by: Sebastian Blank <blank@data-factory.net>

models/DataObject/ClassDefinition/Data/Multiselect.php
@@ -122,8 +122,14 @@ class Multiselect extends Data implements
         return $this->renderType;
     }
 
-    public function setDefaultValue(array $defaultValue): static
+    /**
+     * @return $this
+     */
+    public function setDefaultValue(array|string|null $defaultValue): static
     {
+        if (is_string($defaultValue)) {
+            $defaultValue = $defaultValue !== '' ? [$defaultValue] : null;
+        }
         $this->defaultValue = $defaultValue;
 
         return $this;
@@ -175,8 +181,9 @@ class Multiselect extends Data implements
      */
     public function getDataForQueryResource(mixed $data, DataObject\Concrete $object = null, array $params = []): ?string
     {
-        if (!empty($data) && is_array($data)) {
-            return ','.implode(',', $data).',';
+        $dataForResource = $this->getDataForResource($data, $object, $params);            
+        if ($dataForResource){
+            return ','.$dataForResource.',';
         }
 
         return null;
@@ -189,11 +196,7 @@ class Multiselect extends Data implements
      */
     public function getDataForEditmode(mixed $data, DataObject\Concrete $object = null, array $params = []): ?string
     {
-        if (is_array($data)) {
-            return implode(',', $data);
-        }
-
-        return null;
+        return $this->getDataForResource($data, $object, $params);
     }
 
     public function getDataForGrid(?array $data, Concrete $object = null, array $params = []): array|string|null
@@ -214,7 +217,7 @@ class Multiselect extends Data implements
         }
 
         $context['fieldname'] = $this->getName();
-        $options = $optionsProvider->{'getOptions'}($context, $this);
+        $options = $optionsProvider->getOptions($context, $this);
         $this->setOptions($options);
 
         if (isset($params['purpose']) && $params['purpose'] === 'editmode') {

models/DataObject/ClassDefinition/Data/Select.php
@@ -296,7 +296,7 @@ class Select extends Data implements
             }
 
             $context['fieldname'] = $this->getName();
-            $options = $optionsProvider->{'getOptions'}($context, $this);
+            $options = $optionsProvider->getOptions($context, $this);
             $this->setOptions($options);
 
             if (isset($params['purpose']) && $params['purpose'] == 'editmode') {

models/DataObject/ClassDefinition/DynamicOptionsProvider/SelectOptionsProviderInterface.php
@@ -18,7 +18,7 @@ namespace Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider;
 
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 
-interface SelectOptionsProviderInterface
+interface SelectOptionsProviderInterface extends MultiSelectOptionsProviderInterface
 {
     public function getOptions(array $context, Data $fieldDefinition): array;
 

models/DataObject/ClassDefinition/DynamicOptionsProvider/SelectionProviderTrait.php
@@ -24,7 +24,7 @@ use Pimcore\Model\DataObject\ClassDefinition\Data;
  */
 trait SelectionProviderTrait
 {
-    protected function doEnrichDefinitionDefinition(/*?Concrete */ ?DataObject\Concrete $object, string $fieldname, string $purpose, int $mode, /**  array */ array $context = []): void
+    protected function doEnrichDefinitionDefinition(?DataObject\Concrete $object, string $fieldname, string $purpose, int $mode, array $context = []): void
     {
         if ($this->getOptionsProviderType() === Data\OptionsProviderInterface::TYPE_CONFIGURE) {
             return;
@@ -45,18 +45,17 @@ trait SelectionProviderTrait
                 $context['purpose'] = $purpose;
             }
 
-            $options = DataObject\Service::useInheritedValues(true,
-                fn () => $optionsProvider->getOptions($context, $this),
+            $options = DataObject\Service::useInheritedValues(
+                true,
+                fn () => $optionsProvider->getOptions($context, $this)
             );
 
             $this->setOptions($options);
 
-            if ($this instanceof Data\Select) {
-                $defaultValue = $optionsProvider->{'getDefaultValue'}($context, $this);
-                $this->setDefaultValue($defaultValue);
-            }
+            $defaultValue = $optionsProvider->getDefaultValue($context, $this);
+            $this->setDefaultValue($defaultValue);
 
-            $hasStaticOptions = $optionsProvider->{'hasStaticOptions'}($context, $this);
+            $hasStaticOptions = $optionsProvider->hasStaticOptions($context, $this);
             $this->dynamicOptions = !$hasStaticOptions;
         }
     }