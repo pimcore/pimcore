15ee6ea92f79e544f6d5c51a4e5f3202abbe03d9
robertSt7 <robert.steinkellner@pimcore.com>
2024-04-17 16:25:01 +0200

    Fix: tinymce remove insertdatetime plugin

--- bundles/TinymceBundle/public/build/tinymce/tinymce.js
+++ bundles/TinymceBundle/public/build/tinymce/tinymce.js
@@ -1,1 +1,1 @@
-"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[774],{596:(e,s,u)=>{u(5199),u(7726),u(7741),u(6075),u(378),u(1694),u(5081),u(2205),u(5791),u(8022),u(7426),u(2073),u(5775),u(3847)}},e=>{e.O(0,[328],(()=>{return s=596,e(e.s=s);var s}));e.O()}]);
+"use strict";(self.webpackChunk=self.webpackChunk||[]).push([[774],{596:(e,s,u)=>{u(5199),u(7726),u(7741),u(6075),u(378),u(1694),u(5081),u(2205),u(5791),u(7426),u(2073),u(5775),u(3847)}},e=>{e.O(0,[999],(()=>{return s=596,e(e.s=s);var s}));e.O()}]);
