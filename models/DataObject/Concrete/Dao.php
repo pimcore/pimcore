fba5837bc6ecfce79361d1366d5fdf3d9fbf3494
JiaJia Ji <kingjia90@gmail.com>
2024-04-30 14:24:03 +0200

    this quote seems unneccessary

--- models/DataObject/Concrete/Dao.php
+++ models/DataObject/Concrete/Dao.php
@@ -208,7 +208,7 @@ class Dao extends Model\DataObject\AbstractObject\Dao
             $untouchables = "'" . implode("','", $untouchable) . "'";
             $condition = Helper::quoteInto($this->db, 'src_id = ? AND fieldname not in (' . $untouchables . ") AND ownertype = 'object'", $this->model->getId());
         } else {
-            $condition = 'src_id = ' . $db->quote($this->model->getId()) . ' AND ownertype = "object"';
+            $condition = 'src_id = ' . $this->model->getId() . ' AND ownertype = "object"';
         }
 
         if (!DataObject::isDirtyDetectionDisabled()) {
