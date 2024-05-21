f3b79495d7167cbc5b4eae7b550b806087a103e6
JiaJia Ji <kingjia90@gmail.com>
2024-05-21 11:56:44 +0200

    [Bug]: Fix Application logger datetime filter (#17067)

--- bundles/ApplicationLoggerBundle/public/js/log/admin.js
+++ bundles/ApplicationLoggerBundle/public/js/log/admin.js
@@ -270,28 +270,28 @@ pimcore.bundle.applicationlogger.log.admin = Class.create({
             this.fromDate = new Ext.form.DateField({
                 name: 'from_date',
                 width: 130,
-                invalidText: 'Enter date as MM/DD/YYYY',
+                format: "Y-m-d",
                 xtype: 'datefield'
             });
 
             this.fromTime = new Ext.form.TimeField({
                 name: 'from_time',
                 width: 100,
-                invalidText: 'Enter time as HH:MM AM/PM',
+                format: "H:i",
                 xtype: 'timefield'
             });
 
             this.toDate = new Ext.form.DateField({
                 name: 'to_date',
                 width: 130,
-                invalidText: 'Enter date as MM/DD/YYYY',
+                format: "Y-m-d",
                 xtype: 'datefield'
             });
 
             this.toTime = new Ext.form.TimeField({
                 name: 'to_time',
                 width: 100,
-                invalidText: 'Enter time as HH:MM AM/PM',
+                format: "H:i",
                 xtype: 'timefield'
             });
 
