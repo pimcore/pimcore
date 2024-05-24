be84f3a818fb36ce1946daf433fa6726011af9f6
JiaJia Ji <kingjia90@gmail.com>
2024-05-24 15:47:22 +0200

    [Task]: PhpStan Version Upgrade May (#17044)

--- lib/Helper/Mail.php
+++ lib/Helper/Mail.php
@@ -229,30 +229,30 @@ CSS;
 
         //matches all links
         preg_match_all("@(href|src)\s*=[\"']([^(http|mailto|javascript|data:|#)].*?(css|jpe?g|gif|png)?)[\"']@is", $string, $matches);
-        if (!empty($matches[0])) {
-            foreach ($matches[0] as $key => $value) {
-                $path = $matches[2][$key];
 
-                if (str_starts_with($path, '//')) {
-                    $absolutePath = 'http:' . $path;
-                } elseif (str_starts_with($path, '/')) {
-                    $absolutePath = preg_replace('@^' . $replacePrefix . '(/(.*))?$@', '/$2', $path);
-                    $absolutePath = $hostUrl . $absolutePath;
-                } elseif (str_starts_with($path, 'file://')) {
-                    continue;
-                } else {
-                    $absolutePath = $hostUrl . "/$path";
-                    if ($path[0] == '?') {
-                        $absolutePath = $hostUrl . $document . $path;
-                    }
-                    $netUrl = new \Net_URL2($absolutePath);
-                    $absolutePath = $netUrl->getNormalizedURL();
+        foreach ($matches[0] as $key => $value) {
+            $path = $matches[2][$key];
+
+            if (str_starts_with($path, '//')) {
+                $absolutePath = 'http:' . $path;
+            } elseif (str_starts_with($path, '/')) {
+                $absolutePath = preg_replace('@^' . $replacePrefix . '(/(.*))?$@', '/$2', $path);
+                $absolutePath = $hostUrl . $absolutePath;
+            } elseif (str_starts_with($path, 'file://')) {
+                continue;
+            } else {
+                $absolutePath = $hostUrl . "/$path";
+                if ($path[0] == '?') {
+                    $absolutePath = $hostUrl . $document . $path;
                 }
-
-                $path = preg_quote($path, '!');
-                $string = preg_replace("!([\"'])$path([\"'])!is", '\\1' . $absolutePath . '\\2', $string);
+                $netUrl = new \Net_URL2($absolutePath);
+                $absolutePath = $netUrl->getNormalizedURL();
             }
+
+            $path = preg_quote($path, '!');
+            $string = preg_replace("!([\"'])$path([\"'])!is", '\\1' . $absolutePath . '\\2', $string);
         }
+        
 
         preg_match_all("@srcset\s*=[\"'](.*?)[\"']@is", $string, $matches);
         foreach ($matches[1] as $i => $value) {
@@ -289,7 +289,7 @@ CSS;
 
         //matches all <link> Tags
         preg_match_all("@<link.*?href\s*=\s*[\"'](.*?)[\"'].*?(/?>|</\s*link>)@is", $string, $matches);
-        if (!empty($matches[0])) {
+        if ($matches[0]) {
             $css = '';
 
             foreach ($matches[0] as $key => $value) {
@@ -339,22 +339,20 @@ CSS;
     {
         preg_match_all("@url\s*\(\s*[\"']?(.*?)[\"']?\s*\)@is", $content, $matches);
         $hostUrl = Tool::getHostUrl();
+    
+        foreach ($matches[0] as $key => $value) {
+            $fullMatch = $matches[0][$key];
+            $path = $matches[1][$key];
 
-        if (is_array($matches[0])) {
-            foreach ($matches[0] as $key => $value) {
-                $fullMatch = $matches[0][$key];
-                $path = $matches[1][$key];
-
-                if ($path[0] == '/') {
-                    $imageUrl = $hostUrl . $path;
-                } else {
-                    $imageUrl = dirname($fileInfo['fileUrlNormalized']) . "/$path";
-                    $netUrl = new \Net_URL2($imageUrl);
-                    $imageUrl = $netUrl->getNormalizedURL();
-                }
-
-                $content = str_replace($fullMatch, ' url(' . $imageUrl . ') ', $content);
+            if ($path[0] == '/') {
+                $imageUrl = $hostUrl . $path;
+            } else {
+                $imageUrl = dirname($fileInfo['fileUrlNormalized']) . "/$path";
+                $netUrl = new \Net_URL2($imageUrl);
+                $imageUrl = $netUrl->getNormalizedURL();
             }
+
+            $content = str_replace($fullMatch, ' url(' . $imageUrl . ') ', $content);
         }
 
         return $content;
