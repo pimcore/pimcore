8cf2537fe9a699256cd95e2773c55b0ad6898680
markus-moser <markus.moser@pimcore.com>
2024-06-03 15:23:44 +0200

    Add a mechanism to define the user's timezone for the version preview and CSV exports

---
+++ lib/Tool/UserTimezone.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+namespace Pimcore\Tool;
+
+use Pimcore\Logger;
+
+/**
+ * @internal
+ */
+final class UserTimezone
+{
+    private static ?string $userTimezone = null;
+
+    public static function setUserTimezone(?string $userTimezone): void
+    {
+        if ($userTimezone !== null && !in_array($userTimezone, timezone_identifiers_list())) {
+            Logger::error('Invalid user timezone: ' . $userTimezone);
+            $userTimezone = null;
+        }
+        self::$userTimezone = $userTimezone;
+    }
+
+    public static function getUserTimezone(): ?string
+    {
+        return self::$userTimezone;
+    }
+
+    public static function applyTimezone(\DateTimeInterface $date): \DateTimeInterface
+    {
+        if (self::$userTimezone) {
+            $date->setTimezone(new \DateTimeZone(self::$userTimezone));
+        }
+        return $date;
+    }
+}
