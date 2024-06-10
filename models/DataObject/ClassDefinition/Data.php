c905c4ba6fa41e88b380767ae65c04f0deaa65f2
Sebastian Blank <blank@data-factory.net>
2024-06-10 09:01:37 +0200

    Add validtablecolumns

--- models/DataObject/ClassDefinition/Data.php
+++ models/DataObject/ClassDefinition/Data.php
@@ -85,7 +85,7 @@ abstract class Data implements DataObject\ClassDefinition\Data\TypeDeclarationSu
         'objectvar', 'objectvars', 'omitmandatorycheck', 'parent', 'parentclass', 'parentid', 'path', 'permissions',
         'permissionsforuser', 'properties', 'property', 'published', 'realfullpath', 'realpath', 'relationdata',
         'resource', 'scheduledtasks', 'siblings', 'type', 'types', 'usermodification', 'userowner', 'userpermissions',
-        'value', 'valueforfieldname', 'valuefromparent', 'values', 'versioncount', 'versions',
+        'validtablecolumns', 'value', 'valueforfieldname', 'valuefromparent', 'values', 'versioncount', 'versions',
     ];
 
     /**
