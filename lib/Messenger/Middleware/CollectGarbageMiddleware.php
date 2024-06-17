d0b124ff747be3d3efb4c73840d5277d30ccabb3
Marco Perberschlager <marco.perberschlager@pimcore.com>
2024-06-17 16:37:42 +0200

    17181 Added CollectGarbageMiddleware.php

---
+++ lib/Messenger/Middleware/CollectGarbageMiddleware.php
@@ -0,0 +1,36 @@
+<?php
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Messenger\Middleware;
+
+use Pimcore;
+use Pimcore\Bundle\GenericDataIndexBundle\Traits\LoggerAwareTrait;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\Middleware\MiddlewareInterface;
+use Symfony\Component\Messenger\Middleware\StackInterface;
+
+class CollectGarbageMiddleware implements MiddlewareInterface
+{
+    use LoggerAwareTrait;
+
+    public function handle(Envelope $envelope, StackInterface $stack): Envelope
+    {
+        $this->logger->debug('Execute collect garbage middleware');
+        Pimcore::collectGarbage();
+
+        return $stack->next()->handle($envelope, $stack);
+    }
+}
