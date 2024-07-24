6c69fbaae3f2a853d5c1445267c5925b28042a55
JiaJia Ji <kingjia90@gmail.com>
2024-07-24 10:43:13 +0200

    reduce queries in tags get children by id

--- models/Element/Tag.php
+++ models/Element/Tag.php
@@ -16,10 +16,12 @@ declare(strict_types=1);
 
 namespace Pimcore\Model\Element;
 
+use Pimcore\Cache\RuntimeCache;
 use Pimcore\Event\Model\TagEvent;
 use Pimcore\Event\TagEvents;
 use Pimcore\Event\Traits\RecursionBlockingEventDispatchHelperTrait;
 use Pimcore\Model;
+use Pimcore\Model\Exception\NotFoundException;
 
 /**
  * @method \Pimcore\Model\Element\Tag\Dao getDao()
@@ -70,14 +72,19 @@ final class Tag extends Model\AbstractModel
      */
     public static function getById(int $id): ?Tag
     {
+        $cacheKey = 'tags_' . $id;
         try {
-            $tag = new self();
-            $tag->getDao()->getById($id);
-
-            return $tag;
-        } catch (Model\Exception\NotFoundException $e) {
-            return null;
+            $tag = RuntimeCache::get($cacheKey);
+        } catch (\Exception $ex) {
+            try {
+                $tag = new self();
+                $tag->getDao()->getById($id);
+                RuntimeCache::set($cacheKey, $tag);
+            } catch (NotFoundException $e) {
+                return null;
+            }
         }
+        return $tag;
     }
 
     /**
