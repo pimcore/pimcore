ecd3d6381d02e6942f5373bd7372545ba3598321
kingjia90 <kingjia90@users.noreply.github.com>
2024-08-16 07:10:44 +0000

    Apply php-cs-fixer changes

--- lib/Tool/Text.php
+++ lib/Tool/Text.php
@@ -82,7 +82,7 @@ class Text
 
                             $site = Frontend::getSiteForDocument($element);
                             if ($site instanceof Site) {
-                                if (preg_match('~^' . preg_quote($site->getRootPath(), '~') . '~', $path)){
+                                if (preg_match('~^' . preg_quote($site->getRootPath(), '~') . '~', $path)) {
                                     $path = Tool::getRequestScheme() . '://' . $site->getMainDomain() . preg_replace('~^' . preg_quote($site->getRootPath(), '~') . '~', '', $path);
                                 }
                             }
