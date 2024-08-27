e2cd26195fe36a6be3e7a8e922e05e8a126498bd
Alex Rothberg <agrothberg@gmail.com>
2024-08-27 01:43:08 -0400

    Gracefully handle cases in which the storage adapter fails to calculate a file's checksum. (#17519)
    * Gracefully handle cases in which the storage adapter fails to calculate a file's checksum.
    
    * Apply php-cs-fixer changes
    
    * log error
    
    * Apply php-cs-fixer changes
    
    * Consistency check
    
    ---------
    
    Co-authored-by: cancan101 <cancan101@users.noreply.github.com>
    Co-authored-by: Matthias Schuhmayer <38959016+mattamon@users.noreply.github.com>

--- models/Asset.php
+++ models/Asset.php
@@ -22,6 +22,7 @@ use function is_array;
 use League\Flysystem\FilesystemException;
 use League\Flysystem\FilesystemOperator;
 use League\Flysystem\UnableToMoveFile;
+use League\Flysystem\UnableToProvideChecksum;
 use League\Flysystem\UnableToRetrieveMetadata;
 use Pimcore;
 use Pimcore\Cache;
@@ -1145,7 +1146,8 @@ class Asset extends Element\AbstractElement
             $checksum = $this->getCustomSetting('checksum');
         }
 
-        return $checksum;
+        // generateChecksum may fail to set the checksum, in which case we fall back to empty string.
+        return $checksum ?? '';
     }
 
     /**
@@ -1153,7 +1155,15 @@ class Asset extends Element\AbstractElement
      */
     public function generateChecksum(): void
     {
-        $this->setCustomSetting('checksum', Storage::get('asset')->checksum($this->getRealFullPath()));
+        try {
+            $this->setCustomSetting('checksum', Storage::get('asset')->checksum($this->getRealFullPath()));
+        } catch (UnableToProvideChecksum $e) {
+            // There are circumstances in which the adapter is unable to calculate the checksum for a given file.
+            // In those cases, we ignore the exception.
+            Logger::error((string) $e);
+
+            return;
+        }
         $this->getDao()->updateCustomSettings();
     }
 
