0903abfc9fe74ae6c5c13ea16f4bc975134296f5
Alex Rothberg <agrothberg@gmail.com>
2024-08-26 22:47:00 -0400

    log error

--- models/Asset.php
+++ models/Asset.php
@@ -1159,6 +1159,7 @@ class Asset extends Element\AbstractElement
         } catch(UnableToProvideChecksum $e) {
             // There are circumstances in which the adapter is unable to calculate the checksum for a given file.
             // In those cases, we ignore the exception.
+            Logger::error((string) $e);
             return;
         }
         $this->getDao()->updateCustomSettings();
