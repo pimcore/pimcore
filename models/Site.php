e9a64ff565243bbec40e9cf03513ae31d4af4379
Sebastian Blank <blank@data-factory.net>
2024-10-17 13:23:07 +0200

    Fix sonarcloud

--- models/Site.php
+++ models/Site.php
@@ -234,7 +234,10 @@ final class Site extends AbstractModel
             array_map(static function ($domain) {
                 //replace all wildcards with a placeholder dummy string
                 $wildCardLessDomain = str_replace('*', 'anystring', $domain);
-                if ($wildCardLessDomain && !filter_var(idn_to_ascii($wildCardLessDomain), FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)) {
+                if (
+                    $wildCardLessDomain &&
+                    !filter_var(idn_to_ascii($wildCardLessDomain), FILTER_VALIDATE_DOMAIN, FILTER_FLAG_HOSTNAME)
+                ) {
                     throw new InvalidArgumentException(sprintf('Invalid domain name "%s"', $domain));
                 }
             }, $domains);
