97518925e2d5c414669b0d6f03bbe84453021715
mattamon <matthias.schuhmayer@pimcore.com>
2025-02-18 07:02:13 +0100

    Remove trigger_deprecation, since you cannot resolve the deprecation other by upgrading to new major version

--- lib/Workflow/Manager.php
+++ lib/Workflow/Manager.php
@@ -208,12 +208,6 @@ class Manager
      */
     public function getWorkflowByName(string $workflowName): ?object
     {
-        trigger_deprecation(
-            'pimcore/pimcore',
-            '11.5.4',
-            'Method will return ?WorkflowInterface in 12.0.0'
-        );
-
         $config = $this->getWorkflowConfig($workflowName);
 
         return Pimcore::getContainer()->get($config->getType() . '.' . $workflowName);
