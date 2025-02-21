2e7d7ad7d52138aa667d3878a55f43d5db22e3c0
hitsoul11 <rk@hitcom.de>
2025-02-21 14:57:12 +0100

    #16814: support for preGetData on data types within block elements

--- models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
+++ models/DataObject/ClassDefinition/Data/ManyToManyRelation.php
@@ -463,7 +463,7 @@ class ManyToManyRelation extends AbstractRelations implements QueryResourcePersi
                     $container->markFieldDirty($this->getName(), false);
                 }
             }
-        } elseif ($container instanceof DataObject\Localizedfield) {
+        } elseif ($container instanceof DataObject\Localizedfield || $container instanceof DataObject\Data\BlockElement) {
             $data = $params['data'];
         } elseif ($container instanceof DataObject\Fieldcollection\Data\AbstractData) {
             parent::loadLazyFieldcollectionField($container);
