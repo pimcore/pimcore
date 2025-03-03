f99bce7ec299bc73a802974f3e762c1228f313e4
mtroeger <m.troeger@neusta.de>
2025-03-03 14:22:33 +0100

    [Improvement]: Eventlistener for customizing tags on full page cache entries #18164 * New Event "prepareTags" added. * Event included in FullPageCacheListener.php. * Definition of full page cache events extended.

---
+++ lib/Event/Cache/FullPage/PrepareTagsEvent.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+/**
+ * Pimcore
+ *
+ * This source file is available under two different licenses:
+ * - GNU General Public License version 3 (GPLv3)
+ * - Pimcore Commercial License (PCL)
+ * Full copyright and license information is available in
+ * LICENSE.md which is distributed with this source code.
+ *
+ *  @copyright  Copyright (c) Pimcore GmbH (http://www.pimcore.org)
+ *  @license    http://www.pimcore.org/license     GPLv3 and PCL
+ */
+
+namespace Pimcore\Event\Cache\FullPage;
+
+use Symfony\Contracts\EventDispatcher\Event;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+
+class PrepareTagsEvent extends Event
+{
+    /**
+     * Tags associated with the cache entry
+     *
+     * @var string[]
+     */
+    private array $tags = [];
+
+    public function __construct(
+        private readonly Request $request,
+        private readonly Response $response
+    )  {
+    }
+
+    public function addTag(string $tag): void
+    {
+        $this->tags[] = $tag;
+    }
+    /**
+     * @return string[]
+     */
+    public function getTags(): array
+    {
+        return $this->tags;
+    }
+
+    /**
+     * @param string[] $tags
+     */
+    public function setTags(array $tags): void
+    {
+        $this->tags = $tags;
+    }
+
+    public function getRequest(): Request
+    {
+        return $this->request;
+    }
+
+    public function getResponse(): Response
+    {
+        return $this->response;
+    }
+
+}
