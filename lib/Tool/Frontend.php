21ff009c6446d4ad9f20089f00756a4b26515dae
Sebastian Blank <blank@data-factory.net>
2025-03-14 08:57:16 +0100

    Fix: Frontend::isDocumentInSite() returns false for site root document

--- lib/Tool/Frontend.php
+++ lib/Tool/Frontend.php
@@ -29,7 +29,7 @@ final class Frontend
         $inSite = true;
 
         if ($site && $site->getRootDocument() instanceof Document\Page) {
-            if (!str_starts_with($document->getRealFullPath(), $site->getRootDocument()->getRealFullPath() . '/')) {
+            if (!str_starts_with($document->getRealFullPath() . '/', $site->getRootDocument()->getRealFullPath() . '/')) {
                 $inSite = false;
             }
         }
