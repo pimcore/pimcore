25413544a7a882443e14b487f80cae3cbd1c7325
JiaJia Ji <kingjia90@gmail.com>
2025-03-14 13:35:32 +0100

    test again

--- models/Asset/Listing/Dao.php
+++ models/Asset/Listing/Dao.php
@@ -97,7 +97,9 @@ class Dao extends Model\Listing\Dao\AbstractDao
         $this->prepareQueryBuilderForTotalCount($queryBuilder, $identifierColumn);
 
         if ($this->isQueryBuilderPartInUse($queryBuilder, 'groupBy') || $this->isQueryBuilderPartInUse($queryBuilder, 'having')) {
-            $queryBuilder->select($identifierColumn);
+            if (!$this->isQueryBuilderPartInUse($queryBuilder, 'select')){
+               $queryBuilder->select($identifierColumn);
+            }
             return (int)$this->db->fetchOne('SELECT COUNT(*)  FROM (' . $queryBuilder->getSQL() . ') as XYZ');
         } else {
             return (int)$this->db->fetchOne($queryBuilder->getSql(), $queryBuilder->getParameters(), $queryBuilder->getParameterTypes());
