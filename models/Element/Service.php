f241fa70f5ead21b05b6ace5ff8cfdaa8659751c
mattamon <matthias.schuhmayer@pimcore.com>
2025-03-17 08:55:29 +0100

    Remove comments

--- models/Element/Service.php
+++ models/Element/Service.php
@@ -919,17 +919,6 @@ class Service extends Model\AbstractModel
                     $select->andWhere($where);
                 }
 
-
-                /*$query = (string)$select;
-                $pattern = "/(?i)\bFROM\s+(?:(\(\s*SELECT.*?\))\s+AS\s+(\w+)|(`?\w+`?)(?:\s+(?:AS\s+)?(\w+))?)/";
-
-                $fromAlias = '';
-                if (preg_match($pattern, $query, $matches)) {
-                    $fromAlias = !empty($matches[2]) ? $matches[2] : (!empty($matches[4]) ? $matches[4] : $matches[3]);
-                }*/
-
-                //$fromAlias = $select->getQueryPart('from')[0]['alias'] ?? $select->getQueryPart('from')[0]['table'] ;
-
                 $customViewJoins = $cv['joins'] ?? null;
                 if ($customViewJoins) {
                     foreach ($customViewJoins as $joinConfig) {
