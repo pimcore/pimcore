pimcore/pimcore

commit 445af445d92828b63c897ceaf5b6165b796ed62e

[Bug, EC][PEES-579]: Fix select options translation (#18471)

bundles/CoreBundle/src/OptionsProvider/SelectOptionsOptionsProvider.php
@@ -14,10 +14,12 @@ declare(strict_types=1);
 namespace Pimcore\Bundle\CoreBundle\OptionsProvider;
 
 use Exception;
+use Pimcore;
 use Pimcore\Model\DataObject\ClassDefinition\Data;
 use Pimcore\Model\DataObject\ClassDefinition\DynamicOptionsProvider\SelectOptionsProviderInterface;
 use Pimcore\Model\DataObject\SelectOptions\Config;
 use Pimcore\Model\DataObject\SelectOptions\Data\SelectOption;
+use Pimcore\Tool\Admin;
 
 class SelectOptionsOptionsProvider implements SelectOptionsProviderInterface
 {
@@ -33,10 +35,14 @@ class SelectOptionsOptionsProvider implements SelectOptionsProviderInterface
             throw new Exception('Missing select options configuration ' . $configurationId, 1677137682677);
         }
 
+        $translator = Pimcore::getContainer()->get('translator');
+        $currentUserLocale = Admin::getCurrentUser()->getLanguage();
+        $translator->setLocale($currentUserLocale);
+
         return array_map(
             fn (SelectOption $selectOption) => [
                 'value' => $selectOption->getValue(),
-                'key' => $selectOption->getLabel(),
+                'key' => $translator->trans($selectOption->getLabel(), [], 'admin'),
             ],
             $selectOptionsConfiguration->getSelectOptions(),
         );

commit 4e55d3d2f43c7be6caa8921ebfa23d594eb7f672

Fix select options translation regression caused by #18471 Admin user shouldn't be required, and Admin::getCurrentUser() can return null

bundles/CoreBundle/src/OptionsProvider/SelectOptionsOptionsProvider.php
@@ -36,8 +36,10 @@ class SelectOptionsOptionsProvider implements SelectOptionsProviderInterface
         }
 
         $translator = Pimcore::getContainer()->get('translator');
-        $currentUserLocale = Admin::getCurrentUser()->getLanguage();
-        $translator->setLocale($currentUserLocale);
+        $currentUserLocale = Admin::getCurrentUser()?->getLanguage();
+        if($currentUserLocale) {
+            $translator->setLocale($currentUserLocale);
+        }
 
         return array_map(
             fn (SelectOption $selectOption) => [