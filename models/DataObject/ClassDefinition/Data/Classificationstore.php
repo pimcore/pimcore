7a073c5dcdd58df194c4225feeb7b7caf6f70461
valantic-cx-alps <110961873+valantic-cx-alps@users.noreply.github.com>
2025-06-30 16:11:45 +0200

    Fixed dirty detection of classification store

--- models/DataObject/ClassDefinition/Data/Classificationstore.php
+++ models/DataObject/ClassDefinition/Data/Classificationstore.php
@@ -27,7 +27,7 @@ use Pimcore\Model\DataObject\Localizedfield;
 use Pimcore\Normalizer\NormalizerInterface;
 use Pimcore\Tool;
 
-class Classificationstore extends Data implements CustomResourcePersistingInterface, TypeDeclarationSupportInterface, NormalizerInterface, PreGetDataInterface, LayoutDefinitionEnrichmentInterface, VarExporterInterface, ClassSavedInterface
+class Classificationstore extends Data implements CustomResourcePersistingInterface, TypeDeclarationSupportInterface, NormalizerInterface, PreGetDataInterface, LayoutDefinitionEnrichmentInterface, VarExporterInterface, ClassSavedInterface, EqualComparisonInterface
 {
     use DataObject\Traits\DataHeightTrait;
     use DataObject\Traits\DataWidthTrait;
@@ -1150,4 +1150,26 @@ class Classificationstore extends Data implements CustomResourcePersistingInterf
     {
         return 'classificationstore';
     }
+
+    public function isEqual(mixed $oldValue, mixed $newValue): bool
+    {
+        // Check if they are the same reference
+        if ($oldValue === $newValue) {
+            return true;
+        }
+
+        // Check if related objects are the same
+        if ($oldValue->getObject()->getId() !== $newValue->getObject()->getId()) {
+            return false;
+        }
+
+        // Check if they are of the same type
+        if (get_class($oldValue) !== get_class($newValue)) {
+            return false;
+        }
+
+        // Compare items
+        return serialize($oldValue->getItems()) === serialize($newValue->getItems());
+    }
+
 }
