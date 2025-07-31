57c87a510b72d4a57b5d2a84594915b6f56577fc
JiaJia Ji <kingjia90@gmail.com>
2025-07-31 10:14:28 +0200

    Update Dao.php

--- bundles/SeoBundle/src/Model/Redirect/Dao.php
+++ bundles/SeoBundle/src/Model/Redirect/Dao.php
@@ -61,7 +61,7 @@ class Dao extends Model\Dao\AbstractDao
                 (source = :sourceEntireUri AND `type` = :typeEntireUri)
             ) AND active = 1 AND (regex IS NULL OR regex = 0) AND (expiry > UNIX_TIMESTAMP() OR expiry IS NULL)';
 
-        $sql .= ' AND (sourceSite IS NULL OR sourceSite = '.$siteId ?? 0.')';
+        $sql .= ' AND (sourceSite IS NULL OR sourceSite = '. ($siteId ?? 0) .')';
 
 
         if ($override) {
