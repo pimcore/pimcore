5419a6decf0a6ce03b57ebe2ba8944af908e4ab9
JiaJia Ji <kingjia90@gmail.com>
2025-08-13 07:10:16 +0200

    add suppress_errors

--- lib/Mail.php
+++ lib/Mail.php
@@ -66,6 +66,7 @@ class Mail extends Email
      * @var array<string, mixed>
      */
     private array $html2textOptions = [
+        'suppress_errors' => true,
         'hard_break' => true,
         'strip_tags' => true,
         'remove_nodes' => 'head style',
