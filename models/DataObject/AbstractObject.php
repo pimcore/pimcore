60c8fe230233412c3e58327295db670378de9746
Ji Jia Jia <kingjia90@gmail.com>
2025-09-01 11:30:48 +0200

    stanfix

--- models/DataObject/AbstractObject.php
+++ models/DataObject/AbstractObject.php
@@ -554,14 +554,12 @@ abstract class AbstractObject extends Model\Element\AbstractElement
             ) {
                 $additionalTags = [];
 
-                if (isset($updatedChildren)) {
-                    foreach ($updatedChildren as $objectId) {
-                        $tag = 'object_' . $objectId;
-                        $additionalTags[] = $tag;
+                foreach ($updatedChildren as $objectId) {
+                    $tag = 'object_' . $objectId;
+                    $additionalTags[] = $tag;
 
-                        // remove the child also from registry (internal cache) to avoid path inconsistencies during long running scripts, such as CLI
-                        RuntimeCache::set($tag, null);
-                    }
+                    // remove the child also from registry (internal cache) to avoid path inconsistencies during long running scripts, such as CLI
+                    RuntimeCache::set($tag, null);
                 }
 
                 $this->clearDependentCache($additionalTags);
