f00d2a437b6eef6ef4766123ec02fc04d4bd36ef
Ji Jia Jia <kingjia90@gmail.com>
2025-10-13 09:43:00 +0200

    autodetect previously grid config with filter saved

--- bundles/CoreBundle/src/Migrations/Version20250703080911.php
+++ bundles/CoreBundle/src/Migrations/Version20250703080911.php
@@ -29,10 +29,10 @@ final class Version20250703080911 extends AbstractMigration
         $table = $schema->getTable('gridconfigs');
         if(!$table->hasColumn('saveFilters')) {
             $this->addSql('ALTER TABLE gridconfigs ADD saveFilters TINYINT(1) DEFAULT 0 NOT NULL');
+            //detect if any filter was already set and set to saveFilters to true
+            $this->addSql("UPDATE gridconfigs SET `saveFilters` = 1 WHERE `config` LIKE '%filter\":[{\"%'");
         }
 
-        //detect if any filter was already set and set to saveFilters to true
-        Db::get()->executeQuery("UPDATE gridconfigs SET saveFilters = 1 WHERE `config` LIKE '%filter\":[{\"%'");
     }
 
     public function down(Schema $schema): void
