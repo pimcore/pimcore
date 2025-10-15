a864dc6dbe3a31146c58722ed8ef53569d36e555
Ji Jia Jia <kingjia90@gmail.com>
2025-10-15 15:25:20 +0200

    fix backtick removal

--- models/DataObject/ClassDefinition/Data/Relations/ManyToManyRelationTrait.php
+++ models/DataObject/ClassDefinition/Data/Relations/ManyToManyRelationTrait.php
@@ -97,11 +97,20 @@ trait ManyToManyRelationTrait
             // The brick prefix might be quoted and with a dot suffix, if so, removing the first
             // and second last character to unquote
             $quoteIdentifierSymbol  = substr(Db::get()->quoteIdentifier(''), 0, 1);
+
             if (
-                substr($prefix, 1, -2) === $quoteIdentifierSymbol &&
+                substr($prefix, 0, 1) === $quoteIdentifierSymbol &&
+                substr($prefix, -2, 1) === $quoteIdentifierSymbol &&
+                substr($prefix, -1) === '.'
+            ) {
+                // Case: `db`.
+                $prefix = substr($prefix, 1, -2) . '.';
+            } elseif (
+                substr($prefix, 0, 1) === $quoteIdentifierSymbol &&
                 substr($prefix, -1) === $quoteIdentifierSymbol
             ) {
-                $prefix = substr($prefix, 1, -2) . substr($prefix, -1);
+                // Case: `db`
+                $prefix = substr($prefix, 1, -1);
             }
         }
 
