d94d3e1654812d73729b9eb372ef34253c84a55b
rabume <info@rabu.me>
2025-11-23 21:40:52 +0100

    add 'referrerpolicy' property to configurations for YouTube videos;
    - fixes error 153

--- models/Document/Editable/Video.php
+++ models/Document/Editable/Video.php
@@ -661,8 +661,13 @@ class Video extends Model\Document\Editable implements IdRewriterInterface
             }
         }
 
+        $referrerPolicy = 'strict-origin-when-cross-origin';
+        if (isset($configurations['referrerpolicy'])) {
+            $referrerPolicy = $configurations['referrerpolicy'];
+        }
+
         $code .= '<div id="pimcore_video_' . $this->getName() . '" class="pimcore_editable_video '. ($config['class'] ?? '') .'">
-            <iframe width="' . $width . '" height="' . $height . '" src="https://www.youtube-nocookie.com/embed/' . $seriesPrefix . $youtubeId . $wmode . $additionalParams . '" title="YouTube video" allow="fullscreen" data-type="pimcore_video_editable"></iframe>
+            <iframe width="' . $width . '" height="' . $height . '" src="https://www.youtube-nocookie.com/embed/' . $seriesPrefix . $youtubeId . $wmode . $additionalParams . '" title="YouTube video" allow="fullscreen" referrerpolicy="' . $referrerPolicy . '" data-type="pimcore_video_editable"></iframe>
         </div>';
 
         return $code;
