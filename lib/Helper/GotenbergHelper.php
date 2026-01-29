4076dc80773a6c11d050df0bcf2a6a0bfbd3f7bd
Ji Jia Jia <kingjia90@gmail.com>
2026-01-29 09:25:15 +0100

    Applying suggestions, fix typo related to temporary test
    Co-authored-by: Peter van der Wal <peter.vanderwal@youweagency.com>

--- lib/Helper/GotenbergHelper.php
+++ lib/Helper/GotenbergHelper.php
@@ -14,7 +14,6 @@ declare(strict_types=1);
 namespace Pimcore\Helper;
 
 use Exception;
-use Gotenberg\Gotenberg as GotenbergAPI;
 use Pimcore\Cache;
 use Pimcore\Config;
 
@@ -23,14 +22,18 @@ use Pimcore\Config;
  */
 class GotenbergHelper
 {
-    private static bool $validPing = false;
+    private static ?bool $validPing = null;
+
+    private const CACHE_KEY = 'gotenberg_ping';
+    private const STATUS_AVAILABLE = 'available';
+    private const STATUS_UNAVAILABLE = 'unavailable';
 
     private static function healthPing(): bool
     {
         $gotenbergBaseUrl = Config::getSystemConfiguration('gotenberg')['base_url'];
         if ($gotenbergBaseUrl) {
             try {
-                $ch = curl_init(rtrim($gotenbergBaseUrl, '/') . '1/health');
+                $ch = curl_init(rtrim($gotenbergBaseUrl, '/') . '/health');
 
                 curl_setopt_array($ch, [
                     CURLOPT_RETURNTRANSFER => true,
@@ -55,34 +58,31 @@ class GotenbergHelper
      */
     public static function isAvailable(): bool
     {
-        if (self::$validPing) {
-            return true;
+        if (self::$validPing !== null) {
+            return self::$validPing;
         }
 
-        if (Cache::load('gotenberg_ping') === true) {
+        $cachedStatus = Cache::load(self::CACHE_KEY);
+
+        if ($cachedStatus === self::STATUS_AVAILABLE) {
             self::$validPing = true;
             return true;
         }
 
-        if (Cache::load('gotenberg_inactive') === true) {
+        if ($cachedStatus === self::STATUS_UNAVAILABLE) {
             self::$validPing = false;
             return false;
         }
 
         $ttl = Config::getSystemConfiguration('gotenberg')['ping_cache_ttl'];
 
-        if (!class_exists(GotenbergAPI::class, true)) {
-            Cache::save(true, 'gotenberg_inactive', [], $ttl);
-            return false;
-        }
-
         if (self::healthPing()) {
             self::$validPing = true;
-            Cache::save(true, 'gotenberg_ping', [], $ttl);
+            Cache::save(self::STATUS_AVAILABLE, self::CACHE_KEY, [], $ttl);
             return true;
         }
 
-        Cache::save(true, 'gotenberg_inactive', [], $ttl);
+        Cache::save(self::STATUS_UNAVAILABLE, self::CACHE_KEY, [], $ttl);
         return false;
     }
 }
