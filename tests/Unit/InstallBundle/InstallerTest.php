d5cc2180a3cc4aaafc1413679dd7bd117da054e5
jcPimcore <jochen.califice@pimcore.com>
2026-03-25 15:08:55 +0100

    [Task] Update Installer test

--- tests/Unit/InstallBundle/InstallerTest.php
+++ tests/Unit/InstallBundle/InstallerTest.php
@@ -959,7 +959,6 @@ final class InstallerTest extends TestCase
         $this->assertStringContainsString('connections:', $content);
         $this->assertStringContainsString('default:', $content);
         $this->assertStringContainsString('mapping_types:', $content);
-        $this->assertStringContainsString('enum: string', $content);
         $this->assertStringContainsString('bit: boolean', $content);
     }
 
