4b093ab798c48ab73fdd433dcfe1fab063f9c4bb
robertSt7 <robert.steinkellner@pimcore.com>
2026-04-07 06:19:07 +0000

    use equals as default operator

--- models/Helper/QueryParams.php
+++ models/Helper/QueryParams.php
@@ -123,10 +123,8 @@ class QueryParams
                     $conditions[$f->property][] = ' ' . $db->quoteIdentifier($f->property) . ' LIKE ' . $db->quote('%' . $f->value . '%') . ' ';
                 }
             } elseif ($f->type == 'numeric') {
-                $symbol = null;
-                if ($f->operator == 'eq') {
-                    $symbol = ' = ';
-                } elseif ($f->operator == 'lt') {
+                $symbol = ' = ';
+                if ($f->operator == 'lt') {
                     $symbol = ' < ';
                 } elseif ($f->operator == 'gt') {
                     $symbol = ' > ';
