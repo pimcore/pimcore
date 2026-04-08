37c14edb2693c4632187e2e78b6915c62af43042
kingjia90 <6014195+kingjia90@users.noreply.github.com>
2026-04-08 12:01:34 +0000

    Apply php-cs-fixer changes

--- models/Document.php
+++ models/Document.php
@@ -212,7 +212,7 @@ class Document extends Element\AbstractElement
             // Getting classname from document resolver
             $className = Pimcore::getContainer()->get('pimcore.class.resolver.document')->resolve($document->getType());
 
-            if (!$className){
+            if (!$className) {
                 return null;
             }
             /** @var Document $newDocument */
