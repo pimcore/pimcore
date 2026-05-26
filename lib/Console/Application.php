8d00411855453cb9a68e0cce3db60a9db4e96f70
Marco Perberschlager <marco.perberschlager@pimcore.com>
2026-05-26 16:18:04 +0200

    fix: Move getting maintenance helper into the listener to prevent eager db resolution

--- lib/Console/Application.php
+++ lib/Console/Application.php
@@ -65,8 +65,9 @@ final class Application extends \Symfony\Bundle\FrameworkBundle\Console\Applicat
 
         $this->setDispatcher($dispatcher);
 
-        $maintenanceModeHelper = $kernel->getContainer()->get(MaintenanceModeHelperInterface::class);
-        $dispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $event) use ($kernel, $maintenanceModeHelper) {
+        $dispatcher->addListener(ConsoleEvents::COMMAND, function (ConsoleCommandEvent $event) use ($kernel) {
+            $maintenanceModeHelper = $kernel->getContainer()->get(MaintenanceModeHelperInterface::class);
+
             // skip if maintenance mode is on and the flag is not set
             if (($maintenanceModeHelper->isActive() || Admin::isInMaintenanceMode()) &&
                 !$event->getInput()->getOption('ignore-maintenance-mode')
@@ -106,7 +107,9 @@ final class Application extends \Symfony\Bundle\FrameworkBundle\Console\Applicat
             }
         });
 
-        $dispatcher->addListener(ConsoleEvents::TERMINATE, function (ConsoleTerminateEvent $event) use ($maintenanceModeHelper) {
+        $dispatcher->addListener(ConsoleEvents::TERMINATE, function (ConsoleTerminateEvent $event) use ($kernel) {
+            $maintenanceModeHelper = $kernel->getContainer()->get(MaintenanceModeHelperInterface::class);
+
             if ($event->getInput()->getOption('maintenance-mode')) {
                 $event->getOutput()->writeln('Deactivating maintenance mode...');
                 //BC Layer for Admin::activateMaintenanceMode, if the maintenance file already exists
