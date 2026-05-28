63edf79a1667286c3ed14e00713ec8930f572ca9
Martin Eiber <martin.eiber@pimcore.com>
2026-05-28 14:36:59 +0200

    Add strict_types

--- models/Dependency/Dao.php
+++ models/Dependency/Dao.php
@@ -293,6 +293,11 @@ class Dao extends Model\Dao\AbstractDao
 
         $this->dispatchTargetsChangedMessage($newData, $existingDepencies);
     }
+
+    /**
+     * @param array<array{id: int, type: string}> $newTargets
+     * @param array<string, array<int, int>> $removedTargetsByType targetType => [targetId => rowId]
+     */
     private function dispatchTargetsChangedMessage(array $newTargets, array $removedTargetsByType): void
     {
         $addedTargets = [];
