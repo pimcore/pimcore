07ffa458d4d97a677ee338a1b54d75b6f1ce9b2c
robertSt7 <104770750+robertSt7@users.noreply.github.com>
2026-06-11 12:18:49 +0000

    Fix: check for empty db entry

--- models/Tool/TmpStore/Dao.php
+++ models/Tool/TmpStore/Dao.php
@@ -74,6 +74,10 @@ class Dao extends Model\Dao\AbstractDao
                     \Pimcore\Video\Adapter\Ffmpeg::class,
                 ], $extraAllowedClasses);
 
+                 if ($item['data'] === '' || $item['data'] === null) {
+                     return false;
+                 }
+
                 try {
                     $deserialized = \Pimcore\Tool\Serialize::unserialize($item['data'], $allowedClasses);
                 } catch (\Throwable) {
